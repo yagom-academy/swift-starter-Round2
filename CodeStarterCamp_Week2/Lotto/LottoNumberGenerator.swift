1744b2137d1e5162a063bdddda8a2d96003a6376
Mino777 <jomino7777@gmail.com>
2021-12-15 15:31:36 +0900

    feat: add generateLottoNumbers

--- CodeStarterCamp_Week2/Lotto/LottoNumberGenerator.swift
+++ CodeStarterCamp_Week2/Lotto/LottoNumberGenerator.swift
@@ -8,14 +8,24 @@
 import Foundation
 
 protocol LottoNumberGeneratable {
-
+    func generateLottoNumbers() -> Set<Int>
 }
 
-struct LottoNumberGenerator {
+struct LottoNumberGenerator: LottoNumberGeneratable {
     private enum lottoOptions {
         static let randomNumberRange: ClosedRange<Int> = 1 ... 45
         static let lottoNumbersMaxAmount: Int = 6
         static let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+    }
+    
+    func generateLottoNumbers() -> Set<Int> {
+        var lottoNumbers: Set<Int> = []
+        
+        while lottoNumbers.count != lottoOptions.lottoNumbersMaxAmount {
+            lottoNumbers.insert(generateRandomNumbers())
+        }
+        
+        return lottoNumbers
     }
         
     private func generateRandomNumbers() -> Int {
