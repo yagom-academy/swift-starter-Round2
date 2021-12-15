fb88f4c8c541e1c5394137dcd85d96f5c435ec2f
Mino777 <jomino7777@gmail.com>
2021-12-15 15:22:12 +0900

    feat: add LottoNumberGenerator

---
+++ CodeStarterCamp_Week2/Lotto/LottoNumberGenerator.swift
@@ -0,0 +1,16 @@
+//
+//  LottoNumberGenerator.swift
+//  CodeStarterCamp_Week2
+//
+//  Created by 조민호 on 2021/12/15.
+//
+
+import Foundation
+
+protocol LottoNumberGeneratable {
+    
+}
+
+struct LottoNumberGenerator {
+    
+}
