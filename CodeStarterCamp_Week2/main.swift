8ed3e174c717098d95d4ea7669b901ef3a7dfb64
Quokkaaa <rktrkdml@gmail.com>
2021-12-16 17:23:05 +0900

    feat: confirmLotteryNumbers 함수 구현
    * 랜덤 로또 번호와 나의 번호를 비교해서 메세지를 반환하는 함수 구현
      (맞출시 축하의 메세지와함께 번호를 출력하고  맞춘게없을지 유감의 메세지를 출력)

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -14,3 +14,27 @@ func generateLotteryNumbers(count: Int) -> Set<Int> {
     }
     return numbers
 }
+
+func confirmLotteryNumbers(_ numbers: [Int]) -> String {
+    var lotteryNumbers = generateLotteryNumbers(count: 6)
+    var loteryNumbersArray: [Int] = []
+    var answerNumbers: [Int] = []
+    
+    for _ in 0..<lotteryNumbers.count {
+        loteryNumbersArray.append(lotteryNumbers.removeFirst())
+    }
+    for i in 0..<numbers.count {
+        if numbers[i] == loteryNumbersArray[i] {
+            answerNumbers.append(numbers[i])
+        }
+    }
+    
+    if answerNumbers.count >= 1 {
+        return "축하합니다! 겹치는 번호는 \(answerNumbers) 입니다!"
+    } else {
+        return "아쉽지만 겹치는 번호가 없습니다."
+    }
+}
+
+let myLottoNumbers: [Int] = [7, 8, 15, 20, 23, 38]
+print(confirmLotteryNumbers(myLottoNumbers))
