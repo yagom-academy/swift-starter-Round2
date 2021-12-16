e6902f9a203710599dea178fa41cdcd2f03d3d92
sery <nicesery@naver.com>
2021-12-17 00:55:12 +0900

    Refactor: change some names

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,39 +8,39 @@
 
 import Foundation
 
-var lottoNum: Set<Int> = []
+var lottoNumber: Set<Int> = []
 
-func pickLottoNumUp () {
+func pickLottoNumberUp () {
     var count: Int = 0
     while count < 6 {
-        let randomNum: Int = Int.random(in: 1...45)
-        if lottoNum.contains(randomNum) {
+        let randomNumer: Int = Int.random(in: 1...45)
+        if lottoNumber.contains(randomNumer) {
         } else {
-            lottoNum.insert(randomNum)
+            lottoNumber.insert(randomNumer)
             count = count + 1
         }
     }
 }
 
-func matchLottoNum() -> Array<Int> {
-    let match = lottoNum.intersection(myLottoNumbers)
-    let matchArr = Array(match)
-    return matchArr
+func matchLottoNumber() -> Array<Int> {
+    let match = lottoNumber.intersection(myLottoNumbers)
+    let matchNumbers = Array(match)
+    return matchNumbers
 }
 
 func printMatch(match: Array<Int>) {
         if match == [] {
             print("아쉽지만 겹치는 번호가 없습니다")
         } else {
-            let matchString = match.map({ (number: Int) -> (String) in return "\(number)" })
-            let matchToPrint = matchString.joined(separator: ", ")
+            let matchNumbers = match.map({ (number: Int) -> (String) in return "\(number)" })
+            let matchToPrint = matchNumbers.joined(separator: ", ")
             print("축하합니다! 겹치는 번호는 \(matchToPrint) 입니다")
         }
     }
 
 let myLottoNumbers: Set<Int> = [1, 8, 10, 23, 44, 36]
 
-pickLottoNumUp()
-print("로또 번호: \(lottoNum)")
-printMatch(match: matchLottoNum())
+pickLottoNumberUp()
+print("로또 번호: \(lottoNumber)")
+printMatch(match: matchLottoNumber())
 
