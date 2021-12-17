ad00c21a4f42f5657adc1df769a7da507f98d2ad
sery <nicesery@naver.com>
2021-12-17 15:22:26 +0900

    Feat: Lotto Numbers Saver

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,42 +8,68 @@
 
 import Foundation
 
-var lottoNumber: Set<Int> = []
+//STEP2
+var lottoNumbers: Set<Int> = []
 
 func pickLottoNumberUp () {
     var count: Int = 0
     while count < 6 {
-        let randomNumber: Int = Int.random(in: 1...45)
-        if !lottoNumber.contains(randomNumber) {
-            lottoNumber.insert(randomNumber)
+        let randomNumer: Int = Int.random(in: 1...45)
+        if !lottoNumbers.contains(randomNumer) {
+            lottoNumbers.insert(randomNumer)
             count = count + 1
         }
     }
 }
 
-func matchLottoNumber() -> Array<Int> {
-    let match = lottoNumber.intersection(myLottoNumbers)
-    let matchNumbers = Array(match)
-    return matchNumbers
+func matchLottoNumbers() -> Array<Int> {
+    let matchedNumbers: Array<Int> = Array(lottoNumbers.intersection(myLottoNumbers))
+    return matchedNumbers
 }
 
-func printMatch(match: Array<Int>) {
-    if match.isEmpty {
+func printMatch(matchedNumbers: Array<Int>) {
+    if matchedNumbers.isEmpty {
             print("아쉽지만 겹치는 번호가 없습니다")
         } else {
-            let matchToPrint = convertArrayToString(someArray: match)
+            let matchToPrint = convertArrayToString(someArray: matchedNumbers)
             print("축하합니다! 겹치는 번호는 \(matchToPrint) 입니다")
         }
     }
 
 func convertArrayToString(someArray: Array<Int>) -> String {
-    let setString = someArray.map({ (number: Int) -> (String) in return String(number) })
-    let line = setString.joined(separator: ", ")
-    return line
+    let someString: String = someArray.map { String($0) }.joined(separator: ", ")
+    return someString
 }
 
 let myLottoNumbers: Set<Int> = [1, 8, 10, 23, 44, 36]
 
 pickLottoNumberUp()
-print("로또 번호: \(lottoNumber)")
-printMatch(match: matchLottoNumber())
+print("로또 번호: \(convertArrayToString(someArray: Array(lottoNumbers)))")
+printMatch(matchedNumbers: matchLottoNumbers())
+
+//STEP3
+var lottoLog: [String: Array<Int>] = [:]
+
+func playLotto(endRound: Int){
+    for round in 1...endRound {
+        pickLottoNumberUp()
+        saveLotto(round: round)
+    }
+}
+
+func saveLotto(round: Int) {
+    lottoLog["\(round)회차"] = Array(lottoNumbers)
+    lottoNumbers = []
+}
+
+func searchLottoRoundNumbers(round: Int) {
+    if let lottoLogNumbers = lottoLog["\(round)회차"] {
+        let searchNumbers = convertArrayToString(someArray: lottoLogNumbers)
+        print("\(round)회차의 로또 당첨 번호는 \(searchNumbers) 입니다.")
+    } else {
+        print("해당 라운드는 존재하지 않습니다.")
+    }
+}
+
+playLotto(endRound: 5)
+searchLottoRoundNumbers(round: 2)
