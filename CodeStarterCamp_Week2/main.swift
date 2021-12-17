99f8f7c2b8dffa0520a235a405c9cd97439383a9
Taeangel <wngks2050@gmail.com>
2021-12-18 02:08:06 +0900

    step2PRCorrection

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -7,36 +7,51 @@
 //
 import Foundation
 
-var lottoNumbers: Set<Int> = []
-var intersectionLottoNumber: Set<Int> = []
+var lottoNumbers: Array<Int> = []
 
+func makeLottoNumber() -> Array<Int> {
 
-func makeLottoNumber() -> Set<Int> {
   while lottoNumbers.count < 6 {
     let lottoNumber: Int = Int.random(in: 1...45)
-    if !lottoNumbers.contains(lottoNumber) {
-      lottoNumbers.insert(lottoNumber)
-    }
+      if !lottoNumbers.contains(lottoNumber) {
+        lottoNumbers.append(lottoNumber)
+      }
+
   }
-  return lottoNumbers
+    return lottoNumbers
 }
 
 
-func intersectLooto() -> Set<Int> {
-  intersectionLottoNumber = myLottoNumbers.intersection(makeLottoNumber())
-  return  intersectionLottoNumber
+func overlapedNumber() -> Array<Int> {
+  var intersectionLottoNumber : Array<Int> = []
+    for member in 1...45 {
+      if myLottoNumbers.contains(member){
+          if makeLottoNumber().contains(member){
+              intersectionLottoNumber.append(member)
+      }
+    }
+  }
+  return intersectionLottoNumber
 }
 
 
 func checkLotto() {
-  if intersectLooto().count == 0 {
+  if overlapedNumber().isEmpty {
     print("아쉽지만 겹치는 번호가 없습니다.")
 } else {
-    print("축하합니다! 겹치는 번호는 \(intersectLooto()) 입니다!")
+    print("축하합니다! 겹치는 번호는 \(convertSetToString()) 입니다!")
   }
 }
 
 
-let myLottoNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
+func convertSetToString() -> String {
+  let toStringIntersectionLotto: String = overlapedNumber ().map { String($0) }.joined(separator: ", ")
+  return toStringIntersectionLotto
+}
+
+
+var myLottoNumbers: Array<Int> = [1, 2, 3, 4, 5, 6]
 
 checkLotto()
+
+
