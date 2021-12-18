f9ea7fed2cb9097fcb8c69b6a3e1550487cadebc
Taeangel <wngks2050@gmail.com>
2021-12-19 01:20:40 +0900

    step2Mymethod

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,48 +8,54 @@
 import Foundation
 
 
-func makeLottoNumber() -> Array<Int> {
-  var lottoNumbers: Array<Int> = []
+func makeLottoNumber() -> Set<Int> {
+  var lottoNumbers: Set<Int> = []
   while lottoNumbers.count < 6 {
     let lottoNumber: Int = Int.random(in: 1...45)
-      if !lottoNumbers.contains(lottoNumber) {
-        lottoNumbers.append(lottoNumber)
-    }
+    lottoNumbers.insert(lottoNumber)
   }
-    return lottoNumbers
+  return lottoNumbers
 }
 
 
-func overlapedNumber() -> Array<Int> {
-  var intersectionLottoNumber : Array<Int> = []
-    for member in 1...45 {
-      if myLottoNumbers.contains(member){
-        if makeLottoNumber().contains(member){
-              intersectionLottoNumber.append(member)
-      }
-    }
-  }
-  return intersectionLottoNumber
-}
-
-
-func checkLotto() {
-  if overlapedNumber().isEmpty {
-    print("아쉽지만 겹치는 번호가 없습니다.")
-  } else {
-    print("축하합니다! 겹치는 번호는 \(convertSetToString()) 입니다!")
-  }
+func overlapedNumbers() -> Set<Int> {
+  let intersectionLottoNumbers = myLottoNumbers.intersection(makeLottoNumber())
+  return intersectionLottoNumbers
 }
 
 
 func convertSetToString() -> String {
-  let toStringIntersectionLotto: String = overlapedNumber().map { String($0) }.joined(separator: ", ")
+  let toStringIntersectionLotto: String = overlapedNumbers().map { String($0) }.joined(separator: ", ")
   return toStringIntersectionLotto
+
 }
 
 
-var myLottoNumbers: Array<Int> = [1, 2, 3, 4, 5, 6]
+func checkLotto() {
+  let toStringIntersectionLotto = convertSetToString()
+    print(toStringIntersectionLotto)
+  if toStringIntersectionLotto.isEmpty {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+  } else {
+      print("축하합니다! 겹치는 번호는 \(toStringIntersectionLotto) 입니다!")
+  }
+}
+
+
+var myLottoNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
 
 checkLotto()
+checkLotto()
+checkLotto()
+checkLotto()
+checkLotto()
+checkLotto()
+checkLotto()
+checkLotto()
+checkLotto()
+checkLotto()
+
+
+
 
 
