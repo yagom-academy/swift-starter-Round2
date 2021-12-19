84255afe82129310dcfd67e876724418bb86e8b8
sery <nicesery@naver.com>
2021-12-20 00:30:26 +0900

    Refactor: add new dictionary

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-
 //STEP2
 var lottoNumbers: Set<Int> = []
 
@@ -23,7 +22,7 @@ func pickLottoNumberUp () {
 }
 
 func matchLottoNumbers() -> Array<Int> {
-    let matchedNumbers: Array<Int> = Array(lottoNumbers.intersection(myLottoNumbers))
+    let matchedNumbers = Array(lottoNumbers.intersection(myLottoNumbers))
     return matchedNumbers
 }
 
@@ -37,39 +36,51 @@ func printMatch(matchedNumbers: Array<Int>) {
     }
 
 func convertArrayToString(someArray: Array<Int>) -> String {
-    let someString: String = someArray.map { String($0) }.joined(separator: ", ")
+    let someString = someArray.map { String($0) }.joined(separator: ", ")
     return someString
 }
 
 let myLottoNumbers: Set<Int> = [1, 8, 10, 23, 44, 36]
 
 pickLottoNumberUp()
-print("로또 번호: \(convertArrayToString(someArray: Array(lottoNumbers)))")
+print("로또 당첨 번호: \(convertArrayToString(someArray: Array(lottoNumbers)))")
 printMatch(matchedNumbers: matchLottoNumbers())
 
 //STEP3
-var lottoLog: [String: Array<Int>] = [:]
+var winningLottoLog: [String: [Int]] = [:]
+var lottoMatchedNumberLog: [String: [Int]] = [:]
 
-func playLotto(endRound: Int){
+func playLotto(endRound: Int) {
     for round in 1...endRound {
         pickLottoNumberUp()
+        saveMatchedNumber(round: round)
         saveLotto(round: round)
     }
 }
 
 func saveLotto(round: Int) {
-    lottoLog["\(round)회차"] = Array(lottoNumbers)
+    winningLottoLog["\(round)회차"] = Array(lottoNumbers)
     lottoNumbers = []
 }
 
-func searchLottoRoundNumbers(round: Int) {
-    if let lottoLogNumbers = lottoLog["\(round)회차"] {
-        let searchNumbers = convertArrayToString(someArray: lottoLogNumbers)
+func saveMatchedNumber(round: Int) {
+    lottoMatchedNumberLog["\(round)회차"] = matchLottoNumbers()
+}
+
+func searchLottoRound(round: Int) {
+    if let lottoHistory = winningLottoLog["\(round)회차"] {
+        let searchNumbers = convertArrayToString(someArray: lottoHistory)
         print("\(round)회차의 로또 당첨 번호는 \(searchNumbers) 입니다.")
-    } else {
-        print("해당 라운드는 존재하지 않습니다.")
+    }
+}
+
+func searchMatchedNumberRound(round: Int) {
+    if let matchedHistory = lottoMatchedNumberLog["\(round)회차"] {
+        let searchNumbers = convertArrayToString(someArray: matchedHistory)
+        print("\(round)회차의 겹치는 번호는 \(searchNumbers) 입니다.")
     }
 }
 
 playLotto(endRound: 5)
-searchLottoRoundNumbers(round: 2)
+searchLottoRound(round: 2)
+searchMatchedNumberRound(round: 3)
