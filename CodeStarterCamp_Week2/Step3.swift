4312191c4561491d3ff2e1c5ec814139647feaf9
JoSH0318 <jsh9870@naver.com>
2021-12-20 16:48:49 +0900

    FEAT: Step3 Lotto Numbers Save&Compare

--- CodeStarterCamp_Week2/Step3.swift
+++ CodeStarterCamp_Week2/Step3.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+var lottoNumbersArray: [Int] = []
+var lottoNumbersDictionary: [String: [Int]] = [:]
+var someRoundNumbersArray: [Int] = []
+var someRoundNumbersStringArray: [String] = []
+
+func createLottoNumbers(_ round: Int) {
+    for _ in 0..<round {
+        var lottoNumbers: Set<Int> = Set<Int>()
+        while lottoNumbers.count < 6 {
+            lottoNumbers.insert(Int.random(in: 1...45))
+        }
+        lottoNumbersArray = Array(lottoNumbers)
+        saveLottoNumbersToDictionary()
+    }
+}
+
+func saveLottoNumbersToDictionary() {
+    lottoNumbersDictionary["\(lottoNumbersDictionary.count+1)회차"] = lottoNumbersArray
+    print(lottoNumbersDictionary)
+}
+
+func changeIntToStringArray() {
+    for index in someRoundNumbersArray {
+        someRoundNumbersStringArray.append("\(index)")
+    }
+}
+
+func printMatchedNumbers() {
+    changeIntToStringArray()
+    let joinedArray = someRoundNumbersStringArray.joined(separator: ", ")
+    print(joinedArray, terminator: " ")
+}
+
+func checkLottoNumbers(_ round: Int) {
+    if let someValues = lottoNumbersDictionary["\(round)회차"] {
+        someRoundNumbersArray = someValues
+        print("\(round)회차의 로또 당첨 번호는", terminator: " ")
+        printMatchedNumbers()
+        print("입니다.")
+    } else {
+        print("\(round)회차의 로또 번호가 없습니다.")
+    }
+}
