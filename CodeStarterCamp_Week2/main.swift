6e2d78f2fa4e366086b714aff68044c7e53b7f2e
Woo Jong <dnwhd0112@naver.com>
2021-12-20 23:00:19 +0900

    feat : week2 step2 과제 로또 당첨 번호를 생성하는 함수, 찍은번호와 로또 당첨번호의 겹치는 숫자를 확인하는 함수 생성.

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,41 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week2
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [1,2,3,4,5,6]
+
+func anserLottoNumbers() -> Set<Int> {
+    var lottoNumbers: Set<Int> = Set<Int>()
+    
+    while lottoNumbers.count < 6 {
+
+        let targetNumber = Int.random(in: 1...45)
+        
+        if !lottoNumbers.contains(targetNumber){
+           lottoNumbers.insert(targetNumber)
+        }
+    }
+    
+    return lottoNumbers
+}
+
+func compareLotto(myLottoNumbers my: Array<Int>,answerLottoNumbers answer: Set<Int>){
+    
+    var prize: [Int] = []
+    
+    for index in 0...(my.count-1){
+        if answer.contains(my[index]){
+            prize.append(my[index])
+        }
+    }
+    
+    if prize.count>0 {
+     print("축하합니다! 겹치는 번호는 \(prize) 입니다!")
+    }
+    else {
+     print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+
+compareLotto(myLottoNumbers: myLottoNumbers, answerLottoNumbers: anserLottoNumbers())
+
 
