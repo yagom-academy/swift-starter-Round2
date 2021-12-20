dab8a1cd617e7642bdd25237374f6d869ef52193
Minseong-yagom <msb0987@naver.com>
2021-12-20 19:56:03 +0900

    Feat 찍은번호와 로또당첨번호의 겹치는 숫자 확인 함수 생성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -17,3 +17,22 @@ func createWinnigNumbers() -> Set<Int>{
     
     return numbers
 }
+
+func compare(myLottoNumbers:[Int]) {
+    let winningNumbers: Set<Int> = createWinnigNumbers()
+    var resultNumbers = [Int]()
+    
+    for i in 0...5 {
+        if winningNumbers.contains(myLottoNumbers[i]) {
+            resultNumbers.append(myLottoNumbers[i])
+        }
+    }
+    
+    if resultNumbers.count > 0 {
+        print("축하합니다! 겹치는 번호는 \(resultNumbers)입니다!")
+    }else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+
+let myLottoNumbers: [Int] = [3,8,10,15,13,22]
