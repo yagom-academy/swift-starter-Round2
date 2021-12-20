cc9427b27f5e641c507dbb72bd0d122a79face13
malrang <rue>
2021-12-20 22:45:21 +0900

    Feat : 로또 번호 생성기,로또 번호 당첨비교
    작성한 순서도를 토대로 로또 번호생성기를 만들었습니다
    로또 번호 당첨을 비교하는 함수에서 조언을 희망합니다!

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,28 +1,8 @@
-
-let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-
-//함수 반환 하여 사용예시
-func sum()-> Int {
-    let intA : Int = 4
-    let intB : Int = 3
-    return intA + intB
-}
-
-func sumPrint(sum : Int) {
-    print(sum)
-}
-
-
-//가드렛
-/*
-func abc() {
-    let numbers: Int? = nil
-    guard let number = numbers
-    else {
-     return print("바인딩 실패")
-    }
-}
-*/
+//
+//  main.swift
+//  CodeStarterCamp_Week2
+//
+//  Created by 김동욱 on 2021/12/20.
 
 makeLottonumber1()
 
