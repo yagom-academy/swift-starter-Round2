63a51e4e0965a9b039e3fce9f9cb2abe59e27ec1
Woo Jong <dnwhd0112@naver.com>
2021-12-21 20:24:27 +0900

    refactor : week2 step2 1차 수정 dnwhd0112 컨벤션 수정 및 마지막 출력부분 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -7,7 +7,6 @@ func anserLottoNumbers() -> Set<Int> {
     var lottoNumbers: Set<Int> = Set<Int>()
     
     while lottoNumbers.count < 6 {
-
         let targetNumber = Int.random(in: 1...45)
         
         if !lottoNumbers.contains(targetNumber){
@@ -19,19 +18,23 @@ func anserLottoNumbers() -> Set<Int> {
 }
 
 func compareLotto(myLottoNumbers my: Array<Int>,answerLottoNumbers answer: Set<Int>){
-    
-    var prize: [Int] = []
+    var countMatch: [Int] = []
     
     for index in 0...(my.count-1){
         if answer.contains(my[index]){
-            prize.append(my[index])
+            countMatch.append(my[index])
         }
     }
     
-    if prize.count>0 {
-     print("축하합니다! 겹치는 번호는 \(prize) 입니다!")
-    }
-    else {
+    if countMatch.count > 0 {
+        print("축하합니다! 겹치는 번호는 ", terminator: "")
+        if countMatch.count > 2 {
+         for index in 0...countMatch.count-2{
+            print("\(countMatch[index]), ",terminator: "")
+         }
+        }
+        print("\(countMatch[countMatch.count-1]) 입니다!")
+    } else {
      print("아쉽지만 겹치는 번호가 없습니다.")
     }
 }
