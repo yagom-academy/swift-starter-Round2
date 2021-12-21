0a6888b417b30b14aed3fe9a376bd25325770331
vayne77 <sky891@naver.com>
2021-12-21 15:44:30 +0900

    feat: 중복되지 않는 랜덤한 6개의 숫자를 생성하는 메서드 생성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,16 @@
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [1, 10, 20, 30, 40, 45]
+var winningNumbers: [Int] = []
+
+func makeDeduplicatedNumbers() -> [Int] {
+    var deduplicatedNumbers: Set<Int> = []
+    
+    while deduplicatedNumbers.count < 6 {
+        let deduplicatedNumber = Int.random(in: 1...45)
+        deduplicatedNumbers.insert(deduplicatedNumber)
+    }
+    return Array(deduplicatedNumbers)
+}
 
