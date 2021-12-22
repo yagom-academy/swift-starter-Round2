cfba7a07fdbee79853aa6692be544bc235c33580
malrang <ruehd7924@gmail.com>
2021-12-23 00:49:10 +0900

    Feat : step2 기능구현
    기존 순서도 대로 만들어진 기능에
    문제에서 제시한 값을 비교하여 교집합을 출력 하는 기능 구현

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -4,6 +4,47 @@
 //
 //  Created by 김동욱 on 2021/12/20.
 
-makeLottonumber1()
 
+import Foundation
+
+//겹치지 않는 로또번호6개 생성기 Set<Int>반환!
+func makeLotto (){
+    
+    //만들어진 랜덤넘버
+    var makedLottoNum : Set<Int> = Set<Int>()
+    //step2 로또번호
+    let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+    //교집합 하여 비교된 값
+    var intersectionNum : Set<Int> = Set<Int>()
+    
+    
+    //겹치지 않는 랜덤넘버 생성기
+    func makeRandomNum() {
+        while makedLottoNum.count < 6 {
+            makedLottoNum.insert(Int.random(in: 1...45))}
+    }
+    
+    // Set를 이용해 비교해주는 함수
+    func intersectionCheck () {
+        let checkSet : Set<Int> = makedLottoNum
+        let checkLottonum : Set<Int> = Set(myLottoNumbers)
+        intersectionNum = checkLottonum.intersection(checkSet)
+    }
+    
+    //당첨번호 출력 함수
+    func printWinningNum (){
+        print("내로또 번호는 :", makedLottoNum)
+        if intersectionNum.count > 0 {
+            print("축하합니다! 겹치는 번호는\(intersectionNum) 입니다!")
+        } else {
+            print("아쉽지만 겹치는 번호가 없습니다")
+        }
+    }
+    
+    makeRandomNum()
+    intersectionCheck()
+    printWinningNum()
+}
+
+makeLotto()
 
