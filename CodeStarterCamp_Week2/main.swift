f040ec513bd68c00cc9e2931fe15d4349ad8ebe9
Leebumju <dlqkawns@naver.com>
2021-12-22 18:17:13 +0900

    modify step1

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,14 +8,4 @@
 
 import Foundation
 
-var randomNumber: Int
-var randomNumberArray: Array<Int> = []
 
-while randomNumberArray.count < 6 {
-    randomNumber = Int.random(in: 1...45)
-    if !randomNumberArray.contains(randomNumber) {
-        randomNumberArray.append(randomNumber)
-    }
-}
-
-print(randomNumberArray)
