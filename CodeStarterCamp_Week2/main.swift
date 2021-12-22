24caf66f326783e38084ed5f25944810abe625ed
marisol <marisol.develop@gmail.com>
2021-12-22 23:23:29 +0900

    feat : 딕셔너리에 로또번호 저장하는storeLottoNumbers 함수 생성, 임의 회차의 로또 번호 출력할 수 있는 printAnyLottoNumbers 함수 생성
    refactor : makeLottoNumbers 함수에서 생성한 로또번호를 다른 함수에서도 사용할 수 있도록 makeLottoNumbers 함수에 리턴값을 추가함

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,27 +9,52 @@
 import Foundation
 
 let myLottoNumbers: [Int] = [3,5,17,22,32,39]
-var lottoNumbers: Set<Int> = []
+var lottoDictionary: Dictionary<String, Set<Int>> = [String: Set<Int>]()
+var index = 0
 
-func makeLottoNumbers() {
+func makeLottoNumbers() -> Set<Int> {
     
+    var lottoNumbers: Set<Int> = []
+
     while lottoNumbers.count < 6 {
         let randomInt = Int.random(in: 1...45)
         lottoNumbers.insert(randomInt)
     }
-    print(lottoNumbers.sorted())
+    storeLottoNumbers(lottoNumbers: lottoNumbers)
+    return lottoNumbers
 }
 
-func checkLottoNumbers() {
+func storeLottoNumbers(lottoNumbers: Set<Int>) {
+    
+    index += 1
+    lottoDictionary["\(index)회차"] = lottoNumbers
+}
 
-    let matchedNumbers = lottoNumbers.intersection(myLottoNumbers)
+func checkLottoNumbers(myLottoNumbers: [Int]) {
+    
+    let matchedNumbers = makeLottoNumbers().intersection(myLottoNumbers)
 
     if matchedNumbers.isEmpty {
-        print("아쉽지만 겹치는 번호가 없습니다.")
+        print("아쉽지만 겹치는 번호가 없습니다. \n")
     } else {
-        print("축하합니다! 겹치는 번호는 \(matchedNumbers)입니다!")
+        print("축하합니다! 겹치는 번호는 \(matchedNumbers)입니다! \n")
     }
 }
 
-makeLottoNumbers()
-checkLottoNumbers()
+func printAnyLottoNumbers() {
+
+    index = 2
+    
+    if let randomLottoNumbers: Set<Int> = lottoDictionary["\(index)회차"] {
+        print("\(index)회차의 로또 당첨 번호는 \(randomLottoNumbers.sorted())입니다.")
+    }
+    
+}
+
+checkLottoNumbers(myLottoNumbers: myLottoNumbers)
+checkLottoNumbers(myLottoNumbers: myLottoNumbers)
+checkLottoNumbers(myLottoNumbers: myLottoNumbers)
+checkLottoNumbers(myLottoNumbers: myLottoNumbers)
+checkLottoNumbers(myLottoNumbers: myLottoNumbers)
+printAnyLottoNumbers()
+
