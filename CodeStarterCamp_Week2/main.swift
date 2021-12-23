db75e55d7167374b96fb278270261083f504d7b2
Minseong-yagom <msb0987@naver.com>
2021-12-24 01:57:44 +0900

    Feat 회차와 로또번호 출력 함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -7,6 +7,8 @@ func createWinnigNumbers() -> Set<Int> {
         numbers.insert(Int.random(in: 1...45))
     } while numbers.count < 6
     
+    saveInDictionary(numbers: numbers)
+    
     return numbers
 }
 
@@ -40,6 +42,21 @@ func printOut(resultNumbers: [Int]) {
     } else {
         print("아쉽지만 겹치는 번호가 없습니다.")
     }
+}
+
+func printOut(time: Int) {
+    print("\(time)회차의 로또 당첨 번호는 ", terminator: "")
+    
+    if let printWinning = winningNumbersRepository["\(time)회차"] {
+        let sortNumber: [Int] = printWinning.sorted()
+        for index in 0...5 {
+            print(sortNumber[index], terminator: "")
+            if index < 5{
+                print(", ", terminator: "")
+            }
+        }
+    }
+    print("입니다.")
 }
 
 let myLottoNumbers: [Int] = [3,8,10,15,13,22]
