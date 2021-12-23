ba05247fdda154d628c42d64c5ec2e87c8bf0f42
doylehong <sspaka0422@gmail.com>
2021-12-23 22:10:52 +0900

    fix step3

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,54 +8,36 @@
 
 import Foundation
 
-func makeLottoNumbersSet(lottoDictionary: inout Dictionary<String, Array<Int>>) -> Set<Int> {
+func makeLottoNumbersSet(lottoDictionary: inout [String: [Int]]) -> Set<Int> {
     var lottoNumbersSet: Set<Int> = Set<Int>()
     repeat {
         lottoNumbersSet.insert(Int.random(in: 1...45))
     } while lottoNumbersSet.count < 6
-    var lottoNumbersArray: Array<Int> = Array(lottoNumbersSet)
+    var lottoNumbersArray: [Int] = Array(lottoNumbersSet)
     saveLotto(lottoNumbersArray: lottoNumbersArray, lottoDictionary: &lottoDictionary)
     return lottoNumbersSet
 }
 
 
-func saveLotto(lottoNumbersArray: Array<Int>, lottoDictionary: inout Dictionary<String, Array<Int>> ) ->Void {
+func saveLotto(lottoNumbersArray: [Int], lottoDictionary: inout [String: [Int]] ) ->Void {
     var keyString: String = "\(lottoDictionary.count+1)회차"
     lottoDictionary[keyString] = lottoNumbersArray
 }
 
-var lottoDictionary: Dictionary<String, Array<Int>> = Dictionary<String, Array<Int>>()
-
-// STEP 2
-//var lottoNumbersSet: Set<Int> = makeLottoNumbersSet(lottoDictionary: &lottoDictionary)
-//
-//var myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-//var myLottoNumbersSet: Set<Int> = Set(myLottoNumbers)
-//
-//var intersectNumbers: Set<Int> = myLottoNumbersSet.intersection(lottoNumbersSet)
-//var intersect: [Int] = intersectNumbers.sorted()
-//
-//
-//
-//if intersect.count == 0 {
-//    print("아쉽지만 겹치는 번호가 없습니다.")
-//} else if intersect.count == 1 {
-//    print("축하합니다! 겹치는 번호는 \(intersect[0]) 입니다.")
-//} else {
-//    print("축하합니다! 겹치는 번호는 \(intersect[0])", terminator: "")
-//    for index in 1...intersect.count-1 {
-//        print(", \(intersect[index])", terminator: "")
-//    }
-//    print(" 입니다!")
-//}
+var lottoDictionary: [String: [Int]] = [String: [Int]]()
 
 // STEP 3
-for _ in 1...5 {
+for _ in 1...5{
     var lottoNumbersSet: Set<Int> = makeLottoNumbersSet(lottoDictionary: &lottoDictionary)
 }
-var lottoNumbers2: Array<Int> = lottoDictionary["2회차"]!
-print("2회차의 로또 당첨 번호는 \(lottoNumbers2[0]) ", terminator: "")
-for index in 1...5 {
-    print("\(lottoNumbers2[index]) ", terminator: "")
+
+var lottoNumbersSecond: [Int]? = lottoDictionary["2회차"]
+if let lotto: [Int] = lottoNumbersSecond {
+    print("2회차의 로또 당첨 번호는 \(lotto[0]) ", terminator: "")
+    for index in 1...5 {
+        print("\(lotto[index]) ", terminator: "")
+    }
+    print("입니다.")
+} else {
+    print("There is no 2nd Lotto Number")
 }
-print("입니다.")
