920241fafaf9e6a1965c369b0e914b10c431f869
051kid <94177308+051kid@users.noreply.github.com>
2021-12-24 19:39:22 +0900

    Feat: printWinngNumber

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -15,3 +15,14 @@ func lottoNumbers() -> Set<Int> {
     }
     return lottoNumbers
 }
+
+func printWinngNumber() {
+    let myLottoNumbers: [Int] = [1, 2, 3, 4 ,5, 6]
+    let matchedLottoNumbers = lottoNumbers().intersection(myLottoNumbers)
+    if matchedLottoNumbers.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는 \(matchedLottoNumbers.sorted()) 입니다 ! ")
+    }
+}
+printWinngNumber()
