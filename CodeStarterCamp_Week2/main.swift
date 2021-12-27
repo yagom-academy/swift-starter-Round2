c119c9a99fd518c388150668cd0ee2a09c071369
malrang <ruehd7924@gmail.com>
2021-12-27 23:43:05 +0900

    Fix : 하드코딩 부분 수정, makeRandomNumber 함수 기능 분리

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,4 +1,4 @@
-//
+
 //  step3.swift
 //  CodeStarterCamp_Week2
 //
@@ -13,56 +13,47 @@ func weekWinningLotto (){
     var winningLottoNumber : Set<Int> = Set<Int>()
     //당첨번호를 몇번 호출했는지의 카운트
     var makeRandomNumberCount : Int = 0
-    //몇회 호출했는지의 카운트를 문자열값으로 변환, key 값
-    var countWeekNumber : String = "\(makeRandomNumberCount)회차:"
     //딕셔너리 저장공간
-    var weekLottoNumber : [String : String] = [:]
-    // [String]타입으로 변환된값
-    var stringArrayValue : [String] = []
-    // String 타입으로 변환된값
-    var stringValue : String = ""
-    
+    var weekLottoNumber : [String : [Int]] = [:]
     
     //6개의 겹치지 않는 로또번호 생성기 저장은 Dictionary 에 저장함 저장된 key, value 모두 String 타입
-    func makeRandomNumber() {
+    func makeRandomNumber() -> String  {
+        let countWeekNumber : String = "\(makeRandomNumberCount)회차"
+        winningLottoNumber = []
         while winningLottoNumber.count < 6 {
             winningLottoNumber.insert(Int.random(in: 1...45))
         }
+        return countWeekNumber
+    }
+    
+    // 메이크 랜덤넘버 몇번했는지 카운트 해주는 함수
+    func makeRandomNumberCountUp(){
         makeRandomNumberCount += 1
-        countWeekNumber = "\(makeRandomNumberCount)회차"
-        changeType(intArray: Array(winningLottoNumber))
-        saveDictionary(key: countWeekNumber, value: stringValue)
-        winningLottoNumber = []
     }
     
     //키와 밸류 값을 받아 딕셔너리에 저장해주는 함수 매개변수 타입은 String : String
-    func saveDictionary (key: String, value : String ){
+    func saveDictionary (key: String, value : [Int] ){
         weekLottoNumber.updateValue(value, forKey: key)
-    }
-    //[Int] -> [String] -> String 타입으로 변환 하는함수
-    func changeType (intArray : [Int]){
-        stringArrayValue = intArray.map { String($0)} //이부분 공부해야함 map? 이라는 개념
-        stringValue = stringArrayValue.joined(separator: ", ")
     }
     
     //로또 번호 5번 생성 함수
-    func repeatFive(){
+    func makeRandomNumberRepeatFive(){
         for _ in 1...5 {
-            makeRandomNumber()
+            makeRandomNumberCountUp()
+            saveDictionary(key: makeRandomNumber(), value: Array(winningLottoNumber))
         }
     }
     
     //언래핑, 출력 하는 함수
-    func PrintWeeingNumber (key : String) {
-        if let unwrapValue : String =  weekLottoNumber[key] {
-            print("\(key) 의 로또 당첨 번호는 \(unwrapValue) 입니다")
-        }
+    //오타를 일으킬 위험을 조금 없애주었지만.. 이또한 하드코딩의 방식이아닌가..?
+    func PrintWeekWinningNumber(key : Int){
+        guard let unwrapValue : [Int] =  weekLottoNumber["\(key)회차"] else { return }
+        print("\(key)회차 의 로또 당첨 번호는 \(unwrapValue) 입니다")
     }
     
-    repeatFive()
-   PrintWeeingNumber(key: "2회차")
+    makeRandomNumberRepeatFive()
+    PrintWeekWinningNumber(key: 2)
     
 }
 
 weekWinningLotto()
-
