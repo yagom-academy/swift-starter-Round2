bf7487e391859215d84d1c5508d978bd770fbec5
vayne77 <sky891@naver.com>
2021-12-28 15:14:03 +0900

    feat: 회차에 해당하는 당첨번호를 출력하는 메서드 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -51,6 +51,13 @@ func saveWinningNumbers(round: Int) {
     winningNumbers = []
 }
 
+func searchRoundOfWinningNumbers(round: Int) {
+    if let roundOfWinningNumbers = roundOfWinningNumbers["\(round)회차"] {
+        let searchedWinningNumbers = convertArrayToString(someArray: roundOfWinningNumbers)
+        print("\(round)회차의 로또 당첨 번호는 \(searchedWinningNumbers) 입니다.")
+    }
+}
+
 func startLotto() {
     winningNumbers = makeDeduplicatedNumbers(winningNumbers: winningNumbers)
     printWinningNumbers(myLottoNumbers: myLottoNumbers, with: winningNumbers)
