7445e950786d38f6176d6a7a6524cc119b14c45e
Woo Jong <dnwhd0112@naver.com>
2021-12-28 19:55:45 +0900

    feat : step2 비교함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -42,7 +42,30 @@ func printLottoNDrow(maxDraw: Int,printDraw: Int) {
             
 }
 
-
+func compareLotto(myLottoNumbers my: Array<Int>,answerLottoNumbers answer: Set<Int>){
+    var countMatch: [Int] = []
+    
+    for index in 0...(my.count-1){
+        if answer.contains(my[index]){
+            countMatch.append(my[index])
+        }
+    }
+    
+    if countMatch.count > 0 {
+        var resultSentence: String = "축하합니다! 겹치는 번호는 "
+        
+        if countMatch.count > 2 {
+         for index in 0...countMatch.count-2 {
+            resultSentence = resultSentence + String(countMatch[index]) + ", "
+         }
+        }
+        resultSentence = resultSentence + String(countMatch[countMatch.count-1]) + " 입니다."
+        print(resultSentence)
+        
+    } else {
+     print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
 
 //실행코드
 printLottoNDrow(maxDraw: 5, printDraw: 2)
