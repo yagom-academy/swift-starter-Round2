10acc67481a8a34ffc2fdafb276af9bc360b3822
yoosoony <ystar819@naver.com>
2021-12-28 19:34:53 +0900

    refactor : Delete the optional parts, etc  - Delete the optional parts  - Combine the result parts

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,24 +9,18 @@
 import Foundation
 
 var winningNumbers: Set<Int> = []
-var randomNumber: Int?
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
 
-func createRandomNumber() {
-    randomNumber = Int.random(in: 1...45)
+func createRandomNumber() -> Int {
+    let randomNumber = Int.random(in: 1...45)
+    return randomNumber
 }
 
 func createWinningNumbers() -> Set<Int> {
     
     while winningNumbers.count < 6 {
-        createRandomNumber()
-        if let lottoNumber = randomNumber {
-            winningNumbers.insert(lottoNumber)
-        } else {
-            print("wrong input")
-            break
-        }
+        winningNumbers.insert(createRandomNumber())
     }
     print("당첨 번호: \(winningNumbers)")
     return winningNumbers
@@ -34,13 +28,11 @@ func createWinningNumbers() -> Set<Int> {
 
 func compareLottoNumbers(_ winningNumbers: Set<Int>, _ myNumbers: [Int]) -> String {
     var matchedNumbers: String = ""
-    var winningResult: String = ""
-    var failResult: String = ""
-    var wrongResult: String = ""
+    var resultMessage: String = ""
     
     if myNumbers.count < 1 || myNumbers.count > 7 {
-        wrongResult = "로또 번호를 입력하지 않았거나, 너무 많은 번호가 있습니다."
-        return wrongResult
+        resultMessage = "로또 번호를 입력하지 않았거나, 너무 많은 번호가 있습니다."
+        return resultMessage
     } else {
         for myNumber in myNumbers {
             if winningNumbers.contains(myNumber) {
@@ -52,20 +44,20 @@ func compareLottoNumbers(_ winningNumbers: Set<Int>, _ myNumbers: [Int]) -> Stri
         let lastComma = matchedNumbers.lastIndex(of: ",") ?? matchedNumbers.endIndex
         let removedLastCommaNumbers = matchedNumbers[..<lastComma]
         if matchedNumbers.count > 0 {
-            winningResult = "축하합니다! 겹치는 번호는 \(removedLastCommaNumbers) 입니다!"
-            return winningResult
+            resultMessage = "축하합니다! 겹치는 번호는 \(removedLastCommaNumbers) 입니다!"
+            return resultMessage
         } else {
-            failResult = "아쉽지만 겹치는 번호가 없습니다."
-            return failResult
+            resultMessage = "아쉽지만 겹치는 번호가 없습니다."
+            return resultMessage
         }
     }
 }
 
-func checkLotto(_ myNumbers: [Int]) {
+func checkLotto(myNumbers: [Int]) {
     print(compareLottoNumbers(createWinningNumbers(), myNumbers))
 }
 
-checkLotto(myLottoNumbers)
+checkLotto(myNumbers: myLottoNumbers)
 
 
 
