acb68fbc55d467019829884153c0e3cd3af1a686
doylehong <sspaka0422@gmail.com>
2021-12-29 15:21:16 +0900

    change step3 code

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -10,14 +10,13 @@ import Foundation
 
 var lottoWinningNumbersHistory: Dictionary = [String: [Int]]()
 
-func makeLottoNumbersSet() -> Set<Int> {
+func makeLottoNumbersSet() {
     var lottoNumbersSet: Set<Int> = Set<Int>()
     repeat {
         lottoNumbersSet.insert(Int.random(in: 1...45))
     } while lottoNumbersSet.count < 6
     var lottoNumbersArray: [Int] = Array(lottoNumbersSet)
     saveLottoNumbers(lottoNumbersArray: lottoNumbersArray)
-    return lottoNumbersSet
 }
 
 
@@ -25,11 +24,15 @@ func saveLottoNumbers(lottoNumbersArray: [Int]) {
     lottoWinningNumbersHistory["\(lottoWinningNumbersHistory.count+1)회차"] = lottoNumbersArray
 }
 
+func makeLottoNumbersHistory(count: Int) {
+    for _ in 1...count {
+        makeLottoNumbersSet()
+    }
+    
+}
 
 // STEP 3
-for _ in 1...5{
-    var lottoNumbersSet: Set<Int> = makeLottoNumbersSet()
-}
+makeLottoNumbersHistory(count: 5)
 
 var secondLottoWinningNumbers: [Int]? = lottoWinningNumbersHistory["2회차"]
 if let lottoWinningNumbers: [Int] = secondLottoWinningNumbers {
@@ -39,6 +42,6 @@ if let lottoWinningNumbers: [Int] = secondLottoWinningNumbers {
     }
     print("입니다.")
 } else {
-    print("There is no 2nd Lotto Number")
+    print("아쉽지만 2회차의 로또 당첨 번호가 없습니다.")
 }
 
