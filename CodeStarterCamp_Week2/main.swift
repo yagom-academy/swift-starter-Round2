ca5fd353662e3d882bed927de70f9fd64b6856ca
yoosoony <ystar819@naver.com>
2021-12-29 23:42:14 +0900

    refactor : Change the variables that show the result message

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -34,13 +34,15 @@ func removeLastComma(phrase: String) -> String {
     return String(filteredPhrase)
 }
 
+// 삼항 연산자 >> (condition) ? a : b
+// nil-coalescing >> optional<T> ?? default(value)
+
+
 func compareLottoNumbers(_ winningNumbers: Set<Int>, _ myNumbers: [Int]) -> String {
     var matchedNumbers: String = ""
-    var resultMessage: String = ""
     
     if myNumbers.count < 1 || myNumbers.count > 7 {
-        resultMessage = "로또 번호를 입력하지 않았거나, 너무 많은 번호가 있습니다."
-        return resultMessage
+        return "로또 번호를 입력하지 않았거나, 너무 많은 번호가 있습니다."
     } else {
         for myNumber in myNumbers {
             if winningNumbers.contains(myNumber) {
@@ -50,11 +52,10 @@ func compareLottoNumbers(_ winningNumbers: Set<Int>, _ myNumbers: [Int]) -> Stri
             }
         }
         if matchedNumbers.count > 0 {
-            resultMessage = "축하합니다! 겹치는 번호는 \(removeLastComma(phrase: matchedNumbers)) 입니다!"
-            return resultMessage
+            let matchedNumbersResult = "축하합니다! 겹치는 번호는 \(removeLastComma(phrase: matchedNumbers)) 입니다!"
+            return matchedNumbersResult
         } else {
-            resultMessage = "아쉽지만 겹치는 번호가 없습니다."
-            return resultMessage
+            return "아쉽지만 겹치는 번호가 없습니다."
         }
     }
 }
