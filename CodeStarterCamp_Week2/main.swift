027d1948aa2ae1a07a692a0ec30667143aa6cb7f
Baek <baekjihoon@Baekui-MacBook-Pro.local>
2021-12-30 21:10:41 +0900

    2주차 step2 로또번호 내 번호와 맞추어 보기

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,53 @@
 
 import Foundation
 
-print("Hello, World!")
+func lottoPrizeNumbers() -> Set<Int> {
+    
+    var lottoNumbers: Set<Int> = []
+    
+    while lottoNumbers.count < 6 {
+        
+        lottoNumbers.insert(Int.random(in: 1...45))
+    }
+    
+    return lottoNumbers
+}
+
+func checkLottoPrizeNumber() {
+    
+    let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+    
+    let sameLottoNumbers: Set<Int> = lottoPrizeNumbers().intersection(myLottoNumbers)
+    
+    let arrangeSameLottoNumbers : String = arrangeNumber(numbers: sameLottoNumbers)
+    
+    if sameLottoNumbers.count < 1 {
+        
+        print("아쉽지만 겹치는 번호가 없습니다")
+        
+    } else {
+        
+        print("축하합니다! 겹치는 번호는 \(arrangeSameLottoNumbers) 입니다!")
+    }
+}
+
+func arrangeNumber(numbers : Set<Int>) -> String {
+    
+    var lottoNumbers : [String] = []
+    
+    for numbers in numbers {
+        
+        lottoNumbers.append(String(numbers))
+        
+    }
+    
+    let resultNumbers = lottoNumbers.joined(separator: ", ")
+    
+    return resultNumbers
+    
+}
+
+checkLottoPrizeNumber()
+
+
 
