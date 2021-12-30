d23196569d022ada13c397a2e6c647ca35e6e94e
NAM <nvm@NAMui-MacBookAir.local>
2021-12-30 18:12:01 +0900

    feat : Step 2 로또 당첨 번호 생성 함수 및 겹치는 숫자 확인하는 함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,48 @@
 
 import Foundation
 
-print("Hello, World!")
+
+var lottoNumbers: Set<Int> = Set<Int>()
+
+func assign() {
+    for numbers in 1...45 {
+        lottoNumbers.insert(numbers)
+        
+    }
+    
+}
+
+func remove() {
+    for _ in 1...39 {
+        lottoNumbers.removeFirst()
+    }
+
+}
+
+func drawLottoNumbers() -> Set<Int> {
+    assign()
+    remove()
+    return lottoNumbers
+}
+
+let sortedMyLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+var myLottoNumbers: Set<Int> = Set(sortedMyLottoNumbers)
+
+
+let winningNumber: Set<Int> = drawLottoNumbers().intersection(myLottoNumbers)
+let emptySet: Set<Int> = Set<Int>()
+func checkWinningResult() {
+    if winningNumber == emptySet {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+        
+    } else {
+    print("축하합니다! 겹치는 번호는 \(winningNumber) 입니다!")
+    }
+    
+}
+
+print(drawLottoNumbers())
+print(myLottoNumbers)
+print(winningNumber)
+checkWinningResult()
 
