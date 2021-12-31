7780830d83c135cf994bf521d27476803da35482
NAM <nvm@NAMui-MacBookAir.local>
2021-12-31 19:09:48 +0900

    feat : 고른 번호와 로또 당첨 번호 의 겹치는 숫자를 확인하는 함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -30,3 +30,17 @@ func makeWinningLottoNumbers(number: Int) -> Set<Int> {
 let orderedMyLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 var myLottoNumbers: Set<Int> = Set(orderedMyLottoNumbers)
 
+let winningNumber: Set<Int> = makeWinningLottoNumbers(number: 5).intersection(myLottoNumbers)
+func checkWinningResult() {
+    if winningNumber.count == 0 {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+    print("축하합니다! 겹치는 번호는 \(winningNumber) 입니다!")
+    }
+}
+
+print(makeWinningLottoNumbers(number: 5))
+print(myLottoNumbers)
+print(winningNumber)
+checkWinningResult()
+
