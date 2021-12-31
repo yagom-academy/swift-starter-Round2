cc64c47a4a27ef7166e1bb215ff751ecc815b4f7
NAM <nvm@NAMui-MacBookAir.local>
2021-12-31 18:22:41 +0900

    feat : step 1 순서도 추가, step 2 로또 당첨 번호를 생성하는 함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,47 +9,22 @@
 import Foundation
 
 
-var lottoNumbers: Set<Int> = Set<Int>()
+var selectableLottoNumbers = Set<Int>()
 
-func assign() {
-    for numbers in 1...45 {
-        lottoNumbers.insert(numbers)
-        
+func assignSelectableLottoNumbers(range: Int) {
+    for count in 1...range {
+        selectableLottoNumbers.insert(count)
     }
-    
 }
 
-func remove() {
-    for _ in 1...39 {
-        lottoNumbers.removeFirst()
+let orderedLottoNumbers = Array(selectableLottoNumbers)
+
+func makeWinningLottoNumbers(count: Int) -> Set<Int> {
+    assignSelectableLottoNumbers(range: 45)
+    var winningLottoNumbers = Set<Int>()
+    for count in 0...count {
+        winningLottoNumbers.insert(orderedLottoNumbers[count])
     }
-
+    return winningLottoNumbers
 }
-
-func drawLottoNumbers() -> Set<Int> {
-    assign()
-    remove()
-    return lottoNumbers
-}
-
-let sortedMyLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-var myLottoNumbers: Set<Int> = Set(sortedMyLottoNumbers)
-
-
-let winningNumber: Set<Int> = drawLottoNumbers().intersection(myLottoNumbers)
-let emptySet: Set<Int> = Set<Int>()
-func checkWinningResult() {
-    if winningNumber == emptySet {
-    print("아쉽지만 겹치는 번호가 없습니다.")
-        
-    } else {
-    print("축하합니다! 겹치는 번호는 \(winningNumber) 입니다!")
-    }
-    
-}
-
-print(drawLottoNumbers())
-print(myLottoNumbers)
-print(winningNumber)
-checkWinningResult()
 
