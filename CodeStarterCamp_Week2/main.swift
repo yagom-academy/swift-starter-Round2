8afe2fd1784f0fd41d12cc1c5fa9925ac4da4ad4
yoosoony <ystar819@naver.com>
2022-01-01 12:27:20 +0900

    feat : Add the func that records Winning History

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,86 +8,91 @@
 
 import Foundation
 
-var lottoNumbers: Set<Int> = []
-var randomNumber: Int?
-var myLottoNumbers: [Int] = []
-var matchedNumbers: String = ""
-var result: String = ""
-var lottoHistory: Dictionary<String, Set<Int>> = [:]
-var lottoHistoryCount: Int = 0
+var winningNumbers: Set<Int> = []
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+var randomNumber: Int = 0
+var winningHistory: Dictionary<String, Set<Int>> = [:]
+var winningHistoryCount: Int = 0
+var resultMessage: String = ""
 
 
-func pickRandomNumber() {
+func createRandomNumber() -> Int {
     randomNumber = Int.random(in: 1...45)
+    return randomNumber
 }
 
-func saveWinningHistory(round: Int, numbers: Set<Int>) {
-    lottoHistory["\(round)회차"] = numbers
-}
-
-func removeLastCommaInString(_ phrase: String) -> String {
-    guard let indexOfLastComma = phrase.lastIndex(of: ",") else { return phrase }
-    let removedLastCommaPhrase = phrase[..<indexOfLastComma]
-    return String(removedLastCommaPhrase)
-}
-
-func pickLottoNumbers() -> Set<Int> {
-    
-    while lottoNumbers.count < 6 {
-        pickRandomNumber()
-        if let lottoNumber = randomNumber {
-            lottoNumbers.insert(lottoNumber)
-        } else {
-            print("wrong input")
-            break
-        }
+func createWinningNumbers() -> Set<Int> {
+    winningNumbers.removeAll()
+    while winningNumbers.count < 6 {
+        randomNumber = createRandomNumber()
+        winningNumbers.insert(randomNumber)
     }
-    lottoHistoryCount += 1
-    saveWinningHistory(round: lottoHistoryCount, numbers: lottoNumbers)
-    return lottoNumbers
+    winningHistoryCount += 1
+    recordWinningHistory(round: winningHistoryCount, numbers: winningNumbers)
+    print("당첨 번호: \(winningNumbers)")
+    return winningNumbers
 }
 
-func compareLottoNumbers(_ lottoNumbers: Set<Int>, _ myLottoNumbers: [Int]) -> String {
+func removeLastComma(phrase: String) -> String {
+    guard let indexOfLastComma = phrase.lastIndex(of: ",") else { return phrase }
+    let filteredPhrase = phrase[..<indexOfLastComma]
+    return String(filteredPhrase)
+}
+
+func compareLottoNumbers(_ winningNumbers: Set<Int>, _ myNumbers: [Int]) -> String {
+    var matchedNumbers: String = ""
     
-    if myLottoNumbers.count < 1 {
-        result = "로또 번호를 입력하지 않았습니다."
-        return result
+    if myNumbers.count < 1 || myNumbers.count > 7 {
+        resultMessage = "로또 번호를 입력하지 않았거나, 너무 많은 번호가 있습니다."
     } else {
-        for i in 0...myLottoNumbers.count - 1 {
-            if lottoNumbers.contains(myLottoNumbers[i]) {
-                matchedNumbers += "\(myLottoNumbers[i]), "
+        for myNumber in myNumbers {
+            if winningNumbers.contains(myNumber) {
+                matchedNumbers += "\(myNumber), "
             } else {
                 continue
             }
         }
-        let removeCommaNumbers = removeLastCommaInString(matchedNumbers)
         if matchedNumbers.count > 0 {
-            result = "축하합니다! 겹치는 번호는 \(removeCommaNumbers) 입니다!"
+            resultMessage = "축하합니다! 겹치는 번호는 \(removeLastComma(phrase: matchedNumbers)) 입니다!"
         } else {
-            result = "아쉽지만 겹치는 번호가 없습니다."
+            resultMessage = "아쉽지만 겹치는 번호가 없습니다."
         }
-        return result
     }
+    return resultMessage
 }
 
-func tryLotto(_ myLottoNumbers: [Int]) {
-    print(compareLottoNumbers(pickLottoNumbers(), myLottoNumbers))
+func checkLotto(myNumbers: [Int]) {
+    let winningNumbers = createWinningNumbers()
+    print(compareLottoNumbers(winningNumbers, myNumbers))
 }
 
-func readWinningHistory(round: Int) {
-    var stringLottoHistory = ""
+func recordWinningHistory(round: Int, numbers: Set<Int>) {
+    winningHistory["\(round)회차"] = numbers
+}
+
+
+func readWinningHistory(round: Int) -> String {
+    var winningHistoryNumbers: String = ""
     
-    if let someLottoHistory = lottoHistory["\(round)회차"] {
+    if let someLottoHistory = winningHistory["\(round)회차"] {
         for number in someLottoHistory {
-            stringLottoHistory += "\(number), "
+            winningHistoryNumbers += "\(number), "
         }
-        let removedLastCommaHistory = removeLastCommaInString(stringLottoHistory)
-        print("\(round)회차의 로또 당첨 번호는 \(removedLastCommaHistory) 입니다.")
+        resultMessage = "\(round)회차의 로또 당첨 번호는 \(removeLastComma(phrase: winningHistoryNumbers)) 입니다."
+    } else {
+        resultMessage = "해당 회차가 존재하지 않습니다."
     }
+    
+    return resultMessage
 }
 
-tryLotto([1, 2, 3, 4, 5, 6])
-readWinningHistory(round: 1)
+
+checkLotto(myNumbers: myLottoNumbers)
+checkLotto(myNumbers: myLottoNumbers)
+checkLotto(myNumbers: myLottoNumbers)
+checkLotto(myNumbers: myLottoNumbers)
+checkLotto(myNumbers: myLottoNumbers)
+print(readWinningHistory(round: 2))
 
 
 
