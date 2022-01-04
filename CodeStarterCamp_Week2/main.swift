08b0d354eeb7bc82ee390b67172805dd054280b7
NAM <nvm@NAMui-MacBookAir.local>
2022-01-04 16:53:12 +0900

    feat : 로또 당첨 결과 출력문 문자열로 변환 출력 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,39 +8,44 @@
 
 import Foundation
 
-
 var selectableLottoNumbers = Set<Int>()
+let orderedMyLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+var myLottoNumbers: Set<Int> = Set(orderedMyLottoNumbers)
+var winningNumbers: Set<Int> = makeWinningLottoNumbers().intersection(myLottoNumbers)
+var orderedWinningNumbers = Array(winningNumbers)
 
-func assignSelectableLottoNumbers(range: Int) {
-    for count in 1...range {
-        selectableLottoNumbers.insert(count)
+func assignSelectableLottoNumbers() {
+    for number in 1...45 {
+        selectableLottoNumbers.insert(number)
     }
 }
-
-func makeWinningLottoNumbers(number: Int) -> Set<Int> {
-    assignSelectableLottoNumbers(range: 45)
-    let orderedLottoNumbers = Array(selectableLottoNumbers)
+func makeWinningLottoNumbers() -> Set<Int> {
+    assignSelectableLottoNumbers()
+    let orederedSelectableLottoNumbers = Array(selectableLottoNumbers)
     var winningLottoNumbers = Set<Int>()
-    for count in 0...number {
-        winningLottoNumbers.insert(orderedLottoNumbers[count])
+    for count in 0...5 {
+        winningLottoNumbers.insert(orederedSelectableLottoNumbers[count])
     }
     return winningLottoNumbers
 }
-
-let orderedMyLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-var myLottoNumbers: Set<Int> = Set(orderedMyLottoNumbers)
-
-let winningNumber: Set<Int> = makeWinningLottoNumbers(number: 5).intersection(myLottoNumbers)
 func checkWinningResult() {
-    if winningNumber.count == 0 {
+    if orderedWinningNumbers.count == 0 {
     print("아쉽지만 겹치는 번호가 없습니다.")
+    } else if orderedWinningNumbers.count == 1 {
+        print("축하합니다! 겹치는 번호는", terminator: " ")
+        print("\(orderedWinningNumbers[0])", terminator: " ")
+        print("입니다!")
     } else {
-    print("축하합니다! 겹치는 번호는 \(winningNumber) 입니다!")
+        print("축하합니다! 겹치는 번호는", terminator: " ")
+        for number in 0...orderedWinningNumbers.count - 2 {
+            print("\(orderedWinningNumbers[number])", terminator: ", ")
+                }
+        print("\(orderedWinningNumbers[orderedWinningNumbers.count - 1])", terminator: " ")
+        print("입니다!")
     }
 }
 
-print(makeWinningLottoNumbers(number: 5))
+print(makeWinningLottoNumbers())
 print(myLottoNumbers)
-print(winningNumber)
+print(winningNumbers)
 checkWinningResult()
-
