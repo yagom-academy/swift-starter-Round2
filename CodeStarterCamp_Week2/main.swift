545e43ef03fa9a5ed6c9c6c988b85f446f239d9a
yoosoony <ystar819@naver.com>
2022-01-04 23:04:51 +0900

    refactor : Change the naming which isn't matched with the type, etc  - Combine the result message

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -11,6 +11,7 @@ import Foundation
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 var winningHistory: Dictionary<String, Set<Int>> = [:]
 var winningHistoryCount: Int = 0
+var resultMessage: String = ""
 
 
 func createWinningNumbers() -> Set<Int> {
@@ -51,21 +52,26 @@ func insertMatchedNumber(winningNumbers: Set<Int>, myNumbers: [Int]) -> String {
     return matchedNumbers
 }
 
-func compareLottoNumbers(_ winningNumbers: Set<Int>, _ myNumbers: [Int]) -> String {
-    var compareResultMessage: String = ""
+func checkLottoNumbersCount(_ winningNumbers: Set<Int>, _ myNumbers: [Int]) -> String {
     
     if checkMyNumbersCount(myNumbers) {
-        let resultMatchedNumbers = insertMatchedNumber(winningNumbers: winningNumbers, myNumbers: myNumbers)
-        if resultMatchedNumbers != "" {
-            let matchedNumbersWithoutLastComma = removeLastComma(phrase: resultMatchedNumbers)
-            compareResultMessage = "축하합니다! 겹치는 번호는 \(matchedNumbersWithoutLastComma) 입니다!"
-        } else {
-            compareResultMessage = "아쉽지만 겹치는 번호가 없습니다."
-        }
+        resultMessage = compareLottoNumbers(winningNumbers, myNumbers)
     } else {
-        compareResultMessage = "로또 번호를 입력하지 않았거나, 너무 많은 번호가 있습니다."
+        resultMessage = "로또 번호를 입력하지 않았거나, 너무 많은 번호가 있습니다."
     }
-    return compareResultMessage
+    return resultMessage
+}
+
+func compareLottoNumbers(_ winningNumbers: Set<Int>, _ myNumbers: [Int]) -> String {
+
+    let resultMatchedNumbers = insertMatchedNumber(winningNumbers: winningNumbers, myNumbers: myNumbers)
+    if resultMatchedNumbers != "" {
+        let matchedNumberMessage = "축하합니다! 겹치는 번호는 \(removeLastComma(phrase: resultMatchedNumbers)) 입니다!"
+        resultMessage = matchedNumberMessage
+    } else {
+        resultMessage = "아쉽지만 겹치는 번호가 없습니다."
+    }
+    return resultMessage
 }
 
 func checkLotto(myNumbers: [Int]) {
@@ -79,20 +85,19 @@ func recordWinningHistory(round: Int, numbers: Set<Int>) {
 
 
 func getWinningHistory(round: Int) -> String {
-    var winningHistoryNumbers: String = ""
-    var historyResultMessage: String = ""
+    var winningNumbersHistory: String = ""
     
     if let theLottoHistory = winningHistory["\(round)회차"] {
         for number in theLottoHistory {
-            winningHistoryNumbers += "\(number), "
+            winningNumbersHistory += "\(number), "
         }
-        let winningHistoryWithoutLastComma = removeLastComma(phrase: winningHistoryNumbers)
-        historyResultMessage = "\(round)회차의 로또 당첨 번호는 \(winningHistoryWithoutLastComma) 입니다."
+        let winningHistory = "\(round)회차의 로또 당첨 번호는 \(removeLastComma(phrase: winningNumbersHistory)) 입니다."
+        resultMessage = winningHistory
     } else {
-        historyResultMessage = "해당 회차가 존재하지 않습니다."
+        resultMessage = "해당 회차가 존재하지 않습니다."
     }
     
-    return historyResultMessage
+    return resultMessage
 }
 
 
