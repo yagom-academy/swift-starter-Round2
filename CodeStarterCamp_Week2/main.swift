f24764bb587435934d5444afc093cd1ae39bea0a
cpp2swift <dkseotn123@gmail.com>
2022-02-15 17:59:33 +0900

    함수 호출 구현 및 추가
    원하는 결과를 얻기위해 함수 호출 구현 및 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -41,3 +41,6 @@ func checkLottoNumber(myNumbers: Array<Int>, winningNumber: Set<Int>) -> Void {
         print("축하합니다! 겹치는 번호는 \(result) 입니다!")
     }
 }
+
+generateRandomNumber()
+checkLottoNumber(myNumbers: myLottoNumbers, winningNumber: randomNumber)
