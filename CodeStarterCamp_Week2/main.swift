a75a8d5c95175a58bc04defe86588d560effa50a
cpp2swift <dkseotn123@gmail.com>
2022-02-15 17:49:07 +0900

    로또 당첨 번호 생성 함수 추가
    Step1의 순서도를 바탕으로 로또 당첨 번호를 생성하는 함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,7 +6,15 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
+import Swift
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [1,2,3,4,5,6]
+
+var randomNumber : Set<Int> = Set<Int>()
+
+func generateRandomNumber() -> Void {
+    while randomNumber.count < 6 {
+        randomNumber.insert(Int.random(in: 1...45))
+    }
+}
 
