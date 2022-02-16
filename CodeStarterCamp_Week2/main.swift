a424942f03e60fd8de45b055ec6535fbfa998584
소영 <sso_0022@naver.com>
2022-02-16 15:46:54 +0900

    feat: [step2] 로또 당첨 겹치는 번호 확인 함수 구현

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,12 +8,26 @@
 
 import Foundation
 
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+let winnerLottoNumbers: [Int] = generateLottoNumbers()
+
 func generateLottoNumbers() -> [Int] {
   var lottoNumbers: Set<Int> = Set<Int>()
   while lottoNumbers.count < 6 {
     let randomNumber = Int.random(in: 1...45)
     lottoNumbers.insert(randomNumber)
   }
-  
   return Array(lottoNumbers)
 }
+
+func checkLottoNumbers(mine: [Int], winner: [Int]) {
+  let sameNumbers: Set<Int> = Set(mine).intersection(Set(winner))
+  if sameNumbers.count > 0 {
+    let numbersString = sameNumbers.map { "\($0)" }.joined(separator: ", ")
+    print("축하합니다! 겹치는 번호는 \(numbersString) 입니다!")
+  } else {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+  }
+}
+
+checkLottoNumbers(mine: myLottoNumbers, winner: winnerLottoNumbers)
