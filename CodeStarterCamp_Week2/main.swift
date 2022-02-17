d6ec6cfb1d7ded8247e9219cd1f797a963321e22
lia388 <lia388@naver.com>
2022-02-18 00:25:03 +0900

    [Week2-step2] : 로또 당첨 번호 생성 함수 및 겹치는 숫자 확인 함수 생성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+//Step1의 순서도를 바탕으로 로또 당첨 번호를 생성하는 함수를 생성합니다.
+var numbers: Array<Int> = Array(1...45)
+var randomNumbers = numbers.shuffled()
+func generateLottoNumbers(randomNumbers: Array<Int>) -> Set<Int> {
+    let lottoNumbers: Set<Int> = Set(randomNumbers[0...5])
+    return lottoNumbers
+}
+
+let lottoNumbers = generateLottoNumbers(randomNumbers: randomNumbers)
+
+//내가 찍은 번호 배열을 myLottoNumbers 변수에 할당합니다.
+let myLottoNumbers: Set<Int> = [5, 13, 23, 25, 37, 42]
+
+//찍은 번호와 로또 당첨 번호 의 겹치는 숫자를 확인하는 함수를 생성합니다.
+func checkLottoNumbers(myLottoNumbers: Set<Int>, lottoNumbers: Set<Int>) {
+    let intersectSet = lottoNumbers.intersection(myLottoNumbers)
+    let interection: Int = intersectSet.count
+    if interection < 1 {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는 \(intersectSet) 입니다!")
+    }
+}
+
+checkLottoNumbers(myLottoNumbers: myLottoNumbers, lottoNumbers: lottoNumbers)
 
