fa6aab63fe9d23c5f8428ebd9f0ddc9914f0e4b1
yeeton37 <lyeeun37@naver.com>
2022-02-18 23:34:38 +0900

    step2 : 내 번호와 맞추어보기 코드 제출

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,22 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week2
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
+var makingNumbers = Set<Int>()
+var myLottoNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
 
-import Foundation
+func generateRandomNumber()  {
+    while makingNumbers.count < 6 {
+        makingNumbers.insert(Int.random(in: 1...45))
+    }
+}
 
-print("Hello, World!")
+func checkLottoNumber() {
+    let chosenNumber : Set<Int> = Set(makingNumbers).intersection(Set(myLottoNumbers))
+    let joinedStr = chosenNumber.map { String($0) }.joined(separator: ", ")
+
+    if chosenNumber.count == 0  {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는 \(joinedStr) 입니다.")
+    }
+}
+generateRandomNumber()
+checkLottoNumber()
 
