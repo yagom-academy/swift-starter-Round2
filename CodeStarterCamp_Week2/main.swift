9649045e604f630f8bd5caa2cc20a61907f28855
Juho Jung <jooho293@gmail.com>
2022-02-18 16:53:44 +0100

    Refactor: making code for lotto numbers
    -

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,24 +8,26 @@
 
 import Foundation
 
+let myNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
+var randomNumbers = Set<Int>()
+
 func makeLottoNumbers() {
-    var randomNumbers = Set<Int>()
+
     while randomNumbers.count < 6 {
         randomNumbers.insert(Int.random(in: 1...45))
     }
     print(randomNumbers)
-    
-    let myNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
+}
+func resultLottoNumbers() {
     var winnigLottoNumbers = Set<Int>()
     winnigLottoNumbers = randomNumbers.intersection(myNumbers)
     if winnigLottoNumbers.isEmpty {
         print("맞는번호 없음")
     } else {
-        for numbers in winnigLottoNumbers {
-            print("맞는 번호는 \(numbers) 입니다")
+        print("맞는 번호는 \(winnigLottoNumbers) 입니다")
         }
     }
-}
+
 /* 배열 내의 교집합을 찾는 코드
 let testNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
 let testNumbers2: Set<Int> = [4, 5, 6, 7, 8, 9]
@@ -38,3 +40,4 @@ for numbers in duplicateNumber {
 }
 */
 makeLottoNumbers()
+resultLottoNumbers()
