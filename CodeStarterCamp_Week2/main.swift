efa53bdcea4ef9a3f72ce8955308cd5baf219d94
TaeSeong <ts4683@gmail.com>
2022-02-20 03:06:25 +0900

    feat: 회차별 로또번호저장 기능 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -11,11 +11,19 @@ import Foundation
 var lottoNumbers: Set<Int> = []
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 var matchingNumbers: [Int] = []
+var lottoNumbersPerRound: [String: Any] = [:]
+var round: Int = 0
 
 func generateLottoNumbers() {
     while lottoNumbers.count<6 {
         lottoNumbers.insert(Int.random(in: 1...45))
     }
+    recordLottoNumbersPerRound()
+}
+
+func recordLottoNumbersPerRound() {
+    round += 1
+    lottoNumbersPerRound["\(round)"+"회차"] = lottoNumbers.sorted()
 }
 
 func findMatchingNumbers() {
