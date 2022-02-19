eef7f2e7a82f3b1c0c74e5254fbfe98dfa01fe8a
your_name <sondaehyeon@sondaehyeon-ui-MacBookAir.local>
2022-02-19 22:57:08 +0900

    Week2 [STEP2] 입니다

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -7,6 +7,25 @@
 //
 
 import Foundation
-
-print("Hello, World!")
+var randomNumbers = Set<Int>()
+func makeLottoNumbers() {
+    while randomNumbers.count < 6 {
+        randomNumbers.insert(Int.random(in: 1...45))
+    }
+    print(randomNumbers)
+}
+let myLottoNumbers: Set<Int> = [20, 37, 30, 5, 40 ,22]
+var printlotto: Set<Int> = []
+func compareMyLottonumbers() {
+    let collectionLottoNumbers: Set<Int> = myLottoNumbers.intersection(printlotto)
+    if collectionLottoNumbers.isEmpty == false {
+        print("축하합니다 겹치는 번호는\(collectionLottoNumbers)입니다.")
+    } else if collectionLottoNumbers.isEmpty == true {
+        print("아쉽지만 꽝! 입니다 다음 기회에!")
+    }
+}
+printlotto = [20, 37, 5 , 22, 11]
+compareMyLottonumbers()
+printlotto = [12, 4, 45, 2, 19, 33]
+compareMyLottonumbers()
 
