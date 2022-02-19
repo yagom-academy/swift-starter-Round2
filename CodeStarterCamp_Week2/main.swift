061d27346502eac4b80278482b47eb44e1b44685
yeeton37 <lyeeun37@naver.com>
2022-02-19 16:09:48 +0900

    DOCS: step2 변수 선언 위치 변경

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,22 +1,25 @@
-var makingNumbers = Set<Int>()
-var myLottoNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
+func generateLottoNumber() -> Set<Int> {
+    
+     var makingNumbers = Set<Int>()
+     while makingNumbers.count < 6 {
+         makingNumbers.insert(Int.random(in: 1...45))
+     }
+     return makingNumbers
+    
+ }
 
-func generateRandomNumber()  {
-    while makingNumbers.count < 6 {
-        makingNumbers.insert(Int.random(in: 1...45))
-    }
-}
+ func checkLottoNumber() {
+     
+     let myLottoNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
+     let chosenNumber : Set<Int> = Set(generateLottoNumber()).intersection(Set(myLottoNumbers))
+     let joinedStr = chosenNumber.map { String($0) }.joined(separator: ", ")
 
-func checkLottoNumber() {
-    let chosenNumber : Set<Int> = Set(makingNumbers).intersection(Set(myLottoNumbers))
-    let joinedStr = chosenNumber.map { String($0) }.joined(separator: ", ")
+     if chosenNumber.count == 0  {
+         print("아쉽지만 겹치는 번호가 없습니다.")
+     } else {
+         print("축하합니다! 겹치는 번호는 \(joinedStr) 입니다.")
+     }
+ }
 
-    if chosenNumber.count == 0  {
-        print("아쉽지만 겹치는 번호가 없습니다.")
-    } else {
-        print("축하합니다! 겹치는 번호는 \(joinedStr) 입니다.")
-    }
-}
-generateRandomNumber()
+generateLottoNumber()
 checkLottoNumber()
-
