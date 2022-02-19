550a116a6417a043f642448d9e75b27d983f70f7
sozohoy <sozohoy@gmail.com>
2022-02-20 00:44:42 +0900

    [ADD] step2

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,36 @@
 
 import Foundation
 
-print("Hello, World!")
+var randomNumbers: Set<Int> = Set<Int>()
+let myLottoNumbers: Set<Int> = [1,20,25,30,39,44]
+var printNumber = [Int]()
+var countNumber = 0
 
+func printLotto(){
+    checkNumbers()
+    if countNumber == 0{
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다 ! 겹치는 번호는 \(printNumber) 입니다 !")
+    }
+}
+
+func checkNumbers(){
+    makeRandom()
+    for randomIndex in randomNumbers{
+        for myNumberIndex in myLottoNumbers{
+            if randomIndex == myNumberIndex {
+                countNumber = countNumber + 1
+                printNumber.append(randomIndex)
+            }
+        }
+    }
+}
+
+func makeRandom(){
+    for _ in 0..<6{
+        randomNumbers.insert(Int.random(in: 1...45))
+    }
+}
+
+printLotto()
