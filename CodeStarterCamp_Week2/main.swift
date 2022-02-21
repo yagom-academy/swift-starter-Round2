4bef63a0dafe3c736747ed70e5ec17c798a20790
baard <schy2503@naver.com>
2022-02-22 00:18:54 +0900

    FEAT: Add Step2 swift file

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,30 @@
 
 import Foundation
 
-print("Hello, World!")
+//MARK: - lottoNumberGenerator
+func lottoNumberGenerator() -> Set<Int> {
+    
+    var lottoNumber = Set<Int>()
+    while lottoNumber.count < 6 {
+        var randomNumber = Int.random(in: 1...45)
+        lottoNumber.insert(randomNumber)
+    }
+    return lottoNumber
+}
+lottoNumberGenerator()
+
+//MARK: - winningNumbers
+let myLottoNumbers:[Int] = [42, 2, 10, 14, 25]
+func winningNumbers(lottoNumber: Set<Int>, myNumber: [Int]) {
+    let intersection = lottoNumberGenerator().intersection(myLottoNumbers)
+    let intersectionSet = intersection.map(String.init).joined(separator: ", ") //Set, joined 공식문서 다시 참고하기
+    if intersection == [] {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는 \(intersectionSet)입니다!") //순서가 없는 Set는 joined를 사용하지 못함
+    }
+}
+
+winningNumbers(lottoNumber: lottoNumberGenerator(), myNumber: myLottoNumbers)
+
 
