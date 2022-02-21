eaa55c8834f56859d1f4c978dc72c9d5990938ae
your_name <sondaehyeon@sondaehyeon-ui-MacBookAir.local>
2022-02-21 17:37:08 +0900

    Week2 [STEP2] 수정했습니다!

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,31 +1,25 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week2
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
-var randomNumbers = Set<Int>()
-func makeLottoNumbers() {
-    while randomNumbers.count < 6 {
-        randomNumbers.insert(Int.random(in: 1...45))
-    }
-    print(randomNumbers)
-}
-let myLottoNumbers: Set<Int> = [20, 37, 30, 5, 40 ,22]
-var printlotto: Set<Int> = []
-func compareMyLottonumbers() {
-    let collectionLottoNumbers: Set<Int> = myLottoNumbers.intersection(printlotto)
-    if collectionLottoNumbers.isEmpty == false {
-        print("축하합니다 겹치는 번호는\(collectionLottoNumbers)입니다.")
-    } else if collectionLottoNumbers.isEmpty == true {
-        print("아쉽지만 꽝! 입니다 다음 기회에!")
-    }
-}
-printlotto = [20, 37, 5 , 22, 11]
-compareMyLottonumbers()
-printlotto = [12, 4, 45, 2, 19, 33]
-compareMyLottonumbers()
 
+
+var randomNumbers = Set<Int>()
+func makeLottoNumbers()  -> Set<Int>{
+    while randomNumbers.count < 6 {
+            randomNumbers.insert(Int.random(in: 1...45))
+        }
+        print(randomNumbers)
+    return randomNumbers
+    }
+
+    let myLottoNumbers: Set<Int> = makeLottoNumbers()
+    var printLottoNumber: Set<Int> = []
+    func compareMyLottonumbers() {
+        printLottoNumber = [1, 22, 43, 2, 5, 12]
+        let collectionLottoNumbers: Set<Int> = myLottoNumbers.intersection(printLottoNumber)
+        if collectionLottoNumbers.isEmpty {
+            print("아쉽지만 꽝! 입니다 다음 기회에!")
+        } else  {
+            
+            print("축하합니다 겹치는 번호는\(collectionLottoNumbers)입니다.")
+        }
+    }
+compareMyLottonumbers()
