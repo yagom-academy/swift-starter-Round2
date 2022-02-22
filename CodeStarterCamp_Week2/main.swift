7e6e33aeb481ba6e3a0f48ba09ab16190cb7b20c
SEO-YJ <tjdbwns19@naver.com>
2022-02-22 22:47:51 +0900

    feat: week2-step2 과제 구현. - 로또 번호와 내 번호를 맞춰보는 함수를 구현하였다.

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,38 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week2
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
+func drawLotto() -> Set<Int> {
+    var lottoNumbers = Set<Int>()
+    
+    for _ in 1... {
+        let number = Int.random(in: 1...45)
+        lottoNumbers.insert(number)
+        
+        if lottoNumbers.count == 6 {
+            break
+        }
+    }
+    return lottoNumbers
+}
 
-import Foundation
+func printSet(intersectionNumber: Set<Int>) {
+    var loopCount = 1
+    
+    for number in intersectionNumber.sorted() {
+        loopCount == intersectionNumber.count ? print("\(number)", terminator: " ") : print("\(number)", terminator: ", ")
+            loopCount += 1
+    }
+}
 
-print("Hello, World!")
+func checkNumber(chooseNumber: Set<Int>, winningNumber: Set<Int>) {
+    let intersectionNumber: Set<Int> = chooseNumber.intersection(winningNumber)
+    
+    if intersectionNumber.isEmpty != true {
+        print("축하합니다! 겹치는 번호는", terminator: " ")
+        printSet(intersectionNumber: intersectionNumber)
+        print("입니다!")
+    } else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
 
+var lotto = drawLotto()
+let myLottoNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
+checkNumber(chooseNumber: myLottoNumbers, winningNumber: lotto)
