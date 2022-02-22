a7b1caf77c016656b50b40ec417ddb62aa063dc0
lia388 <lia388@naver.com>
2022-02-23 01:19:35 +0900

    [Week2-step3] : 코멘트를 반영한 전체 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,29 +8,29 @@
 
 import Foundation
 
+var lottoNumberDict: [String: Set<Int>] = [String: Set<Int>]()
+
 func generateLottoNumbers() -> Set<Int> {
     let numbers: Array<Int> = Array(1...45)
     let randomNumbers = numbers.shuffled()
     let lottoNumbers: Set<Int> = Set(randomNumbers[0...5])
     return lottoNumbers
 }
-let lottoNumbers = generateLottoNumbers()
 
-func generateLottoPack(lottoNumbers: Set<Int>) -> Dictionary<String, String> {
-    var lottoNumberDict: Dictionary<String, String> = Dictionary<String, String>()
+func generateLottoPack() {
     for count in 1...5 {
-        lottoNumberDict["\(count)회차"] = "\(lottoNumbers)"
+        lottoNumberDict["\(count)회차"] = generateLottoNumbers()
     }
-    return lottoNumberDict
 }
-let lottoNumberDict = generateLottoPack(lottoNumbers: lottoNumbers)
 
-func findSecondLottoNumber(week: String, lottoNumberDict: Dictionary<String, String>) {
-    if let winnigLottoNumber = lottoNumberDict["\(week)"] {
+func findWeekLottoNumber(in week: String, from lottoNumberDict: [String: Set<Int>]) {
+    if let winnigLottoNumber = lottoNumberDict[week] {
         let finalLottoNumber = "\(winnigLottoNumber)".trimmingCharacters(in: ["[","]"])
         print("\(week) 로또 당첨 번호는 \(finalLottoNumber) 입니다.")
     } else {
         print("선택하신 회차의 로또 번호는 없습니다.")
     }
 }
-findSecondLottoNumber(week: "2회차", lottoNumberDict: lottoNumberDict)
+
+generateLottoPack()
+findWeekLottoNumber(in: "2회차", from: lottoNumberDict)
