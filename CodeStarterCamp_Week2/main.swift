d0d961c9789e72c76ef8e703f068fbb0ccc04aed
ryu-yeon <rytak108@gmail.com>
2022-02-23 00:12:29 +0900

    함수명과 변수명 Num -> Numbers 수정, makeLottoNumbers 함수 코드 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -11,27 +11,27 @@ import Foundation
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
 func makeLottoNumbers() -> Array<Int> {
-    var totalNum: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45]
-    var lottoNum: [Int] = []
-    for _ in 1...6 {
-        let pickNum = totalNum.remove(at: Int.random(in: 0...44))
-        lottoNum.append(pickNum)
+    var totalNumbers: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45]
+    var lottoNumbers: [Int] = []
+    for count in 0...5 {
+        let pickNumbers = totalNumbers.remove(at: Int.random(in: 0...(44 - count)))
+        lottoNumbers.append(pickNumbers)
     }
-    return lottoNum
+    return lottoNumbers
 }
 
-func checkMyLottoNum(myLottoNumbers: Array<Int>) {
-    let lottoNum = makeLottoNumbers()
-    var winningMyNum: [Int] = []
+func checkMyLottoNumbers(myLottoNumbers: Array<Int>) {
+    let lottoNumbers = makeLottoNumbers()
+    var winningMyNumbers: [Int] = []
     for member in myLottoNumbers {
-        if lottoNum.contains(member) {
-            winningMyNum.append(member)
+        if lottoNumbers.contains(member) {
+            winningMyNumbers.append(member)
         }
     }
-    if winningMyNum.count > 0 {
+    if winningMyNumbers.count > 0 {
         print("축하합니다! 겹치는 번호는 ", terminator: "")
-        for member in winningMyNum {
-            if member == winningMyNum.last {
+        for member in winningMyNumbers {
+            if member == winningMyNumbers.last {
                 print("\(member) 입니다!")
             } else {
                 print(member, terminator: ", ")
@@ -42,4 +42,4 @@ func checkMyLottoNum(myLottoNumbers: Array<Int>) {
     }
 }
 
-checkMyLottoNum(myLottoNumbers: myLottoNumbers)
+checkMyLottoNumbers(myLottoNumbers: myLottoNumbers)
