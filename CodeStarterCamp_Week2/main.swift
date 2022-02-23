b69ba9182798c8126bc6dbacf64e84c2331f83d2
SEO-YJ <tjdbwns19@naver.com>
2022-02-23 21:27:39 +0900

    feat: checkNumber 함수 수정 - 함수명 및 로직 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -23,8 +23,11 @@ func printIntersectionNumber(intersectionNumber: Set<String>) {
     print("축하합니다! 겹치는 번호는 " + intersectionNumber.sorted().joined(separator: ", ") + " 입니다!")
 }
 
-func checkLotto(chooseNumber: Set<Int>, winningNumber: Set<Int>) {
-    let intersectionNumber = chooseNumber.intersection(winningNumber)
+func compareNumbers(chooseNumbers: Set<Int>, winningNumbers: Set<Int>) -> Set<Int> {
+    let sameNumbers = chooseNumbers.intersection(winningNumbers)
+    
+    return sameNumbers
+    /*
     let literalNumber = changeLiteral(integerNumber: intersectionNumber)
     
     if literalNumber.isEmpty != true {
@@ -32,13 +35,19 @@ func checkLotto(chooseNumber: Set<Int>, winningNumber: Set<Int>) {
     } else {
         print("아쉽지만 겹치는 번호가 없습니다.")
     }
+     */
 }
+
+func printLottoResult(
 
 
 let myLottoNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
 var winnigLottoNumbers = makeLottoNumbers()
 
 // 번호를 체크하자
+compareNumbers(chooseNumbers: myLottoNumbers, winningNumbers: winnigLottoNumbers)
 // 당첨여부를 출력하자
+printLottoResult
 
-checkNumber(chooseNumber: myLottoNumbers, winningNumber: lotto)
+
+
