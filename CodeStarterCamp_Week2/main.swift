5808bc2e38b41a2f63db045ddeff856e383cbfc1
Makseong2 <wshstjddl@gmail.com>
2022-02-23 20:07:11 +0900

    [Week2-step2] : step2 함수 재구성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,5 @@
 
 import Foundation
 
-compare(lotteryNumbers : lotteryNumbers, myLotteryNumbers: myLotteryNumbers)
+compareNumbers()
 
--- CodeStarterCamp_Week2/step2.swift
+++ CodeStarterCamp_Week2/step2.swift
@@ -18,11 +18,13 @@ func lotteryNumberGenerator() -> Set<Int> {
     return lotteryNumbers
 }
 
-func compare(lotteryNumbers: Set<Int>, myLotteryNumbers: Array<Int>) {
-    if Set(myLotteryNumbers) == lotteryNumbers {
-        let winningMessage = "축하합니다! 겹치는 번호는 \(myLotteryNumbers[0]),\(myLotteryNumbers[1]),\(myLotteryNumbers[2]),\(myLotteryNumbers[3]),\(myLotteryNumbers[4]),\(myLotteryNumbers[5]) 입니다!"
-        print(winningMessage)
-    } else {
+func compareNumbers() {
+    let compareLotteryNumbers: Set<Int> = lotteryNumberGenerator().intersection(myLotteryNumbers)
+    
+    if compareLotteryNumbers.isEmpty {
         print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        let lotteryNumber = compareLotteryNumbers.map{String($0)}.joined(separator: ", ")
+        print("축하합니다! 겹치는 번호는 \(lotteryNumber) 입니다!")
     }
 }
