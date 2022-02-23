89163184c19ba8ed04007e765d7ab53e7be75ce9
ryu-yeon <rytak108@gmail.com>
2022-02-24 07:28:59 +0900

    makeLottoNumbers함수의 반환값 표기 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -10,7 +10,7 @@ import Foundation
 
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
-func makeLottoNumbers() -> Array<Int> {
+func makeLottoNumbers() -> [Int] {
     var totalNumbers: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45]
     var lottoNumbers: [Int] = []
     for count in 0...5 {
