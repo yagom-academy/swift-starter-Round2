d89e2f3490f1ad0971984b87c7f00481a0efe570
mskaang <mskaang@gmail.com>
2022-02-24 02:53:01 +0900

    [UPDATE & FIX] 과제에 없는 checkLottoWinningByRound 함수 추가(당첨번호가 generate 완료된 회차들과 자신의 입력 번호를 비교하는 기능. 아직 generate 완료되지 않은 회차를 입력할 경우 에러메시지 출력), 해당 기능 구현을 위해 기존 코드의 데이터 타입 변경 완료

--- CodeStarterCamp_Week2/step 3_storeLottoNumbers.swift
+++ CodeStarterCamp_Week2/step 3_storeLottoNumbers.swift
@@ -7,20 +7,22 @@
 
 import Foundation
 
-var numbersByRounds : [Int: Array<String>] = [:]
+var numbersByRounds : [Int: Set<Int>] = [:]
 var round = 1
 
 
 func storeWinningNumbersByRounds(roundNumber: Int, winningNumbers: Set<Int>) {
-    numbersByRounds.updateValue(convertingIntSetToStringArray(winningNumbers), forKey: round)
+    numbersByRounds.updateValue(winningNumbers, forKey: round)
     round = round + 1
 }
 
 
 func printWinningNumbersByRounds(roundNumber: Int) {
-    if let numbersForThisRound = numbersByRounds[roundNumber] {
-        print("\(roundNumber)회차의 로또 당첨 번호는 \(numbersForThisRound.joined(separator: ", ")) 입니다.")
+    guard numbersByRounds[roundNumber] != nil else {
+        printUnvalidRoundMessage(inputRound: roundNumber)
+        return
     }
+    print("\(roundNumber)회차의 로또 당첨 번호는 \(convertingIntSetToStringArray(numbersByRounds[roundNumber]!).joined(separator: ", ")) 입니다.")
 }
 
 
@@ -31,4 +33,34 @@ func convertingIntSetToStringArray(_ intSet: Set<Int>) -> Array<String> {
         stringArray.append("\(intArray[index])")
     }
     return stringArray
+}
+
+
+func checkLottoWinningByRound(round: Int, myNumberSet: Set<Int>) {
+    
+    guard let temp = numbersByRounds[round] else {
+        printUnvalidRoundMessage(inputRound: round)
+        return
+    }
+    
+    let winningNumbers = temp.intersection(myNumberSet)
+    if winningNumbers.count > 0 {
+        print("축하합니다! \(round)회차 결과와 겹치는 번호는 \(convertingIntSetToStringArray(winningNumbers).joined(separator: ", ")) 입니다!")
+    } else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+
+
+func printUnvalidRoundMessage(inputRound: Int) {
+    let message = """
+    
+        -----------------<오류>-------------------
+             입력하신 \(inputRound)회는 아직 추첨되지 않았습니다.
+             마지막으로 추첨 완료된 회차는 \(round)회 입니다.
+             추첨 완료된 회차를 입력해주시기 바랍니다.
+        -----------------------------------------
+    
+    """
+    print(message)
 }
