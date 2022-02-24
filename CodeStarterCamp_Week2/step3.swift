29fb7041fdf54bbbbd1e0da62d4478309e3c2832
Makseong2 <wshstjddl@gmail.com>
2022-02-24 22:36:11 +0900

    [Week2-step3] : 함수구현

--- CodeStarterCamp_Week2/step3.swift
+++ CodeStarterCamp_Week2/step3.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+var lotteryNumbers : Set<Int> = Set<Int>()
+var lotteryHistory : Dictionary<String,Array<Int>> = [String:Array<Int>]()
+
+func generateLotteryNumbers(round: Int) {
+    while lotteryNumbers.count <= 5 {
+        let pickedNumber = Int.random(in: 1...45)
+        lotteryNumbers.update(with: pickedNumber)
+    }
+    saveLotteryNumbers(round: round)
+}
+
+func saveLotteryNumbers(round: Int) {
+    lotteryHistory["\(round)회차"] = Array(lotteryNumbers)
+}
+
+
+func generateAndSaveLotteryNumbers(showFullRound lastRound: Int) -> Dictionary<String,Array<Int>> {
+    for round in 1...lastRound {
+        generateLotteryNumbers(round: round)
+        lotteryNumbers.removeAll()
+    }
+    return lotteryHistory
+}
+
+func lookUpLotteryNumbers(round: Int) {
+    if let secondRoundNumbers = lotteryHistory["\(round)회차"] {
+        print("\(round)회차의 로또 당첨 번호는 \(secondRoundNumbers.map{String($0)}.joined(separator: ", ")) 입니다.")
+    }
+}
