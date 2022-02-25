ec6ae988fe5e95970ba83f10225e48f609e34c60
iwonman <tjdnjswls3@naver.com>
2022-02-25 18:15:46 +0900

    Step02 Code commit

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
+func GenerateRandNum() -> Set<Int>{
+    while randomLottoNum.count != 6 {
+        let randNum = Int.random(in: 1...45)
+        if randomLottoNum.contains(randNum) {
+            continue
+        }
+        else {
+            randomLottoNum.insert(randNum)
+        }
+    }
+    return randomLottoNum
+}
+
+func checkLottoNum(){
+    let intersection: Set<Int> = randomLottoNum.intersection(myLottoNumbers)
+    if intersection.count == 0 {
+            print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+    else {
+        print("축하합니다! 겹치는 번호는",intersection,terminator: "입니다!\n")
+    }
+}
+var randomLottoNum: Set<Int> = Set<Int>()
+randomLottoNum=GenerateRandNum()
+print("<당첨번호>\n",randomLottoNum)
+let myLottoNumbers: [Int] = [12,24,32,41,5,16]
+checkLottoNum()
+
 
