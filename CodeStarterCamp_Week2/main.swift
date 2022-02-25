9ecf8c64e7c8eaba488755b4b0463aba5d226159
jun <jun@junui-MacBook-Pro.local>
2022-02-26 01:26:06 +0900

    Add : 함수 printLottoResult 생성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -27,17 +27,21 @@ let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
 func checkLottoNumbers() {
     let winningLottoNumbers: Set<Int> = lottoNumbers.intersection(myLottoNumbers)
-    if winningLottoNumbers.count > 0 {
-        print("축하합니다! 겹치는 번호는 \(winningLottoNumbers) 입니다!")
+}
+
+func printLottoResult() {
+    if winningLottoNumbers.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
     }
     else {
-        print("아쉽지만 겹치는 번호가 없습니다.")
+        print("축하합니다! 겹치는 번호는 \(winningLottoNumbers) 입니다!")
     }
 }
 
 func playLotto() {
     saveLottoNumbers()
     checkLottoNumbers()
+    printLottoResult()
 }
 
 playLotto()
