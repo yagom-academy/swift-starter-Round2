ada1ff37eaae70f4fdfb5bf02e8993197f08d056
Jun <jun@Junui-Macmini.local>
2022-02-27 14:56:28 +0900

    Week2 [STEP 2] 로또 번호 생성 및 당첨 확인

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,41 @@
 
 import Foundation
 
-print("Hello, World!")
+var myLottoNumbers: [Int] = []
+var lottoNumbers: [Int] = []
 
+func chooseNumbers() {
+    while lottoNumbers.count < 6 {
+        var randomNumber: Int = Int.random(in: 1...45)
+        if !lottoNumbers.contains(randomNumber) {
+            lottoNumbers.append(randomNumber)
+        }
+    }
+    while myLottoNumbers.count < 6 {
+        var randomNumber: Int = Int.random(in: 1...45)
+        if !myLottoNumbers.contains(randomNumber) {
+            myLottoNumbers.append(randomNumber)
+        }
+    }
+}
+
+var correctNumbers: Set<Int> = Set<Int>()
+
+func compareNumbers() {
+    for index in 0...5 {
+        if lottoNumbers.contains(myLottoNumbers[index]) {
+            correctNumbers.insert(myLottoNumbers[index])
+        }
+    }
+}
+
+chooseNumbers()
+compareNumbers()
+switch correctNumbers.count {
+case 0:
+    print("아쉽지만 겹치는 번호가 없습니다.")
+case 1...6:
+    print("축하합니다! 겹치는 번호는 \(correctNumbers) 입니다!")
+default:
+    print("unknown")
+}
