5e521e1602989c0bb093f8641f6b7060b4f3ee5a
baard <schy2503@naver.com>
2022-02-28 00:27:49 +0900

    REFACTOR: Refactoring code about generating lotto numbers

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -40,33 +40,25 @@ func overlappingNumbers(lottoNumber: Set<String>, myNumber: [String]) {
 overlappingNumbers(lottoNumber: lottoNumberGenerator(), myNumber: myLottoNumbers)
 
 //MARK: - arrayLottoNumberGenerator()
-var lottoDictionary:[String : [Int]] = [:]
+var lottoDictionary: [String : [Int]] = [:] // 로또번호 저장할 빈 딕셔너리
+let arrayNumbers = Array<Int>(1...45)   // 랜덤숫자 6개 만들어내는 함수 - 로또번호  -> 로또번호만 단순히 만드는 함수
 
 func arrayLottoNumberGenerator() -> [Int] {
-    var lottoNumber = Set<Int>()
-    
-    while lottoNumber.count < 6 {
-        var randomNumber = Int.random(in: 1...45)
-        lottoNumber.insert(randomNumber)
-    }
-    let arraylottonumber = Array(lottoNumber)
-    return arraylottonumber
+    let shuffledNumbers = arrayNumbers.shuffled()[0...5]
+    let arrayShuffledNumbers = Array(shuffledNumbers)
+    return arrayShuffledNumbers
 }
 
-//MARK: - saveLottoNumbers
-func saveLottoNumbers(round: Int) -> [String : [Int]] {
-    var n = 1
-    
-    while n <= round {
-        var joinedLottoNumberGenerator = arrayLottoNumberGenerator().sorted()
-        lottoDictionary["\(n)회차"] = joinedLottoNumberGenerator
-        n = n + 1
-    }
-    return lottoDictionary
+//MARK: - lottoNumbersWithRounds()
+func lottoNumbersWithRounds() {             // 로또 번호와 회차를 같이 출력해 딕셔너리에 저장하는 함수
+    let round = "\(lottoDictionary.count + 1)회차"
+    lottoDictionary[round] = arrayLottoNumberGenerator()
 }
 
-print(saveLottoNumbers(round: 5))
-
+//MARK: - 로또번호와 회차를 같이 출력하는 함수 5번 반복
+for _ in 1...5{
+    lottoNumbersWithRounds()
+}
 //MARK: - showWinningNumbers
 func showWinningNumbers(round: Int) {
     if let winningNumbers = lottoDictionary["\(round)회차"] {
@@ -75,5 +67,4 @@ func showWinningNumbers(round: Int) {
         print("아직 \(round)회차의 당첨 번호가 아직 추첨되지 않았습니다.")   //  round의 값을 벗어나면 출력
     }
 }
-showWinningNumbers(round: 3)
-print(lottoNumberGenerator())
+showWinningNumbers(round: 2)
