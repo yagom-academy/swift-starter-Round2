ae228c646db94d46637efcdc182d38ed607c27ca
iwonman <tjdnjswls3@naver.com>
2022-02-28 17:48:35 +0900

    Step02 Code edit

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,21 +8,15 @@
 
 import Foundation
 
-func GenerateRandNum() -> Set<Int>{
-    while randomLottoNum.count != 6 {
-        let randNum = Int.random(in: 1...45)
-        if randomLottoNum.contains(randNum) {
-            continue
-        }
-        else {
-            randomLottoNum.insert(randNum)
-        }
+func generateNumber(){
+    while lottoNumbers.count < 6 {
+        let randomNumber = Int.random(in: 1...45)
+            lottoNumbers.insert(randomNumber)
     }
-    return randomLottoNum
 }
 
-func checkLottoNum(){
-    let intersection: Set<Int> = randomLottoNum.intersection(myLottoNumbers)
+func checkNumber(){
+    let intersection: Set<Int> = lottoNumbers.intersection(myLottoNumbers)
     if intersection.count == 0 {
             print("아쉽지만 겹치는 번호가 없습니다.")
     }
@@ -30,10 +24,10 @@ func checkLottoNum(){
         print("축하합니다! 겹치는 번호는",intersection,terminator: "입니다!\n")
     }
 }
-var randomLottoNum: Set<Int> = Set<Int>()
-randomLottoNum=GenerateRandNum()
-print("<당첨번호>\n",randomLottoNum)
-let myLottoNumbers: [Int] = [12,24,32,41,5,16]
-checkLottoNum()
 
+var lottoNumbers: Set<Int> = Set<Int>()
+generateNumber()
+print("<당첨번호>\n", lottoNumbers)
+let myLottoNumbers: [Int] = [12,24,32,41,5,16]
+checkNumber()
 
