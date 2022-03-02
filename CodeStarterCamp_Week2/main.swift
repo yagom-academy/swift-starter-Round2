99d9219791392dceb7d37e5ec2d2d56d6c793fca
Jun <jun@Junui-Macmini.local>
2022-03-03 00:27:33 +0900

    compareNumbers 내의 for 반복문으로 리스트 비교 -> let intersection: Set<Int>로 교집합 반환 correctNumbers 변수 삭제 switch 구문 correctNumbers -> compareNumbers

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -25,21 +25,17 @@ func chooseAllNumbers() {
     lottoNumbers = chooseNumbers()
 }
 
-var correctNumbers: Set<Int> = Set<Int>()
 
-func compareNumbers() {
-    for index in 0...5 {
-        if lottoNumbers.contains(myLottoNumbers[index]) {
-            correctNumbers.insert(myLottoNumbers[index])
-        }
-    }
+func compareNumbers() -> Set<Int> {
+    let intersection: Set<Int> = myLottoNumbers.intersection(lottoNumbers)
+    return intersection
 }
 
-switch correctNumbers.count {
+switch compareNumbers().count {
 case 0:
     print("아쉽지만 겹치는 번호가 없습니다.")
 case 1...6:
-    print("축하합니다! 겹치는 번호는 \(correctNumbers) 입니다!")
+    print("축하합니다! 겹치는 번호는 \(compareNumbers) 입니다!")
 default:
     print("unknown")
 }
