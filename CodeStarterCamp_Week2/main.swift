0acfce6b6bdfe1fb49fc6b9030fa42f79d4e01de
jun <jun@junui-MacBook-Pro.local>
2022-03-03 00:02:19 +0900

    Add : 함수 printWinnerMessage 추가
    함수 printLottoResult 내용 변경 및
    함수 printWinnerMessage 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -28,7 +28,23 @@ func printLottoResult(winningLottoNumbers: Set<Int> = checkLottoNumbers()) {
         print("아쉽지만 겹치는 번호가 없습니다.")
     }
     else {
-        print("축하합니다! 겹치는 번호는 \(winningLottoNumbers) 입니다!")
+        printWinnerMessage()
+    }
+}
+
+func printWinnerMessage(winningLottoNumbers: Set<Int> = checkLottoNumbers()) {
+    let sortedNumbers = winningLottoNumbers.sorted()
+    let arrayNumbers = [Int](sortedNumbers)
+    let maxCount = arrayNumbers.count - 1
+    if maxCount == 0 {
+        print("축하합니다! 겹치는 번호는 \(arrayNumbers[0]) 입니다!")
+    }
+    else {
+        print("축하합니다! 겹치는 번호는 \(arrayNumbers[0])", terminator: "")
+        for count in 1...maxCount {
+            print(", \(arrayNumbers[count])", terminator: "")
+        }
+        print(" 입니다!")
     }
 }
 
