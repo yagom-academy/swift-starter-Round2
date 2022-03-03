ceaacdcc43444c70e09147146800f76ef7205b53
freemandeveloper <freemandeveloper1@gmail.com>
2022-03-03 19:43:17 +0900

    찍은 6개번호와 로또 당첨번호의 겹치는 번호를 확인하는 함수 코드 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -11,3 +11,14 @@ print(lotterySet)
 
 let myLottoNumbers: [Int] = [3, 5, 11, 14, 20, 26]
 print(myLottoNumbers)
+
+let intersection: Set<Int> = lotterySet.intersection(myLottoNumbers)
+print(intersection)
+func verifyLottoResult(){
+    if intersection.count != 0 {
+        print("축하합니다! 겹치는 번호는 \(intersection) 입니다 !")
+    }        else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+verifyLottoResult()
