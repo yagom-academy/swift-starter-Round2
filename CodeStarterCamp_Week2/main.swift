18320afd0ec038c09b3d181b8162c8e9cc101055
jun <jun@junui-MacBook-Pro.local>
2022-03-04 15:10:01 +0900

    Fix : 함수printLottoResult 수정
    함수의 if 구문의 else 부분 내용 변경

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -17,7 +17,6 @@ func makeLottoNumbers() -> Set<Int> {
         let random = Int.random(in: 1...45)
         lottoNumbers.insert(random)
     }
-    //print("\(lottoNumbers) 이건로또넘버")
     lottoCount = lottoCount + 1
     lottoNumbersList["\(lottoCount)회차"] = lottoNumbers
     return lottoNumbers
@@ -25,10 +24,8 @@ func makeLottoNumbers() -> Set<Int> {
 
 func checkLottoNumbers() -> Set<Int> {
     let lottoNumbers = makeLottoNumbers()
-    let myLottoNumbers = [23, 15, 25, 42, 5, 37]
+    let myLottoNumbers = [1, 2, 3, 4, 5, 6]
     let winningLottoNumbers = lottoNumbers.intersection(myLottoNumbers)
-    //print("\(myLottoNumbers) 이건마이넘버")
-    //print("\(winningLottoNumbers) 이건 같은숫자")
     return winningLottoNumbers
 }
 
@@ -38,44 +35,19 @@ func printLottoResult() {
         print("아쉽지만 겹치는 번호가 없습니다.")
     }
     else {
-        let sortedNumbers = winningLottoNumbers.sorted()
-        let arrayNumbers = [Int](sortedNumbers)
-        let maxCount = arrayNumbers.count - 1
-        if maxCount == 0 {
-            print("축하합니다! 겹치는 번호는 \(arrayNumbers[0]) 입니다!")
-        }
-        else if maxCount > 0 {
-            print("축하합니다! 겹치는 번호는 \(arrayNumbers[0])", terminator: "")
-            for count in 1...maxCount {
-                print(", \(arrayNumbers[count])", terminator: "")
-            }
-            print(" 입니다!")
-        }
+        let arrayNumbers = [Int](winningLottoNumbers).sorted().map{String($0)}
+        print("축하합니다! 겹치는 번호는 \(arrayNumbers.joined(separator: ", ")) 입니다!")
     }
 }
 
-//func printWinnerMessage() {
-//    let winningLottoNumbers = checkLottoNumbers()
-//    let sortedNumbers = winningLottoNumbers.sorted()
-//    let arrayNumbers = [Int](sortedNumbers)
-//    let maxCount = arrayNumbers.count - 1
-//    if maxCount == 0 {
-//        print("축하합니다! 겹치는 번호는 \(arrayNumbers[0]) 입니다!")
-//    }
-//    else if maxCount > 0 {
-//        print("축하합니다! 겹치는 번호는 \(arrayNumbers[0])", terminator: "")
-//        for count in 1...maxCount {
-//            print(", \(arrayNumbers[count])", terminator: "")
-//        }
-//        print(" 입니다!")
-//    }
-//}
-
 func playLotto() {
-    printLottoResult()}
+    printLottoResult()
+}
 
 playLotto()
 playLotto()
 playLotto()
 playLotto()
 playLotto()
+
+print(lottoNumbersList["\(2)회차"])
