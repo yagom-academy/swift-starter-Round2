2bbc91a59d98d681c6d4a4456a920d8e768f13fd
TerryEHLee <capssemerll@gmail.com>
2022-03-08 16:59:08 +0900

    main.swift Lotto번호비교

---
+++ CodeStarterCamp_Week2/Step3.swift
@@ -0,0 +1,8 @@
+//
+//  Step3.swift
+//  CodeStarterCamp_Week2
+//
+//  Created by Terry on 2022/03/07.
+//
+
+import Foundation
--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,7 +6,34 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
+func generateLottoNumber() -> Set<Int> {
+    var lottoNumber: Set<Int> = Set<Int>()
+    while lottoNumber.count < 6 {
+        let number = Int.random(in: 1...45)
+        lottoNumber.insert(number)
+    }
+    return lottoNumber
+}
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+
+func confirmWinningLotto(myLottoNumbers: [Int]) {
+    var matchingNumbers: [Int] = []
+    let winningNumbers: Set<Int> = generateLottoNumber()
+    for myLottoNumber in myLottoNumbers{
+        for winningNumber in winningNumbers{
+            if myLottoNumber == winningNumber{
+                matchingNumbers.append(winningNumber)
+            }
+        }
+    }
+    print(winningNumbers)
+    if matchingNumbers.isEmpty == true {
+        print("Sorry;;; It's not your day :(")
+    }else{
+        print("Congrat. ! Your matching numbers are \(matchingNumbers).")
+    }
+}
+
+confirmWinningLotto(myLottoNumbers: myLottoNumbers)
 
