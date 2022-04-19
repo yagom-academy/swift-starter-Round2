4289dbbf6c19ee5964d18a6d9947f5818cfc14c9
yuvinrho <yvrho@naver.com>
2022-04-20 06:54:23 +0900

    로또번호 생성함수 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,34 +1,32 @@
 import Foundation
 
 func generateLottoNumber() -> [Int] {
-    let shuffledNumbers: [Int] = (1...45).shuffled()
-    let lottoNumber: [Int] = Array(shuffledNumbers[0...5])
-        
+    let lottoNumber: [Int] = Array((1...45).shuffled()[0...5])
+    
     return lottoNumber
 }
 
-func checkLottoNumber(compare lottoNumber: [Int], with myLottoNumber: [Int]) {
-    var hitNumbers: [String] = []
+func checkLottoNumber(compare lottoNumber: [Int], with myLottoNumber: [Int]) -> [Int] {
+    var hitNumbers: [Int] = []
     
     for number in myLottoNumber {
         if lottoNumber.contains(number) {
-            hitNumbers.append(String(number))
+            hitNumbers.append(number)
         }
     }
-
-    if hitNumbers.count == 0 {
-        print("아쉽지만 겹치는 번호가 없습니다.")
-    }
-    else {
-        print("축하합니다! 겹치는 번호는 \(hitNumbers.joined(separator: ", ")) 입니다!")
-    }
-            
+    
+    return hitNumbers
 }
 
 
-print(generateLottoNumber())
+//        print("아쉽지만 겹치는 번호가 없습니다.")
+//    }
+//    else {
+//        print("축하합니다! 겹치는 번호는 \(hitNumbers.joined(separator: ", ")) 입니다!")
+//    }
 
-//let lottoNumber = generateLottoNumber()
-//let myLottoNumber: [Int] = [1, 2, 3, 4, 5, 6]
+
+let lottoNumber = generateLottoNumber()
+let myLottoNumber: [Int] = [1, 2, 3, 4, 5, 6]
 //
 //checkLottoNumber(compare: lottoNumber, with: myLottoNumber)
