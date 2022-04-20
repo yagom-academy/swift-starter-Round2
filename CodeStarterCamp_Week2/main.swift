daead8b05e912cd9d9b52cf2b6600182e97a6d3e
yuvinrho <yvrho@naver.com>
2022-04-20 10:38:41 +0900

    로또번호 확인하는 함수 수정 및 로또결과 출력하는 함수 생성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -14,19 +14,15 @@ func checkLottoNumber(compare lottoNumber: [Int], with myLottoNumber: [Int]) -> 
             hitNumbers.append(number)
         }
     }
-    
     return hitNumbers
 }
 
-
-//        print("아쉽지만 겹치는 번호가 없습니다.")
-//    }
-//    else {
-//        print("축하합니다! 겹치는 번호는 \(hitNumbers.joined(separator: ", ")) 입니다!")
-//    }
-
+func showLottoResult(of hitNumbers: [Int]) {
+    hitNumbers.isEmpty ? print("아쉽지만 겹치는 번호가 없습니다.") : print("축하합니다! 겹치는 번호는 \(hitNumbers.reduce("") { String($0) == "" ? String($1) : String($0) + ", " + String($1) } ) 입니다!")
+}
 
 let lottoNumber = generateLottoNumber()
 let myLottoNumber: [Int] = [1, 2, 3, 4, 5, 6]
-//
-//checkLottoNumber(compare: lottoNumber, with: myLottoNumber)
+let hitNumber = checkLottoNumber(compare: lottoNumber, with: myLottoNumber)
+
+showLottoResult(of: hitNumber)
