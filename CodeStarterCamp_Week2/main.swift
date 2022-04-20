6aab0481d9563fcf8a8da1a7a80f5f294a5be503
yuvinrho <yvrho@naver.com>
2022-04-20 20:31:41 +0900

    회차별 로또 저장하는 함수 생성 및 회차에 따른 로또보여주기 함수 생성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -21,8 +21,28 @@ func showLottoResult(of hitNumbers: [Int]) {
     hitNumbers.isEmpty ? print("아쉽지만 겹치는 번호가 없습니다.") : print("축하합니다! 겹치는 번호는 \(hitNumbers.reduce("") { String($0) == "" ? String($1) : String($0) + ", " + String($1) } ) 입니다!")
 }
 
+func storeLotto() -> [String : [Int]] {
+    var lottoHistory = [String:[Int]]()
+    
+    for index in 1...5 {
+        lottoHistory["\(index)회차"] = generateLottoNumber()
+    }
+    
+    return lottoHistory
+}
+
+func showLotto(of round: String, from lottoHistory: [String : [Int]]) {
+    if let lottoNumber = lottoHistory[round] {
+        print("\(round)의 로또 당첨 번호는 \(lottoNumber.reduce("") { String($0) == "" ? String($1) : String($0) + ", " + String($1) } ) 입니다!")
+    }
+}
+
+/* Step2 실행하기 위한 코드
 let lottoNumber = generateLottoNumber()
 let myLottoNumber: [Int] = [1, 2, 3, 4, 5, 6]
 let hitNumber = checkLottoNumber(compare: lottoNumber, with: myLottoNumber)
-
 showLottoResult(of: hitNumber)
+ */
+
+var lottoHistory: [String : [Int]] = storeLotto()
+showLotto(of: "2회차", from: lottoHistory)
