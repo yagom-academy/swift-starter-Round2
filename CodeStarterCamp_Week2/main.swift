35436ae1fecef1771b4d7919ad9509711bd34f3c
Onestar <onestar7984@gmail.com>
2022-04-20 19:25:05 +0900

    refector: 선언 컨벤션 일치화.

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 func generateWinningLottery() -> Array<Int> {
-    var winningLottery: Set<Int> = Set<Int>()
+    var winningLottery = Set<Int>()
     while winningLottery.count < 6 {
         winningLottery.insert(Int.random(in: 1...45))
     }
@@ -18,19 +18,19 @@ func generateWinningLottery() -> Array<Int> {
 }
 
 func checkLotteryResults(of winningLottery: Array<Int>, with myLottery: Array<Int>) -> Array<Int> {
-    var myLotteryResults = [Int]()
+    var myLotteryResults = Array<Int>()
     myLotteryResults = Set(myLottery).intersection(winningLottery).sorted()
     return myLotteryResults
 }
 
-func receiveWinningMessage(to myLotteryResults: [Int]) -> String {
+func receiveWinningMessage(to myLotteryResults: Array<Int>) -> String {
     let lotteryNumbersToString = changeIntArrayToString(target: myLotteryResults)
     let winningMessage = "축하합니다! 겹치는 번호는 \(lotteryNumbersToString) 입니다!"
     let losingMessage = "아쉽지만 겹치는 번호가 없습니다."
     return myLotteryResults.isEmpty ? losingMessage : winningMessage
 }
 
-func changeIntArrayToString(target array: [Int]) -> String {
+func changeIntArrayToString(target array: Array<Int>) -> String {
     var resultString = ""
     for value in array {
         if value == array.first {
@@ -42,7 +42,7 @@ func changeIntArrayToString(target array: [Int]) -> String {
     return resultString
 }
 
-let myLottery: [Int] = [3, 5, 7, 11, 18, 27]
+let myLottery = [3, 5, 7, 11, 18, 27]
 let winningLottery = generateWinningLottery()
 let myLotteryResults = checkLotteryResults(of: winningLottery, with: myLottery)
 print(receiveWinningMessage(to: myLotteryResults))
