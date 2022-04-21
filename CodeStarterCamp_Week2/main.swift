d578075f9e21f9b78457fe283755765d18decd64
lnhk98 <hozzang666@gmai.com>
2022-04-21 11:46:23 +0900

    Step3: 로또 회차와 당첨번호 저장 및 출력 함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,11 +8,17 @@
 
 import Foundation
 
+var roundOfLotto = 0
+var lottoWinningNumberList = [String: [Int]]()
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+
 func createLottoNumbers() -> Set<Int> {
+    roundOfLotto += 1
     var lottoNumbers = Set<Int>()
     while lottoNumbers.count < 6 {
         lottoNumbers.insert(Int.random(in: 1...45))
     }
+    storeLottoNumberList(using: roundOfLotto, and: lottoNumbers)
     return lottoNumbers
 }
 
@@ -36,8 +42,26 @@ func showResult(use sameNumbers: [String]) {
     }
 }
 
-let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-let createdLottoNumbers = createLottoNumbers()
-let sameNumbers = checkWinning(with: createdLottoNumbers, and: myLottoNumbers)
+func storeLottoNumberList(using round: Int, and lottoNumbers: Set<Int>) {
+    lottoWinningNumberList["\(round)회차"] = lottoNumbers.sorted()
+}
 
-showResult(use: sameNumbers)
+func findLottoNumberList(of round: Int) {
+    if let winningNumbers = lottoWinningNumberList["\(round)회차"] {
+        let stringWinningNumbers = winningNumbers.map { String($0) }
+        print("\(round)회차의 로또 당첨 번호는 \(stringWinningNumbers.joined(separator: ", ")) 입니다.")
+    } else {
+        print("\(round)회차의 로또가 아직 진행되지 않았습니다.")
+    }
+}
+
+func repeatCreatingLotto(for times: Int) {
+    for _ in 1...times {
+        let createdLottoNumbers = createLottoNumbers()
+        let sameNumbers = checkWinning(with: createdLottoNumbers, and: myLottoNumbers)
+        showResult(use: sameNumbers)
+    }
+}
+
+repeatCreatingLotto(for: 5)
+findLottoNumberList(of: 2)
