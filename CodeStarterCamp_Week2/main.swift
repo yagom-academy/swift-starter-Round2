a4a81413a2695bdeb57c6692d85fec9b4db9223a
jeongmin <lj845854@gmail.com>
2022-04-22 01:08:57 +0900

    refactor : pickRandomNumber() 의 구조 수정
    랜덤으로 숫자를 생성하는 함수 생성하여 상수를 대체

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -10,36 +10,39 @@ import Foundation
 
 func generateLottoNumbers() -> Set<Int> {
     var lottoTray = Set<Int>()
-    
+
     repeat {
-        let pickedRandomNumber = Int.random(in: 1...45)
-        lottoTray.insert(pickedRandomNumber)
+        lottoTray.insert(pickRandomNumber(in: 45))
     } while lottoTray.count < 6
-    
+
     return lottoTray
+}
+
+func pickRandomNumber(in endNum: Int) -> Int {
+    return Int.random(in: 1...endNum)
 }
 
 func checkMyLottoNumbers(from myNumbers: Array<Int>, to winNumbers: Set<Int>) {
     var hittedNumbers: Array<Int> = []
-    
+
     for number in myNumbers {
         if winNumbers.contains(number) {
             hittedNumbers.append(number)
         }
     }
-    
+
     if hittedNumbers.count != 0 {
         var hitNumsStr = ""
-        
+
         for number in hittedNumbers {
             let separator = ", "
-            
+
             hitNumsStr += "\(number)"
             if number != hittedNumbers.last {
                 hitNumsStr += separator
             }
         }
-        
+
         print("축하합니다! 겹치는 번호는",hitNumsStr,"입니다!")
     }
     else {
