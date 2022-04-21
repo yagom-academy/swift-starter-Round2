3568a6d06d98a943a5299ed69ec7486c12cc2ad9
suyeon <higewnag@naver.com>
2022-04-22 03:33:05 +0900

    [step2] lotto 번호 생성 및 맞춰보기_수정(네이밍)_recommit

--- CodeStarterCamp_Week2/lotto645.swift
+++ CodeStarterCamp_Week2/lotto645.swift
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> a5dbcb29408642f52464c895909c200d11547934
->>>>>>> 401ad32 ([step2] lotto 번호 생성 및 맞춰보기_수정(네이밍))
 //
 //  lotto6:45.swift
 //  CodeStarterCamp_Week2
@@ -16,9 +8,6 @@
 import Foundation
 
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 var lottoNumber: Set<Int> = Set<Int>()
 
 func pickLottoNumber() {
@@ -33,27 +22,6 @@ let lottoNumberList: Set<Int> = Set<Int>(lottoNumber)
 let intersection: Set<Int> = lottoNumberList.intersection(myLottoNumbers)
 
 func lottoCompareResult() {
-=======
->>>>>>> 401ad32 ([step2] lotto 번호 생성 및 맞춰보기_수정(네이밍))
-
-var lottoNumber: Set<Int> = Set<Int>()
-
-func lottoNumberSix() {
-    while lottoNumber.count < 6 {
-        lottoNumber.insert(Int.random(in: 1...45))
-    }
-	print(lottoNumber)
-}
-
-let myLottoNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
-let lottoNumber001: Set<Int> = Set<Int>(lottoNumber)
-let intersection: Set<Int> = lottoNumber001.intersection(myLottoNumbers)
-
-func winningResult() {
-<<<<<<< HEAD
-=======
->>>>>>> a5dbcb29408642f52464c895909c200d11547934
->>>>>>> 401ad32 ([step2] lotto 번호 생성 및 맞춰보기_수정(네이밍))
     if intersection.count >= 1 {
         print("축하합니다! 겹치는 번호는 \(intersection) 입니다!")
     }
@@ -61,15 +29,3 @@ func winningResult() {
         print("아쉽지만 겹치는 번호가 없습니다")
     }
 }
-<<<<<<< HEAD
-
-
-
-=======
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> a5dbcb29408642f52464c895909c200d11547934
->>>>>>> 401ad32 ([step2] lotto 번호 생성 및 맞춰보기_수정(네이밍))
--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,26 +1,14 @@
-
 //
-//  lotto6:45.swift
+//  main.swift
 //  CodeStarterCamp_Week2
 //
-//  Created by 수염 on 2022/04/19.
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
 //
 
 import Foundation
 
 
-<<<<<<< HEAD
-lottoNumberSix()
-print(intersection)
-winningResult()
-=======
-<<<<<<< HEAD
 pickLottoNumber()
 print(intersection)
 lottoCompareResult()
-=======
-lottoNumberSix()
-print(intersection)
-winningResult()
->>>>>>> a5dbcb29408642f52464c895909c200d11547934
->>>>>>> 401ad32 ([step2] lotto 번호 생성 및 맞춰보기_수정(네이밍))
