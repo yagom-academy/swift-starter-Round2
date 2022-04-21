8250755019a795be01e1b98c27e4377ccd7a0842
hililyy <joun406@gmail.com>
2022-04-21 13:05:47 +0900

    week2 step2

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,38 @@
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+var winLottoNumbers: [Int] = []
+var myWinLottoNumbers: [Int] = []
 
+makeLottoNumbers()
+checkNumbers()
+
+func makeLottoNumbers() {
+    while winLottoNumbers.count < 6 {
+        let winNumber = Int(arc4random_uniform(45) + 1)
+        
+        if !winLottoNumbers.contains(winNumber) {
+            winLottoNumbers.append(winNumber)
+        }
+    }
+}
+
+func checkNumbers() {
+    for count in 0...5 {
+        for _count in 0...5 {
+            if myLottoNumbers[count] == winLottoNumbers[_count] {
+                myWinLottoNumbers.append(myLottoNumbers[count])
+            }
+        }
+    }
+    if myWinLottoNumbers.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는 ", terminator: "")
+        for count in 0...myWinLottoNumbers.count-1 {
+            count < myWinLottoNumbers.count-1 ? print("\(myWinLottoNumbers[count])", terminator: ", ") : print("\(myWinLottoNumbers[count])", terminator: " ")
+        }
+        print("입니다!")
+    }
+}
