4f094cd27bb118de4f4b2ee813bb867cdf55cf7a
MANSU.J <seunghyun.jung.84@gmail.com>
2022-04-23 00:24:58 +0900

    Feat: 겹치는 숫자 확인 함수
    - 내가 찍은번호와 로또 당첨번호의 겹치는 숫자를 확인하는 함수

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -10,3 +10,17 @@ func makeNumberOfLotto() -> [Int] {
 	let mappedNumbers = lottoNumbers.map{ Int($0) }
 	return mappedNumbers
 }
+
+func matchOfNumbers(_ myLottoNumbers:[Int]) {
+	let matchOfNumbers: Set<Int> = Set(makeNumberOfLotto()).intersection(myLottoNumbers)
+	let mappedNumbers = matchOfNumbers.map{ String($0) }.joined(separator: ", ")
+	var resultMessage: String = ""
+	if matchOfNumbers.isEmpty {
+		resultMessage = "아쉽지만 겹치는 번호가 없습니다."
+	} else {
+		resultMessage = "축하합니다! 겹치는 번호는 \(mappedNumbers) 입니다!"
+	}
+	print(resultMessage)
+}
+
+matchOfNumbers([1,2,3,4,5,6])
