dc533c87eefc53d5c0d814c33815d2c115572b08
Dylan-yoon <d.dylany21@gmail.com>
2022-04-22 20:48:22 +0900

    refactor : runningLotto 5번 반복문으로 실, 필요없는 띄어쓰기 수정, myLottoNumbers 상수 타입추론으로 [Int] 삭제

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,22 +8,18 @@
 
 import Foundation
 
-
-let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]      //선택 번호 생성
-
+let myLottoNumbers = [1, 2, 3, 4, 5, 6]      //선택 번호 생성
 var randomNums : Set<Int> = Set<Int>()
 var correctNums : [Int] = [Int]()
 var countRound : Int = 0
 var saveLottos = [Int: Set<Int>]()  // 회차 입력할 딕셔너리 생성
 
-//랜덤 숫자 생성 후 randomNums에 숫자 입력
 func makingNumbers() {
     while randomNums.count <= 6 {
         randomNums.insert(Int.random(in: 1...45))
     }
 }
 
-//번호 확인
 func checkingNumber() {
     makingNumbers()
     for i in myLottoNumbers {
@@ -34,7 +30,6 @@ func checkingNumber() {
     countRound += 1 //매 회차 시 카운트up
 }
 
-//함수 출력
 func runningLotto() {
     checkingNumber()
     if correctNums.count == 0 {
@@ -46,7 +41,6 @@ func runningLotto() {
     //전역변수 초기화
     correctNums = []
     randomNums = Set<Int>()
-    
 }
 
 
@@ -54,12 +48,9 @@ func savingLottoNumbers() {
     saveLottos[countRound] = randomNums
 }
 
-//5번 실행
-runningLotto()
-runningLotto()
-runningLotto()
-runningLotto()
-runningLotto()
+for _ in 1...5 {
+    runningLotto()
+}
 
 if let xx = saveLottos[2] {
     print("2회차의 로또 당첨 번호는 \(Array(xx).sorted()) 입니다.") // set 를 array로 담아 로또 번호를 순서대로 변경.
