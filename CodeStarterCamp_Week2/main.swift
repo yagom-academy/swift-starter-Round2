2e3a4f7294498d7ccd6c427c3c310057513375bf
Much3m <kga1800@uos.ac.kr>
2022-04-22 11:49:12 +0900

    Feat: 로또번호 생성 기능 추가
    STEP1에서 작성한 순서도를 바탕으로 로또번호를 생상하여 lottoSet변수에 저장하는 lottoGenerator 함수 작성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,3 +6,11 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+var lottoSet: Set<Int> = Set<Int> ()
+
+func lottoGenerator () {
+    while lottoSet.count < 6 {
+        lottoSet.insert(Int.random(in: 1..<45))
+    }
+}
