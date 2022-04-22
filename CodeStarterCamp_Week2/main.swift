485c016a359b1e8ef69cde57a14b2b06d42acf0c
MANSU.J <seunghyun.jung.84@gmail.com>
2022-04-23 00:20:54 +0900

    Feat: 로또당첨번호 생성 함수
    - 로또당첨번호 1~45까지의 범위에서 6자리를 생성하는 함수

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,12 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week2
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
-
+func makeNumberOfLotto() -> [Int] {
+	var lottoNumbers: Set<Int> = Set<Int>()
+	var ballOfNumber: Int
+	while lottoNumbers.count<6 {
+		ballOfNumber = Int.random(in: 1...45)
+		lottoNumbers.insert(ballOfNumber)
+	}
+	let mappedNumbers = lottoNumbers.map{ Int($0) }
+	return mappedNumbers
+}
