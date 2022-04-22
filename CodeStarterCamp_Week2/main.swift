4106a30b4b20bebf3f378270179d60a51b9481e8
Much3m <kga1800@uos.ac.kr>
2022-04-22 11:51:56 +0900

    Feat: 당첨번호 확인 기능 추가
    lottoSet과 myLottoNumbers를 비교하여 일치하는 번호를 표현하도록 함수를 구현함.

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -14,3 +14,31 @@ func lottoGenerator () {
         lottoSet.insert(Int.random(in: 1..<45))
     }
 }
+
+func checkLotto () {
+    var coordinate: Set<Int> = Set<Int> ()
+    var count: Int = 0
+    for tofind in myLottoNumbers {
+        for lotto in lottoSet {
+            if tofind == lotto {
+                coordinate.insert(tofind)
+            }
+        }
+    }
+    if coordinate.count == 0 {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+        return
+    }
+    print("축하합니다! 겹치는 번호는 ",terminator: "")
+    for same in coordinate {
+        count += 1
+        print("\(same)",terminator: "")
+        if count != coordinate.count {
+            print(", ",terminator:"")
+        }
+    }
+    print(" 입니다!")
+}
+
+lottoGenerator()
+checkLotto()
