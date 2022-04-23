27eda113714cd6bab7f20b7f3284fdfcff9b8e0c
홍승주 <hongseungju@hongseungjuui-MacBookAir.local>
2022-04-23 18:20:15 +0900

    로또당첨번호확인함수 수정본

--- CodeStarterCamp_Week2/LottoMaking.swift
+++ CodeStarterCamp_Week2/LottoMaking.swift
@@ -9,10 +9,11 @@
 import Foundation
 
 
-var lottoNumbers: Set<Int> = Set<Int>()
+
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
-func makeLottoNumbers() -> Set<Int> {
+func generateLottoNumbers() -> Set<Int> {
+    var lottoNumbers: Set<Int> = Set<Int>()
     for _ in 0...5 {
         lottoNumbers.insert(Int.random(in: 1...45))
     }
@@ -20,22 +21,22 @@ func makeLottoNumbers() -> Set<Int> {
 }
 
 func checkLottoNumbers(lottoNumbers: Set<Int>, myLottoNumbers: [Int]) {
-    let sortLottoNumber = lottoNumbers.sorted()
-    var winLottoNumbers: [Int] = []
+    let sortedLottoNumbers = lottoNumbers.sorted()
+    var winnedLottoNumbers: [Int] = []
     
-    for num in 0...5 {
-        if sortLottoNumber.contains(myLottoNumbers[num]) {
-            winLottoNumbers.append(myLottoNumbers[num])
+    for number in 0...5 {
+        if sortedLottoNumbers.contains(myLottoNumbers[number]) {
+            winnedLottoNumbers.append(myLottoNumbers[number])
         }
     }
     
-    if winLottoNumbers.isEmpty {
+    if winnedLottoNumbers.isEmpty {
         print("아쉽지만 겹치는 번호가 없습니다.")
     } else {
-        var winLottoNumberToString = winLottoNumbers.map{ String($0)}
-        print("축하합니다! 겹치는 번호는 \(winLottoNumberToString.joined(separator: ",")) 입니다!")
+        let winnedLottoNumberToString = winnedLottoNumbers.map{ String($0)}
+        print("축하합니다! 겹치는 번호는 \(winnedLottoNumberToString.joined(separator: ",")) 입니다!")
     }
 }
-makeLottoNumbers()
-checkLottoNumbers(lottoNumbers: lottoNumbers, myLottoNumbers: myLottoNumbers)
+//generateLottoNumbers()
+checkLottoNumbers(lottoNumbers: generateLottoNumbers(), myLottoNumbers: myLottoNumbers)
 
