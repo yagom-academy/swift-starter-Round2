9abe3644fff56aec0ee475d9f4562335913043db
MANSU.J <seunghyun.jung.84@gmail.com>
2022-04-24 04:12:00 +0900

    Refactor: 함수명 변경
    좀 더 적합한 동사로 변경

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,18 +1,17 @@
 import Foundation
 
-func makeNumberOfLotto() -> [Int] {
-	var lottoNumbers: Set<Int> = Set<Int>()
+func generateNumberOfLotto() -> Set<Int> {
+	var lottoNumbers = Set<Int>()
 	var ballOfNumber: Int
 	while lottoNumbers.count<6 {
 		ballOfNumber = Int.random(in: 1...45)
 		lottoNumbers.insert(ballOfNumber)
 	}
-	let mappedNumbers = lottoNumbers.map{ Int($0) }
-	return mappedNumbers
+	return lottoNumbers
 }
 
 func matchOfNumbers(_ myLottoNumbers:[Int]) {
-	let matchOfNumbers: Set<Int> = Set(makeNumberOfLotto()).intersection(myLottoNumbers)
+	let matchOfNumbers: Set<Int> = generateNumberOfLotto().intersection(myLottoNumbers)
 	let mappedNumbers = matchOfNumbers.map{ String($0) }.joined(separator: ", ")
 	var resultMessage: String = ""
 	if matchOfNumbers.isEmpty {
