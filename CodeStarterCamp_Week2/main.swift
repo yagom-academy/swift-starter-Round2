03d3a0150f049116c06083d65c23fc8d80ad6e98
장경빈 <jang@jang-gyeongbin-ui-MacBookAir.local>
2022-04-24 23:26:25 +0900

    Week2 [Step2] BaDook2.vers

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,32 +6,8 @@
 //
 import Foundation
 
-let myLottoNumbers : [Int] = [1,2,3,4,5,6]
-let lottoNumbers = createLotto()
-var correctNumbers = [Int]()
+let myLottoNumbers: [Int] = [1,2,3,4,5,6]
+let correctNumbers = compareToMachine(user: myLottoNumbers, machine: createLotto())
 
-for i in 1...myLottoNumbers.count{
-    if lottoNumbers.contains(myLottoNumbers[i-1]){
-        correctNumbers.append(myLottoNumbers[i-1])
-    }
-}
+printLottoResult()
 
-if correctNumbers.count != 0 {
-    print("축하합니다! 겹치는 번호는 ",terminator: "")
-    for i in correctNumbers{
-        print(i, terminator: "")
-        if correctNumbers[correctNumbers.count-1] == i{
-            print(" ",terminator: "")
-        }
-        else if correctNumbers.count > 1{
-            print(",",terminator: " ")
-        }
-        else{
-            print(" ",terminator: "")
-        }
-    }
-    print("입니다!")
-}
-else{
-    print("아쉽지만 겹치는 번호가 없습니다.")
-}
