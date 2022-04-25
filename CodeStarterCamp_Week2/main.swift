5adec89bb546c466a2556958efe16c35a84b4a6c
ssyoun4092 <ssyoun4092@naver.com>
2022-04-25 13:51:57 +0900

    feat: map을 for문으로 변환, randomElement -> shuffled로 변환, 가독성을 위한 함수 위치 조절

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,46 +1,43 @@
 import Foundation
 
-let totalLottoNumbers: [Int] = Array(1...45)
-let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 var lottoNumbers: [Int] = []
 
-
 func pickLottoNumbers() {
-    while lottoNumbers.count < 6 {
-        let newNumber = totalLottoNumbers.randomElement()
-        
-        if !lottoNumbers.contains(newNumber!) {
-            lottoNumbers.append(newNumber!)
-        }
-    }
-}
-
-func compare(_ myLottoNumber: [Int], with lottoNumbers: [Int]) {
-
-    var containNumbers: [Int] = [ ]
-    
-    for i in myLottoNumber {
-        if myLottoNumber.contains(lottoNumbers[i-1]) {
-            containNumbers.append(lottoNumbers[i-1])
-        }
-    }
-    
-    func convertStringArray(from array: [Int]) -> [String] {
-        let convertedStringArray = array.map({ (num: Int) -> String in
-            return "\(num)"
-        })
-        
-        return convertedStringArray
-    }
-    
-    let containNumbersWithString = convertStringArray(from: containNumbers)
-    
-    if containNumbersWithString.isEmpty {
-        print("아쉽지만 겹치는 번호가 없습니다.")
-    } else {
-        print("축하합니다! 겹치는 번호는 \(containNumbersWithString.joined(separator: ", "))입니다!")
-    }
+  var totalLottoNumbers: [Int] = Array(1...45)
+  totalLottoNumbers.shuffle()
+  totalLottoNumbers.removeSubrange(6...(totalLottoNumbers.count-1))
+  lottoNumbers = totalLottoNumbers
+  print(lottoNumbers)
 }
 
 pickLottoNumbers()
+
+func compare(_ myLottoNumber: [Int], with lottoNumbers: [Int]) {
+
+  var containNumbers: [Int] = []
+
+  for i in myLottoNumber {
+    if myLottoNumber.contains(lottoNumbers[i-1]) {
+      containNumbers.append(lottoNumbers[i-1])
+    }
+  }
+
+  func convertStringArray(from array: [Int]) -> [String] {
+    var convertedStringArray: [String] = []
+    for num in array {
+      convertedStringArray.append(String(num))
+    }
+    return convertedStringArray
+  }
+
+  let containNumbersWithString = convertStringArray(from: containNumbers)
+
+  if containNumbersWithString.isEmpty {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+  } else {
+    print("축하합니다! 겹치는 번호는 \(containNumbersWithString.joined(separator: ", ")) 입니다!")
+  }
+}
+
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 compare(myLottoNumbers, with: lottoNumbers)
