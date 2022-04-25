e8d576aa01ba97eb6ab52b6c81e67faaa31b6808
Much3m <kga1800@uos.ac.kr>
2022-04-25 14:04:38 +0900

    Chore: 변수명 수정, STEP2 분리
    부적절한 변수명들을 수정하고 추후 STEP3과제를 위해 STEP2와 main파일을 따로 구성함.

---
+++ CodeStarterCamp_Week2/STEP2.swift
@@ -0,0 +1,43 @@
+//
+//  STEP2.swift
+//  CodeStarterCamp_Week2
+//
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+let myLottoNumbers: [Int] = [4,8,23,27,44,32]
+var lottoSet: Set<Int> = Set<Int> ()
+
+func generateLotto () {
+    while lottoSet.count < 6 {
+        lottoSet.insert(Int.random(in: 1..<46))
+    }
+}
+
+func checkLotto () {
+    var coincidence: Set<Int> = Set<Int> ()
+    var printedCount: Int = 0
+    for toFind in myLottoNumbers {
+        for lottoNumber in lottoSet {
+            if toFind == lottoNumber {
+                coincidence.insert(toFind)
+            }
+        }
+    }
+    if coincidence.count == 0 {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+        return
+    }
+    print("축하합니다! 겹치는 번호는 ",terminator: "")
+    for number in coincidence {
+        printedCount += 1
+        print("\(number)",terminator: "")
+        if printedCount != coincidence.count {
+            print(", ",terminator:"")
+        }
+    }
+    print(" 입니다!")
+}
+
+
--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,39 +6,5 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-var lottoSet: Set<Int> = Set<Int> ()
-
-func generateLotto () {
-    while lottoSet.count < 6 {
-        lottoSet.insert(Int.random(in: 1..<46))
-    }
-}
-
-func checkLotto () {
-    var coordinate: Set<Int> = Set<Int> ()
-    var count: Int = 0
-    for tofind in myLottoNumbers {
-        for lotto in lottoSet {
-            if tofind == lotto {
-                coordinate.insert(tofind)
-            }
-        }
-    }
-    if coordinate.count == 0 {
-        print("아쉽지만 겹치는 번호가 없습니다.")
-        return
-    }
-    print("축하합니다! 겹치는 번호는 ",terminator: "")
-    for same in coordinate {
-        count += 1
-        print("\(same)",terminator: "")
-        if count != coordinate.count {
-            print(", ",terminator:"")
-        }
-    }
-    print(" 입니다!")
-}
-
 generateLotto()
 checkLotto()
