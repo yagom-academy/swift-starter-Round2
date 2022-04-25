fc7b84979b4aaadce236c992582061555454a78c
jsa0224 <puccav@naver.com>
2022-04-25 13:31:41 +0900

    로또 당첨 번호 회차별 저장 코드 작성

---
+++ CodeStarterCamp_Week2/PrintSecondWinningLotto.swift
@@ -0,0 +1,40 @@
+//
+//  PrintSecondWinningLotto.swift
+//  CodeStarterCamp_Week2
+//
+//  Created by 정선아 on 2022/04/25.
+//
+
+import Foundation
+
+//찾은 2회차의 로또 당첨 번호를 출력하는 함수
+@discardableResult
+func printSecondWinningLotto(from round: Int) -> String {
+    let secondWinningLottoNumbersAsString = findSecondWinningLotto(round: round).map{ number in String(number) }.joined(separator: ", ")
+    let message = "\(round)회차의 로또 당첨 번호는 " + secondWinningLottoNumbersAsString + " 입니다."
+    print(message)
+    return secondWinningLottoNumbersAsString
+}
+
+//5번 생성한 로또 당첨번호를 딕셔너리에 저장하는 함수
+func saveWinningLottoNumbers() -> [String:Array<Int>] {
+    let playLottoRound = 5
+    let showRound = "회차"
+    
+    var winningLottoData: [String:Array<Int>] = [:]
+    
+    for round in 1...playLottoRound {
+        let winningLottoDataMessage = "\(round)" + showRound
+        winningLottoData[winningLottoDataMessage] = lottoNumbers
+    }
+    return winningLottoData
+}
+
+//2회차의 로또 당첨 번호를 찾는 함수
+func findSecondWinningLotto(round: Int) -> [Int] {
+    let roundMessage = "\(round)회차"
+    let winningLottoData = saveWinningLottoNumbers()
+    
+    guard let secondWinningLottoNumbers = winningLottoData[roundMessage] else { return [] }
+    return secondWinningLottoNumbers
+}
--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,4 +8,8 @@
 
 import Foundation
 
+//Step2: 내 번호와 맞추어보기!
 playLotto()
+
+//Step 3: 로또 당첨 번호를 회차별로 저장하고, 확인하자!
+printSecondWinningLotto(from: 2)
