2c65ad161c052fd46424669dec76b4536546ccf3
Dorii0513 <bobodorii0513@gmail.com>
2022-04-25 17:24:26 +0900

    step2 코드 작성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,44 @@
 
 import Foundation
 
-print("Hello, World!")
+var lottoNumbers: Set<Int> = Set<Int>()
 
+func pickLottoNumbers() -> Set<Int> {
+    while lottoNumbers.count < 6{
+        lottoNumbers.insert(Int.random(in: 0...46))
+    }
+    return lottoNumbers
+}
+func compareNumbers() {
+    let myLootoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+    var myNumbers: Set<Int> = Set<Int>()
+    for index in 0..<myLootoNumbers.count{
+        myNumbers.insert(myLootoNumbers[index])
+    }
+    let intersection: Set<Int> = myNumbers.intersection(lottoNumbers)
+    switch intersection.count {
+    case 0:
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    case 1:
+        print("축하합니다! 겹치는 번호는", terminator: " ")
+        for num in intersection {
+            print(num, terminator: " ")
+        }
+        print("입니다!")
+    default:
+        print("축하합니다! 겹치는 번호는", terminator: " ")
+        var index = 1
+        for num in intersection {
+            if index == intersection.count {
+                print(num, terminator: " ")
+            }else {
+                print("\(num),", terminator: " ")
+            }
+            index += 1
+        }
+        print("입니다!")
+    }
+}
+pickLottoNumbers()
+compareNumbers()
+    
