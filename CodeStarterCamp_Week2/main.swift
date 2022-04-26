452d39ff7cbfb578198e871449455733a7295903
ssyoun4092 <ssyoun4092@naver.com>
2022-04-26 21:24:24 +0900

    refactor: 변수이름 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,17 +9,17 @@ func makeLottoNumbersFor(times numbers: Int) {
 }
 
 func showLottoNumbersAt(order number: Int) {
-    var arrayForFindingNumbers: [Int] = [ ]
+    var arrayForShowingNumbers: [Int] = [ ]
     if number >= 1 && number <= lottoNumbersDictionary.count {
         if let numbers = lottoNumbersDictionary["\(number)회차"] {
-            arrayForFindingNumbers = numbers
+            arrayForShowingNumbers = numbers
         } else {
             print("해당 회차의 로또번호를 찾을 수 없습니다.")
         }
     } else {
         print("해당 회차의 로또번호는 없거나, 찾고자 하는 회차 숫자가 양수가 아닙니다.")
     }
-    let findingNumbersStringArray = convertStringArray(from: arrayForFindingNumbers)
+    let findingNumbersStringArray = convertStringArray(from: arrayForShowingNumbers)
     print("\(number)회차의 로또 당첨 번호는 \(findingNumbersStringArray.joined(separator: ", ")) 입니다.")
 }
 
