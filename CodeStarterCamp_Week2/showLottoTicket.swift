09f5fff5eb1469e111824deb8561308612fd404f
jsa0224 <puccav@naver.com>
2022-04-26 20:41:10 +0900

    파일 이름 수정

--- CodeStarterCamp_Week2/showLottoTicket.swift
+++ CodeStarterCamp_Week2/showLottoTicket.swift
@@ -7,16 +7,6 @@
 
 import Foundation
 
-//5번 생성한 로또 당첨번호를 딕셔너리에 저장하는 함수
-func buyLottoTicket(count: Int) -> [String: [Int]] {
-    var lottoTickets = [String: [Int]]()
-    
-    for round in 1...count {
-        lottoTickets.updateValue(pickLottoNumbers(), forKey: "\(round)회차")
-    }
-    return lottoTickets
-}
-
 //해당 회차의 로또 당첨 번호를 찾고 출력해주는 함수
 func showLottoTicket(round: Int, totalCount: Int) {
     let lottoTickets = buyLottoTicket(count: totalCount)
@@ -27,4 +17,14 @@ func showLottoTicket(round: Int, totalCount: Int) {
     }
     
     print("\(round)회차의 로또 당첨 번호는 \(convertToString(from: ticketToShow)) 입니다.")
+}
+
+//5번 생성한 로또 당첨번호를 딕셔너리에 저장하는 함수
+func buyLottoTicket(count: Int) -> [String: [Int]] {
+    var lottoTickets = [String: [Int]]()
+    
+    for round in 1...count {
+        lottoTickets.updateValue(pickLottoNumbers(), forKey: "\(round)회차")
+    }
+    return lottoTickets
 }
