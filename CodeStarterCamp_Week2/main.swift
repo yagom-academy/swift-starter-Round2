42b54733df035de99e59d4283d90f78b8dc141d3
ssyoun4092 <ssyoun4092@naver.com>
2022-04-26 20:01:15 +0900

    feat: Lotto 번호들을 저장하는 Dictionary타입의 변수 생성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,44 +1,46 @@
 import Foundation
 
 func pickLottoNumbers() -> [Int] {
-  var lottoNumbers: [Int] = []
-  var totalLottoNumbers: [Int] = Array(1...45)
-  totalLottoNumbers.shuffle()
-  totalLottoNumbers.removeSubrange(6...(totalLottoNumbers.count-1))
-  lottoNumbers = totalLottoNumbers
+    var lottoNumbers: [Int] = []
+    var totalLottoNumbers: [Int] = Array(1...45)
+    totalLottoNumbers.shuffle()
+    totalLottoNumbers.removeSubrange(6...(totalLottoNumbers.count-1))
+    lottoNumbers = totalLottoNumbers
 
-  return lottoNumbers
+    return lottoNumbers
 }
 
 func compare(_ myLottoNumber: [Int], with lottoNumbers: [Int]) {
-  var containNumbers: [Int] = []
-  for number in myLottoNumber {
-    if myLottoNumber.contains(lottoNumbers[number-1]) {
-      containNumbers.append(lottoNumbers[number-1])
+    var containNumbers: [Int] = []
+    for number in myLottoNumber {
+        if myLottoNumber.contains(lottoNumbers[number-1]) {
+            containNumbers.append(lottoNumbers[number-1])
+        }
     }
-  }
-  let containNumbersWithString = convertStringArray(from: containNumbers)
-  printContainNumbers(in: containNumbersWithString)
+    let containNumbersWithString = convertStringArray(from: containNumbers)
+    printContainNumbers(in: containNumbersWithString)
 }
 
 func convertStringArray(from array: [Int]) -> [String] {
-  var convertedStringArray: [String] = []
-  for number in array {
-    convertedStringArray.append(String(number))
-  }
+    var convertedStringArray: [String] = []
+    for number in array {
+        convertedStringArray.append(String(number))
+    }
 
-  return convertedStringArray
+    return convertedStringArray
 }
 
 func printContainNumbers(in array: [String]) {
-  if array.isEmpty {
-    print("아쉽지만 겹치는 번호가 없습니다.")
-  } else {
-    print("축하합니다! 겹치는 번호는 \(array.joined(separator: ", ")) 입니다!")
-  }
+    if array.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는 \(array.joined(separator: ", ")) 입니다!")
+    }
 }
 
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 let lottoNumbers = pickLottoNumbers()
 
 compare(myLottoNumbers, with: lottoNumbers)
+
+var lottoNumbersDictionary: [String: [Int]] = ["1회차": [1, 2, 3, 4, 5, 6]]
