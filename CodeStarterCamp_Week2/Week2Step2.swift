db19f37e1a7df32196de1ef0ee155e3db9837d81
hansm <hanmix2003@naver.com>
2022-04-26 22:26:32 +0900

    Fix print pharse location
    randomNumbers, myLottoNumbers 출력하는 print문 위치 수정

--- CodeStarterCamp_Week2/Week2Step2.swift
+++ CodeStarterCamp_Week2/Week2Step2.swift
@@ -18,12 +18,12 @@ func createRandomLottoNumbers() -> Set<Int> {
 
 func checkInterserctionNumbers(lottoNumbers: Set<Int>, myLottoNumbers: Array<Int>) -> Set<Int> {
     let intersectionSet = lottoNumbers.intersection(Set(myLottoNumbers))
-    print("랜덤으로 생성된 숫자 : \(lottoNumbers)")
-    print("내가 고른 숫자 : \(myLottoNumbers)")
     return intersectionSet
 }
 
 func showIntersectionNumbers() {
+    print("랜덤으로 생성된 숫자 : \(randomNumbers)")
+    print("내가 고른 숫자 : \(myLottoNumbers)")
     let intersectionNumbers = checkInterserctionNumbers(lottoNumbers: randomNumbers, myLottoNumbers: myLottoNumbers)
     if intersectionNumbers.isEmpty {
         print("아쉽게도 겹치는 번호가 없습니다.")
