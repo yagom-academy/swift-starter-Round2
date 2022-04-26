27678aa1022dc48323e70cbaeae51054cba6bece
ssyoun4092 <ssyoun4092@naver.com>
2022-04-26 19:19:41 +0900

    chore: 가독성 향상을 위한 공백 제거, for 문에서의 제어변수 이름 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,11 +1,8 @@
 import Foundation
 
-
 func pickLottoNumbers() -> [Int] {
-
   var lottoNumbers: [Int] = []
   var totalLottoNumbers: [Int] = Array(1...45)
-
   totalLottoNumbers.shuffle()
   totalLottoNumbers.removeSubrange(6...(totalLottoNumbers.count-1))
   lottoNumbers = totalLottoNumbers
@@ -14,32 +11,26 @@ func pickLottoNumbers() -> [Int] {
 }
 
 func compare(_ myLottoNumber: [Int], with lottoNumbers: [Int]) {
-
   var containNumbers: [Int] = []
-
-  for i in myLottoNumber {
-    if myLottoNumber.contains(lottoNumbers[i-1]) {
-      containNumbers.append(lottoNumbers[i-1])
+  for number in myLottoNumber {
+    if myLottoNumber.contains(lottoNumbers[number-1]) {
+      containNumbers.append(lottoNumbers[number-1])
     }
   }
-
   let containNumbersWithString = convertStringArray(from: containNumbers)
   printContainNumbers(in: containNumbersWithString)
 }
 
 func convertStringArray(from array: [Int]) -> [String] {
-
   var convertedStringArray: [String] = []
-
-  for num in array {
-    convertedStringArray.append(String(num))
+  for number in array {
+    convertedStringArray.append(String(number))
   }
 
   return convertedStringArray
 }
 
 func printContainNumbers(in array: [String]) {
-
   if array.isEmpty {
     print("아쉽지만 겹치는 번호가 없습니다.")
   } else {
