bef97dde9047c5ad142e9f0c93e9725e7dd28c4a
Dorii0513 <bobodorii0513@gmail.com>
2022-04-27 16:59:49 +0900

    코멘트 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -11,41 +11,36 @@ import Foundation
 var lottoNumbers: Set<Int> = Set<Int>()
 
 func pickLottoNumbers() -> Set<Int> {
-    while lottoNumbers.count < 6{
+    while lottoNumbers.count < 6 {
         lottoNumbers.insert(Int.random(in: 0...46))
     }
     return lottoNumbers
 }
-func compareNumbers() {
-    let myLootoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+
+func compareNumbers() -> Set<Int> {
+    let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
     var myNumbers: Set<Int> = Set<Int>()
-    for index in 0..<myLootoNumbers.count{
-        myNumbers.insert(myLootoNumbers[index])
-    }
-    let intersection: Set<Int> = myNumbers.intersection(lottoNumbers)
-    switch intersection.count {
-    case 0:
-        print("아쉽지만 겹치는 번호가 없습니다.")
-    case 1:
-        print("축하합니다! 겹치는 번호는", terminator: " ")
-        for num in intersection {
-            print(num, terminator: " ")
-        }
-        print("입니다!")
-    default:
-        print("축하합니다! 겹치는 번호는", terminator: " ")
-        var index = 1
-        for num in intersection {
-            if index == intersection.count {
-                print(num, terminator: " ")
-            }else {
-                print("\(num),", terminator: " ")
-            }
-            index += 1
-        }
-        print("입니다!")
-    }
-}
-pickLottoNumbers()
-compareNumbers()
     
+    for index in 0..<myLottoNumbers.count {
+        myNumbers.insert(myLottoNumbers[index])
+    }
+    
+    let overlapping: Set<Int> = myNumbers.intersection(lottoNumbers)
+    
+    return overlapping
+}
+
+pickLottoNumbers()
+
+var overlappingNumbers: Set<String> = Set<String>()
+
+for num in compareNumbers() {
+    overlappingNumbers.insert(String(num))
+}
+
+if compareNumbers().isEmpty {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+} else {
+    let joinedNumbers = overlappingNumbers.joined(separator: ", ")
+    print("축하합니다! 겹치는 번호는 \(joinedNumbers) 입니다!")
+}
