e28e12d652fea87ed0e96954f55e587f5c1dbb0c
CHOSION <chosion0107@gmail.com>
2022-04-27 22:29:49 +0900

    STEP2 미션

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,56 @@
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [6, 27, 13, 44, 35, 18]
+var numbersSet: Set<Int> = Set<Int>()
+var autoLottoNumbers: Set<Int> = Set<Int>()
 
+func makeLottoNumbers() -> Set<Int> {
+    
+    var lottoNum: Int = 0
+    
+    for number in 1...45 {
+        numbersSet.insert(number)
+    }
+    
+    while lottoNum > 6 {
+        lottoNum += 1
+        autoLottoNumbers.insert(numbersSet.randomElement() ?? 0)
+    }
+    
+    return autoLottoNumbers
+}
+
+func compareLottoNumbers() {
+    
+    var matchedNumbers: [Int] = []
+    
+    for index in 0...myLottoNumbers.count-1 {
+        if autoLottoNumbers.contains(myLottoNumbers[index]) {
+            matchedNumbers.append(myLottoNumbers[index])
+        }
+    }
+    
+    switch matchedNumbers.count {
+        
+    case 0:
+        print("아쉽지만 겹치는 번호가 없습니다.")
+        
+    case 1...autoLottoNumbers.count :
+        print("축하합니다. 겹치는 번호는", terminator: "")
+        
+        for index in 1...matchedNumbers.count {
+            if index != (matchedNumbers.count - 1) {
+                print(matchedNumbers[index], terminator: ", ")
+            } else {
+                print(matchedNumbers[index], terminator: "")
+            }
+        }
+        
+        print(" 입니다!")
+    default:
+        print("unknown")
+    }
+}
+
+compareLottoNumbers()
