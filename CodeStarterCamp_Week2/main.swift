e148779177393a7572ddc0334a42735a76deeb78
Seyong Oh <alaala3@gmail.com>
2022-04-29 17:05:49 +0900

    feat: checkLotto() 함수 생성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,10 +8,31 @@
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+var lottoSet: Set<Int> = Set<Int>()
 
-var integerSet: Set<Int> = Set<Int>()
-var rand = Int.random(in: 1 ... 45)
+func makeNumber() -> Int {
+    let rand = Int.random(in: 1 ... 45)
+    return rand
+}
 
-print(rand)
+func makeLottoSet() {
+    while lottoSet.count < 6 {
+        lottoSet.insert(makeNumber())
+    }
+}
+
+func checkLotto(lottoNum: Set<Int>, myNum: Set<Int>) {
+    let intersection: Set<Int> = lottoNum.intersection(myNum)
+    if intersection == [] {
+        print("일치하는 번호 없음")
+    }
+    else {
+        print(intersection, "가 일치합니다.")
+    }
+}
+
+makeLottoSet()
+print(lottoSet)
+checkLotto(lottoNum: lottoSet, myNum: myLottoNumbers)
 
