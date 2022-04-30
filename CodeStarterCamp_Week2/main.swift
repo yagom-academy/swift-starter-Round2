e3c6da4cf836109e4d5fd7c4728537aaecae0976
OneE <top9712@daum.net>
2022-04-30 14:20:27 +0900

    feat: 로또 번호 만드는 함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -5,8 +5,22 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
-
 import Foundation
 
-print("Hello, World!")
+var newLottoNum = Set<Int>()
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+
+func makeNewLottoNumber() {
+    var randomNumber: Int
+    
+    while newLottoNum.count != 6 {
+        randomNumber = Int.random(in: 1...45)
+        if newLottoNum.contains(randomNumber) != true {
+            newLottoNum.insert(randomNumber)
+        }
+    }
+    print(newLottoNum)
+}
+
+makeNewLottoNumber()
 
