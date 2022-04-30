399549dc5a57797a88b63abaa5f450ddbf7f71ae
delight010 <81559026+delight010@users.noreply.github.com>
2022-04-30 17:09:26 +0900

    step2 코드 작성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
+func pickedLottoNumbers() -> Set<Int>{
+    var lottoNumbers = Set<Int>()
 
+    while lottoNumbers.count < 6 {
+        let num = Int.random(in: 1...45)
+        lottoNumbers.insert(num)
+    }
+
+    return lottoNumbers
+}
+
+func checkLottoNumbers(winTheLotteryNumbers: Set<Int>, myLottoNumbers: Set<Int>) {
+    let intersectionNumbers = winTheLotteryNumbers.intersection(myLottoNumbers)
+    let arrayIntersectionNumbers = intersectionNumbers.map { String($0) }
+    var resultOfLotto = String()
+    
+    if arrayIntersectionNumbers.count > 0 {
+        resultOfLotto = "축하합니다! 겹치는 번호는 \(arrayIntersectionNumbers.joined(separator: ", ")) 입니다!"
+    } else {
+        resultOfLotto = "아쉽지만 겹치는 번호가 없습니다."
+    }
+    
+    print(resultOfLotto)
+}
+
+let winTheLotteryNumbers = pickedLottoNumbers()
+let myLottoNumbers = pickedLottoNumbers()
+
+checkLottoNumbers(winTheLotteryNumbers: winTheLotteryNumbers, myLottoNumbers: myLottoNumbers)
