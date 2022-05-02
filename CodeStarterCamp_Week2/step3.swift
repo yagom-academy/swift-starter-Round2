07eec871ca9f526003bc7d1d7bedd1b2a1c61334
dev-summer <summerly812@gmail.com>
2022-05-02 14:15:42 +0900

    Week2 Step3 swift convention applied

--- CodeStarterCamp_Week2/step3.swift
+++ CodeStarterCamp_Week2/step3.swift
@@ -9,23 +9,23 @@ import Foundation
 
 var lottoGames = [String: Array<Int>]()
 
-func createLottoGame(drawNumber: Int, totalGameNumber: Int) -> Dictionary<String, Array<Int>>{
+func createLottoGame(drawNumber: Int, totalGameNumber: Int) -> Dictionary<String, Array<Int>> {
     for i in 1...totalGameNumber {
         lottoGames["\(i)회차"] = Array(createWinNumbers(number: drawNumber)).sorted()
     }
     return(lottoGames)
 }
 
-func showEachGame(totalGameNumber:Int, gameNumber:Int) -> String{
+func showEachGame(gameNumber:Int) -> String {
     var winNumberToString: String = ""
     if let chosenGame = lottoGames["\(gameNumber)회차"] {
-        winNumberToString = chosenGame.map {String($0)}.joined(separator: ", ")
+        winNumberToString = chosenGame.map { String($0) }.joined(separator: ", ")
     }
     return winNumberToString
 }
 
 func printEachGame(drawNumber:Int, gameNumber: Int, totalGameNumber: Int) {
     let lottoGames = createLottoGame(drawNumber: drawNumber, totalGameNumber: totalGameNumber)
-    let winNumberToString = showEachGame(totalGameNumber: totalGameNumber, gameNumber: gameNumber)
+    let winNumberToString = showEachGame(gameNumber: gameNumber)
     print("\(gameNumber)회차의 로또 당첨 번호는 \(winNumberToString) 입니다.")
 }
