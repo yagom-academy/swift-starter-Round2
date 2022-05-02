177a04d1c5f9c2a3dc1b511cb727f5ff7de7659c
OneE <top9712@daum.net>
2022-05-02 16:02:41 +0900

    docs: 함수 및 교집합 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -7,35 +7,28 @@
 //
 import Foundation
 
-var newLottoNum = Set<Int>()
-let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+var newLottoNumbers = Set<Int>()
+let myLottoNumbers = [1, 2, 3, 4, 5, 6]
 
 func makeNewLottoNumber() {
-    var randomNumber: Int
-    
-    while newLottoNum.count != 6 {
-        randomNumber = Int.random(in: 1...45)
-        if newLottoNum.contains(randomNumber) != true {
-            newLottoNum.insert(randomNumber)
-        }
+    while newLottoNumbers.count != 6 {
+        let randomNumbers = Int.random(in: 1...45)
+            newLottoNumbers.insert(randomNumbers)
     }
-    print(newLottoNum)
 }
 
 func matchingLottoNumber() {
-    let convertMyNumArrayToSet = Set(myLottoNumbers)
-    let matchedLottoNumber = newLottoNum.intersection(convertMyNumArrayToSet)
+    makeNewLottoNumber()
+    let matchedLottoNumbers = newLottoNumbers.intersection(myLottoNumbers)
     
-    if matchedLottoNumber.count > 0 {
-        let winLottoNumber = Array(matchedLottoNumber)
-        let convertIntToString = winLottoNumber.map { String($0)}
-        let printWinNumber = convertIntToString.joined(separator: ", ")
-        
-        print("축하합니다! 겹치는 번호는 \(printWinNumber)입니다.")
+    if matchedLottoNumbers.count > 0 {
+        let winLottoNumbers = Array(matchedLottoNumbers)
+        let convertIntToString = winLottoNumbers.map { String($0) }
+        let printWinNumbers = convertIntToString.joined(separator: ", ")
+        print("축하합니다! 겹치는 번호는 \(printWinNumbers)입니다.")
     } else {
         print("아쉽지만 겹치는 번호가 없습니다.")
     }
 }
 
-makeNewLottoNumber()
 matchingLottoNumber()
