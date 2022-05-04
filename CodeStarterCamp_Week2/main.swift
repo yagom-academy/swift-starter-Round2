38531622dac5c83462492a984d11628741e5bd58
ueunli <67406889+J-unely@users.noreply.github.com>
2022-05-04 11:39:10 +0900

    refactor: 로또 생성/저장, 회차별 조회 함수를 기능별로 정리

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-//회차별 로또번호
-var lottoList: [String: [Int]] = [:]  //이니셜라이저?
-saveLottoHistory(count: 5)
-print("2회차의 로또 당첨 번호는 \(lottoList["2회차"]!.map(String.init).joined(separator: ", ")) 입니다.")
+//MARK: - 로또를 5회 생성 및 저장
+addLottos(of: 5)
+
+//MARK: - 2회차의 로또 당첨 번호를 조회
+searchLottoHistory(of: 2)
--- CodeStarterCamp_Week2/step3_lotto.swift
+++ CodeStarterCamp_Week2/step3_lotto.swift
@@ -7,9 +7,32 @@
 
 import Foundation
 
-func saveLottoHistory(count: Int) {
-    for i in 1...count {
-        lottoList["\(i)회차"] = generateLotto()
+//MARK: - 변수 선언) 회차별 로또 목록을 저장
+var lottoList: [String: [Int]] = [:]
+
+//MARK: - 변수 선언) 누적회차를 반영
+var round: Int = lottoList.count
+
+//MARK: - 함수 선언) 로또 1회를 생성 및 저장
+func addLotto() {
+    round += 1
+    lottoList["\(round)회차"] = generateLotto()
+}
+
+//MARK: - 함수 선언) 로또 n회를 생성 및 저장
+func addLottos(of times: Int) {
+    for _ in 1...times {
+        addLotto()
     }
 }
 
+//MARK: - 함수 선언) 회차별 로또 목록 중 특정 회차를 조회
+func searchLottoHistory(of index: Int) {
+    let lottoListKeys = lottoList["\(index)회차"]?.map(String.init)
+    switch lottoListKeys {
+    case nil:
+        print("검색 결과가 없습니다. 마지막 회차를 조회하시려면 \(round)를 입력하세요.")
+    default:
+        print("\(index)회차의 로또 당첨 번호는 \(lottoListKeys!.joined(separator: ", ")) 입니다.")
+    }
+}
