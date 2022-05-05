4165fa9490e0e037710239d550ed023a6af1ff6d
피우리 <piuri@piuliui-MacBookPro.local>
2022-05-05 23:20:00 +0900

    수정본추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -11,28 +11,23 @@ import Foundation
 func createRandomNumber() -> Int {
         return Int.random(in: 1..<46)
 }
-var winnerNumber: Set<Int> = Set<Int>()
+var winnerNumbers: Set<Int> = Set<Int>()
 func createWinnerNumbers() -> Set<Int> {
     repeat {
-        winnerNumber.insert(createRandomNumber())
-    } while winnerNumber.count < 6
+        winnerNumbers.insert(createRandomNumber())
+    } while winnerNumbers.count < 6
     
-    return winnerNumber
+    return winnerNumbers
 }
 createWinnerNumbers()
 
 let myLottoNumbers: [Int] = [7, 12, 15, 22, 43, 44]
-func createLottoNumberIndex(indexNum: Int) -> Int {
-    for indexNumber in 0...indexNum{
-        myLottoNumbers[indexNumber]
-    }
-    return myLottoNumbers[indexNum]
-}
-func resultOfLotto() {
-    if winnerNumber.contains(createLottoNumberIndex(indexNum: 5)) {
+func printLottoResult() {
+    let arrayToSetConverter = Set(myLottoNumbers)
+    if arrayToSetConverter == winnerNumbers {
         print("축하합니다! 겹치는 번호는 \(myLottoNumbers) 입니다!")
     } else {
         print("아쉽지만 겹치는 번호가 없습니다.")
     }
 }
-resultOfLotto()
+printLottoResult()
