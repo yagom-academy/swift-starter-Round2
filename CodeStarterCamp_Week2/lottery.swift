3cca1aaf387598186dc027552eaacba1ab662ee0
wecaners <wecaners@naver.com>
2022-05-06 22:50:26 +0900

    step3_fix_01

--- CodeStarterCamp_Week2/lottery.swift
+++ CodeStarterCamp_Week2/lottery.swift
@@ -7,16 +7,16 @@
 
 import Foundation
 
-func makeLottoNumbers(number: Int) -> [Int] {
-    var lottoNumbers: Array<Int> = []
+func makeLottoNumbers(number: Int) -> Set<Int> {
+    var lottoNumbers: Set<Int> = Set<Int>()
     while lottoNumbers.count < number {
-        lottoNumbers.append(Int.random(in: 1...45))
+        lottoNumbers.insert(Int.random(in: 1...45))
     }
     return lottoNumbers
 }
 
-func saveLottoSeries(count: Int) -> Dictionary<String, [Int]> {
-    var lottoSeries: Dictionary<String, [Int]> = [String: [Int]]()
+func saveLottoSeries(count: Int) -> Dictionary<String, Set<Int>> {
+    var lottoSeries: Dictionary<String, Set<Int>> = [String: Set<Int>]()
     for i in 1...count {
         lottoSeries["\(i)회차"] = makeLottoNumbers(number: 6)
     }
@@ -24,9 +24,9 @@ func saveLottoSeries(count: Int) -> Dictionary<String, [Int]> {
 }
 
 func printWinningNumbers(count: Int) {
-    let winningNumbers: [String: [Int]] = saveLottoSeries(count: 6)
+    let winningNumbers: [String: Set<Int>] = saveLottoSeries(count: 6)
     if let winningNumbers = winningNumbers["\(count)회차"] {
-        print("\(count)회차의 번호는 : ", "\(winningNumbers)", "입니다.")
+        print("\(count)회차의 번호는 :\(winningNumbers)입니다.")
     } else {
         print("값이 없습니다.")
     }
