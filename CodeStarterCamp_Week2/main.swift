e02ec430745e4de0ebcdd69dd615d679cc7ad08f
limdumb <dlaruddls@nate.com>
2022-05-08 18:02:15 +0900

    Step2 불필요한 주석 삭제

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,35 +8,6 @@
 
 import Foundation
 
-//let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-//var randomNumber: Set<Int> = Set<Int>()
-//
-//func printRandomNumber() {
-//    while randomNumber.count < 6 {
-//        randomNumber.insert(Int.random(in: 1...45))
-//    }
-//    print(randomNumber.sorted())
-//}
-//
-//var sameNumbers: [Int] = []
-//
-//func printWinningNumber(lottoNumber: Set<Int>, myNumbers: [Int]) {
-//    let numbers = lottoNumber
-//    for lottoNumber in numbers {
-//        if myLottoNumbers.contains(lottoNumber) {
-//            sameNumbers.append(Int(lottoNumber))
-//        }
-//    }
-//}
-//
-//func mainLottoResult(sameNumbers: [Int]) {
-//    if sameNumbers.isEmpty {
-//        print("아쉽지만 겹치는 번호가 없습니다.")
-//    } else {
-//        print("축하합니다! 겹치는 번호는 \(sameNumbers) 입니다!")
-//    }
-//}
-
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
 func creatRandomNumbers() -> Set<Int> {
