b5cf543f554b4727e5992c7aeb70fea1de5abfbb
limdumb <dlaruddls@nate.com>
2022-05-10 19:50:24 +0900

    Step2 출력값 및 결과 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -10,6 +10,11 @@ import Foundation
 
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
+//func shuffledLottoNumbers() -> [Int] {
+//    let numbers: [Int] = Array((1...45).shuffled()[0...6])
+//    return numbers
+//}
+
 func creatRandomNumbers() -> Set<Int> {
     var randomNumber: Set<Int> = Set<Int>()
     while randomNumber.count < 6 {
@@ -28,13 +33,18 @@ func checkWinningNumbers(lottoNumber: Set<Int>, myNumbers: [Int]) -> [Int] {
     return sameNumbers
 }
 
+
+
 func printWinningResultNumbers(sameNumbers: [Int]) {
+    let stringSameNumbers = sameNumbers.map { String($0) }
+    let joinedNumbers = stringSameNumbers.joined(separator: ", ")
     if sameNumbers.isEmpty {
         print("아쉽지만 겹치는 번호가 없습니다.")
     } else {
-        print("축하합니다! 겹치는 번호는 \(sameNumbers) 입니다!")
+        print("축하합니다! 겹치는 번호는 \(joinedNumbers) 입니다!")
     }
 }
+
 // randomNumber 결과값 출력 [ 확인용 ]
 let randomNumber = creatRandomNumbers()
 print(randomNumber)
