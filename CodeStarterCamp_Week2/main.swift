1ee7f77bfdf4003b61048ad727b08eb74d425178
jonghancha <jonghancha@gmail.com>
2022-06-28 17:00:17 +0900

    feat: 당첨번호와 겹치는 숫자를 확인하는 함수 추가 - 변수 winningNumber 함수안으로 이동 - checkOverlap함수의 전달인자로 쓰기 위해pickWinningNumber함수의 return값을 Set로 변경

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,10 +8,25 @@
 
 import Foundation
 
-// 로또 당첨 번호를 생성하는 함수
-var winningNumber: Set<Int> = Set<Int>()
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
-while winningNumber.count < 6 {
-    winningNumber.insert(Int.random(in: 1...45))
+// 로또 당첨 번호를 생성하는 함수
+func pickWinningNumber() -> Set<Int> {
+    var winningNumber: Set<Int> = Set<Int>()
+    while winningNumber.count < 6 {
+        winningNumber.insert(Int.random(in: 1...45))
+    }
+    return winningNumber
 }
 
+// 겹치는 숫자를 확인하는 함수
+func checkOverlap(win: Set<Int>) {
+    let overlapNumber: Set<Int> = win.intersection(myLottoNumbers)
+    guard overlapNumber.count > 0 else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+        return
+    }
+    print("축하합니다! 겹치는 번호는 \(overlapNumber)입니다!")
+}
+// 실행
+checkOverlap(win: pickWinningNumber())
