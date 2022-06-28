0dcb478d96e32b2a9115b4c66ea9a50fda833ad2
jonghancha <jonghancha@gmail.com>
2022-06-28 16:14:07 +0900

    feat: 로또 당첨번호 생성함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,10 @@
 
 import Foundation
 
-print("Hello, World!")
+// 로또 당첨 번호를 생성하는 함수
+var winningNumber: Set<Int> = Set<Int>()
+
+while winningNumber.count < 6 {
+    winningNumber.insert(Int.random(in: 1...45))
+}
 
