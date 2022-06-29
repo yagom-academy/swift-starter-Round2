3680e1509f1769b5d1647f222db0526a13d4ce99
hanjongwoo <gwd0311@naver.com>
2022-06-30 00:29:00 +0900

    STEP2 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -16,10 +16,11 @@ func makeWinLottoNumbers() -> Set<Int> {
     for _ in 1...6 {
         var randomNumber: Int = 0
         
-        repeat{
-            randomNumber = lottoNumbers.randomElement()!
+        if let randomNum:Int = lottoNumbers.randomElement() {
+            randomNumber = randomNum
+        } else {
+            print("There is nil in lottoNumbers")
         }
-        while !lottoNumbers.contains(randomNumber)
                 
         winLottoNumbers.insert(randomNumber)
         lottoNumbers = lottoNumbers.filter() {$0 != randomNumber}
@@ -28,21 +29,25 @@ func makeWinLottoNumbers() -> Set<Int> {
     return winLottoNumbers
 }
 
+func printLottoResult(_ numbers:[Int]) {
+    if numbers.count == 0 {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다. 겹치는 번호는 ",terminator: "")
+        for i in numbers {
+            if i == numbers[numbers.count - 1] {
+                print(i,terminator: "")
+            }
+            else {
+                print(i,terminator: ",")
+            }
+        }
+        print(" 입니다!")
+    }
+}
+
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 let overlappingNumbers: [Int] = Array(makeWinLottoNumbers().intersection(myLottoNumbers)).sorted()
 
-if overlappingNumbers.count == 0 {
-    print("아쉽지만 겹치는 번호가 없습니다.")
-} else {
-    print("축하합니다. 겹치는 번호는 ",terminator: "")
-    for i in overlappingNumbers {
-        if i == overlappingNumbers[overlappingNumbers.count - 1] {
-            print(i,terminator: "")
-        }
-        else {
-            print(i,terminator: ",")
-        }
-    }
-    print(" 입니다!")
-}
+printLottoResult(overlappingNumbers)
 
