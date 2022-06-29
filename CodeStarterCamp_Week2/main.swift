261682bc0bbce8bc47a1e4a585566ae93a9dc828
chacha <hyhy0429_@nvaer.com>
2022-06-29 16:30:22 +0900

    MatchLottoNumber

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,30 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week2
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
 
-import Foundation
+func MakemyLottoNumber() -> Set<Int> {
+ var myLottoNumber: Set<Int> = Set<Int>()
+    while myLottoNumber.count < 6 {
+    myLottoNumber.insert(Int.random(in:1...45))
+    }
+    return myLottoNumber
+}
+print(MakemyLottoNumber())
 
-print("Hello, World!")
+func MakeRealLottoNumber() -> Set<Int> {
+ var realLottoNumber: Set<Int> = Set<Int>()
+    while realLottoNumber.count < 6 {
+    realLottoNumber.insert(Int.random(in:1...45))
+    }
+    return realLottoNumber
+}
+print(MakeRealLottoNumber())
 
+let myLottoNumbers: Set<Int> = [1, 2, 12, 9, 36, 35]
+let realLottoNumbers: Set<Int> = [1, 12, 36, 6, 27, 7]
+
+let intersection: Set<Int> = myLottoNumbers.intersection(realLottoNumbers)
+
+if intersection.count > 0 {
+    print("축하합니다! 겹치는 번호는 \(intersection)입니다!")
+}
+    else {
+        print ("아쉽지만 겹치는 번호가 없습니다.")
+    }
