e70805134700e03ca999d25af0dd45bb1a108fdb
codesparrow12 <kanghd97@gmail.com>
2022-06-29 22:41:53 +0900

    checkLottoNumber 메서드 구현

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -10,6 +10,7 @@ import Foundation
 
 var winningLottoNumber: [Int] = []
 var chosenWinningNum: Int = 0
+var sameLottoNumber: [Int] = []
 
 func castWinningLotto() {
     while winningLottoNumber.count < 6 {
@@ -21,3 +22,26 @@ func castWinningLotto() {
         }
     }
 }
+
+func checkLottoNumber(with winningLotto: Array<Int>, with myLotto: Array<Int> ) {
+    
+    for winningNumber in winningLotto {
+        for myNumber in myLotto {
+            if winningNumber == myNumber {
+                sameLottoNumber.append(myNumber)
+            }
+        }
+    }
+    
+    if sameLottoNumber.count == 0 {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+    else {
+        print("축하합니다! 겹치는 번호는 \(sameLottoNumber) 입니다!")
+    }
+}
+
+let myLottoNumber: [Int] = [13, 3, 37, 7, 11, 23]
+
+castWinningLotto()
+checkLottoNumber(with: winningLottoNumber, with: myLottoNumber)
