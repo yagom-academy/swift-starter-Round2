594b036fbf849a16c641b4f709c32b23c1d74954
Jae Hyung Kim <smfc.mene@gmail.com>
2022-06-29 18:19:32 +0800

    fix: 당첨번호 안내문구에 [ ] 표시 보이지 않도록 수정

--- CodeStarterCamp_Week2/step2.swift
+++ CodeStarterCamp_Week2/step2.swift
@@ -18,8 +18,21 @@ func generateLottoNumber() -> Set<Int> {
 func commpareLottoNumbers(myNumber: Array<Int>, thisWeekNumber: Set<Int>) {
     let myLottoNumberSet = Set(myLottoNumbers)
     let matchingNumbers: Set<Int> = myLottoNumberSet.intersection(thisWeekNumber)
-    if(matchingNumbers.count > 1) {
-        print("축하합니다! 겹치는 번호는 \(matchingNumbers)입니다!")
+    let matchingNumberArray = matchingNumbers.sorted()
+    let matchingNumberQuantity = matchingNumbers.count
+    let matchingNumberIndex = matchingNumberArray.count
+    if(matchingNumberQuantity == 1) {
+        print("축하합니다! 겹치는 번호는 \(matchingNumberArray[0]) 입니다!")
+    }
+    else if(matchingNumberQuantity == 2) {
+        print("축하합니다! 겹치는 번호는 \(matchingNumberArray[0]), \(matchingNumberArray[1]) 입니다!")
+    }
+    else if(matchingNumberQuantity >= 3) {
+        print("축하합니다! 겹치는 번호는 ", terminator: "")
+        for matchingNumberArrayIndex in 0...matchingNumberIndex - 2 {
+            print(matchingNumberArray[matchingNumberArrayIndex], terminator: ", ")
+        }
+        print("\(matchingNumberArray[matchingNumberIndex - 1]) 입니다!")
     }
     else {
         print("아쉽지만 겹치는 번호가 없습니다.")
