c16d9c59687244ffce37ccb9b4c37d100e375fe1
Jae Hyung Kim <smfc.mene@gmail.com>
2022-06-29 17:19:20 +0800

    feat: 2개의 콜렉션타입을 받아 공통된 값을 비교하여 값을 출력하는 `commpareLottoNumbers` 함수 구현

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -11,3 +11,4 @@ import Foundation
 //MARK: STEP 2
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 let thisWeekLottoNumbers: Set<Int> = generateLottoNumber()
+commpareLottoNumbers(myNumber: myLottoNumbers, thisWeekNumber: thisWeekLottoNumbers)
--- CodeStarterCamp_Week2/step2.swift
+++ CodeStarterCamp_Week2/step2.swift
@@ -14,3 +14,14 @@ func generateLottoNumber() -> Set<Int> {
     }
     return generatedNumbers
 }
+
+func commpareLottoNumbers(myNumber: Array<Int>, thisWeekNumber: Set<Int>) {
+    let myLottoNumberSet = Set(myLottoNumbers)
+    let matchingNumbers: Set<Int> = myLottoNumberSet.intersection(thisWeekNumber)
+    if(matchingNumbers.count > 1) {
+        print("축하합니다! 겹치는 번호는 \(matchingNumbers)입니다!")
+    }
+    else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
