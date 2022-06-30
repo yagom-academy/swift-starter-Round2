a8d11770e4a31666cb260e2140511806106bb442
gye0rehan <101076197+gye0rehan@users.noreply.github.com>
2022-06-30 14:23:37 +0900

    Week2 [STEP2] gye0rehan

---
+++ CodeStarterCamp_Week2/STEP3.swift
@@ -0,0 +1,40 @@
+//
+//  STEP3.swift
+//  CodeStarterCamp_Week2
+//
+//  Created by 한겨레 on 2022/06/29.
+//
+
+import Foundation
+
+//로또 번호 추첨
+func makeLottoNumbers() -> Set<Int>{
+    
+    var lottoNumbers: Set<Int> = Set<Int>()
+    
+    while lottoNumbers.count<6 {
+        
+        lottoNumbers.insert(Int.random(in: 1...45))
+        
+    }
+    
+    return lottoNumbers
+}
+
+//로또 당첨 여부 조회
+func compareLottoNumbers() {
+    let myLottoNumbers: [Int] = [1,3,4,5,34,31]
+    
+    var sameLottoNumbers: Set<Int> = makeLottoNumbers().intersection(myLottoNumbers)
+  
+    if sameLottoNumbers.isEmpty {
+        
+        print("아쉽지만 탈락입니다.")
+        
+    }
+    
+    else {
+        let resultNumber = sameLottoNumbers.map{String($0)}.joined(separator: ", ")
+        print(resultNumber)
+    }
+}
