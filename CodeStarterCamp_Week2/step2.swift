48eb1693b6a1fe02ced1af26ac73133541e8854c
Jae Hyung Kim <smfc.mene@gmail.com>
2022-06-30 21:17:05 +0800

    fix: Array 인덱스 값을 이용하지 않고 for loop에서 당첨번호 출력하도록 수정

--- CodeStarterCamp_Week2/step2.swift
+++ CodeStarterCamp_Week2/step2.swift
@@ -10,29 +10,25 @@ import Foundation
 func generateLottoNumber() -> Set<Int> {
     var generatedNumbers: Set<Int> = Set<Int>()
     while generatedNumbers.count < 6 {
-        generatedNumbers.insert(Int.random(in: 1..<46))
+        generatedNumbers.insert(Int.random(in: 1...45))
     }
     return generatedNumbers
 }
 
 func commpareLottoNumbers(myNumber: Array<Int>, thisWeekNumber: Set<Int>) {
-    let myLottoNumberSet = Set(myLottoNumbers)
-    let matchingNumbers: Set<Int> = myLottoNumberSet.intersection(thisWeekNumber)
-    let matchingNumberArray = matchingNumbers.sorted()
-    let matchingNumberQuantity = matchingNumbers.count
-    let matchingNumberIndex = matchingNumberArray.count
-    if(matchingNumberQuantity == 1) {
-        print("축하합니다! 겹치는 번호는 \(matchingNumberArray[0]) 입니다!")
-    }
-    else if(matchingNumberQuantity == 2) {
-        print("축하합니다! 겹치는 번호는 \(matchingNumberArray[0]), \(matchingNumberArray[1]) 입니다!")
-    }
-    else if(matchingNumberQuantity >= 3) {
+    let myLottoNumberSet = Set(myNumber)
+    let matchingNumberArray = myLottoNumberSet.intersection(thisWeekNumber).sorted()
+    let matchingNumberQuantity = matchingNumberArray.count
+    if(matchingNumberQuantity != 0) {
         print("축하합니다! 겹치는 번호는 ", terminator: "")
-        for matchingNumberArrayIndex in 0...matchingNumberIndex - 2 {
-            print(matchingNumberArray[matchingNumberArrayIndex], terminator: ", ")
+        for index in 1...matchingNumberQuantity {
+            if(index != matchingNumberQuantity) {
+                print(index, terminator: ", ")
+            }
+            else {
+                print(index, terminator: " 입니다!\n")
+            }
         }
-        print("\(matchingNumberArray[matchingNumberIndex - 1]) 입니다!")
     }
     else {
         print("아쉽지만 겹치는 번호가 없습니다.")
