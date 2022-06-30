d50a21141d89deaf32db77c80d2bd5624fe8793c
hanjongwoo <gwd0311@naver.com>
2022-06-30 23:56:54 +0900

    STEP3

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-func makeWinLottoNumbers() -> Set<Int> {
+func makeWinLottoNumbers() -> Array<Int> {
     
     var lottoNumbers = Array<Int>(1...45)
     var winLottoNumbers: Set<Int> = []
@@ -26,28 +26,54 @@ func makeWinLottoNumbers() -> Set<Int> {
         lottoNumbers = lottoNumbers.filter() {$0 != randomNumber}
     }
     
-    return winLottoNumbers
+    return Array(winLottoNumbers)
 }
 
-func printLottoResult(_ numbers:[Int]) {
-    if numbers.count == 0 {
-        print("아쉽지만 겹치는 번호가 없습니다.")
-    } else {
-        print("축하합니다. 겹치는 번호는 ",terminator: "")
-        for i in numbers {
-            if i == numbers[numbers.count - 1] {
-                print(i,terminator: "")
-            }
-            else {
-                print(i,terminator: ",")
-            }
+// 결과 출력함수
+func printLottoResult(_ round:Int) {
+    print("\(round)회차의 로또 당첨번호는 ",terminator: "")
+    let numbers = chooseLottoRound(round)
+    for number in numbers{
+        if number == numbers[numbers.count - 1]{
+            print(number,terminator: "")
+        } else {
+            print(number,terminator: ", ")
         }
-        print(" 입니다!")
     }
+    print(" 입니다.")
 }
 
-let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-let overlappingNumbers: [Int] = Array(makeWinLottoNumbers().intersection(myLottoNumbers)).sorted()
+// 로또번호 생성해서 딕셔너리에 저장하는 함수
+func tryLotto() {
+    let round = "\(lottoNumberDic.count+1)회차"
+    let numbers = makeWinLottoNumbers()
+    
+    lottoNumberDic[round] = numbers
+}
 
-printLottoResult(overlappingNumbers)
+// 원하는 회차 옵셔널 값 안전하게 추출하는 함수
+func chooseLottoRound(_ round:Int) -> Array<Int> {
+    var lottoNumbers: Array<Int> = []
+    for i in 0...5{
+        if let lottoNumber:Int = lottoNumberDic["\(round)회차"]?[i] {
+            lottoNumbers.append(lottoNumber)
+        } else {
+            print("There is nil in lottoNumbers")
+        }
+    }
+    return lottoNumbers.sorted()
+}
+
+// 딕셔너리 생성
+var lottoNumberDic: [String:Array<Int?>] = [:]
+
+// 로또번호 5번 생성
+for _ in 1...5 {
+    tryLotto()
+}
+
+// 원하는 회차 선택
+let round = 3
+printLottoResult(round)
+
 
