979d59336eb28b30cf63186f289a36f470252615
gye0rehan <101076197+gye0rehan@users.noreply.github.com>
2022-07-01 04:27:41 +0900

    feat : 피드백 반영

--- CodeStarterCamp_Week2/STEP2.swift
+++ CodeStarterCamp_Week2/STEP2.swift
@@ -3,9 +3,7 @@ import Foundation
 
 //로또 번호 추첨
 func makeLottoNumbers() -> Set<Int> {
-    
     var lottoNumbers: Set<Int> = Set<Int>()
-    
     while lottoNumbers.count<6 {
         lottoNumbers.insert(Int.random(in: 1...45))
     }
@@ -15,11 +13,8 @@ func makeLottoNumbers() -> Set<Int> {
 //로또 당첨 여부 조회
 func compareLottoNumbers() {
     let myLottoNumbers = lottoNumberFunc(number: [1,2,3,4,5,6])
-    
     let sameLottoNumbers: Set<Int> = makeLottoNumbers().intersection(myLottoNumbers)
-    
     let arraySameLottoNumbers : String = arrangeNumber(numbers: sameLottoNumbers)
-    
     if sameLottoNumbers.isEmpty {
         print("아쉽지만 겹치는 번호가 없습니다.")
     }
@@ -29,28 +24,19 @@ func compareLottoNumbers() {
 }
 
 func arrangeNumber(numbers : Set<Int>) -> String {
-    
     var arraylottoNumbers : [String] = []
-    
     for numbers in numbers {
         arraylottoNumbers.append(String(numbers))
     }
-    
     let resultNumbers = arraylottoNumbers.joined(separator: ", ")
     return resultNumbers
-    
 }
-
-
-
 struct lottoNumbersStruct {
     var myLotto: [Int]
     init(myLotto: [Int]) {
         self.myLotto = myLotto
     }
-    
 }
-
 func lottoNumberFunc(number: [Int]) -> [Int] {
     let lottoNumberStructMyLotto: lottoNumbersStruct = lottoNumbersStruct(myLotto: number)
     
