4ccc5bb55f78753ea32ad80bf7e1dbe59411383b
LimKiWoong <lkw4670@naver.com>
2022-07-04 19:57:58 +0900

    step3 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -11,18 +11,5 @@ import Foundation
 for _ in 1...5{
     makeLottoNumbers(count: 6)
 }
-if let value:[Int] = lottoHistory["1회차"]{
-    var intNumbers: [Int] = []
-    var stringNumbers: [String] = []
-    
-    for num in value{
-        intNumbers.append(num)
-        intNumbers.sort()
-    }
-    
-    for num in intNumbers{
-        stringNumbers.append(String(num))
-    }
-    
-    print("1회차의 로또 당첨 번호는 \(stringNumbers.joined(separator: ", ")) 입니다.")
-}
+
+printRoundOfHistory(round: "1회차")
--- CodeStarterCamp_Week2/step3.swift
+++ CodeStarterCamp_Week2/step3.swift
@@ -15,3 +15,21 @@ func insertHistory(){
     lottoHistory["\(round)회차"] = intNumber
     round += 1
 }
+
+func printRoundOfHistory(round:String){
+    if let value:[Int] = lottoHistory[round]{
+        var intNumbers: [Int] = []
+        var stringNumbers: [String] = []
+        
+        for num in value{
+            intNumbers.append(num)
+            intNumbers.sort()
+        }
+        
+        for num in intNumbers{
+            stringNumbers.append(String(num))
+        }
+        
+        print("\(round)의 로또 당첨 번호는 \(stringNumbers.joined(separator: ", ")) 입니다.")
+    }
+}
