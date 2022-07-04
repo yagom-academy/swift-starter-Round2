fc72b1a7bfc5e846855815a685acf6f9efdb2ce4
sunny <maengseon-a@maengseon-aui-MacBookAir.local>
2022-07-04 20:59:46 +0900

    내 로또번호와 맞춰보기 ver.1

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,27 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week2
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
 
 import Foundation
 
-print("Hello, World!")
 
+//Step 2: 내 번호와 맞추어보기!
+
+var lottoWinningNumbers: Set<Int> = []
+
+for _ in 1...12{
+    lottoWinningNumbers.insert(Int.random(in: 1...45))
+    if lottoWinningNumbers.count == 6 {
+        break
+    }
+}
+
+let myLottoNumbers: [Int] = [2, 3, 5, 10, 21, 28]
+
+
+print("이번주 당첨번호는 \(lottoWinningNumbers) 입니다")
+print("이번주 내번호는 \(myLottoNumbers) 입니다")
+
+if lottoWinningNumbers.intersection(myLottoNumbers).count != 0 {
+    print("축하합니다! 겹치는 번호는 \(lottoWinningNumbers.intersection(myLottoNumbers)) 입니다!")
+}
+else {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+}
