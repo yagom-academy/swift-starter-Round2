13593e89139c3ba7ceee66b420c077bfa2bb230f
sunny <maengseon-a@maengseon-aui-MacBookAir.local>
2022-07-05 09:22:02 +0900

    함수로 표현

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,19 +6,31 @@ import Foundation
 
 var lottoWinningNumbers: Set<Int> = []
 
-while lottoWinningNumbers.count < 6 {
-    lottoWinningNumbers.insert(Int.random(in: 1...45))
+func drawLottoWinnigNumbers() {
+    while lottoWinningNumbers.count < 6 {
+        lottoWinningNumbers.insert(Int.random(in: 1...45))
+    }
 }
 
-let myLottoNumbers: [Int] = [2, 3, 5, 10, 21, 28]
+var myLottoNumbers: Set<Int> = []
 
+myLottoNumbers.insert(2)
+myLottoNumbers.insert(3)
+myLottoNumbers.insert(5)
+myLottoNumbers.insert(10)
+myLottoNumbers.insert(21)
+myLottoNumbers.insert(28)
 
+func checkMylottoNumbers() {
+    if lottoWinningNumbers.intersection(myLottoNumbers).count != 0 {
+        print("축하합니다! 겹치는 번호는 \(lottoWinningNumbers.intersection(myLottoNumbers)) 입니다!")
+    }
+    else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+
+drawLottoWinnigNumbers()
 print("이번주 당첨번호는 \(lottoWinningNumbers) 입니다")
 print("이번주 내번호는 \(myLottoNumbers) 입니다")
-
-if lottoWinningNumbers.intersection(myLottoNumbers).count != 0 {
-    print("축하합니다! 겹치는 번호는 \(lottoWinningNumbers.intersection(myLottoNumbers)) 입니다!")
-}
-else {
-    print("아쉽지만 겹치는 번호가 없습니다.")
-}
+checkMylottoNumbers()
