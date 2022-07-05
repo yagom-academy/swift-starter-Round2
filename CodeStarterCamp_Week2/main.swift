c60443d9aea41acc151882a4f0c3392cce0c2112
sunny <maengseon-a@maengseon-aui-MacBookAir.local>
2022-07-05 09:28:52 +0900

    네이밍 대문자로 변경

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -21,7 +21,7 @@ myLottoNumbers.insert(10)
 myLottoNumbers.insert(21)
 myLottoNumbers.insert(28)
 
-func checkMylottoNumbers() {
+func checkMyLottoNumbers() {
     if lottoWinningNumbers.intersection(myLottoNumbers).count != 0 {
         print("축하합니다! 겹치는 번호는 \(lottoWinningNumbers.intersection(myLottoNumbers)) 입니다!")
     }
@@ -33,4 +33,4 @@ func checkMylottoNumbers() {
 drawLottoWinnigNumbers()
 print("이번주 당첨번호는 \(lottoWinningNumbers) 입니다")
 print("이번주 내번호는 \(myLottoNumbers) 입니다")
-checkMylottoNumbers()
+checkMyLottoNumbers()
