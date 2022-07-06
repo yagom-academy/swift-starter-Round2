8249a4377a156cb1537ffdf7878ace799320ee36
sunny <maengseon-a@maengseon-aui-MacBookAir.local>
2022-07-07 08:50:24 +0900

    naming 수정, string타입변환방법수정, 파일분리

---
+++ codeStarterCamp_Week2-2/main.swift
@@ -0,0 +1,34 @@
+
+import Foundation
+
+
+//Step 2: 내 번호와 맞추어보기!
+
+var lottoWinningNumbers: Set<Int> = []
+var myLottoNumbers: Set<Int> = [2, 3, 5, 10, 21, 28]
+
+func drawLottoWinnigNumbers() {
+    while lottoWinningNumbers.count < 6 {
+        lottoWinningNumbers.insert(Int.random(in: 1...45))
+    }
+}
+
+func checkMyLottoNumbers() {
+    let overlappingNumbers = lottoWinningNumbers.intersection(myLottoNumbers)
+    
+    if overlappingNumbers.count == 0 {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+    else {
+        var StringOverlappingNumbers: Array<String> = []
+        for overlappingNumber in overlappingNumbers {
+            StringOverlappingNumbers.append(String(overlappingNumber))
+        }
+        print("축하합니다! 겹치는 번호는 \(StringOverlappingNumbers.joined(separator: ", ")) 입니다.")
+    }
+}
+
+drawLottoWinnigNumbers()
+print("이번주 당첨번호는 \(lottoWinningNumbers.sorted()) 입니다")
+print("이번주 내번호는 \(myLottoNumbers.sorted()) 입니다")
+checkMyLottoNumbers()
