d05e2b954203f7520d0be0b2f38a8461dcbf5003
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2022-07-07 13:53:50 +0900

    step1-1

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,4 @@
 import Foundation
 
-var lottoNumber = Set<Int>()
+print("리셋")
 
-var numbers = Array<Int>(1...45)
-
-
-
-while lottoNumber.count < 6 {
-    lottoNumber.insert(numbers.randomElement()!)
-}
-print(lottoNumber)
