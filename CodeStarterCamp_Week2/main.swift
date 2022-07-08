7d8eb6d6e4cb0004fc420eb805cdc99dee231a50
Hwiwon Lee <dkwmakrk123@naver.com>
2022-07-08 14:14:31 +0900

    당첨번호를 내 번호와 겹치는지 확인하는 함수구현

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -23,3 +23,15 @@ print(pickedLotteryNumbers)
 
 var myLottoNumbers: Set<Int> = Set<Int>()
 myLottoNumbers = [1,2,3,4,5,6]
+
+func checkingMyNumbers(from: Set<Int>) {
+    let commonNumbers: Set<Int> = myLottoNumbers.intersection(pickedLotteryNumbers)
+    
+    if commonNumbers.count == 0 {
+        print("아쉽지만 겹치는 번호가 없습니다")
+    } else {
+        print("축하합니다 겹치는 번호는 \(commonNumbers.sorted())입니다.")
+    }
+}
+
+checkingMyNumbers(from: myLottoNumbers)
