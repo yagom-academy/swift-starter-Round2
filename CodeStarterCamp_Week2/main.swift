f4d360389fe33400522df33512b552afe6fc8209
ericwonjoonk98 <wonjoonkang94@gmail.com>
2022-07-09 17:03:15 +0900

    week2step3 수정완료

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -35,17 +35,17 @@ func checksameNumbers() {
 }
 
 checksameNumbers()
-var saveLottoRecord : Dictionary<String,[Int]> = [:]
+var savedLottoRecord : Dictionary<String,[Int]> = [:]
 
-func createLottoNumber(iteratedLottonumber: Int) {
-    for n in 1...iteratedLottonumber {
-        saveLottoRecord["\(n)회차"] = printLottoNumber().sorted()
+func createLottoNumber(totalLottonumber: Int) {
+    for n in 1...totalLottonumber {
+        savedLottoRecord["\(n)회차"] = printLottoNumber().sorted()
     }
 }
 func findLottoNumber(round: Int) {
-    if let lottoNumbers: [Int] = saveLottoRecord["\(round)회차"] {
+    if let lottoNumbers: [Int] = savedLottoRecord["\(round)회차"] {
         print("\(round)회차의 로또 당첨 번호는 \(lottoNumbers)입니다.")
     }
 }
-createLottoNumber(iteratedLottonumber: 5 )
+createLottoNumber(totalLottonumber: 5 )
 findLottoNumber(round: 3)
