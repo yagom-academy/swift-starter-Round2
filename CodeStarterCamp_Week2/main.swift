18cc527abdf69538dcbbc4a6ed322249364227ce
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2022-07-10 15:02:42 +0900

    step2-2

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-var numbers = Set<Int>(1...45)
+var numbers = Array<Int>(1...45)
 
 var lottoNumber = Set<Int>()
 //1부터 45중 랜덤한 숫자를 받을 컬렉션
@@ -8,32 +8,43 @@ var lottoNumber = Set<Int>()
 let myLottoNumbers: Set<Int> = [3,7,8,23,29,43]
 //내가 임의로 정한 6개의 숫자 컬렉션
 
-
 while lottoNumber.count < 6 {
-    lottoNumber.insert(numbers.randomElement()!)
+    lottoNumber.insert(Int.random(in: 1...45))
 }
 
 var intersectionNumber: Set<Int> = myLottoNumbers.intersection(lottoNumber)
 //1~45부터 랜덤하게 뽑힌숫자와 내 임의 숫자의 교집합
 
-var intersectionNumMap = intersectionNumber.map { Int in return String(Int)
-}
-//map = 특정 배열에 String으로 모두 mapping 변환해주는 함수
-var intersectionNumJoined = intersectionNumMap.joined(separator: ",")
-
-
-print(lottoNumber)
-print(intersectionNumber)
-print(intersectionNumJoined)
-
-
+//방법1
 if intersectionNumber.count > 0 {
+    print("축하합니다! 겹치는 번호는 " , terminator: "")
+    for (index, number) in intersectionNumber.enumerated() {
+        if index == intersectionNumber.count - 1 {
+            print(number,terminator: "")
+        } else {
+            print(number,terminator: ",")
+            }
+        }
+    print(" 입니다! ", terminator: "")
     
-    for _ in intersectionNumber {}
-    print("축하합니다! 겹치는 번호는 \(intersectionNumJoined) 입니다")
 } else if intersectionNumber.count == 0 {
     print("아쉽지만 겹치는 번호가 없습니다")
 }
+
+
+//방법2 ,map joined
+//var intersectionNumMap = intersectionNumber.map ({ Int in return String(Int)
+//})
+//map = 특정 배열에 String으로 모두 mapping 변환해주는 함수
+//var intersectionNumJoined = intersectionNumMap.joined(separator: ",")
+
+//if intersectionNumber.count > 0 {
+//    print("축하합니다! 겹치는 번호는 \(intersectionNumJoined) 입니다")
+//} else if intersectionNumber.count == 0 {
+//    print("아쉽지만 겹치는 번호가 없습니다")
+//}
+
+
 
 
 
---
+++ step2.swift
@@ -0,0 +1,8 @@
+//
+//  step2.swift
+//  CodeStarterCamp_Week2
+//
+//  Created by 송종환 on 2022/07/08.
+//
+
+import Foundation
