8865b5adbefa43c1e4b7b7946511b1ae672b61ca
oh namjun <ohnamjun@ohui-MacBookAir.local>
2022-07-12 01:20:12 +0900

    STEP2

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 
-print("Hello, World!")
+func createRandomNumbers() -> Set<Int> {
+    var randomNumbers: Set<Int> = Set<Int>()
 
+    while randomNumbers.count < 6 {
+        randomNumbers.insert(Int.random(in: 1...45))
+    }
+
+    return randomNumbers
+}
+
+func raffleLotto(lottoNumbers: Set<Int>, RandomNubers: Set<Int>) {
+
+    let intersectionNumbers: Set<Int> = RandomNubers.intersection(lottoNumbers)
+
+    if intersectionNumbers.count == 6 {
+        print("축하합니다! 겹치는 번호는 1, 2, 3, 4, 5, 6 입니다!")
+    } else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+
+let winLottoNumbers: Set<Int> = createRandomNumbers()
+let myLottoNumbers: Set<Int> = createRandomNumbers()
+
+raffleLotto(lottoNumbers: winLottoNumbers, RandomNubers: myLottoNumbers)
