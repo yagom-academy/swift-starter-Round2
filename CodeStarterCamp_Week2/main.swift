b3442a6f434591896016b337b2b71dacc9589e40
Hwiwon Lee <dkwmakrk123@naver.com>
2022-07-12 22:05:34 +0900

    checkingMyNumbers -> comparingMyNumbers로 함수명 변경, comparingMyNumbers 함수 전체 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -20,17 +20,32 @@ func lotteryNumbers() -> Set<Int> {
 let pickedLotteryNumbers = lotteryNumbers()
 print(pickedLotteryNumbers)
 
-var myLottoNumbers: Set<Int> = Set<Int>()
+var myLottoNumbers = Set<Int>()
 myLottoNumbers = [1,2,3,4,5,6]
 
-func checkingMyNumbers(from: Set<Int>) {
-    let commonNumbers: Set<Int> = myLottoNumbers.intersection(pickedLotteryNumbers)
+func comparingMyNumbers(from lotteryNumbers: Set<Int>,to myNumbers: Set<Int>) {
+    let commonNumbers: Set<Int> = lotteryNumbers.intersection(myNumbers)
     
     if commonNumbers.count == 0 {
         print("아쉽지만 겹치는 번호가 없습니다")
     } else {
-        print("축하합니다! 겹치는 번호는 \(commonNumbers.sorted())입니다!")
+        print("축하합니다! 겹치는 번호는 ",terminator: "")
+        var commonNuberInRows = String()
+        for ArrangingInRow in commonNumbers {
+            commonNuberInRows += String(ArrangingInRow) + ","
+        }
+        if commonNuberInRows.last == "," {
+            commonNuberInRows.removeLast()
+        }
+        
+        print(commonNuberInRows, terminator: "")
+        print("입니다!")
     }
 }
 
-checkingMyNumbers(from: myLottoNumbers)
+comparingMyNumbers(from: pickedLotteryNumbers, to: myLottoNumbers)
+
+
+// print("축하합니다! 겹치는 번호는 \(commonNumbers.sorted())입니다!")
+
+
