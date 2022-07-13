d727ea79614ce90500611e38dba11a248595ea6e
chacha <hyhy0429_@nvaer.com>
2022-07-13 11:30:21 +0900

    lottoNumber5th

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,3 +1,4 @@
+import Foundation
 
 func makeRandomLottoNumber() -> Set<Int> {
     var myLottoNumber: Set<Int> = Set<Int>()
@@ -14,7 +15,7 @@ func checkTheNumber(){
     print (realLottoNumbers)
     
     let intersection: Set<Int> = myLottoNumbers.intersection(realLottoNumbers)
-    print(intersection)
+    print("intersection", intersection)
     
     if intersection.count == 0 {
         print ("아쉽지만 겹치는 번호가 없습니다.")
@@ -32,3 +33,24 @@ func checkTheNumber(){
 }
 
 checkTheNumber()
+
+
+var lottoNumberCollection : Dictionary<String,Set<Int>> = [ : ]
+
+    for x in 1...5  {
+        lottoNumberCollection["\(x)회차"] =  makeRandomLottoNumber()
+    }
+
+if let lottoNumber5th : Set<Int> = lottoNumberCollection["5회차"]{
+    print("5회차의 로또 당첨 번호는")
+    for (index, number) in lottoNumber5th.enumerated() {
+        if index == lottoNumber5th.count - 1{
+            print(number, terminator: "")
+        } else{
+            print(number, terminator: ",")
+        }
+    }
+    print(" 입니다.", terminator: "")
+} else{
+    print("lottoNumberCollection == nill")
+}
