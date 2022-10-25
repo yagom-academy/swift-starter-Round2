350f3fd08309d8a7935abf94e3c8643742fdbf53
hyemory <cjstksla73@gmail.com>
2022-10-25 16:13:45 +0900

    Step2 미션 수행

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,24 @@
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [3, 7, 15, 27, 37, 41]
+var winLottoNumbers: Set<Int> = []
+
+func callLottoNumbers() {
+    while winLottoNumbers.count < 6 {
+        winLottoNumbers.insert(Int.random(in: 1...45))
+    }
+}
+
+func checkOverlapNumbers() {
+    if winLottoNumbers.intersection(myLottoNumbers).count > 0 {
+        let overlapNumber: String = "축하합니다! 겹치는 번호는 \(winLottoNumbers.intersection(myLottoNumbers).sorted()) 입니다!"
+        print(overlapNumber.components(separatedBy: ["[","]"]).joined())
+    } else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+
+callLottoNumbers()
+checkOverlapNumbers()
 
