edea00ff5e4ea76c0105426820c80ea736518a6a
parkmuri <smxhmu@naver.com>
2022-10-25 14:50:35 +0900

    lotto맞추기

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: - 로또
+// MARK: 로또 추첨기
+var lotto: Set<Int> = []
 
+while lotto.count < 6 {
+    let lottoNumber = Int.random(in: 1...45)
+    //print(lottoNumber)
+    lotto.insert(lottoNumber)
+}
+
+print("이번 당첨 번호는 \(lotto)입니다.")
+
+
+// MARK: 내 번호와 맞추기
+
+let myLottoNumbers: Set<Int> = [1, 4, 9, 17, 29, 33]
+let intersection: Set<Int> = lotto.intersection(myLottoNumbers)
+let intersectionCount = intersection.count
+
+print("내가 찍은 번호는 \(myLottoNumbers)입니다.")
+if intersectionCount != 0 {
+    print("축하합니다! 겹치는 번호는 \(intersection) 입니다!")
+} else {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+}
