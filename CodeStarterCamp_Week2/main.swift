49cf24d4a31f855af5091233e160660fbf8691e6
김석우 <noix@gimseog-uui-MacBookPro.local>
2022-10-28 22:27:58 +0900

    Step2_2

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -20,8 +20,19 @@ let myLottoNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
 
 func compareLotteryNumber(_ winningLottoNumbers: Set<Int> , with myLottoNumbers: Set<Int>) {
     let lottoResult: Set<Int> = winningLottoNumbers.intersection(myLottoNumbers)
+    var resultCounter: Int = 1
     if lottoResult.count != 0{
-        print("축하합니다! 겹치는 번호는 \(lottoResult) 입니다")
+        print("축하합니다! 겹치는 번호는 ", terminator: "")
+        for lottoNumber in lottoResult{
+            if resultCounter < lottoResult.count{
+                print(lottoNumber, terminator: ", ")
+                resultCounter += 1
+            }
+            else{
+                print(lottoNumber, terminator: " ")
+            }
+        }
+        print("입니다!")
     }
     else{
         print("아쉽지만 겹치는 번호가 없습니다.")
