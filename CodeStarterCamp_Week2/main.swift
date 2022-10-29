867be5248f99a4499dd3842e5aa08a058275cbce
siwol <siwolssi@gmail.com>
2022-10-29 19:02:20 +0900

    STEP2 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,25 +8,26 @@
 
 import Foundation
 
-func winLottoNumbers() -> Set<Int> {
+func generateLottoNumbers() -> Set<Int> {
     var lottoNumbers: Set<Int> = Set<Int>()
     
     while lottoNumbers.count < 6 {
         let randomNumbers = Int.random(in: 1...45)
         lottoNumbers.insert(randomNumbers)
     }
+    
     return lottoNumbers
 }
 
 let myLottoNumbers: Set<Int> = [5, 19, 34, 25, 45, 23]
-let intersection: Set<Int> = winLottoNumbers().intersection(myLottoNumbers)
+let intersectionNumbers: Set<Int> = generateLottoNumbers().intersection(myLottoNumbers)
 
-func checkLottoNumbers() {
-    if intersection.count > 0 {
-        print("축하합니다! 겹치는 번호는 \(intersection.map {String($0)}.joined(separator: ", ")) 입니다!")
+func viewLottoResults() {
+    if intersectionNumbers.count > 0 {
+        print("축하합니다! 겹치는 번호는 \(intersectionNumbers.map {(intersection: Int) -> String in return String(intersection)}.joined(separator: ", ")) 입니다!")
     } else {
         print("아쉽지만 겹치는 번호가 없습니다.")
     }
 }
 
-checkLottoNumbers()
+viewLottoResults()
