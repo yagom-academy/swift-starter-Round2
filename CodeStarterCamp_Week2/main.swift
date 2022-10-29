2575c662a43bec26a9230b1d5070b92d84f6c439
김석우 <noix@gimseog-uui-MacBookPro.local>
2022-10-29 10:46:04 +0900

    Step2_3

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -12,26 +12,26 @@ func pickLotteryNumber() -> Set<Int> {
         let randomNumber = Int.random(in: 1...45)
         lotteryNumberSet.insert(randomNumber)
     }
-    return (lotteryNumberSet)
+    return lotteryNumberSet
 }
 
 let winningLottoNumbers: Set<Int> = pickLotteryNumber()
 let myLottoNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
 
-func compareLotteryNumber(_ winningLottoNumbers: Set<Int> , with myLottoNumbers: Set<Int>) {
-    let lottoResult: Set<Int> = winningLottoNumbers.intersection(myLottoNumbers)
-    var resultCounter: Int = 1
-    if lottoResult.count != 0{
+func returnJoinedLotteryResult(lotteryResult: Set<Int>) -> String {
+    var arrayLotteryResult: Array<String> = []
+    for lotteryNumber in lotteryResult {
+        arrayLotteryResult.append(String(lotteryNumber))
+    }
+    let joinedLotteryResult: String = arrayLotteryResult.joined(separator: ", ")
+    return joinedLotteryResult
+}
+
+func compareLotteryNumber(_ winningLottoNumbers: Set<Int>, with myLottoNumbers: Set<Int>) {
+    let lotteryResult: Set<Int> = winningLottoNumbers.intersection(myLottoNumbers)
+    if lotteryResult.count != 0 {
         print("축하합니다! 겹치는 번호는 ", terminator: "")
-        for lottoNumber in lottoResult{
-            if resultCounter < lottoResult.count{
-                print(lottoNumber, terminator: ", ")
-                resultCounter += 1
-            }
-            else{
-                print(lottoNumber, terminator: " ")
-            }
-        }
+        print(returnJoinedLotteryResult(lotteryResult: lotteryResult), terminator: " ")
         print("입니다!")
     }
     else{
@@ -39,4 +39,4 @@ func compareLotteryNumber(_ winningLottoNumbers: Set<Int> , with myLottoNumbers:
     }
 }
 
-compareLotteryNumber(_: winningLottoNumbers, with: myLottoNumbers)
+compareLotteryNumber(winningLottoNumbers, with: myLottoNumbers)
