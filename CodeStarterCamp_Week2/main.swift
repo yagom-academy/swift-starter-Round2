732c425f8b78bd9c264b5df006dc3f5a302e4dc5
yijiye <jiyee9932@daum.net>
2022-10-29 15:58:58 +0900

    [] 없이 번호 추출하기 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,10 +9,11 @@
 import Foundation
 
 
-var lottoNumber: Set<Int> = Set<Int>()
+
 
 // 로또번호 생성하는 함수
 func makeLottoNumbers() -> Set<Int> {
+    var lottoNumber: Set<Int> = Set<Int>()
     while lottoNumber.count < 7 {
         let randomNumber = Int.random(in: 1...45)
         lottoNumber.insert(randomNumber)
@@ -24,39 +25,17 @@ let winningNumbers: Set<Int> = makeLottoNumbers()
 var myLottoNumbers: Set<Int> = [2, 18, 5, 28, 12, 8]
 
 func checkLottoNumbers() {
-        let intersection: Set<Int> = winningNumbers.intersection(myLottoNumbers)
-    if intersection.count > 0 {
-        print("축하합니다! 겹치는 번호는 \(intersection) 입니다!")
-    }
-    else if intersection.count == 0 {
+    let lottoNumbers = Array( winningNumbers.intersection(myLottoNumbers))
+    let stringOverlapNumbers = lottoNumbers.map {String($0)}
+    let overlapNumbers = stringOverlapNumbers.joined(separator: ", ")
+    if lottoNumbers.count > 0 {
+        print("축하합니다! 겹치는 번호는", overlapNumbers, "입니다!")
+    } else if lottoNumbers.count == 0 {
         print("아쉽지만 겹치는 번호가 없습니다.")
-        }
     }
+}
 
 checkLottoNumbers()
 
-//optional ()! 사용
-let lottoNumberArray: Array<Int> = Array<Int>(1...45)
-var luckyNumber: Set<Int> = Set<Int>()
 
-func makeLottoNumber() -> Set<Int> {
-    while luckyNumber.count < 7 {
-        let randomNumber = lottoNumberArray.randomElement()!
-        luckyNumber.insert(randomNumber)
-    }
-    return luckyNumber
-}
 
-let winningNumbers2: Set<Int> = makeLottoNumber()
-var yourLottoNumbers: Set<Int> = [2, 18, 5, 28, 12, 8]
-
-func confirmLottoNumbers() {
-        let intersection: Set<Int> = winningNumbers2.intersection(yourLottoNumbers)
-    if intersection.count > 0 {
-        print("축하합니다! 겹치는 번호는 \(intersection) 입니다!")
-    }
-    else if intersection.count == 0 {
-        print("아쉽지만 겹치는 번호가 없습니다.")
-        }
-    }
-confirmLottoNumbers()
---
+++ File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  CodeStarterCamp_Week2
+//
+//  Created by jiye Yi on 2022/10/29.
+//
+
+import Foundation
