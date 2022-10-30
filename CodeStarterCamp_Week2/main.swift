b4a8bba2eb642a4b587a4e513e1ed86fb5ab8014
Duckduck1596 <108049138+Duckduck1596@users.noreply.github.com>
2022-10-30 17:05:26 +0900

    코드 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -17,7 +17,6 @@ repeat { let randomInt = Int.random(in: 1...45)
     lottoNumbes.insert(randomInt)
 } while lottoNumbes.count < 6
 
-let overlapNumbers: Set<Int> = lottoNumbes.intersection(myLottoNumbers)
 
 func checkOverlapNumbers() {
     let overlapNumbers: Set<Int> = lottoNumbes.intersection(myLottoNumbers)
