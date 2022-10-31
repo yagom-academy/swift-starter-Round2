62272401e0d1b81f0fdc56f2fe0f5d7b6f328357
kimseongj <ksj8277@naver.com>
2022-10-31 14:20:52 +0900

    Step3

--- CodeStarterCamp_Week2/Step3.swift
+++ CodeStarterCamp_Week2/Step3.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-var lottoSeries = [Int: [String]]()
+var lottoSeries = [String: [String]]()
 
 func makeLottoSeries() {
     for seriesNumber in 1...5 { // 5회차까지 생성
@@ -9,7 +9,7 @@ func makeLottoSeries() {
             let randomNumber = Int.random(in: 1...45)
             lottoArr.append(String(randomNumber))
         }
-      lottoSeries[seriesNumber] = lottoArr
+      lottoSeries["\(seriesNumber)회차"] = lottoArr
     }
 }
 
@@ -19,7 +19,7 @@ func selectLottoSeries() {
     print("몇 회차를 찾으십니까? :", terminator: " ") // 회차 입력값을 받음
     let number = Int(readLine()!)!
     
-    if let safeLottoSeries = lottoSeries[number] {
+    if let safeLottoSeries = lottoSeries["\(number)회차"] {
         let lottoSeriesNumber = safeLottoSeries.joined(separator: ", ")
         print("\(number)회차의 로또 당첨 번호는 \(lottoSeriesNumber) 입니다", terminator: " ")
         
