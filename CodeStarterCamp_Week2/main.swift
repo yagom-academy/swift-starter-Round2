bf349a11399e3e1b2f49b5b971c3342e9c7f56e1
김석우 <noix@gimseog-uui-MacBookPro.local>
2022-10-31 19:45:23 +0900

    Step2_1

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -12,19 +12,22 @@ func saveLotteryNumber(roundNumber: Int, pickedNumber: Int){
         print("로또를 추첨한 횟수가 원하는 로또 회차의 횟수보다 적습니다")
     }
     else{
-        let lotteryAllDictionary: Dictionary<Int, Set<Int>> = picktimes(times: pickedNumber)
-        if let unwrappedLotteryNumber = lotteryAllDictionary[roundNumber] {
-            let joinedUnwrapedLotteryNumber: String = returnJoinedLotteryResult(lotteryResult: unwrappedLotteryNumber)
-            print("\(roundNumber)회차의 로또 당첨 번호는 \(joinedUnwrapedLotteryNumber)입니다.")
+        let round: String = "\(roundNumber)회차"
+        let lotteryDictionary: [String: Array<Int>] = picktimes(times: pickedNumber)
+        if let unwrappedLotteryNumber = lotteryDictionary[round] {
+            let unwrappedSet = Set(unwrappedLotteryNumber)
+            let joinedLottery = returnJoinedLotteryResult(unwrappedSet)
+            print("\(round)의 로또 당첨 번호는 \(joinedLottery)입니다.")
         }
     }
 }
 
-func picktimes(times: Int) -> Dictionary<Int, Set<Int>> {
-    var lotteryNumberDictionary: Dictionary<Int, Set<Int>> = [Int: Set<Int>]()
+func picktimes(times: Int) -> Dictionary<String, Array<Int>> {
+    var lotteryNumberDictionary: [String: Array<Int>] = [String: Array<Int>]()
     for pickCounter in 1...times {
-        let lotteryNumberSet: Set<Int> = pickLotteryNumber()
-        lotteryNumberDictionary[pickCounter] = lotteryNumberSet
+        let arrayLottery: Array<Int> = Array(pickLotteryNumber())
+        let pickTime: String = "\(pickCounter)회차"
+        lotteryNumberDictionary[pickTime] = arrayLottery
     }
     return lotteryNumberDictionary
 }
@@ -44,7 +47,7 @@ func pickLotteryNumber() -> Set<Int> {
 let winningLottoNumbers: Set<Int> = pickLotteryNumber()
 let myLottoNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
 
-func returnJoinedLotteryResult(lotteryResult: Set<Int>) -> String {
+func returnJoinedLotteryResult(_ lotteryResult: Set<Int>) -> String {
     var arrayLotteryResult: Array<String> = []
     for lotteryNumber in lotteryResult {
         arrayLotteryResult.append(String(lotteryNumber))
@@ -56,12 +59,13 @@ func returnJoinedLotteryResult(lotteryResult: Set<Int>) -> String {
 func compareLotteryNumber(_ winningLottoNumbers: Set<Int>, with myLottoNumbers: Set<Int>) {
     let lotteryResult: Set<Int> = winningLottoNumbers.intersection(myLottoNumbers)
     if lotteryResult.count != 0 {
-        let sameLotteryNumber = returnJoinedLotteryResult(lotteryResult: lotteryResult)
+        let sameLotteryNumber = returnJoinedLotteryResult(lotteryResult)
         print("축하합니다! 겹치는 번호는 \(sameLotteryNumber) 입니다!")
     }
     else{
         print("아쉽지만 겹치는 번호가 없습니다.")
     }
 }
+
 
 
