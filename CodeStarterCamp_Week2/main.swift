62a56cf0bfd2b818c34d44b476fc508e03f299c7
parkmuri <smxhmu@naver.com>
2022-11-01 12:11:18 +0900

    step3

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -10,36 +10,57 @@ import Foundation
 
 // MARK: - 로또
 // MARK: 로또 추첨기
-var lotto: Set<Int> = []
-while lotto.count < 6 {
-    let lottoNumber = Int.random(in: 1...45)
-    lotto.insert(lottoNumber)
+func makeLotto() -> Set<Int> {
+    var lotto: Set<Int> = []
+    while lotto.count < 6 {
+        let lottoNumber = Int.random(in: 1...45)
+        lotto.insert(lottoNumber)
+    }
+    return lotto
+}
+
+// MARK: 로또번호 저장
+func saveLottoNums() -> Dictionary<String, [Int]>{
+    var winNumsHistory: [String : [Int]] = [:]
+    for n in 1...5{
+        let round = "\(n)회차"
+        let lotto = makeLotto()
+        winNumsHistory[round] = lotto.sorted()
+    }
+
+    return winNumsHistory
+}
+
+// MARK: 로또번호 출력함수
+func printWinNums(array: [Int]) {
+    for i in array {
+        if i != array[array.count - 1] {
+            print(i, terminator: ", ")
+        }else {
+            print(i, terminator: " ")
+        }
+    }
+}
+
+// MARK: 회차로 로또 당첨번호 찾기
+func searchWinNum(round: Int) {
+    let winNumsHistory: Dictionary<String, [Int]> = saveLottoNums()
+    let round = "\(round)회차"
+    
+    for item in winNumsHistory {
+        print(item)
+    }
+    
+    if let search = winNumsHistory[round] {
+        print("\(round)회차의 로또 당첨 번호는", terminator: " ")
+        printWinNums(array: search)
+        print("입니다")
+    } else {
+        print("\(round)회차가 진행되지 않았거나 저장된 당첨 번호가 없습니다")
+    }
 }
 
 
-// MARK: 내가 찍은 번호
-let myLottoNumbers: [Int] = [1, 4, 9, 17, 29, 33]
-
-
-// MARK: 내 번호와 맞추기 - 반복문&joined
-let intersection: Set<Int> = lotto.intersection(myLottoNumbers)
-let intersectionCount = intersection.count
-let intersectionSorted = intersection.sorted()
-
-var winNumsArray: [String] = []
-
-for winNums in intersectionSorted {
-    let winNumsToStr : String = String(winNums)
-    winNumsArray.append(winNumsToStr)
-}
-
-
-// MARK: 출력 부분
-print("이번 당첨 번호는 \(lotto)입니다.")
-print("내가 찍은 번호는 \(myLottoNumbers)입니다.")
-
-if intersectionCount != 0 {
-    print("축하합니다! 겹치는 번호는 \(winNumsArray.joined(separator: ", ")) 입니다!")
-} else {
-    print("아쉽지만 겹치는 번호가 없습니다.")
-}
+// MARK: 출력
+saveLottoNums()
+searchWinNum(round: 3)
