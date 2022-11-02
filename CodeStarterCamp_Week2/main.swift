ca35bc70ec843846a79d0a80ebf79d7588e97723
minsu3007 <minsu3007@gmail.com>
2022-11-02 22:48:30 +0900

    2022.11.2.STEP2 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,44 +8,47 @@
 
 import Foundation
 
-func makeLottoNumber() -> [Int] {
+func makeLottoNumber() -> Set<Int> {
     var numbers = Set<Int>()
     
     while numbers.count < 6 {
         numbers.insert(Int.random(in: 1...45))
     }
     
-    return Array(numbers)
+    return numbers
 }
 
-func checkCorrectNumber(myLottoNum: [Int], lottoNum: [Int]) -> [Int] {
-    var corretNumber: [Int] = []
+func checkCorrectNumber(myLottoNum: [Int], lottoNum: Set<Int>) -> [Int] {
+    let myLotto = Set(myLottoNum)
+    let correctNumber = myLotto.intersection(lottoNum)
     
-    for number in lottoNum {
-        if myLottoNum.contains(number) {
-            corretNumber.append(number)
+    return Array(correctNumber).sorted()
+}
+
+func printCorrectNumber(correctNum: [Int]) {
+    if correctNum.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는", terminator: " ")
+        for number in correctNum {
+            if correctNum.last != number {
+                print("\(number)", terminator: ", ")
+            } else {
+                print("\(number)", terminator: " ")
+            }
         }
+        print("입니다!")
     }
-    
-    return corretNumber.sorted()
+}
+
+func checkMyLottoNumber(myLottoNumber: [Int]) {
+    let randomLottoNumbers = makeLottoNumber()
+    let correctNumber = checkCorrectNumber(myLottoNum: myLottoNumbers, lottoNum: randomLottoNumbers)
+
+    printCorrectNumber(correctNum: correctNumber)
 }
 
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-let randomLottoNumbers = makeLottoNumber()
-var correctNumber: [Int] = checkCorrectNumber(myLottoNum: myLottoNumbers, lottoNum: randomLottoNumbers)
 
-if correctNumber.isEmpty {
-    print("아쉽지만 겹치는 번호가 없습니다.")
-} else {
-    //축하합니다! 겹치는 번호는 1, 2, 3, 4, 5, 6 입니다!
-    print("축하합니다! 겹치는 번호는", terminator: " ")
-    for number in correctNumber {
-        if correctNumber.last != number {
-            print("\(number)", terminator: ", ")
-        }
-        else {
-            print("\(number)", terminator: " ")
-        }
-    }
-    print("입니다!")
-}
+checkMyLottoNumber(myLottoNumber: myLottoNumbers)
+
