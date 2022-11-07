40e19896ca2afda5f34b651d90057945f928129f
karmanx <wjdchl@ewhain.net>
2022-11-08 01:08:23 +0900

    Week2 [STEP 2] 아샷추

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,7 +6,33 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
+func generateLottoNumbers() -> Set<Int> {
+    var lottoNumbers: Set<Int> = Set<Int>()
+    
+    while lottoNumbers.count < 6 {
+        let randomNumber = Int.random(in: 1...45)
+        lottoNumbers.insert(randomNumber)
+    }
+    
+    return lottoNumbers
+}
 
-print("Hello, World!")
+func checkLottoResult() {
+    let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+    var resultNumbers: Array<Int> = []
 
+    for number in generateLottoNumbers() {
+        if myLottoNumbers.contains(number) {
+            resultNumbers.append(number)
+        }
+    }
+    
+    if resultNumbers.count == 0 {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        let lottoResult = resultNumbers.map({ (value: Int) -> String in return String(value) }).joined(separator: ", ")
+        print("축하합니다! 겹치는 번호는 \(lottoResult) 입니다!")
+    }
+}
+
+checkLottoResult()
