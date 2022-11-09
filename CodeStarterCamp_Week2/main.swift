5b3c7fc7294a6ad6b1a6da8c396adb58a09a49ea
Duckduck1596 <108049138+Duckduck1596@users.noreply.github.com>
2022-11-09 12:30:40 +0900

    week2 step3 upload

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -10,41 +10,61 @@ import Foundation
 
 
 
-
 var myLottoNumbers: [Int] = [8, 10, 19, 21, 29, 37]
-var lottoNumbes: Set<Int> = []
+var lottoNumbers: Set<Int> = []
 
 
 repeat {
-    let randomInt = Int.random(in: 1...45)
-    lottoNumbes.insert(randomInt)
-} while lottoNumbes.count < 6
+   let randomInt = Int.random(in: 1...45)
+   lottoNumbers.insert(randomInt)
+} while lottoNumbers.count < 6
 
-let overlapNumbers: Set<Int> = lottoNumbes.intersection(myLottoNumbers)
+let overlapNumbers: Set<Int> = lottoNumbers.intersection(myLottoNumbers)
+
+func deleteBracket(numbers: Set<Int>) {
+    var printednumbers = 0
+    for number in numbers {
+        
+        if printednumbers < numbers.count - 1 {
+            print(number, terminator: ", ")
+            
+            printednumbers = printednumbers + 1
+            
+        } else {
+            print(number, terminator: " 입니다!.\n")
+        }
+    }
+}
 
 func checkOverlapNumbers() {
-    let overlapNumbers =  lottoNumbes.intersection(myLottoNumbers)
-    var printednumbers = 0
     
     if overlapNumbers.count != 0 {
         print("축하합니다! 겹치는 번호는 ", terminator: "")
+        deleteBracket(numbers: overlapNumbers)
         
-        for number in overlapNumbers {
-            if printednumbers < overlapNumbers.count - 1 {
-                print(number, terminator: ", ")
-                
-                printednumbers = printednumbers + 1
-            } else {
-                print(number, terminator: " 입니다!")
-            }
+    } else {
+            print("아쉽지만 겹치는 번호가 없습니다.")
         }
-    }
-    else {
-        print("아쉽지만 겹치는 번호가 없습니다.")
-            }
-    }
-    
-checkOverlapNumbers()
+}
 
 
+var savedLottoNumbers = [String: Set<Int>]()
+var lottoRound = 0
+
+func saveLottoNumbers() {
+    lottoRound = lottoRound + 1
+    savedLottoNumbers["\(lottoRound)회차"] = lottoNumbers
+}
+
+for _ in 1...5 {
+    saveLottoNumbers()
+}
+
+var wantLottoRound = "1회차"
+
+if let numberOfRound = savedLottoNumbers[wantLottoRound]{
+    print("\(wantLottoRound)의 로또 당첨 번호는", terminator: "")
+
+    deleteBracket(numbers: numberOfRound)
+}
 
