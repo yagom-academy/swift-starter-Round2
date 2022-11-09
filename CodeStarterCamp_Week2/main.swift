578b99e57fbf571ca3f5208238cdcd201a318b5b
yegang <yeggang0602@gmail.com>
2022-11-09 15:33:26 +0900

    Step2 코드

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,10 +8,47 @@
 
 import Foundation
 
-var integers = [1, 2, 3]
-let people = ["yagom": 10, "eric": 15, "mike": 12]
+func createLottoNumbers() -> Set<Int> {
+    var lottoNumbers: Set<Int> = []
+    
+    while lottoNumbers.count < 6 {
+        lottoNumbers.insert((Int.random(in: 1...10)))
+    }
+    
+    return lottoNumbers
+}
 
-repeat {
-    integers.removeLast()
-    print(integers)
-} while integers.count > 0
+func compareLottoNumbers(lottoNumbers: Set<Int>, myLottoNumbers: [Int]) -> Array<Int> {
+    var compareNumbers: [Int] = []
+    
+    for num in myLottoNumbers {
+        if lottoNumbers.contains(num) {
+            compareNumbers.append(num)
+        }
+    }
+    
+    return compareNumbers
+}
+
+func printLottoNumbers(compareNumber: [Int]) {
+    if compareNumber.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는 ", terminator: "")
+        for num in compareNumber {
+            if num == compareNumber[0] {
+                print("\(num)", terminator: "")
+            } else {
+                print(", \(num)", terminator: "")
+            }
+        }
+        print(" 입니다!")
+    }
+}
+
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+let lottoNumbers = createLottoNumbers()
+var compareNumbers = compareLottoNumbers(lottoNumbers: lottoNumbers, myLottoNumbers: myLottoNumbers)
+compareNumbers = compareNumbers.sorted()
+
+printLottoNumbers(compareNumber: compareNumbers)
