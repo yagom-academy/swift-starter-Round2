8c68d8b7ffbe900a80d9ca24faedd2c5feecea6f
whk1140 <whk1140@gmail.com>
2022-11-10 23:53:17 +0900

    Step 2: lottoCreate revise

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,10 +1,13 @@
 import Foundation
 
+let myLottoNumbers: [Int] = [1,2,3,4,5,6]
 var lottoNumber: [Int] = []
+var winNumber: [Int] = []
+let changeString = ""
 
 func createLotto() {
     while true {
-        let randomNumber = Int.random(in: 1..<46)
+        let randomNumber = Int.random(in: 1..<10)
 
         if lottoNumber.count == 6 {
             break
@@ -15,5 +18,23 @@ func createLotto() {
 
     }
 }
+
+func compareWithNumber() {
+    
+    for number in 0...5 {
+        if lottoNumber.contains(myLottoNumbers[number]) == true {
+            winNumber.append(myLottoNumbers[number])
+        }
+    }
+    
+    if winNumber.count == 0 {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        let stringArray = winNumber.map { String($0) }
+        print("축하합니다! 겹치는 번호는 \(stringArray.joined(separator: ",") ) 입니다!")
+    }
+    
+}
+
 createLotto()
-print(lottoNumber)
+compareWithNumber()
