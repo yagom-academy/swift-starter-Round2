55c5822e55bd816af6ec855ad56d266fe7789678
karmanx <wjdchl@ewhain.net>
2022-11-10 20:47:41 +0900

    Week2 [STEP 3] 아샷추 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -20,13 +20,13 @@ func storeLottoNumbers() -> Dictionary<String, Array<Int>> {
 }
 
 func printLottoNumbers(myRound: Int) {
-    let lottoDict = storeLottoNumbers()
+    let lottoDictionary = storeLottoNumbers()
     
-    if let myRoundLottoNumbers: Array<Int> = lottoDict["\(myRound)회차"] {
-        let myRoundLottoNums = myRoundLottoNumbers.map{ String($0) }.joined(separator: ", ")
-        print("\(myRound)회차의 로또 당첨 번호는 \(myRoundLottoNums) 입니다")
+    if let myRoundLottoNumbers: Array<Int> = lottoDictionary["\(myRound)회차"] {
+        let myRoundJoinedLottoNumbers = myRoundLottoNumbers.map { String($0) }.joined(separator: ", ")
+        print("\(myRound)회차의 로또 당첨 번호는 \(myRoundJoinedLottoNumbers) 입니다")
     } else {
-        print("nil")
+        print("해당 회차가 없습니다")
     }
 }
 
