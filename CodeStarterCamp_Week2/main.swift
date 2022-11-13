3f4207d5868e6f03e36c51a67e8406b7690847b6
yegang <yeggang0602@gmail.com>
2022-11-13 22:17:11 +0900

    STEP3 진행중

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+// Mark: - 로또 번호 생성
+func makeLottoNumbers() -> Set<Int> {
+    var lottoNumbers: Set<Int> = []
+    
+    while lottoNumbers.count < 6 {
+        lottoNumbers.insert((Int.random(in: 1...45)))
+    }
+    return lottoNumbers
+}
+
+// Makr: - 회차 번호 생성
+func makeLottoByRounds() -> Dictionary<String, [Int]> {
+    var lottoDictionary: Dictionary<String, [Int]> = [String: [Int]]()
+    
+    for round in 1...5 {
+        let lottoNumbersArray = Array(makeLottoNumbers())
+        let round = "\(round)회차"
+        lottoDictionary[round] = lottoNumbersArray
+    }
+    return lottoDictionary
+}
+
+// Mark: -원하는 회차를 출력하는 함수
