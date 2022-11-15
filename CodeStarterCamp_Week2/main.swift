1d1b1c8d288fc4621e9062a131435a588a3c357e
망망 <mang@mangmang-ui-MacBookPro.local>
2022-11-16 01:37:15 +0900

    week2_step3

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,57 +1,69 @@
 //
-//  main.swift
+//  File.swift
 //  CodeStarterCamp_Week2
 //
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
+//  Created by 망망 on 2022/11/16.
 //
 
-let myLottoNumbers: [Int] = [1, 3, 10, 16, 30, 45]
-var lottoNumbers: [Int] = []
-var equalNumber: [Int] = []
+import Foundation
 
+/* 기능목록
+ 1. 회차, 로또번호를 저장하는 딕셔너리 타입 변수
+ 2. 로또번호 생성함수
+ 3. 로또번호 생성함수 호출 시 딕셔너리에 저장할 함수 (새로운 로또번호 시 회차번호 +1
+ 4. 로또 번호 5회 생성
+ 5. 원하는 회차의 로또번호 찾기 (옵셔널 값으로 안전하게 추출
+ 6. 원하는 회차 로또번호 출력
+ */
 
-// 로또 번호 생성
-func createNumber() {
-        while lottoNumbers.count < 6 {
-            let num = Int.random(in: 1...45)
-            if !lottoNumbers.contains(num) {
-                lottoNumbers.append(num)
-            }
-        }
-    print(lottoNumbers)
-}
+var lotto: Dictionary<String, Array<Int>> = [String: Array]()
 
-// 로또 번호 비교
-func compareLottoNumbers() {
-    for myLottoNumberCount in myLottoNumbers {
-        for lottoNumberCount in lottoNumbers {
-            if myLottoNumberCount == lottoNumberCount {
-                equalNumber.append(myLottoNumberCount)
-            }
+// 로또번호 생성
+//func createlotto() -> Array<Int> {
+//    createNumber()
+//    return lottoNumbers
+//}
+func createlotto() -> Array<Int> {
+    var lottoNumber: [Int] = []
+    while lottoNumber.count < 6 {
+        let number = Int.random(in: 1...45)
+        if !lottoNumber.contains(number) {
+            lottoNumber.append(number)
         }
     }
-    print(equalNumber)
+    return lottoNumber
 }
 
-// 겹치는 번호 출력
-func printNumber() {
-    let equalNumberString = equalNumber.map { String($0) }
-    let equalString = equalNumberString.joined(separator: ", ")
 
-    if !equalNumber.isEmpty {
-        print("축하합니다! 겹치는 번호는 \(equalString) 입니다!")
-    } else {
-        print("아쉽지만 겹치는 번호가 없습니다.")
+// 로또번호, 회차 딕셔너리 저장
+func lottoTimes(count: Int) {
+    for countNumber in 1...count {
+        lotto.updateValue(createlotto(), forKey: "\(countNumber)회차")
     }
 }
 
-createNumber()
-compareLottoNumbers()
-printNumber()
+// 로또번호 딕셔너리 출력
+func outputLottoNumber() {
+    let sortedLotto = lotto.sorted { (first, second) in return first.key < second.key}
+    for (key, value) in sortedLotto {
+        print("\(key) : \(value)")
+    }
+}
+
+// 원하는 회차 출력
+func chooseLottoNumber() {
+    print("원하시는 회차를 입력하세요" , terminator: ": ")
+    if let choosenum = readLine() {
+        for (key, value) in lotto {
+            if key == "\(choosenum)회차" {
+                print("\(choosenum)회차의 로또 당첨 번호는 \(value) 입니다.")
+            }
+        }
+    }
+}
 
 
+lottoTimes(count: 5)
+outputLottoNumber()
+chooseLottoNumber()
 
-// map 메소드는 제공된 클로저를 각 행렬 항목마다 적용한 후, 새롭게 매핑된 값들이 원래 행렬의 해당 값들의 순서와 같도록 배치된 새 행렬을 반환합니다.
-// $0는 value의 다른 말로 축약형태로 이용되고 있음
-// https://zeddios.tistory.com/72
