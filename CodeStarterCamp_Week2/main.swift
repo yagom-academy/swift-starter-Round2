769a9afe214b6ed385520baf2730c5ed7f4b71c0
Charles <kch1759@naver.com>
2023-02-28 22:09:21 +0900

    Step2

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,36 @@
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+
+
+func drawLottoNumbers() -> Set<Int> {
+    var lottoNumbers: Set<Int> = Set<Int>()
+    
+    for var count in 1 ... 6 {
+        let randomNumber1to45 = Int.random(in: 1 ... 45)
+        
+        if lottoNumbers.contains(randomNumber1to45) {
+            count -= count
+            continue
+        }
+        lottoNumbers.insert(randomNumber1to45)
+    }
+    
+    return lottoNumbers
+}
+
+
+func compareLottoNumbers() {
+    let lottoNumber: Set<Int> = drawLottoNumbers()
+    
+    if !lottoNumber.intersection(Set(myLottoNumbers)).isEmpty {
+        let intersectionLottoNumbers = lottoNumber.intersection(Set(myLottoNumbers)).sorted().map {String($0)}.joined(separator: ", ")
+        print("축하합니다! 겹치는 번호는 \(intersectionLottoNumbers) 입니다!")
+    }else {
+        print("아쉽지만 겹치는 번호가 없습니다")
+    }
+}
+
+compareLottoNumbers()
 
