3067bbcf2365eb0e538ee2583f84f7f1a1b4353b
Hyeok <huk000808@gmail.com>
2023-03-01 15:52:04 +0900

    Week2 Step2 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,19 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week2
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
+var randomNumbers: Set<Int> = Set()
 
-import Foundation
+func creatRandomNumbers() -> Set<Int> {
+    while randomNumbers.count < 6 {
+        randomNumbers.insert(Int.random(in: 1...45))
+    }
+    return randomNumbers
+}
 
-print("Hello, World!")
+func compareNumbers(myLottoNumbers: Set<Int>) {
+    if myLottoNumbers == creatRandomNumbers() {
+        print("축하합니다! 겹치는 번호는 \(randomNumbers) 입니다!")
+    }
+    else {
+        print("아쉽지만 겹치는 번호는 없습니다.")
+    }
+}
 
+compareNumbers(myLottoNumbers: [4, 6, 8, 20, 25, 39])
