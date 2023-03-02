d0bd1bfdbfea85989d830b557b52f754d022f3df
Charles <kch1759@naver.com>
2023-03-02 10:53:15 +0900

    Step2 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -12,18 +12,12 @@ let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
 
 func drawLottoNumbers() -> Set<Int> {
-    var lottoNumbers: Set<Int> = Set<Int>()
-    
-    for var count in 1 ... 6 {
-        let randomNumber1to45 = Int.random(in: 1 ... 45)
-        
-        if lottoNumbers.contains(randomNumber1to45) {
-            count -= count
-            continue
-        }
-        lottoNumbers.insert(randomNumber1to45)
+    var lottoNumbers = Set<Int>()
+
+    while lottoNumbers.count < 6  {
+        lottoNumbers.insert(Int.random(in: 1 ... 45))
     }
-    
+
     return lottoNumbers
 }
 
@@ -31,10 +25,16 @@ func drawLottoNumbers() -> Set<Int> {
 func compareLottoNumbers() {
     let lottoNumber: Set<Int> = drawLottoNumbers()
     
-    if !lottoNumber.intersection(Set(myLottoNumbers)).isEmpty {
-        let intersectionLottoNumbers = lottoNumber.intersection(Set(myLottoNumbers)).sorted().map {String($0)}.joined(separator: ", ")
-        print("축하합니다! 겹치는 번호는 \(intersectionLottoNumbers) 입니다!")
-    }else {
+    let intersectionLottoNumbers = lottoNumber.intersection(Set(myLottoNumbers))
+    
+    if !intersectionLottoNumbers.isEmpty {
+        var winningNumbers = intersectionLottoNumbers.description
+        
+        winningNumbers.removeFirst()
+        winningNumbers.removeLast()
+        
+        print("축하합니다! 겹치는 번호는 \(winningNumbers) 입니다!")
+    } else {
         print("아쉽지만 겹치는 번호가 없습니다")
     }
 }
