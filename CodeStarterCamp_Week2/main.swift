2e3d48e094ead42831bdf20448390bf0cb7beb67
idinaloq <idinaloq@icloud.com>
2023-03-02 21:42:50 +0900

    week2_step2 PR사항 반영

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,38 +8,31 @@
 
 import Foundation
 
-var lottoNumbers: Set<Int> = Set<Int>()
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-//let myLottoNumbers: [Int] = [13, 25, 27, 31, 34, 35]
-var resultLottoNumbers: [Int] = []
-
-createRandomLottoNumbers()
-//print("생성된 로또 번호: \(lottoNumbers)")
 //print("선택한 로또 번호: \(myLottoNumbers)")
+//let myLottoNumbers: [Int] = [13, 25, 27, 31, 34, 35]
 compareLottoNumber()
 
-//찍은 번호와 로또 당첨번호 겹치는 숫자 확인 함수
+//찍은 번호와 로또 당첨번호 겹치는 숫자 확인하고 출력하는 함수
 func compareLottoNumber() {
-    let lottoNumbers = Array(lottoNumbers.sorted())
-//    print("정렬된 로또 번호: \(lottoNumbers)")
-    
-    for a in 0...5 {
-        for b in 0...5 {
-            if myLottoNumbers[a] == lottoNumbers[b] {
-                resultLottoNumbers.append(myLottoNumbers[a])
+    var resultLottoNumbers: [Int] = []
+    var sameLottoNumbers: String = ""
+    let lottoNumbers = createRandomLottoNumbers()
+//    print("생성된 로또 번호: \(lottoNumbers)")
+
+    for myLottoNum in myLottoNumbers {
+        for lottoNum in lottoNumbers {
+            if myLottoNum == lottoNum {
+                resultLottoNumbers.append(myLottoNum)
+                sameLottoNumbers += String(myLottoNum) + ", "
+                
             }
         }
     }
     
     if resultLottoNumbers.count != 0 {
-        print("축하합니다! 겹치는 번호는 ", terminator: "")
-        for a in 0...resultLottoNumbers.count - 1 {
-            print("\(resultLottoNumbers[a])", terminator: "")
-            if a != resultLottoNumbers.count - 1 {
-                print(", ", terminator: "")
-            }
-        }
-        print(" 입니다.")
+        print("축하합니다! 겹치는 번호는 \(sameLottoNumbers.dropLast(2)) 입니다.")
+        //print("축하합니다! 겹치는 번호는 \(sameLottoNumbers) 입니다.")
     }
     else {
         print("아쉽지만 겹치는 번호가 없습니다.")
@@ -47,13 +40,13 @@ func compareLottoNumber() {
     
 }
 
-    
 //로또 숫자 6개를 랜덤으로 생성하는 함수
-func createRandomLottoNumbers() -> Set<Int> {
-    while lottoNumbers.count < 6 {
-        lottoNumbers.insert(Int.random(in: 1...45))
-//        lottoNumbers.insert(Int.random(in: 1...10))
+func createRandomLottoNumbers() -> Array<Int> {
+    var setLottoNumbers: Set<Int> = Set<Int>()
+    while setLottoNumbers.count < 6 {
+        setLottoNumbers.insert(Int.random(in: 1...45))
+//        setLottoNumbers.insert(Int.random(in: 1...10))
     }
+    let lottoNumbers = Array(setLottoNumbers.sorted())
     return lottoNumbers
 }
-
