073b914f5da452ff7b01a13f610b865e9923a5f4
Toby Kim <yechan123456@gmail.com>
2023-03-02 16:16:30 +0900

    week2 step2 commit

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,27 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week2
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
+func makeRandomWinningLottoNumbers() -> [Int] {
+    var randomWinningLottoNumbers = Set<Int>()
+    while randomWinningLottoNumbers.count < 6 {
+        randomWinningLottoNumbers.insert(Int.random(in: 1...45))
+    }
+    return randomWinningLottoNumbers.sorted()
+}
 
-import Foundation
+func checkMyLottoNumbers(_ myLottoNumbers: [Int] = myLottoNumbers) {
+    var sameNumbers: [Int] = []
+    for nums in myLottoNumbers {
+        if winningLottoNumbers.contains(nums) == true {
+            sameNumbers.append(nums)
+        }
+    }
+    if sameNumbers.count >= 1 {
+        print("축하합니다! 겹치는 번호는\(sameNumbers) 입니다!")
+    } else if sameNumbers.count == 0 {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [9, 11, 20, 30, 32, 33]
+var winningLottoNumbers = makeRandomWinningLottoNumbers()
+print("이번 주 당첨번호: \(winningLottoNumbers)")
+checkMyLottoNumbers(myLottoNumbers)
 
