42dbac0df9cf3b8c9530e260fa7a153aeef32407
Toby Kim <yechan123456@gmail.com>
2023-03-04 08:47:40 +0900

    week2 step2 commit ver1.1

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,27 +1,26 @@
-func makeRandomWinningLottoNumbers() -> [Int] {
-    var randomWinningLottoNumbers = Set<Int>()
-    while randomWinningLottoNumbers.count < 6 {
-        randomWinningLottoNumbers.insert(Int.random(in: 1...45))
+func makeWinningLottoNumbers() -> [Int] {
+    var winningLottoNumbers = Set<Int>()
+    while winningLottoNumbers.count < 6 {
+        winningLottoNumbers.insert(Int.random(in: 1...45))
     }
-    return randomWinningLottoNumbers.sorted()
+    return winningLottoNumbers.sorted()
 }
 
-func checkMyLottoNumbers(_ myLottoNumbers: [Int] = myLottoNumbers) {
+func checkMyLottoNumbers(myLottoNumbers: [Int]) {
+    let winningLottoNumbers = makeWinningLottoNumbers()
+    print("이번 당첨번호: \(winningLottoNumbers)")
     var sameNumbers: [Int] = []
+    
     for nums in myLottoNumbers {
         if winningLottoNumbers.contains(nums) == true {
             sameNumbers.append(nums)
         }
     }
     if sameNumbers.count >= 1 {
-        print("축하합니다! 겹치는 번호는\(sameNumbers) 입니다!")
+        print("축하합니다! 겹치는 번호는 \(sameNumbers.map {String($0)}.joined(separator: ",")) 입니다!")
     } else if sameNumbers.count == 0 {
         print("아쉽지만 겹치는 번호가 없습니다.")
     }
 }
 
-let myLottoNumbers: [Int] = [9, 11, 20, 30, 32, 33]
-var winningLottoNumbers = makeRandomWinningLottoNumbers()
-print("이번 주 당첨번호: \(winningLottoNumbers)")
-checkMyLottoNumbers(myLottoNumbers)
-
+checkMyLottoNumbers(myLottoNumbers: [9, 11, 20, 30, 32, 33])
