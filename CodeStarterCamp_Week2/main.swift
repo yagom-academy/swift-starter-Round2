499026dda4b20472694af832ffec755a43c95e48
redmango1446 <redmango1446@redmango1446ui-MacBookAir.local>
2023-03-03 12:28:21 +0900

    week2 step2 1.2v

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -27,7 +27,7 @@ func comparisonLottoNumbers(my number : [Int]) -> (matchingResult : Set<Int>, lo
     if number.count == 6 {
         matchingNumbers = Set<Int>(number).intersection(lottoNumber)
     } else {
-        matchingNumbers = [0] // matchingNumbers 변수에 멤버가 아예 없으면 로또번호와 일치하는 번호가 없다는 뜻으로도 해석됨으로 1~45사이의 값이 아닌 0을 넣어 반환하게함.
+        matchingNumbers = [0]
     }
     
     return (matchingNumbers, lottoNumber)
