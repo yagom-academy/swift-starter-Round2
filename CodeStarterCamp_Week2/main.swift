fadc456ed4711f94dcd6126792817263164a9422
Charles <kch1759@naver.com>
2023-03-03 14:21:34 +0900

    Step3

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+var lottoRound: [Int: [Int]] = [:]
 
 
 func drawLottoNumbers() -> Set<Int> {
@@ -21,14 +21,24 @@ func drawLottoNumbers() -> Set<Int> {
     return lottoNumbers
 }
 
+func makeLottoRound(lottoNumbers: Set<Int>, round: Int) {
+    
+    let lottoNumbers = lottoNumbers.sorted()
+    
+    for count in 1 ... round {
+        lottoRound[count] = lottoNumbers
+    }
+    
+}
 
-func compareLottoNumbers() {
+
+func compareLottoNumbers(myLottoNumbers: [Int]) {
     let lottoNumber: Set<Int> = drawLottoNumbers()
     
     let intersectionLottoNumbers = lottoNumber.intersection(Set(myLottoNumbers))
     
     if !intersectionLottoNumbers.isEmpty {
-        var winningNumbers = intersectionLottoNumbers.description
+        var winningNumbers = intersectionLottoNumbers.sorted().description
         
         winningNumbers.removeFirst()
         winningNumbers.removeLast()
@@ -39,5 +49,20 @@ func compareLottoNumbers() {
     }
 }
 
-compareLottoNumbers()
+func printLotto(roundNumber: Int) {
+    guard let lottoNumbers = lottoRound[roundNumber] else {
+        print("잘못된 회차를 입력했습니다")
+        return
+    }
+    
+    var winningNumbers = lottoNumbers.description
+    winningNumbers.removeFirst()
+    winningNumbers.removeLast()
+    
+    print("\(roundNumber)회차의 로또 당첨 번호는 \(winningNumbers) 입니다.")
+}
 
+
+
+makeLottoRound(lottoNumbers: drawLottoNumbers(), round: 5)
+printLotto(roundNumber: 1)
