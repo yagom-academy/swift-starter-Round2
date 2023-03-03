02456ddbf6b2894bcc99ca71779df0c085fa79cc
karen <karenrodanthe@icloud.com>
2023-03-03 12:53:58 +0900

    230303 12:53 순서도png파일 추가, Step2 코드 첫 작성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
+var lottoNumber: Set<Int> = [];
+let myLottoNumbers: [Int] = [1,2,3,4,5,6]
+var sameLottoNumbers = Set<Int>()
+var sameLottoNumbersChangeString = String()
+
+func createLottoNumbers() {
+    while lottoNumber.count < 6 {
+        lottoNumber.insert(Int.random(in:1...45))
+    }
+    print("이번 주 로또 번호 :",lottoNumber)
+}
+
+func compareLottoNumbers(confirmedNumber: Set<Int>, myLottoNumber: [Int]) {
+    sameLottoNumbers = confirmedNumber.intersection(myLottoNumber)
+    if sameLottoNumbers.count > 0 {
+        for stringCount in sameLottoNumbers {
+            sameLottoNumbersChangeString += String(stringCount) + ","
+        }
+        sameLottoNumbersChangeString.removeLast()
+        print("축하합니다! 겹치는 번호는 \(sameLottoNumbersChangeString)입니다!")
+    } else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+createLottoNumbers()
+compareLottoNumbers(confirmedNumber: lottoNumber, myLottoNumber: myLottoNumbers)
+
+
 
