8af10220a7f5151e6f2e45292a1b96ab2d259d0e
Hyeok <huk000808@gmail.com>
2023-03-03 16:51:14 +0900

    Week2 Step3 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,34 +9,24 @@ func addRandomNubers() -> Array<Int>{
     return randomNumbers
 }
 
-func wantLottoTurn(count: Int, wantTurn: Int, randomNubers: [Int]) {
+func addLottoTurn(count : Int) {
     for turn in 1...count {
-        lottoList["\(turn)회차"] = randomNubers
+        lottoList["\(turn)회차"] = addRandomNubers()
     }
-    print(lottoList)
+    print(lottoList)   // 회차번호끼리 값이 같은지에 대한 확인용
+}
+
+func wantLottoTurn(count: Int, wantTurn: Int, randomNubers: [Int]) {
+    addLottoTurn(count: count)
+    
     if let randomNubers = lottoList["\(wantTurn)회차"] {
-            print("\(wantTurn)회차의 로또 당첨 번호는", randomNubers)
-              }
+           print("\(wantTurn)회차의 로또 당첨 번호는", randomNubers)
+    }
     else {
-        print("아직 진행되지 않았습니다.")
+        print("원하시는 회차는 아직 진행되지 않았습니다.")
     }
 }
 
-//func addLottoTurn(count: Int) {
-//
-//    for turn in 1...count {
-//        lottoList[turn] = randomNumbers
-//    }
-//    print(lottoList)
-//}
-//
-//func findLottoTurn(wantTurn: Int) {
-//    if let wantList = lottoList[wantTurn] {
-//
-//    }
-//}
-
-
-wantLottoTurn(count: 10, wantTurn: 4, randomNubers: addRandomNubers())
+wantLottoTurn(count: 5, wantTurn: 3, randomNubers: addRandomNubers())
 
 
