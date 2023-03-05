e9eb4bf7c26cbf97ed5b5c4562ef51f6439e7f61
deanidh <deanidh1011@gmail.com>
2023-03-06 02:52:02 +0900

    feat: 로또 당첨 번호 생성 후 대조 확인 기능 추가
    로또 당첨 번호 생성 함수와 Array를 String으로 바꾸는 함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,40 @@
 
 import Foundation
 
-print("Hello, World!")
+func createLottoNumbers() -> Set<Int> {
+    var lottoNumbers: Set<Int> = []
+    while lottoNumbers.count < 6 {
+        let randomInt = Int.random(in: 1...45)
+        lottoNumbers.insert(randomInt)
+    }
+    return lottoNumbers
+}
+
+func changeNumbersToString(from numbers: [Int]) -> String {
+    var stringNumbers = ""
+    for i in 0...(numbers.count - 1) {
+        if i == (numbers.count - 1) {
+            stringNumbers.append("\(numbers[i])")
+        } else {
+            stringNumbers.append("\(numbers[i]), ")
+        }
+    }
+    return stringNumbers
+}
+
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+
+var sameNumbers: [Int] = []
+
+for i in 0...5 {
+    if createLottoNumbers().contains(myLottoNumbers[i]) {
+        sameNumbers.append(myLottoNumbers[i])
+    }
+}
+
+if sameNumbers.isEmpty {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+} else {
+    print("축하합니다! 겹치는 번호는 \(changeNumbersToString(from: sameNumbers)) 입니다!")
+}
 
