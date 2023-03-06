8e11a90b63c63ccb310be1a731c53b0a319dc203
Hyeok <huk000808@gmail.com>
2023-03-06 10:44:45 +0900

    main 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,6 +1,6 @@
 var lottoList: Dictionary<String, Array> = [String: Array<Int>]()
 
-func addRandomNubers() -> Array<Int>{
+func addRandomNumbers() -> Array<Int>{
     var randomNumbers: Array<Int> = []
     
     while randomNumbers.count < 6 {
@@ -11,22 +11,23 @@ func addRandomNubers() -> Array<Int>{
 
 func addLottoTurn(count : Int) {
     for turn in 1...count {
-        lottoList["\(turn)회차"] = addRandomNubers()
+        lottoList["\(turn)회차"] = addRandomNumbers()
     }
-    print(lottoList)   // 회차번호끼리 값이 같은지에 대한 확인용
 }
 
-func wantLottoTurn(count: Int, wantTurn: Int, randomNubers: [Int]) {
+func searchLottoTurn(count: Int, turn: Int, randomNumbers: [Int]) {
     addLottoTurn(count: count)
     
-    if let randomNubers = lottoList["\(wantTurn)회차"] {
-           print("\(wantTurn)회차의 로또 당첨 번호는", randomNubers)
+    if let randomNumbers = lottoList["\(turn)회차"] {
+        let stringArray = randomNumbers.map { String($0) }
+        let addSeparator = stringArray.joined(separator: ", ")
+        print("\(turn)회차의 로또 당첨 번호는 \(addSeparator) 입니다!")
     }
     else {
         print("원하시는 회차는 아직 진행되지 않았습니다.")
     }
 }
 
-wantLottoTurn(count: 5, wantTurn: 3, randomNubers: addRandomNubers())
+searchLottoTurn(count: 5, turn: 3, randomNumbers: addRandomNumbers())
 
 
