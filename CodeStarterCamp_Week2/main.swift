f5094e5b85935826e45a2bf76da4183f84d9a8ae
Tag <sea15510@gmail.com>
2023-03-07 19:17:45 +0900

    main.swift 파일 2차 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,9 +8,6 @@
 
 import Foundation
 
-let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-let myLottoNumbersSet: Set<Int> = Set(myLottoNumbers)
-
 func selectLottoNumbers() -> Set<Int> {
     var lottoNumbers: Set<Int> = []
     while lottoNumbers.count < 6 {
@@ -21,24 +18,23 @@ func selectLottoNumbers() -> Set<Int> {
     return lottoNumbers
 }
 
-func checkLottoNumbers(selectLottoNumbers: Set<Int>, myLottoNumbers: Set<Int>) {
-    let lottoNumbersSorted: [Int] = selectLottoNumbers.intersection(myLottoNumbers).sorted()
-//    let numbersIntersection: Set<Int> = collectNumbers.intersection(myNumbers)
-//    let numbersSorted = numbersIntersection.sorted()
-    if lottoNumbersSorted.count == 0 {
+func checkLottoNumbers(_ selectLottoNumbers: Set<Int>) {
+    let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+    let duplicationSortedLottoNumbers: [Int] = selectLottoNumbers.intersection(myLottoNumbers).sorted()
+    if duplicationSortedLottoNumbers.count == 0 {
         print("아쉽지만 겹치는 번호가 없습니다.")
     } else {
         print("축하합니다! 겹치는 번호는 ", terminator: "")
-        for numberIndex in 0...lottoNumbersSorted.count-1 {
-            let checkNumber = lottoNumbersSorted[numberIndex]
-            if numberIndex < lottoNumbersSorted.count-1 {
-                print("\(checkNumber), ", terminator: "")
+        for numberIndex in 0...duplicationSortedLottoNumbers.count-1 {
+            let lottoNumber = duplicationSortedLottoNumbers[numberIndex]
+            if numberIndex < duplicationSortedLottoNumbers.count-1 {
+                print("\(lottoNumber), ", terminator: "")
             } else {
-                print("\(checkNumber) ", terminator: "")
+                print("\(lottoNumber) ", terminator: "")
             }
         }
         print("입니다!")
     }
 }
 
-checkLottoNumbers(selectLottoNumbers: selectLottoNumbers(), myLottoNumbers: myLottoNumbersSet)
+checkLottoNumbers(selectLottoNumbers())
