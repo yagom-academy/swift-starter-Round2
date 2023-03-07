9ebdeda3410c2e7731e03c78da7f41d02375a937
dayoung <celeste7297@naver.com>
2023-03-08 00:44:32 +0900

    main.swift

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,7 +6,40 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
+let myLottoNumbers: [Int] = [4, 31, 11, 7, 28, 45]
 
-print("Hello, World!")
+var lottoNumbers: Set<Int> = Set<Int>()
+
+func lottoPlease() -> Set<Int>{
+    repeat {
+        let lottoNumber = Int.random(in: 1...45)
+        lottoNumbers.insert(lottoNumber)
+        if lottoNumbers.contains(lottoNumber) {
+        } else {
+            lottoNumbers.insert(lottoNumber)
+        }
+    } while lottoNumbers.count < 6
+    return lottoNumbers
+}
+
+func checkLottoNumbers(numbers: Array<Int>) {
+    let sameNumbers: [Int] = Array(Set(myLottoNumbers).intersection(lottoNumbers))
+    if sameNumbers.count > 0 {
+        print("축하합니다! 겹치는 번호는 ", terminator: "")
+        for value in sameNumbers {
+            if value == sameNumbers[sameNumbers.count-1]{
+                print(value, terminator:" ")
+            } else {
+                print(value, terminator: ", ")
+            }
+        }
+        print("입니다!")
+    } else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+
+lottoPlease()
+checkLottoNumbers(numbers: myLottoNumbers)
+
 
