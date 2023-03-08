39946859e36cd74c32876aeb7708b2cdaedd13b6
dayoung <celeste7297@naver.com>
2023-03-08 18:27:25 +0900

    main.swift

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,26 +8,26 @@
 
 let myLottoNumbers: [Int] = [4, 31, 11, 7, 28, 45]
 
-var lottoNumbers: Set<Int> = Set<Int>()
-
-func lottoPlease() -> Set<Int>{
-    repeat {
+func makeLottoNumbers() -> Set<Int> {
+    var lottoNumbers: Set<Int> = Set<Int>()
+    while lottoNumbers.count < 6 {
         let lottoNumber = Int.random(in: 1...45)
         lottoNumbers.insert(lottoNumber)
-        if lottoNumbers.contains(lottoNumber) {
-        } else {
+        if lottoNumbers.contains(lottoNumber) == false {
             lottoNumbers.insert(lottoNumber)
         }
-    } while lottoNumbers.count < 6
+    }
     return lottoNumbers
 }
 
-func checkLottoNumbers(numbers: Array<Int>) {
+
+func checkLottoNumbers(_: Array<Int>) {
+    let lottoNumbers = makeLottoNumbers()
     let sameNumbers: [Int] = Array(Set(myLottoNumbers).intersection(lottoNumbers))
     if sameNumbers.count > 0 {
         print("축하합니다! 겹치는 번호는 ", terminator: "")
         for value in sameNumbers {
-            if value == sameNumbers[sameNumbers.count-1]{
+            if value == sameNumbers[sameNumbers.count - 1] {
                 print(value, terminator:" ")
             } else {
                 print(value, terminator: ", ")
@@ -39,7 +39,6 @@ func checkLottoNumbers(numbers: Array<Int>) {
     }
 }
 
-lottoPlease()
-checkLottoNumbers(numbers: myLottoNumbers)
+checkLottoNumbers(myLottoNumbers)
 
 
