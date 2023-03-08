2e02be25715150ccca581222bc39ccc9d94a498b
kunhojeong <121784465+kunhojeong@users.noreply.github.com>
2023-03-08 19:43:00 +0900

    로또당첨되라_v1.0

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [1, 7, 8, 9, 21, 31]
+var thisWeekLottoNumber: Array<Int> = []
+var winningNumber: Set<Int> = []
 
+func generateLottoNumber() -> Array<Int> {
+    var drawLotto: Array<Int> = []
+    
+    while drawLotto.count <= 6 {
+        drawLotto.append(Int.random(in: (1...45)))
+    }
+    return(drawLotto)
+}
+
+func compareLottoNumber(drawLotto: [Int]) {
+    for temp in myLottoNumbers {
+        if thisWeekLottoNumber.contains(temp) {
+            winningNumber.insert(temp)
+        }
+    }
+    switch winningNumber.count {
+    case 1 ..< 6:
+        print("축하합니다. 겹치는 번호는 \(winningNumber.sorted()) 입니다!")
+    default:
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+
+thisWeekLottoNumber = generateLottoNumber()
+compareLottoNumber(drawLotto: thisWeekLottoNumber)
