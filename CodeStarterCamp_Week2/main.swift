93adb14f11ae7d81d07ccb11c5a1fabd5de0dac6
Tag <sea15510@gmail.com>
2023-03-08 17:04:44 +0900

    week2_step3 v1.0 작성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,51 @@
 
 import Foundation
 
-print("Hello, World!")
+func selectLottoNumbers() {
+    var lottoNumbers: Set<Int> = []
+    while lottoNumbers.count < 6 {
+        let lottoNumber = Int.random(in: 1...45)
+        lottoNumbers.insert(lottoNumber)
+    }
+    
+    return saveLottoTimes(lottoNumbers: lottoNumbers)
+}
 
+//로또 회차별 당첨번호 저장
+var lottoTimes: Dictionary<String, Set<Int>> = [:]
+
+func saveLottoTimes(lottoNumbers: Set<Int>) {
+    if lottoTimes.count == 0 {
+        let times = 1
+        let time: String = "\(times)" + "회차"
+        lottoTimes[time] = lottoNumbers
+    } else {
+        let times = lottoTimes.count + 1
+        let time: String = "\(times)" + "회차"
+        lottoTimes[time] = lottoNumbers
+    }
+}
+
+func findLottoTimes(at findLottoTime: Int) {
+    if let lottoTime: Set<Int> = lottoTimes["\(findLottoTime)회차"] {
+        print("\(findLottoTime)회차의 로또 당첨 번호는 ", terminator: "")
+        for lottoNumberCount in 0...lottoTime.count - 1 {
+            let a = lottoTime.sorted()[lottoNumberCount]
+            if lottoNumberCount < lottoTime.count - 1 {
+                print("\(a), ", terminator: "")
+            } else {
+                print("\(a) ", terminator: "")
+            }
+        }
+        print("입니다.")
+    } else {
+        print("해당 회차가 존재하지 않습니다.")
+    }
+}
+
+selectLottoNumbers()
+selectLottoNumbers()
+selectLottoNumbers()
+selectLottoNumbers()
+selectLottoNumbers()
+findLottoTimes(at: 2)
