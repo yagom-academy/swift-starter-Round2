f36d3e4feb2c01d694f3b90410aad9744d562e6c
ny <nayeon6868@gmail.com>
2023-03-08 18:53:18 +0900

    swift-starter-week2

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -12,28 +12,25 @@ let myLottoNumbers: Set<Int> = [2,5,16,27,35,40]
 
 var randomLottoNumbers: Set<Int> = Set<Int>()
 
-func makeLottoSet() {
+func makeLottoNumbers() {
     while randomLottoNumbers.count <= 6 {
-        let randomInt = Int.random(in: 1...45)
-        randomLottoNumbers.insert(randomInt)
+        let randomNum = Int.random(in: 1...45)
+        randomLottoNumbers.insert(randomNum)
     }
-    print(randomLottoNumbers)
+    return
 }
 
-func checkSameNumbers() {
+func checkAndAlertSameLottoNumbers() {
     let sameNumbers : Set<Int> = myLottoNumbers.intersection(randomLottoNumbers)
-    
     switch sameNumbers.count {
     case 0:
         print("아쉽지만 겹치는 번호가 없습니다.")
     default:
-        let sameNumbersArray = Array(sameNumbers)
-        let arrayToStr = sameNumbersArray.map { String($0) }
-        let result = arrayToStr.joined(separator: ", ")
+        let sameNumbersList = Array(sameNumbers)
+        let result = sameNumbersList.map { String($0) }.joined(separator: ", ")
         print("축하합니다! 겹치는 번호는 \(result)입니다!")
     }
 }
 
-makeLottoSet()
-checkSameNumbers()
+checkAndAlertSameLottoNumbers()
 
