b3430be60a6273536dd4af95013d61d2bcd6a578
michael <heojunha3691@gmail.com>
2023-03-15 22:19:35 +0900

    step2 ver.1 출력전 for_1구문에서 오류발생

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,35 +9,53 @@
 import Foundation
 
 func getlotto() -> Set<Int>{
-
+    
     var lotto: Set<Int> = Set<Int>()
-
+    
     repeat {
-        lotto = Set<Int>()
-
-        for _ in 1...6 {
-            lotto.insert(Int.random(in: (1...45)))
-        }
-
+        
+        lotto.insert(Int.random(in: (1...45)))
+        
     } while (lotto.count < 6)
-
+  
     return lotto
+    
+}
 
+func getIntersectionLottoNumber() -> [Int]{
+    
+    let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+    var lottoNumber: Set<Int> = Set<Int> (myLottoNumbers)
+    
+    lottoNumber = lottoNumber.intersection(getlotto())
+    
+    var lottoIntersectionArray: [Int] = [Int](lottoNumber)
+    
+    lottoIntersectionArray = lottoIntersectionArray.sorted()
+   
+    
+    return lottoIntersectionArray
 }
 
 
 
+func printArrayMemberOneByOne() {
+    
+    let lottoNumberCount: Int = getIntersectionLottoNumber().count
+    for _ in 1...lottoNumberCount {
+        print("\(getIntersectionLottoNumber()[(lottoNumberCount - 1)]), ",terminator: "")
+    }
+}
+
 func printLottoResult() {
+    let lottoNumberCount: Int = getIntersectionLottoNumber().count
     
-    let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-   
-    var lottoNumber: Set<Int> = Set<Int> (myLottoNumbers)
-    
-    let lottoIntersection: Set<Int> = lottoNumber.intersection(getlotto())
-    
-    if lottoIntersection.count > 0 {
-        print("축하합니다! 겹치는 번호는 \(lottoIntersection) 입니다")
-    } else if lottoIntersection.count == 0 {
+    if lottoNumberCount > 0 {
+        print("축하합니다! 겹치는 번호는 ",terminator: "")
+        printArrayMemberOneByOne()
+        print("입니다")
+        
+    } else if lottoNumberCount == 0 {
         print("아쉽지만 겹치는 번호가 없습니다")
         
     }
