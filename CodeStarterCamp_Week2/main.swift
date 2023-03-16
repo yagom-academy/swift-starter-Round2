847cb9fb5ed74f330c4ca3d28d65919933e34482
Hot-ttu <ghkdwndud7@naver.com>
2023-03-16 22:14:37 +0900

    Week2 Step2 PR

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,39 @@
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumber: [Int] = [1, 2, 3, 4, 5, 6]
 
+func pickLottoNumber() -> Set<Int> {
+    var lottoNumber: Set<Int> = []
+    
+    while lottoNumber.count < 6 {
+        let randomNumber: Int = Int.random(in: 1...45)
+        lottoNumber.insert(randomNumber)
+    }
+    return lottoNumber
+}
+
+func isWinning(myLottoNumber: [Int], pickedLottoNumber: Set<Int>) {
+    var correctNumber: [Int] = []
+    for number in myLottoNumber {
+        if pickedLottoNumber.contains(number) {
+            correctNumber.append(number)
+        }
+    }
+    if correctNumber.count > 0 {
+        print("축하합니다! 겹치는 번호는 ", terminator: "")
+        for index in 0...correctNumber.count - 1 {
+            if index == correctNumber.count - 1 {
+                print(correctNumber[index], terminator: " ")
+                break
+            }
+            print(correctNumber[index], terminator: ", ")
+        }
+        print("입니다!")
+    } else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+
+let pickedLottoNumber: Set<Int> = pickLottoNumber()
+isWinning(myLottoNumber: myLottoNumber, pickedLottoNumber: pickedLottoNumber)
