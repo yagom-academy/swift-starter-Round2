9eca90d3abd191cf256bde565d8f31aecb8f85dd
김광준 <mskang@xingxingmobility.com>
2023-03-18 17:19:31 +0900

    Week2 [STEP 2] Kobe
     @Gundy93
     건디, 피드백 받은대로 수정했습니다 :)
    
     ## 고민했던 점
     ### 네이밍에 대해서 고민을 하게 되었습니다.
     - 네이밍에 대해서 조금 더 신중하게 생각하고 고민했습니다.
    
     ---
    
     ## 해결 되지 않은 점
     ### 해결이 됮 않은 점은 없었습니다.
    
     ---
    
     ## 조언을 얻고 싶은 부분
     ### 얼마남지 않은 기간 더 열심히 하겠습니다.
     - 조언보다는 지금 더 열심히 하겠다는 의지를 보여드리고 싶었습니다 :)

--- CodeStarterCamp_Week2/STEP2.swift
+++ CodeStarterCamp_Week2/STEP2.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Minseong Kang on 2023/03/16.
 //
-
 import Foundation
 
 func makeLottoNumbers() -> [Int] {
@@ -20,23 +19,21 @@ func makeLottoNumbers() -> [Int] {
 	return lottoNumbers
 }
 
-func matchedLottoNumbers(myLottoNumbers: [Int]) {
-	var matchLottoNumbers: [Int] = []
+func matchLottoNumbers(myLottoNumbers: [Int]) {
+	var matchedLottoNumbers: [Int] = []
 	let winnerLottoNumbers = makeLottoNumbers()
 	
 	for number in winnerLottoNumbers {
 		if myLottoNumbers.contains(number) == true {
-			matchLottoNumbers.append(number)
+			matchedLottoNumbers.append(number)
 		}
 	}
 	
-	let winnerNumbers = matchLottoNumbers.map { String($0) }.joined(separator: ", ")
+	let winnerNumbers = matchedLottoNumbers.map { String($0) }.joined(separator: ", ")
 	
-	if matchLottoNumbers.count >= 1 {
+	if matchedLottoNumbers.count >= 1 {
 		print("축하합니다! 겹치는 번호는 \(winnerNumbers) 입니다.")
 	} else {
 		print("아쉽지만 겹치는 번호가 없습니다.")
 	}
 }
-
-
--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,3 +9,4 @@
 import Foundation
 
 matchedLottoNumbers(myLottoNumbers: [1,2,3,4,5,6])
+
