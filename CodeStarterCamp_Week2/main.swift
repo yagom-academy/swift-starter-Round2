1c5b0d4ed34d5e7f3de39ef1068b050d5625d65b
madcow95 <lobasketve@naver.com>
2023-05-23 20:54:29 +0900

    WEEK2 STEP2

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,3 +9,12 @@
 import Foundation
 
 print("Hello, World!")
+/*
+var lotteryArray = Set<Int>()
+while lotteryArray.count != 6 {
+  if let randomNumber = (1...45).randomElement() {
+    lotteryArray.insert(randomNumber)
+  }
+}
+print(lotteryArray)
+*/
