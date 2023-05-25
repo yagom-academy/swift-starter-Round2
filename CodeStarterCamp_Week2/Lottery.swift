138f960d8a2c27052b1429a0d37afffdf8c0dfdd
madcow95 <lobasketve@naver.com>
2023-05-25 18:59:52 +0900

    WEEK2 STEP3

--- CodeStarterCamp_Week2/Lottery.swift
+++ CodeStarterCamp_Week2/Lottery.swift
@@ -24,3 +24,19 @@ func checkLottoNumber(picked: [Int], goal: [Int]) {
     let sameNumberList = sameNumbers.sorted().map{ String($0) }.joined(separator: ", ")
     print("축하합니다! 겹치는 번호는 \(sameNumberList) 입니다!")
 }
+
+func createLottoStorageByCount(count: Int) -> [String: [Int]] {
+    var lottoStorage: [String: [Int]] = [:]
+    for round in 1...count {
+        lottoStorage["\(round)회차"] = getLottoNumbers()
+    }
+    return lottoStorage
+}
+
+func showTargetRoundLotto(target: Int, lotto: [String: [Int]]) {
+    guard let targetRoundLotto = lotto["\(target)회차"] else {
+        print("\(target)회차의 로또번호가 없습니다.")
+        return
+    }
+    print("\(target)회차의 로또 당첨 번호는 \(targetRoundLotto.sorted().map{ String($0) }.joined(separator: ", ")) 입니다.")
+}
