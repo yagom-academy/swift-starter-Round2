995b51f159ebaa4d3a22105279e4a98e8168765b
norang0 <kyuheehong0@gmail.com>
2023-05-27 19:10:41 +0900

    feat: Week2 Step2 add lotto number picking functions

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,36 @@
 
 import Foundation
 
-print("Hello, World!")
+var numbersPool: Array<Int> = Array<Int>(1...45)
+var winningNumbers: Array<Int> = Array<Int>()
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+var matchingNumbers: Array<Int> = Array<Int>()
 
+func pickWinningNumbers() {
+    numbersPool.shuffle()
+    for i in 0...5 {
+        winningNumbers.append(numbersPool[i])
+    }
+}
+
+func compareLottoNumbers() {
+    for placeInMyNumbers in 0...5 {
+        for placeInWinningNumbers in 0...5 {
+            if myLottoNumbers[placeInMyNumbers] == winningNumbers[placeInWinningNumbers] {
+                matchingNumbers.append(myLottoNumbers[placeInMyNumbers])
+            }
+        }
+    }
+}
+
+func printLottoResult() {
+    if matchingNumbers.isEmpty == true {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는 \(matchingNumbers) 입니다!")
+    }
+}
+
+pickWinningNumbers()
+compareLottoNumbers()
+printLottoResult()
