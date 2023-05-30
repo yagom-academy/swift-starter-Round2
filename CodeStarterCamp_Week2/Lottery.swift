63ca443b7b9e74dbd621e897e8e3939735fc5041
Kim, Yerim <yerim343@naver.com>
2023-05-31 00:02:48 +0900

    :hammer: 변수명 수정 및 당첨 비교 함수 수정
    set의 intersection 사용. guard 에서 if 구문 사용으로 변경

--- CodeStarterCamp_Week2/Lottery.swift
+++ CodeStarterCamp_Week2/Lottery.swift
@@ -9,31 +9,30 @@ import Foundation
 
 // MARK: - 로또 번호 생성 함수
 func generateLotteryNum() -> Set<Int> {
-    var randomNumSet: Set<Int> = Set()
+    var randomNumberSet: Set<Int> = Set()
     
-    while randomNumSet.count < 6 {
+    while randomNumberSet.count < 6 {
         let randomNumber = Int.random(in: 1...45)
-        randomNumSet.insert(randomNumber)
+        randomNumberSet.insert(randomNumber)
     }
     
-    return randomNumSet
+    return randomNumberSet
 }
 
 
 // MARK: - 당첨 번호 확인 함수
+
 func compareLotteryNum(lottoNum: Set<Int>) {
     let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-    var matchNums: [Int] = []
+    var matchNumbers: Set<Int> = []
     
-    for num in lottoNum {
-        if myLottoNumbers.contains(num) {
-            matchNums.append(num)
-            continue
-        }
+    matchNumbers = lottoNum.intersection(myLottoNumbers)
+    
+    if matchNumbers.isEmpty {
+        return print("아쉽지만 겹치는 번호가 없습니다.")
     }
     
-    guard matchNums.isEmpty == false else { return print("아쉽지만 겹치는 번호가 없습니다.")}
-    
-    let matchString = matchNums.map { String($0) }.joined(separator: ", ")
+    let matchString = matchNumbers.map { String($0) }.joined(separator: ", ")
     print("축하합니다! 겹치는 번호는 \(matchString) 입니다!")
+    
 }
