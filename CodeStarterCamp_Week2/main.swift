dc41b62295d7d081de07ce43f21d569a265ebeb2
ltw <witlim1225@gmail.com>
2023-05-30 20:42:34 +0900

    WEEK2 [STEP2] Tony

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,41 @@
 
 import Foundation
 
-print("Hello, World!")
+func myLottoNumbers() -> Set<Int> {
+    var numbers: Set<Int> = []
+    
+    while numbers.count < 6 {
+        if let number = (1...45).randomElement() {
+            numbers.insert(number)
+        }
+    }
+    
+    return numbers
+}
+
+func checkLottoNumbers(_ myNumbers: Set<Int>, with winNumbers: Set<Int>) -> String {
+    
+    print("내가 찍은 번호 \(myNumbers)")
+    print("당첨 번호 \(winNumbers)")
+    
+    let failMsg = "아쉽지만 겹치는 번호가 없습니다."
+    let winner = Array(myNumbers.intersection(winNumbers)).sorted()
+    
+    guard winner.isEmpty == false else { return failMsg }
+    
+    var winnerMsg = "축하합니다! 겹치는 번호는"
+    for number in winner {
+        winnerMsg += " \(number),"
+    }
+    winnerMsg.removeLast()
+    winnerMsg += " 입니다!"
+    
+    return winnerMsg
+}
+
+let winNumbers: Set<Int> = [1,2,3,4,5,6]
+let failNumbers: Set<Int> = [46, 47, 48, 49, 50]
+
+print(checkLottoNumbers(myLottoNumbers(), with: winNumbers))
+
 
