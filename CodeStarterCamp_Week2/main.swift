7f518cce739d32b9154764ebf400665faa82e1ab
Ipjak <sciencerhgkr@gmail.com>
2023-05-31 01:38:11 +0900

    edit

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -12,14 +12,17 @@ let myLottoNumbers: [Int] = [1, 10, 20, 40, 25, 30]
 var winningNumbers: Set<Int> = Set<Int>()
 var duplicationNumber = [Int]()
 
-while(winningNumbers.count < 6){
-    winningNumbers.insert(Int.random(in: 1...45))
+func makeLottoNumbers() -> Set<Int>{
+    while(winningNumbers.count < 6){
+        winningNumbers.insert(Int.random(in: 1...45))
+    }
+    return winningNumbers
 }
 func checkNumbers() -> Set<Int>{
     //myLottoNumbers 겹치는 번호 비교 winningNumbers
     for number in 0...5{
-        if(myLottoNumbers.contains(Array(winningNumbers)[number])) == true{
-            duplicationNumber.append(Array(winningNumbers)[number])
+        if(myLottoNumbers.contains(Array(makeLottoNumbers())[number])) == true{
+            duplicationNumber.append(Array(makeLottoNumbers())[number])
         }
     }
     return Set(duplicationNumber)
@@ -28,5 +31,5 @@ if checkNumbers().isEmpty == true{
     print("아쉽지만 겹치는 번호가 없습니다.")
 }
 else{
-    print(checkNumbers().sorted())
+    print("축하합니다! 겹치는 번호는", checkNumbers().map{String($0)}.joined(separator:", "), "입니다!")
 }
