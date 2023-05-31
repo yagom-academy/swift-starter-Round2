f00d53f81282a477995dc77d7f66e967f2748414
JminSim <jeong0983@naver.com>
2023-05-31 17:41:12 +0900

    feat: step2 code

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,47 @@
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
+var lottoNumbers: Array<Int> = [Int]()
+
+var resultLotto: [Int] = [Int]()
+
+func makeLotto() {
+
+    repeat {
+        let num = Int.random(in: 1...45)
+
+        if lottoNumbers.contains(num){
+
+        } else {
+            lottoNumbers.append(num)
+        }
+
+    } while lottoNumbers.count < 6
+
+}
+
+func checkLotto() {
+    
+    var i = 0
+    
+    for _ in 1...6 {
+        
+        if lottoNumbers.contains(myLottoNumbers[i]) {
+            resultLotto.append(myLottoNumbers[i])
+        }
+        i = i + 1
+        
+    }
+    
+    if resultLotto.count > 0 {
+        print("축하합니다! 겹치는 번호는 \(resultLotto) 입니다!")
+    } else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+
+}
+
+makeLotto()
+checkLotto()
