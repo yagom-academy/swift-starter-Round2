067969d5071b17d53e622b2af0d681d2800b775b
mirue <leeminyeol@Lees-MacBook-Pro.local>
2023-06-01 19:10:38 +0900

    STEP3 첫번째

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,40 @@
 
 import Foundation
 
-print("Hello, World!")
+var lottoDictionary = [String : Set<Int>]()
+var numberFull: [Int] = Array<Int>(1...45)
+var lottoNumber: Set<Int> = Set<Int>()
+
+func pickRandomLottoNumber() -> Set<Int> {
+    var lottoNumber: Set<Int> = Set<Int>()
+    while lottoNumber.count < 6 {
+        lottoNumber.insert(Int.random(in: 1...45))
+    }
+    return lottoNumber
+}
+
+func storeLottoNumber(round: Int) {
+    lottoDictionary["\(round)회차"] = pickRandomLottoNumber()
+}
+
+func getLottoNumber() {
+    for i in 1...5 {
+        storeLottoNumber(round: i)
+    }
+}
+
+func searchLottoNumber(times: Int) {
+    if let lottoOptional = lottoDictionary["\(times)회차"] {
+        print("\(times)회차의 로또 당첨 번호는 \(lottoOptional.map { String($0) }.joined(separator: ", ")) 입니다.")
+    } else {
+        print("\(times)회차는 존재하지 않습니다.")
+    }
+}
+
+getLottoNumber()
+searchLottoNumber(times: 1)
+searchLottoNumber(times: 2)
+searchLottoNumber(times: 3)
+searchLottoNumber(times: 4)
+searchLottoNumber(times: 5)
 
