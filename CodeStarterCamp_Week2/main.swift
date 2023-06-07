671c362f4cb1567556d36ae6087d0f1d0baf63d4
JminSim <jeong0983@naver.com>
2023-06-08 08:45:29 +0900

    feat: code수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,12 +9,10 @@
 import Foundation
 
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-
 var lottoResult: [String: [Int]] = [String: [Int]]()
-
 var count: Int = 0
 
-func makeLottoNumbers() {
+func makeLottoNumbers() -> [Int] {
 
     var lottoNumbers: [Int] = [Int]()
     var randNum: Int
@@ -29,25 +27,51 @@ func makeLottoNumbers() {
         
     } while lottoNumbers.count < 6
     
-    count += 1
-    recordLottoResults(turn: String(count), lottoNumbers: lottoNumbers)
+    return lottoNumbers
+}
+
+
+func compareLotto(myLotto: [Int], Lotto: [Int]) {
+    
+    var resultNumbers: [Int] = [Int]()
+    
+    for numbers in Lotto {
+        if myLotto.contains(numbers) {
+            resultNumbers.append(numbers)
+        }
+    }
+    
+    if resultNumbers.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는 \(resultNumbers.map {String($0)}.joined(separator: ", ")) 입니다!")
+    }
     
 }
 
 func recordLottoResults(turn: String, lottoNumbers: [Int]) {
     
-  lottoResult[turn] = lottoNumbers
-   
+    lottoResult[turn] = lottoNumbers
+    
 }
 
+func printLottoResult(turn: String, lottoResults: [String:[Int]]) {
+    
+    if lottoResult[turn] == nil {
+        print("회차 정보가 없습니다.")
+    } else {
+        print("\(turn)회차의 로또 당첨 번호는", terminator: " ")
+        print("\((lottoResult[turn] ?? [0]).map {String($0)}.joined(separator: ", ")) 입니다")
+    }
+    
+}
 
 for _ in 1...5 {
-    makeLottoNumbers()
+    count += 1
+    recordLottoResults(turn: String(count), lottoNumbers: makeLottoNumbers())
 }
 
-var turn: String
+let turn: String
 print("원하는 회차를 입력하세요: ", terminator: "")
 turn = readLine() ?? ""
-
-print("\(turn)회차의 로또 당첨 번호는 \((lottoResult[turn] ?? [0]).map {String($0)}.joined(separator: ", ")) 입니다")
-
+printLottoResult(turn: turn, lottoResults: lottoResult)
