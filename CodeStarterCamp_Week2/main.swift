b74e24e6ef101461a40a75b332464a036273cf0d
Ipjak <sciencerhgkr@gmail.com>
2023-06-15 14:55:44 +0900

    first

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,31 +8,21 @@
 
 import Foundation
 
-let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-var winningNumbers: Set<Int> = Set<Int>()
-var duplicationNumber = [Int]()
-var dictionary = [String: Any]()
-
-func makeNumbers(){
-    while(winningNumbers.count < 6){
-        winningNumbers.insert(Int.random(in: 1...45))
-    }
-}
-func checkNumbers(){
-    for number in 0...5{
-        if(myLottoNumbers.contains(Array(winningNumbers)[number])) == true{
-            duplicationNumber.append(Array(winningNumbers)[number])
+var lottoNumbers = [Int : Set<Int>]()
+func makeLottoNumbers() {
+    var winngNumbers = Set<Int>()
+    for round in 1...5{
+        while (winngNumbers.count < 6) {
+            winngNumbers.insert(Int.random(in: 1...45))
         }
+        lottoNumbers[round] = winngNumbers
+        winngNumbers.removeAll()
     }
 }
-func saveWinningNumbers(round:Int){
-    dictionary = ["\(round)회차": Array(winningNumbers)]
-}
-for round in 1...5{
-    makeNumbers()
-    saveWinningNumbers(round: round)
-    if (round == 4){ // 원하는 회차 입력
-        print("\(round)회차의 로또 당첨 번호는 \(dictionary.values)입니다")
-    }
-    winningNumbers.removeAll()
+makeLottoNumbers()
+let round = 3
+if let ChekingOptional = lottoNumbers[round] {
+    print("\(round)회차의 로또 당첨 번호는 \(ChekingOptional.map{String($0)}.joined(separator:", ")) 입니다.")
+} else {
+    print("no value")
 }
