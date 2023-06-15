c02e49907c9015d487e3aea81d627b73c469e5e9
Ipjak <sciencerhgkr@gmail.com>
2023-06-16 00:05:31 +0900

    Last

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,20 +9,22 @@
 import Foundation
 
 var lottoNumbers = [Int : Set<Int>]()
-func makeLottoNumbers() {
-    var winngNumbers = Set<Int>()
+
+func makeFiveRoundLottoNumbers() {
     for round in 1...5{
+        var winngNumbers = Set<Int>()
         while (winngNumbers.count < 6) {
             winngNumbers.insert(Int.random(in: 1...45))
         }
         lottoNumbers[round] = winngNumbers
-        winngNumbers.removeAll()
     }
 }
-makeLottoNumbers()
+
+makeFiveRoundLottoNumbers()
 let round = 3
-if let ChekingOptional = lottoNumbers[round] {
-    print("\(round)회차의 로또 당첨 번호는 \(ChekingOptional.map{String($0)}.joined(separator:", ")) 입니다.")
+if let lottoNumber = lottoNumbers[round] {
+    print("\(round)회차의 로또 당첨 번호는 \(lottoNumber.map{String($0)}.joined(separator:", ")) 입니다.")
 } else {
     print("no value")
 }
+
