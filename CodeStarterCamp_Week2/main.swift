7e01532e662cf8e039148e9730e295cfe67c9491
Hisop <hisop@Hisopui-MacBookPro.local>
2023-07-22 02:19:47 +0900

    Refactor: for-in 제거, repeat-while 조건 변경 기존 코드는 insert의 리턴값을 검사, .count의 개수를 검사하도록 변경 코드에 맞춰 플로우차트도 수정작업함

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -10,21 +10,16 @@ import Foundation
 
 func makeLottoNumber() -> Set<Int> {
     var numbers: Set<Int> = []
-    var inserted: Bool
     
-    for _ in 1...6 {
-        repeat {
-            let random = Int.random(in: 1...45)
-            inserted = numbers.insert(random).0
-        } while inserted == false
-    }
+    repeat {
+        let random = Int.random(in: 1...45)
+        numbers.insert(random)
+        } while numbers.count < 6
     return numbers
 }
 
 let myLottoNumbers: [Int] = [1, 6, 12, 15, 23, 45]
 let winningNumber = makeLottoNumber()
-//print("make : \(winningNumber)")
-//print("my : \(myLottoNumbers)")
 let result = winningNumber.intersection(myLottoNumbers).sorted().map { String($0) }.joined(separator:", ")
 if result.isEmpty {
     print("아쉽지만 겹치는 번호가 없습니다.")
