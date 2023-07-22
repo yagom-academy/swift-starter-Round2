3e08b4528a653b2aa75b259ef3efbe69ef0b3212
suhjuho <sspaka0422@gmail.com>
2023-07-22 18:24:16 +0900

    feat Set 집합연산 사용

--- CodeStarterCamp_Week2/randomLottoNumber.swift
+++ CodeStarterCamp_Week2/randomLottoNumber.swift
@@ -20,6 +20,9 @@ func makeLottoNumbers() -> Set<Int> {
 
 // 찍은 번호와 로또 당첨 번호의 겹치는 숫자를 확인하는 함수
 func checkLottoNumbers(myNumbers: [Int], lottoNumbers: Set<Int>) -> Void {
+    //Set의 집합연산을 활용하여 같은 번호를 골라줍니다.
+    //let correctNumbers: Set<Int> = lottoNumbers.intersection(myNumbers)
+    
     var correctNumbers: Array<Int> = []
     
     for myNumber in myNumbers {
@@ -28,8 +31,11 @@ func checkLottoNumbers(myNumbers: [Int], lottoNumbers: Set<Int>) -> Void {
         }
     }
     
+    // 겹치는 숫자들 문자열로
+    let overlapNumbers: String = correctNumbers.map {String($0)}.joined(separator: ", ")
+    
     if correctNumbers.count > 0 {
-        print("축하합니다! 겹치는 번호는 \(correctNumbers.map {String($0)}.joined(separator: ", ") ) 입니다!")
+        print("축하합니다! 겹치는 번호는 \(overlapNumbers) 입니다!")
     } else {
         print("아쉽지만 겹치는 번호가 없습니다.")
     }
