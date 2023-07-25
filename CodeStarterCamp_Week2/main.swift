d2b7a4871a839a1ccf3903eae0df63793d2fa7a1
Hisop <hisop@Hisopui-MacBookPro.local>
2023-07-26 03:34:28 +0900

    Feat: Step 요구사항 구현 Dictionary 타입을 사용하여 당첨번호 회차별로 기록 당첨번호 생성이 잘 이루어졌는지 checkNumbers로 확인 step2의 기능은 compareNumbers 함수로 변경하여 유지 기능별로 최대한 분리하여 함수로 나눔

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,9 +8,12 @@
 
 import Foundation
 
-func makeLottoNumber() -> Set<Int> {
+func makeRound(round: Int) -> String {
+    return "\(round)회차"
+}
+
+func makeNumber() -> Set<Int> {
     var numbers: Set<Int> = []
-    
     repeat {
         let random = Int.random(in: 1...45)
         numbers.insert(random)
@@ -18,12 +21,45 @@ func makeLottoNumber() -> Set<Int> {
     return numbers
 }
 
+func makeLottoNumber(roundByNumbers: inout [String: Set<Int>]) {
+    let round = makeRound(round: roundByNumbers.count + 1)
+    roundByNumbers[round] = makeNumber()
+}
+
+func checkNumbers(roundByNumbers: [String: Set<Int>]) {
+    print(roundByNumbers) //check Dictionary
+    for num in 1...6 {
+        let round = "\(num)회차"
+        guard let numbers = roundByNumbers[round] else {
+            break
+        }
+        let sortNumbers = numbers.sorted().map{ String($0) }
+        let numbersAddSeparator = sortNumbers.joined(separator: ", ")
+        print("\(round)의 로또 당첨 번호는 \(numbersAddSeparator) 입니다.")
+    }
+}
+
+func compareNumbers(roundByNumbers: [String: Set<Int>], round num: Int, myLottoNumbers: [Int]) {
+    let round = "\(num)회차"
+    print("\(round)의 결과를 내 번호와 비교합니다")
+    
+    guard let numbers = roundByNumbers[round] else {
+        return
+    }
+    let result = numbers.intersection(myLottoNumbers).sorted().map { String($0) }
+    if result.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        let resultAddSeparator = result.joined(separator:", ")
+        print("축하합니다! 겹치는 번호는 \(resultAddSeparator) 입니다.")
+    }
+}
+
+var roundByNumbers: [String: Set<Int>] = [:]
+for _ in 1...5 {
+    makeLottoNumber(roundByNumbers: &roundByNumbers)
+}
+checkNumbers(roundByNumbers: roundByNumbers)
+//step2
 let myLottoNumbers: [Int] = [1, 6, 12, 15, 23, 45]
-let winningNumber = makeLottoNumber()
-let result = winningNumber.intersection(myLottoNumbers).sorted().map { String($0) }.joined(separator:", ")
-if result.isEmpty {
-    print("아쉽지만 겹치는 번호가 없습니다.")
-}
-else {
-    print("축하합니다! 겹치는 번호는 \(result) 입니다.")
-}
+compareNumbers(roundByNumbers: roundByNumbers, round: 3, myLottoNumbers: myLottoNumbers)
