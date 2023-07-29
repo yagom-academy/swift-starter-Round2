9acbd622fd3ca732837130da84195eb6ef19eadf
ParkSY0919 <qhr498@gmail.com>
2023-07-29 10:48:55 +0900

    refactor: 코드리뷰 이후 Step2 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -18,53 +18,43 @@ func makeLottoNumbers() -> Set<Int>{
 }
 
 // MARK: - 내 번호와 로또 당첨번호 맞추는 함수
-func lottoMachine(winnerNumbers: Array<Int>) -> Array<Int> {
+func saveCorrectNumber(winnerNumbers: [Int]) -> [Int] {
 
-    var count = 0
     var correct = [Int]()
 
-    for i in winnerNumbers {
-        if myLottoNumbers.contains(i) {
-            count += 1
-            correct.append(i)
+    for number in winnerNumbers {
+        if myLottoNumbers.contains(number) {
+            correct.append(number)
         }
     }
     return correct
 }
 
 
-func step2Solution() {
+func correctNumberReturner() {
     let winnerNumbers = Array(makeLottoNumbers())
-    let correctNumbers = lottoMachine(winnerNumbers: winnerNumbers)
+    let correctNumbers = saveCorrectNumber(winnerNumbers: winnerNumbers)
 
-    if correctNumbers.count == 0 {
+    if correctNumbers.isEmpty {
         print("아쉽지만 겹치는 번호가 없습니다.")
     }
     else {
         print("축하합니다! 겹치는 번호는 ",terminator: "")
-        for i in correctNumbers {
-            if i != correctNumbers[correctNumbers.count-1] {
-                print(i, terminator: ",")
+        for num in correctNumbers {
+            if num != correctNumbers[correctNumbers.count-1] {
+                print(num, terminator: ", ")
             }
             else {
-                print(i, terminator: " 입니다!")
+                print(num, terminator: " 입니다!\n")
             }
         }
-        print("")
     }
 }
 
 
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-step2Solution()
+correctNumberReturner()
 
 
 
-// MARK: 아래 코드에서 3을 꺼내 그 값을 출력하는 가장 안전한 코드를 작성해보세요.
-let someArray: [[[Int?]?]?] = [[[1, 2], nil, [3, nil, 4], nil, [5, 6]]]
-if let num = someArray[0]?[2]?[0] {
-    print(num)
-}
-else {
-    print("해당 값은 optional 입니다.")
-}
+
