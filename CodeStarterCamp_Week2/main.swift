c6e5d25f406ca82abfed39ada43107df80101b0f
Booyouhada <booyouhada@gmail.com>
2023-08-01 21:27:40 +0900

    feat: A new feature

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,7 +6,24 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
+func drawLottoNums() -> Set<Int> {
+    var sixRandomNums: Set<Int> = []
+    while sixRandomNums.count < 6 {
+        let randomInt: Int = Int.random(in: 1...45)
+        sixRandomNums.insert(randomInt)
+    }
+    return sixRandomNums
+}
+let lottoNums = drawLottoNums()
+let myLottoNums: [Int] = [1, 2, 3, 4, 5, 6]
 
-print("Hello, World!")
+func compareLottoNums(){
+    let commonLottoNums = lottoNums.intersection(myLottoNums)
+    if commonLottoNums.count > 0 {
+        print("축하합니다! 겹치는 번호는 \(commonLottoNums.map{String($0)}.joined(separator: ",")) 입니다!")
+    } else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
 
+compareLottoNums()
