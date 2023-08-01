78d2c5a13bbf7be69ba692cc169b9d4fa34b784c
cognd98 <139201159+cognd98@users.noreply.github.com>
2023-08-01 16:00:13 +0900

    refactor: step3

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -4,30 +4,37 @@ import Foundation
 //step3
 
 
-var lottoNumbers: Set<Int> = []
-var resultNumbers: [String : Set<Int>] = [:]
+var resultNumbers: [String : [Int]] = [:]
 
-func makeLottoNumbers() {
+func makeLottoNumbers() -> [Int] {
+    var lottoNumbers: [Int] = []
     while lottoNumbers.count < 6 {
-        lottoNumbers.insert(Int.random(in: 1...45))
+        lottoNumbers.append(Int.random(in: 1...45))
     }
+    return lottoNumbers
 }
 
 func storeLottoNumbers() {
-    makeLottoNumbers()
     for _ in 0..<5 {
-        resultNumbers["\(resultNumbers.keys.count + 1)회차"] = lottoNumbers
+        if resultNumbers.keys.count == 0 {
+            let times = "1회차"
+            resultNumbers[times] = makeLottoNumbers()
+        }else {
+            let times = "\(resultNumbers.keys.count + 1)회차"
+            resultNumbers[times] = makeLottoNumbers()
+        }
     }
 }
 
 func printWinningNumbers(round: Int) {
     storeLottoNumbers()
-    let winningNumbers = lottoNumbers.map { String($0) }.joined(separator: ", ")
     if let checkNumbers = resultNumbers["\(round)회차"] {
+        let winningNumbers = checkNumbers.map { String($0) }.joined(separator: ", ")
         print("\(round)회차의 로또 당첨 번호는 \(winningNumbers) 입니다.")
     }else {
         print("해당 회차의 로또 당첨 번호는 없습니다.")
     }
+    print(resultNumbers.sorted(by: { $0.key < $1.key }))
 }
 
 printWinningNumbers(round: 2)
