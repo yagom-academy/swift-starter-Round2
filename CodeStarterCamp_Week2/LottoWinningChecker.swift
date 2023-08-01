1f7c259ce9b5bdf1a0113369827303e799866f0b
yawoong2 <adamzoong@naver.com>
2023-08-01 21:12:56 +0900

    style: 배열 타입 표현 변경, temp 변수명 수정
    - 배열 타입 표현 시 축약형을 사용하도록 통일
    - tempNumber 변수명 수정(축약어 사용 줄이기)

--- CodeStarterCamp_Week2/LottoWinningChecker.swift
+++ CodeStarterCamp_Week2/LottoWinningChecker.swift
@@ -9,17 +9,17 @@ import Foundation
 
 // 당첨번호를 생성하는 함수
 func makeLottoNumber() -> Set<Int> {
-    var tempNumber: Int
+    var randomNumber: Int
     var lottoNumbers: Set<Int> = []
     while lottoNumbers.count < 6 {
-        tempNumber = Int.random(in: 1...45)
-        lottoNumbers.insert(tempNumber)
+        randomNumber = Int.random(in: 1...45)
+        lottoNumbers.insert(randomNumber)
     }
     return lottoNumbers
 }
 
 // 찍은 번호와 로또 당첨 번호의 겹치는 숫자를 확인하는 함수
-func checkLottoNumber(compare myLotto: Array<Int>, with winningNumbers: Set<Int>) {
+func checkLottoNumber(compare myLotto: [Int], with winningNumbers: Set<Int>) {
     let myNumbers: Set<Int> = Set(myLotto)
     let checkedNumbers: Set<Int> = myNumbers.intersection(winningNumbers)
     if checkedNumbers.isEmpty {
