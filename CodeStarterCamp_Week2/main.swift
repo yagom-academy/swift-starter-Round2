608bf953247b4ec3abce183ec29cac72b2638ebb
yawoong2 <adamzoong@naver.com>
2023-08-06 23:00:15 +0900

    Style: 불필요한 주석 제거

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,18 +8,12 @@
 
 import Foundation
 
-// STEP - 2
-// 당첨번호 생성
 //let winningNumbers = makeLottoNumbers()
-
-// 내가 찍은 번호
 //let myLottoNumbers: [Int] = [9, 22, 33, 19, 30, 35]
 
-// 로또 번호 비교
 //checkLottoNumbers(compare: myLottoNumbers, with: winningNumbers)
 
 for _ in 1...5 {
     makeLottoNumbers()
 }
-
 readLottoNumbers(from: lottoNumbersDictionary, round: 2)
