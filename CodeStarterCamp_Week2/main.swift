6013e3c4cac235c0f785dddc756200ec51b0431a
greatness3991 <toon_3@naver.com>
2023-08-07 20:12:57 +0900

    Feat: 로또 번호 맞추기 함수 작성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,7 +6,38 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
 
-print("Hello, World!")
+// 로또생성함수
+func createLottoNumbers(_: (Int)) -> Set<Int> {
+    let randomNum45 = Array(1...45)
+    var randomNum6 : Set<Int> = []
+    while randomNum6.count < 6 {
+        randomNum6.insert(randomNum45.randomElement() ?? 0)
+    }
+    return randomNum6
+}
+
+// 내가 찍은 번호, 로또 당첨번호 변수 선언
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+let jackPotNum: Set<Int> = createLottoNumbers(1).intersection(myLottoNumbers)
+
+// 겹치는 숫자 확인 함수
+func comeTrueNumbers () {
+    
+    switch jackPotNum.isEmpty {
+    case true:
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    case false:
+        print("축하합니다! 겹치는 번호는" , jackPotNum.map{(number: Int) -> String in
+            return String(number)}.joined(separator: ",") + " 입니다!")
+    
+    break
+
+}
+    
+    
+}
+
+comeTrueNumbers()
+
 
