f85b6c9f592557c5db1813416d80147beb22eb45
uemu <kim-youngmin@uemu-ui-MacBookAir.local>
2023-08-08 17:13:10 +0900

    Fix: 피드백 반영

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -16,25 +16,34 @@ func makeLottoNumbers() -> Set<Int> {
     return randomNumbers
 }
 
-var lottoResults: [Int: Set<Int>] = [:]
-var presentRound: Int = 0
-
-func progressPresentRound() {
-    let lottoNumbers = makeLottoNumbers()
-    presentRound += 1
-    lottoResults[presentRound] = lottoNumbers
+func printLottoNumbers() {
+    var lottoResults: [String: Set<Int>] = [:]
+    var presentRound: Int = 0
+    
+    func progressPresentRound() {
+        let lottoNumbers = makeLottoNumbers()
+        presentRound += 1
+        lottoResults["\(presentRound)회차"] = lottoNumbers
+    }
+    
+    for _ in 1...5 {
+        progressPresentRound()
+    }
+    
+    let sortedResults = lottoResults.sorted(by: { $0.key < $1.key })
+    for (round, numbers) in sortedResults {
+        print("\(round)의 로또 당첨 번호는 \(numbers.sorted()) 입니다.")
+    }
+    print()
+    
+    func decideRoundNumber(round: String) {
+        if let roundNumbers = lottoResults[round] {
+            let numbersString = roundNumbers.sorted().map { String($0) }.joined(separator: ", ")
+            print("\(round)의 로또 당첨 번호는 \(numbersString) 입니다.")
+        }
+    }
+    
+    decideRoundNumber(round: "2회차")
 }
 
-for _ in 1...5 {
-    progressPresentRound()
-}
-
-let sortedResults = lottoResults.sorted(by: { $0.key < $1.key })
-for (round, numbers) in sortedResults {
-    print("\(round)회차의 로또 당첨 번호는 \(numbers) 입니다.")
-}; print()
-
-if let round2Numbers = lottoResults[2] {
-    let numbersString = round2Numbers.map { String($0) }.joined(separator: ", ")
-    print("2회차의 로또 당첨 번호는 \(numbersString) 입니다.")
-}
+printLottoNumbers()
