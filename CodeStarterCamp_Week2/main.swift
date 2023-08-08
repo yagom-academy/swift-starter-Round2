db9ccb1e103c6add5547409666f7c8da0f644da1
LeeHyunsoo <mm083434@naver.com>
2023-08-08 21:35:10 +0900

    Refactor : : 피드백 수용 후 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,21 +1,28 @@
 import Foundation
 
-var lottoNumbers = [String : Set<Int>]()
-var round : Int = 1
+var lottoNumbers = [String : Array<Int>]()
 func makeFiveRoundLottoNumbers() {
-    for _ in stride(from: 1, through: 5, by: 1) {
-        var lottoNumbersList = Set<Int>()
+    for round in 1...5 {
+        let lottoNumbersList = Array(makeLottoNumbers())
+        lottoNumbers["\(round) + 회차"] = lottoNumbersList
+    }
+}
+
+
+func makeLottoNumbers() -> Set<Int> {
+    var lottoNumbersList = Set<Int>()
         while (lottoNumbersList.count < 6) {
             lottoNumbersList.insert(Int.random(in: 1...45))
         }
-        lottoNumbers["\(round) + 회차"] = lottoNumbersList
-        round = round + 1
+    return lottoNumbersList
+}
+
+func printLottoResult(round: Int) {
+    if let lottoNumber = lottoNumbers["\(round) + 회차"] {
+        print("\(round)회차의 로또 당첨 번호는 \(lottoNumber.map{String($0)}.joined(separator:", ")) 입니다.")
     }
 }
 makeFiveRoundLottoNumbers()
-round = 4
-if let lottoNumber = lottoNumbers["\(round) + 회차"] {
-    print("\(round)회차의 로또 당첨 번호는 \(lottoNumber.map{String($0)}.joined(separator:", ")) 입니다.")
-}
+printLottoResult(round: 3)
 
 
