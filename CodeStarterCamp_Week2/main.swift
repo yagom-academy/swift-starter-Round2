5143b96c567f947f3fe06dea731691b1624d8a1b
uemu <kim-youngmin@uemu-ui-MacBookAir.local>
2023-08-11 01:01:54 +0900

    Fix: 함수의 분리

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -16,34 +16,38 @@ func makeLottoNumbers() -> Set<Int> {
     return randomNumbers
 }
 
-func printLottoNumbers() {
-    var lottoResults: [String: Set<Int>] = [:]
-    var presentRound: Int = 0
-    
-    func progressPresentRound() {
-        let lottoNumbers = makeLottoNumbers()
-        presentRound += 1
-        lottoResults["\(presentRound)회차"] = lottoNumbers
-    }
-    
-    for _ in 1...5 {
-        progressPresentRound()
-    }
-    
-    let sortedResults = lottoResults.sorted(by: { $0.key < $1.key })
-    for (round, numbers) in sortedResults {
-        print("\(round)의 로또 당첨 번호는 \(numbers.sorted()) 입니다.")
-    }
-    print()
-    
-    func decideRoundNumber(round: String) {
-        if let roundNumbers = lottoResults[round] {
-            let numbersString = roundNumbers.sorted().map { String($0) }.joined(separator: ", ")
-            print("\(round)의 로또 당첨 번호는 \(numbersString) 입니다.")
-        }
-    }
-    
-    decideRoundNumber(round: "2회차")
+var lottoResults: [String: Set<Int>] = [:]
+
+func progressPresentRound() {
+    let lottoNumbers = makeLottoNumbers()
+    let newRound = lottoResults.count + 1
+    lottoResults["\(newRound)회차"] = lottoNumbers
 }
 
-printLottoNumbers()
+func carryOutRound(numberOfRound: Int) {
+    for _ in 1...numberOfRound {
+        progressPresentRound()
+    }
+}
+
+carryOutRound(numberOfRound: 5)
+
+func printResult() {
+    let sortedResults = lottoResults.sorted(by: { $0.key < $1.key })
+    for (round, numbers) in sortedResults {
+        let formattedNumbers = numbers.sorted().map(String.init).joined(separator: ", ")
+        print("\(round)의 로또 당첨 번호는 \(formattedNumbers) 입니다.")
+    }
+    print()
+}
+
+printResult()
+
+func decideRoundNumber(round: String) {
+    if let roundNumbers = lottoResults[round] {
+        let numbersString = roundNumbers.sorted().map { String($0) }.joined(separator: ", ")
+        print("\(round)의 로또 당첨 번호는 \(numbersString) 입니다.")
+    }
+}
+
+decideRoundNumber(round: "2회차")
