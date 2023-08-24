6ba05f787e56604bf23c91f48fec0ae035818de8
chaloopy <sorrowsi93@naver.com>
2023-08-24 10:49:01 +0900

    add step 2

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+//Step 2
+func makeLottoNumbers() -> [Int] {
+    var lottoNumbers: Set<Int> = []
+    while lottoNumbers.count < 6,
+          let randomInt: Int = (1...45).randomElement() {
+        lottoNumbers.insert(randomInt)
+    }
+    return Array(lottoNumbers)
+}
+
+let myLottoNumbers: [Int] = [6, 11, 17, 32, 41, 44]
+
+var winLottoNumbers = makeLottoNumbers()
+
+let matchLottoNumber = myLottoNumbers.filter {
+    winLottoNumbers.contains($0)
+}.map {
+    String($0)
+}.joined(separator: ", ")
+
+if !matchLottoNumber.isEmpty {
+    print("축하합니다! 겹치는 번호는 \(matchLottoNumber) 입니다!")
+} else {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+}
 
