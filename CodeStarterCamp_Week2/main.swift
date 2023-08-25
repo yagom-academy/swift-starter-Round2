e2af57d8c3a0360576a2365613f5ecca473f2621
chaloopy <sorrowsi93@naver.com>
2023-08-25 09:47:44 +0900

    modify function

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,28 +8,24 @@
 
 import Foundation
 
-//Step 2
-func makeLottoNumbers() -> [Int] {
+func makeLottoNumbers() -> Set<Int> {
     var lottoNumbers: Set<Int> = []
-    while lottoNumbers.count < 6,
-          let randomInt: Int = (1...45).randomElement() {
-        lottoNumbers.insert(randomInt)
+    
+    while lottoNumbers.count < 6 {
+          lottoNumbers.insert(Int.random(in: 1...45))
     }
-    return Array(lottoNumbers)
+
+    return lottoNumbers
 }
 
 let myLottoNumbers: [Int] = [6, 11, 17, 32, 41, 44]
 
-var winLottoNumbers = makeLottoNumbers()
+let winningLottoNumbers = makeLottoNumbers()
 
-let matchLottoNumber = myLottoNumbers.filter {
-    winLottoNumbers.contains($0)
-}.map {
-    String($0)
-}.joined(separator: ", ")
+let matchingLottoNumber = winningLottoNumbers.intersection(myLottoNumbers)
 
-if !matchLottoNumber.isEmpty {
-    print("축하합니다! 겹치는 번호는 \(matchLottoNumber) 입니다!")
+if !matchingLottoNumber.isEmpty {
+    print("축하합니다! 겹치는 번호는 \(matchingLottoNumber) 입니다!")
 } else {
     print("아쉽지만 겹치는 번호가 없습니다.")
 }
