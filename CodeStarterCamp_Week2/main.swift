41aefbb59d8845508c24a9fb5c32273b90ede04b
y0unghyun <ikim4127@gmail.com>
2023-08-25 16:27:38 +0900

    Step2. 내 번호와 맞추어보자! 완료

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,20 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week2
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
+let myLottoNumbers: [Int] = [4, 5, 10, 18, 22, 32]
 
-import Foundation
 
-print("Hello, World!")
+var lottoNumSet: Set<Int> = Set<Int>()
 
+while lottoNumSet.count < 6 {
+    lottoNumSet.insert(Int.random(in: 1...45))
+}
+
+print(lottoNumSet.sorted())
+print(myLottoNumbers)
+
+let matchedLottoNumbers: Set<Int> = lottoNumSet.intersection(myLottoNumbers)
+
+if matchedLottoNumbers.count >= 1 {
+    print("축하합니다! 겹치는 번호는 \(matchedLottoNumbers.sorted()) 입니다!")
+}
+else {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+}
