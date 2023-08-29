783a6dc16d10eaa8fd88d83b56857ada8d8b0e03
jiwan-swfit <yoojiwan@icloud.com>
2023-08-29 15:03:07 +0900

    Week2[STEP2]yuujiwan

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,31 @@
 //
 //  main.swift
-//  CodeStarterCamp_Week2
+//  CodeStarProject
 //
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
+//  Created by 유지완 on 2023/08/27.
 //
 
+
+
+// STEP 2
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6] //찍은 번호
 
+// 로또 생성번호
+var generateLotto : Set<Int> = []
+
+while generateLotto.count < 6{
+    let number = Int.random(in: 1...45)
+    generateLotto.insert(number)
+}
+print("당첨번호 \(generateLotto) 입니다")
+
+// 교집합 
+let intersection: Set<Int> = generateLotto.intersection(myLottoNumbers)
+
+if intersection.count == 0{
+    print("아쉽지만 겹치는 번호가 없습니다.")
+}else{
+    print("축하합니다! 겹치는 번호는 \(intersection) 입니다!")
+}
