07f3c8613da772dc453e0e1a5e3d46f2f4e42a6f
jjwon2149 <45623603+jjwon2149@users.noreply.github.com>
2023-08-29 15:40:54 +0900

    Week2 [STEP 2] smith_useIntersection

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,30 +1,26 @@
 import Foundation
 
-let myLottoNumber: [Int] = [7, 9, 12, 15, 22, 43]
-let rottoNumber: [Int] = Array(generateRottoNumber())
+let myLottoNumbers: Set<Int> = [7, 9, 12, 15, 22, 43]
+let rottoNumbers: Set<Int> = generateRottoNumber()
 
-lotteryWinningCheck(for: myLottoNumber, with: rottoNumber)
+lotteryWinningCheck(with: myLottoNumbers, by: rottoNumbers)
 
 //로또번호 생성 함수
 func generateRottoNumber() -> Set<Int> {
-    var rotteryNumber: Set<Int> = []
+    var rotteryNumbers: Set<Int> = []
     
-    while rotteryNumber.count < 6 {
-        rotteryNumber.insert(Int.random(in: 1...45))
+    while rotteryNumbers.count < 6 {
+        rotteryNumbers.insert(Int.random(in: 1...45))
     }
     
-    return rotteryNumber
+    return rotteryNumbers
 }
 
 //나의 번호와 로또번호 중복 확인 함수
-func lotteryWinningCheck(for myLottoNumber: Array<Int>, with rottoNumber: Array<Int>) {
+func lotteryWinningCheck(with myLottoNumbers: Set<Int>, by rottoNumbers: Set<Int>) {
     
-    var resultSet: Set<Int> = []
+    let resultSet: Set<Int> = Set(myLottoNumbers).intersection(rottoNumbers)
     
-    for x in myLottoNumber {
-          if rottoNumber.contains(x) {
-              resultSet.insert(x)
-          }
-       }
     resultSet.isEmpty ? print("아쉽지만 겹치는 번호가 없습니다.") : print("축하합니다! 겹치는 번호는 \(resultSet.map { String($0) }.joined(separator: ", ")) 입니다!")
 }
+
