8eeb160a10954390154f2ed350152b211f22f62b
jjwon2149 <45623603+jjwon2149@users.noreply.github.com>
2023-08-31 16:31:55 +0900

    Week2 [STEP 3] smith_회차추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,40 +1,40 @@
 import Foundation
 
 let myLottoNumbers: Set<Int> = [7, 9, 12, 15, 22, 43]
-let rottoNumbers: Set<Int> = generateRottoNumber()
+let rottoNumbers: Set<Int> = generateLottoNumber()
+let nTimeLottoNumbers: Dictionary<String, Set<Int>> = generateNtimesLottoNumber(count: 5)
+let numberOfRounds: String = "2회차"
 
 
-
-let numberOfRounds = 5
-let nTimeRottoNumber = generateNtimesRottoNumber(rounds: numberOfRounds)
-lotteryWinningCheck(with: myLottoNumbers, by: nTimeRottoNumber, for: numberOfRounds)
+lottoWinningCheck(with: myLottoNumbers, by: nTimeLottoNumbers, for: numberOfRounds)
 
 //n회차 로또 번호 생성 함수
-func generateNtimesRottoNumber(rounds: Int) -> Dictionary<Int, Set<Int>> {
-    var nTimesWinningLottoNumbers: [Int : Set<Int>] = [:]
+func generateNtimesLottoNumber(count rounds: Int) -> Dictionary<String, Set<Int>> {
+    var nTimesWinningLottoNumbers: [String : Set<Int>] = [:]
     
     for round in 1...rounds {
-            let lottoNumbers = generateRottoNumber()
-        nTimesWinningLottoNumbers[round] = lottoNumbers
-        }
+            let lottoNumbers = generateLottoNumber()
+            nTimesWinningLottoNumbers["\(round)회차"] = lottoNumbers
+    }
     
     return nTimesWinningLottoNumbers
 }
 
 //로또 번호 생성 함수
-func generateRottoNumber() -> Set<Int> {
-    var rotteryNumbers: Set<Int> = []
+func generateLottoNumber() -> Set<Int> {
+    var lottoNumbers: Set<Int> = []
     
-    while rotteryNumbers.count < 6 {
-        rotteryNumbers.insert(Int.random(in: 1...45))
+    while lottoNumbers.count < 6 {
+        lottoNumbers.insert(Int.random(in: 1...45))
     }
     
-    return rotteryNumbers
+    return lottoNumbers
 }
 
 //n회차 로또 당첨 번호 확인
-func lotteryWinningCheck(with myLottoNumbers: Set<Int>, by rottoNumbers: Dictionary<Int, Set<Int>>, for rounds: Int) {
+func lottoWinningCheck(with myLottoNumbers: Set<Int>, by lottoNumbers: Dictionary<String, Set<Int>>, for rounds: String) {
+    let turn: Int = rounds[rounds.startIndex].wholeNumberValue!
+    let winninglottoNumber = lottoNumbers["\(turn)회차"]!.map { String($0) }.joined(separator: ", ")
     
-    let winningRottoNumber = rottoNumbers[rounds]!.map { String($0) }.joined(separator: ", ")
-    print("\(rounds)회차의 로또 당첨 번호는 \(winningRottoNumber) 입니다.")
+    print("\(rounds)의 로또 당첨 번호는 \(winninglottoNumber) 입니다.")
 }
