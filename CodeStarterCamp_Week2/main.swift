c0b9cba7ee355518721ef4db3170154bef66eef4
sueunal <doms@kakao.com>
2023-09-02 20:40:53 +0900

    Week2 [STEP2] 수하

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,26 +8,41 @@
 
 import Foundation
 
-func createLotto()->Set<Int>{
+func createLotto()-> Dictionary<Int,Set<Int>>{
     var lottoNumber : Set<Int> = []
-    while lottoNumber.count < 6{
-        lottoNumber.insert(Int.random(in: 1...45))
+    var roundOfLotto : [Int:Set<Int>] = [:]
+    var roundOfNumber : Int = 1
+    while roundOfNumber < 6{
+        lottoNumber.removeAll()
+        while lottoNumber.count < 6{
+            lottoNumber.insert(Int.random(in: 1...45))
+        }
+        roundOfLotto.updateValue(lottoNumber, forKey: roundOfNumber)
+        roundOfNumber += 1
     }
-    return lottoNumber
+    return roundOfLotto
 }
-func checkLotto(){
-    let lottoNumber : Set<Int> = createLotto()
-    let myLottoNumbers : [Int] = [1,2,3,4,5,6]
-    var bingGo : [Int] = []
-    for i in myLottoNumbers{
-        if lottoNumber.contains(i){
-            bingGo.append(i)
+func getRoundOfLotto(){
+    let roundOfLotto : [Int:Set<Int>] = createLotto()
+    for roundOfInquiry in roundOfLotto{
+        if roundOfInquiry.key == 2{
+            print("\(roundOfInquiry.key)회차의 로또 당첨 번호는 \(roundOfInquiry.value.map({String($0)}).joined(separator: ", ")) 입니다.")
         }
     }
-    if bingGo.isEmpty{
-        print("아쉽지만 겹치는 번호가 없습니다.")
-    }else{
-        print("축하합니다! 겹치는 번호는 \(bingGo.map({String($0)}).joined(separator: ", ")) 입니다!")
-    }
 }
-checkLotto()
+//func checkLotto(){
+//    let myLottoNumbers : [Int] = [1,2,3,4,5,6]
+//    var bingGo : [Int] = []
+//    for i in myLottoNumbers{
+//        if lottoNumber.contains(i){
+//            bingGo.append(i)
+//        }
+//    }
+//    if bingGo.isEmpty{
+//        print("아쉽지만 겹치는 번호가 없습니다.")
+//    }else{
+//        print("축하합니다! 겹치는 번호는 \(bingGo.map({String($0)}).joined(separator: ", ")) 입니다!")
+//    }
+//}
+//roundOfLotto()
+getRoundOfLotto()
