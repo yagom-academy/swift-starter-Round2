c0848d5372d1410a614e1859ec8fcbed99849f38
black <oilpott12@naver.com>
2023-09-02 17:36:58 +0900

    feat: 로또 번호 생성 및 당첨여부 기능 구현

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,28 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week2
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+var lottoNumbers: Set<Int> = Set<Int>()
+var a = 0
 
+while lottoNumbers.count < 6 {
+    lottoNumbers.insert(Int.random(in: 1...45))
+}
+
+for i in myLottoNumbers {
+    if lottoNumbers.contains(i) {
+        a = a + 1
+    }
+}
+
+if a == 6 {
+    print("축하합니다! 겹치는 번호는 ", terminator: "")
+    for i in 0...5 {
+        if i == 5 {
+            print(myLottoNumbers[i], "입니다.")
+        } else {
+            print(myLottoNumbers[i], terminator: ", ")
+        }
+    }
+} else {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+}
