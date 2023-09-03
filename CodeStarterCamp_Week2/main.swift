092b7c42da017c1caef1f63ff63381fdd0d44d66
aldalddl <aldalddl2007@gmail.com>
2023-09-03 11:11:29 +0900

    style: 불필요한 타입 어노테이션 삭제

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -25,8 +25,7 @@ func createLottoNumbers() -> Set<Int> {
 func checkSameNumbers(myLottoNumbers: [Int], lottoNumbers: Set<Int>) {
     let sameNumbers: Set<Int> = lottoNumbers.intersection(Set(myLottoNumbers))
     let sortedSameNumbers = sameNumbers.sorted()
-    var array = [[1,2], [2,3]].joined()
-    print(array)
+    
     if !sortedSameNumbers.isEmpty {
         print("축하합니다! 겹치는 번호는 \(sortedSameNumbers.map{ String($0) }.joined(separator: ", ")) 입니다!")
     } else {
