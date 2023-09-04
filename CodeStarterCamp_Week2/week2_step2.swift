47589b9a02bf28a53eaca53eb912373f7c573bfa
jjudy_kim <jjudykim0@gmail.com>
2023-09-04 12:32:16 +0900

    STEP 2 - 내 번호와 로또 번호 맞춰보기 (파일 나누기)

---
+++ CodeStarterCamp_Week2/week2_step2.swift
@@ -0,0 +1,37 @@
+//
+//  week2_step2.swift
+//  CodeStarterCamp_Week2
+//
+//  Created by jjudy on 2023/09/04.
+//
+
+import Foundation
+
+func createLottoNumbers() -> Set<Int> {
+    var lottoSet: Set<Int> = Set()
+    
+    while lottoSet.count < 6 {
+        lottoSet.insert(Int.random(in: 1...45))
+    }
+    
+    return lottoSet
+}
+
+func checkSameLottoNumbers(myNumbers: [Int], setLottoNumbers: Set<Int>) {
+    var sameNumArray: Array<Int> = [Int]()
+    
+    for num in myLottoNumbers {
+        if (lottoNumbers.contains(num)) {
+            sameNumArray.append(num)
+        }
+    }
+    
+    if(sameNumArray.count > 0) {
+        print("축하합니다! 겹치는 번호는 ", terminator: "")
+        print(sameNumArray.map{String($0)}.joined(separator:", "), terminator: "")
+        print(" 입니다!")
+    } else {
+        print("아쉽지만 겹치는 번호가 없습니다")
+    }
+    
+}
