c9ca381236378ae5f0a31cefdc8f9801bc8e5f63
Camilla S. Park <camilla@Camillas-Air.skbroadband>
2023-09-04 14:16:50 +0900

    step2 commit

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,38 @@
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+var generatedNumbers: Set<Int> = Set<Int>()
+var correctNumbers = [Int]()
+
+//번호생성 함수
+func generateNumbers() {
+    while(generatedNumbers.count < 6){
+        generatedNumbers.insert(Int.random(in: 1...45))
+    }
+}
+    
+ 
+//번호비교 함수
+func checkNumbers(){
+    for number in 0...5{
+        if(myLottoNumbers.contains(Array(generatedNumbers)[number])) == true{
+            correctNumbers.append(Array(generatedNumbers)[number])
+        }
+    }
+    if correctNumbers.isEmpty == true{
+        print("아쉽지만 겹치는 번호가 없습니다.")
+        }
+        else{
+            print("축하합니다. 겹치는 번호는 ", correctNumbers.sorted(), "입니다!")
+    }
+}
+
+
+
+//로또번호 생성
+generateNumbers()
+
+//번호비교후 출력
+checkNumbers()
 
