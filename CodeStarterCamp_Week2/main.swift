fee7d84f557879152e7502de6d809fa537de84ee
jiwan-swfit <yoojiwan@icloud.com>
2023-09-04 09:26:40 +0900

    코드 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,26 +9,25 @@
 import Foundation
 
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-var generateLotto: Set<Int> = []
+var lottoNumderSet: Set<Int> = []
 
 
-func creationLottoNumder() -> Set<Int>{
+func creationLottoNumders() -> Set<Int>{
 
-    while generateLotto.count < 6{
+    while lottoNumderSet.count < 6{
         let numder = Int.random(in: 1...45)
-        generateLotto.insert(numder)
+        lottoNumderSet.insert(numder)
     }
-    return generateLotto
+    return lottoNumderSet
 }
-print("당첨번호 \(creationLotto())")
+print("당첨번호 \(creationLottoNumders())입니다.")
 
-let winningNumder: Set<Int> = creationLottoNumder().intersection(myLottoNumbers)
-
-if winningNumder.count == 0{
-    print("아쉽지만 겹치는 번호가 없습니다")
-}else{
-    print("축하합니다! 겹치는 번호는 \(winningNumder)입니다!")
-}
+let overlappingLottoNumder: Set<Int> = creationLottoNumders().intersection(myLottoNumbers)
 
 
+    if overlappingLottoNumder.count == 1{
+        print("축하합니다! 겹치는 번호는 \(overlappingLottoNumder)입니다!")
+    }else{
+        print("아쉽지만 겹치는 번호가 없습니다")
+    }
 
