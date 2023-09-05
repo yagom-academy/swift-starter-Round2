a792524b4f0def75dc597e58c602a46e9492109d
Synn <synn@Synnui-MacBookAir.local>
2023-09-05 20:04:51 +0900

    WEEK2 STEP2_1

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,39 @@
 
 import Foundation
 
-print("Hello, World!")
+func makePool() -> Array<Int> {
+    var poolNumbers: [Int] = []
+    
+    while poolNumbers.count < 6 {
+        let pickedNumber = Int.random(in: 1...45)
+        if !poolNumbers.contains(pickedNumber) {
+            poolNumbers.append(pickedNumber)
+        } else {
+            continue
+        }
+    }
+    poolNumbers.sort()
+    return poolNumbers
+    }
+    
+var winningNumbers = makePool()
+print(winningNumbers)
 
+let myLottoNumbers: [Int] = [11, 22, 33, 44, 45, 46]
+
+func checkingNumbers(){
+    var matchedNumbers: [Int] = []
+
+    for myNumber in myLottoNumbers {
+        if winningNumbers.contains(myNumber) {
+            matchedNumbers.append(myNumber)
+        }
+    }
+    if matchedNumbers.count > 0 {
+        print("축하합니다! 겹치는 번호는 \(matchedNumbers) 입니다!")
+    } else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+
+checkingNumbers()
