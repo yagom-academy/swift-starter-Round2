f2de4b9fb91c254fe6272099a40a4f41be04c0db
Camilla S. Park <camilla@Camillas-Air.skbroadband>
2023-09-07 09:45:46 +0900

    step3 commit

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -5,38 +5,39 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
-
 import Foundation
 
-let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-var generatedNumbers: Set<Int> = Set<Int>()
-var correctNumbers = [Int]()
+func generateLottoNumbers() -> [Int] {
+    var lottoNumbers = Set<Int>()
 
-// 번호 생성 함수
-func makeNumbers() {
-    while generatedNumbers.count < 6 {
-        generatedNumbers.insert(Int.random(in: 1...45))
+    while lottoNumbers.count < 6 {
+        let randomNumber = Int.random(in: 1...45)
+        lottoNumbers.insert(randomNumber)
+    }
+
+    return Array(lottoNumbers).sorted()
+}
+
+var lottoResults: [Int: [Int]] = [:]
+
+func generateLottoResults(rounds: Int) {
+    for round in 1...rounds {
+        let lottoNumbers = generateLottoNumbers()
+        lottoResults[round] = lottoNumbers
     }
 }
 
-// 번호 비교 함수
-func checkNumbers() {
-    for number in 0...5 {
-        if myLottoNumbers.contains(Array(generatedNumbers)[number]) == true {
-            correctNumbers.append(Array(generatedNumbers)[number])
-        }
-    }
-    if correctNumbers.isEmpty == true {
-        print("아쉽지만 겹치는 번호가 없습니다.")
+func printLottoNumbersForRound(round: Int) {
+    if let numbers = lottoResults[round] {
+        let numbersString = numbers.map { String($0) }.joined(separator: ", ")
+        print("\(round)회차의 로또 번호는 \(numbersString) 입니다")
     } else {
-        let correctNumbersString = correctNumbers.map { String($0) }.joined(separator: ", ")
-        print("축하합니다. 겹치는 번호는 \(correctNumbersString) 입니다!")
+        print("\(round)회차의 로또 번호가 없습니다.")
     }
 }
 
-// 로또 번호 생성
-makeNumbers()
+//로또번호 생성
+generateLottoResults(rounds: 5)
 
-// 번호 비교 후 출력
-checkNumbers()
-
+//특정회차 출력
+printLottoNumbersForRound(round: 2)
