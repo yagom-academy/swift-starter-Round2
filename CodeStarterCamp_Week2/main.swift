ce4432613f00fa1436fe4801ef74b4196e7f5ee8
bangMinji98 <whoru9806@gmail.com>
2023-09-08 21:46:55 +0900

    week2_step2 콩지

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -2,11 +2,26 @@
 //  main.swift
 //  CodeStarterCamp_Week2
 //
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
+//  Created by 콩지.
+
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [3,10,5,8,28]
 
+var randomNum: Set<Int> = []
+while randomNum.count<5 {
+    let random = Int.random(in: 1...45)
+    randomNum.insert(random)
+}
+//print(randomNum)
+
+let intersection = randomNum.intersection(myLottoNumbers)
+let joinedElements = intersection.map { String($0) }.joined(separator: ", ")
+
+if intersection.count>0 {
+    print("축하합니다! 겹치는 번호는 \(joinedElements) 입니다!")
+}
+else{
+    print("아쉽지만 겹치는 번호가 없습니다.")
+}
