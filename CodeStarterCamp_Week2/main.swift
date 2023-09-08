1dc326380000cf51e16ecdb4d13f97d25a4209be
유지완 <yujiwan@yujiwan-ui-MacBookPro.local>
2023-09-08 14:05:43 +0900

    STEP3 생성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,43 @@
 //
-//  main.swift
-//  CodeStarterCamp_Week2
+//  main5.swift
+//  CodeStarProject
 //
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
+//  Created by 유지완 on 2023/08/31.
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumber: [Int] = [1, 2, 3, 4, 5, 6]
+var lottoNumber: Set<Int> = []
+let LottoRound = 5 // 로또 당첨 번호 5번 생성
+
+// STEP2 함수 가져온것
+func createLottoNumder() -> Set<Int>{
+    var generateLotto : Set<Int> = []
+
+    while generateLotto.count < 6{
+        let number = Int.random(in: 1...45)
+        generateLotto.insert(number)
+    }
+    return generateLotto
+}
+
+var lottoResults: [Int: Set<Int>] = [:] // 각 회차의 로또 당첨 번호를 저장할 Dictionary
+
+// 로또 당첨 번호 5번 생성
+for _ in 1...LottoRound {
+
+    for round in 1...5 {
+        let LottoNumber = createLottoNumder()
+        lottoResults[round] = LottoNumber
+
+    print("\(round)회차의 로또 당첨 번호는 \(LottoNumber) 입니다.")
+    }
+
+    let desiredRound = 2 // 원하는 회차 설정 (여기서는 2회차)
+    if let desiredLotto = lottoResults[desiredRound]{
+        print("\(desiredRound)회차의 로또 당첨 번호는 \(desiredLotto) 입니다.")
+    } else {
+        print("\(desiredRound)회차의 로또 당첨 번호를 찾을 수 없습니다.")
+    }
+}
 
