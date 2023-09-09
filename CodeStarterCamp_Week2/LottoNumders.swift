ddc517bf3cef445ada1251d80731628964b67bf0
유지완 <yujiwan@yujiwan-ui-MacBookPro.local>
2023-09-09 23:19:48 +0900

    해결

---
+++ CodeStarterCamp_Week2/LottoNumders.swift
@@ -0,0 +1,19 @@
+//
+//  LottoNumder.swift
+//  CodeStarProject
+//
+//  Created by 유지완 on 2023/09/09.
+//
+
+import Foundation
+
+func createLottoNumder() -> Set<Int>{
+    var generateLotto : Set<Int> = []
+
+    while generateLotto.count < 6{
+        let number = Int.random(in: 1...45)
+        generateLotto.insert(number)
+    }
+    return generateLotto
+}
+
