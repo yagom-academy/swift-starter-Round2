e59a9b107e93da4ce66ce15e27877a44f17b9240
lsm23456 <lsm23456@naver.com>
2023-09-11 15:54:29 +0900

    feat: 문구 출력 로직 수정 및 자잘한 코드 수정

--- CodeStarterCamp_Week2/guessMyLottoNumbers.swift
+++ CodeStarterCamp_Week2/guessMyLottoNumbers.swift
@@ -12,51 +12,40 @@ let myLottoNumbers: [Int] = [3, 6, 9, 10, 11, 12]
 
 
 // 로또 추첨
-func drawLottos() -> Set<Int> {
-    var lottoNumbers: Set<Int> = Set<Int>() // 반환될 로또번호
-    var randomNumber : Int  // 랜덤번호
+func randomLottos() -> Set<Int> {
+    var lottoNumbers = Set<Int>()
+    var randomNumber : Int
     
-    // 로또 번호 생성 겹치지 않는 숫자가 6개가 될때까지 반복한다
     while lottoNumbers.count < 6 {
         randomNumber = Int.random(in: 1...45)
         lottoNumbers.insert(randomNumber)
     }
     
-    //생성된 로또번호 반환
     return lottoNumbers
 }
 
 //당첨 확인
 func checkLottoNumbers(myLottoNumbers: [Int]) -> Void {
-    // 맞은 번호를 저장할 Set 컬렉션 생성.
-    var correctNumber: Set<Int> = Set<Int>()
-    // 로또 당첨 번호 저장
-    let lottoNumbers = drawLottos()
+    var correctNumbers = Array<Int>()
+    let lottoNumbers = randomLottos()
+    var lottoString = ""
     
-    // 내 번호가 로또 당첨번호에 포함되는지 확인.
     for number in myLottoNumbers {
         if(lottoNumbers.contains(number)) {
-            correctNumber.insert(number)
+            correctNumbers.append(number)
         }
     }
-    print(myLottoNumbers)
-    print(lottoNumbers)
-    //결과 출력
-    if(correctNumber.isEmpty) {
+
+    if(correctNumbers.isEmpty) {
         print("아쉽지만 겹치는 번호가 없습니다.")
     } else {
-        print("축하합니다! 겹치는 번호는 ", terminator: "")
-        for number in correctNumber {
-            // ,출력을 위한 마지막 값 체크 조건
-            if(correctNumber.count != 1) {
-                print("\(number) ", terminator: ",")
-            } else {
-                print("\(number) ", terminator: "")
-            }
-            // 번호 갯수를 줄여서 확인
-            correctNumber.removeFirst()
+        for number in correctNumbers {
+            lottoString.append("\(number), ")
         }
-        print("입니다!")
+        lottoString.removeLast()
+        lottoString.removeLast()
+        
+        print("축하합니다! 겹치는 번호는 \(lottoString) 입니다!")
     }
 }
 
