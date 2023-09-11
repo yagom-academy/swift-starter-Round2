6247b16c4fd6283d55e2d87d562b1c03cdd78e57
jjudy_kim <jjudykim0@gmail.com>
2023-09-11 11:40:27 +0900

    STEP 3 - 로또 당첨 번호 회차별 저장 및 확인 (수정사항 반영 0911_02 - 타입표기 통일)

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 var lottoNumbersDictionary: Dictionary<String, Set<Int>> = [:]
-let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+let myLottoNumbers: Array<Int> = [1, 2, 3, 4, 5, 6]
 var sameNumArray: Array<Int> = [Int]()
 
 // 원하는 회차의 로또 번호를 확인하기 위한 변수
-let roundForLottoNumbers: Int = -6
+let roundForLottoNumbers: Int = 2
 
 createSeveralLottoNumbersByCustom(customTimes: 5)
 checkSameLottoNumbers(myLottoNumbers: myLottoNumbers, roundForLottoNumbers: roundForLottoNumbers)
