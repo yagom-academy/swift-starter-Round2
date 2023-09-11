c678e2281dbe33bb435c7f8b33963faa4d9562b6
유지완 <yujiwan@yujiwan-ui-MacBookPro.local>
2023-09-11 20:40:20 +0900

    수정함

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,28 +8,29 @@
 
 import Foundation
 
-let myLottoNumber: [Int] = [1, 2, 3, 4, 5, 6]
-var lottoNumber: Set<Int> = []
-
 createLottoNumder()
 
-var lottoResults: [Int: Set<Int>] = [:] // 각 회차의 로또 당첨 번호를 저장할 Dictionary
+var lottoResults: [Int: Set<Int>] = [:]
 
-
+func lottoNumderRound() -> [Int: Set<Int>]{
     for round in 1...5 {
         let LottoNumber = createLottoNumder()
         lottoResults[round] = LottoNumber
-
-    print("\(round)회차의 로또 당첨 번호는 \(LottoNumber) 입니다.")
+        print("\(round)회차의 로또 당첨 번호는 \(LottoNumber) 입니다.")
     }
+    return lottoResults
+}
+lottoNumderRound()
 
-    let desiredRound = 2 // 원하는 회차 설정 (여기서는 2회차 입니다)
+func lottoDesiredRound(desiredRound: Int = 2){
+    
     if let desiredLotto = lottoResults[desiredRound]{
-        print("\(desiredRound)회차의 로또 당첨 번호는 \(desiredLotto) 입니다.")
+        print("\(desiredRound)회차의 로또 당첨 번호는 \(desiredLotto.sorted()) 입니다.")
     } else {
         print("\(desiredRound)회차의 로또 당첨 번호를 찾을 수 없습니다.")
     }
-
+}
+lottoDesiredRound(desiredRound: 2)
 
 
 
