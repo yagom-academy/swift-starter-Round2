fd71dd2395b3da699f60e3cbf1f9aa63bcdb529e
DTS <rlgus621@naver.com>
2023-09-11 21:28:33 +0900

    Wee2 [step3] 야호

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -20,35 +20,29 @@ func generateLottoNumbers() -> Array<Int> {
     return lottoNumbers.sorted()
 }
 
-var winningNumbers: [Int] = generateLottoNumbers()
-
-print("로또당첨번호는 \(convertArray(arr: winningNumbers))입니다")
-
-
-func confirmWinningLotto() {
-    var myWinningNumbers: [Int] = []
-
-    for number in myLottoNumbers {
-        if winningNumbers.contains(number) {
-            myWinningNumbers.append(number)
-        }
-    }
-
-    if myWinningNumbers.isEmpty {
-        // myWinningNumbers.isEmpty() --> myWinningNumbers.count == 0
-        print("아쉽지만 겹치는 번호가 없습니다.")
-    } else {
-        print("축하합니다! 겹치는 번호는 \(convertArray(arr:  myWinningNumbers.sorted()))입니다!")
-    }
-}
-
-var myLottoNumbers: [Int] = [13, 10, 6, 37, 24, 45] // 나의 로또번호 입력
-
 func convertArray(arr: [Int]) -> String {
     let strArray: [String] = arr.map( {String($0)} )
     let resultStr: String = strArray.joined(separator: ", ")
     return resultStr
 }
 
-confirmWinningLotto()
+func saveNthTimeLottoNumbers() {
+    i += 1
+    let winningNumbersValue = generateLottoNumbers()
+    print("\(i)회차의 로또 당첨 번호는 \(winningNumbersValue)입니다.")
+    NthTimeLottoNumbers["\(i)회차"] = winningNumbersValue
+}
 
+var NthTimeLottoNumbers : [String : Array<Int>] = [ : ]
+var i = 0
+
+for _ in 1...5 {
+    saveNthTimeLottoNumbers()
+}
+print(NthTimeLottoNumbers)
+
+
+let n = 5
+if let nthNumbers = NthTimeLottoNumbers["\(n)회차"]{
+    print("\(n)회차의 로또 당첨 번호는 \(convertArray(arr: nthNumbers.sorted()))입니다.")
+}
