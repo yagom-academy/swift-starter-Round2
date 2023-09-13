d73b6793d2c6f7b928c7d047c523a90cf133a93a
유지완 <yujiwan@yujiwan-ui-MacBookPro.local>
2023-09-13 23:17:27 +0900

    제출

--- CodeStarterCamp_Week2/createNumder.swift
+++ CodeStarterCamp_Week2/createNumder.swift
@@ -6,8 +6,7 @@
 //
 import Foundation
 
-
-func createLottoNumder() -> Set<Int>{
+func createLottoNumder() -> Set<Int> {
     var generateLotto : Set<Int> = []
 
     while generateLotto.count < 6{
@@ -15,4 +14,23 @@ func createLottoNumder() -> Set<Int>{
         generateLotto.insert(number)
     }
     return generateLotto
+}
+
+func generateLottoNumbersForRounds(_ round: Int) -> [Int: [Int]] {
+    var lottoResults: [Int: [Int]] = [:]
+    
+    for round in 1...round {
+        let LottoNumber = createLottoNumder()
+        lottoResults[round] = Array(LottoNumber)
+        print("\(round)회차의 로또 당첨 번호는 \(LottoNumber) 입니다.")
+    }
+    return lottoResults
+}
+
+func displayLottoResultForRound(desiredRound: Int) {
+    if let desiredLotto = lottoResults[desiredRound] {
+        print("\(desiredRound)회차의 로또 당첨 번호는 \(desiredLotto.sorted()) 입니다.")
+    } else {
+        print("\(desiredRound)회차의 로또 당첨 번호를 찾을 수 없습니다.")
+    }
 }
--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,29 +8,9 @@
 
 import Foundation
 
-createLottoNumder()
 
-var lottoResults: [Int: Set<Int>] = [:]
+var lottoResults = generateLottoNumbersForRounds(5)
+let myNumder = createLottoNumder()
 
-func lottoNumderRound() -> [Int: Set<Int>]{
-    for round in 1...5 {
-        let LottoNumber = createLottoNumder()
-        lottoResults[round] = LottoNumber
-        print("\(round)회차의 로또 당첨 번호는 \(LottoNumber) 입니다.")
-    }
-    return lottoResults
-}
-lottoNumderRound()
-
-func lottoDesiredRound(desiredRound: Int = 2){
-    
-    if let desiredLotto = lottoResults[desiredRound]{
-        print("\(desiredRound)회차의 로또 당첨 번호는 \(desiredLotto.sorted()) 입니다.")
-    } else {
-        print("\(desiredRound)회차의 로또 당첨 번호를 찾을 수 없습니다.")
-    }
-}
-lottoDesiredRound(desiredRound: 2)
-
-
+displayLottoResultForRound(desiredRound: 2)
 
