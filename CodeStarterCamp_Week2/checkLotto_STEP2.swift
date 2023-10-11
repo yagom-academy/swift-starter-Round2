1435cb63075cbd3fa1b4e665854573ca5925d243
marukim <sjybext@naver.com>
2023-10-11 12:48:01 +0900

    feat: 로또 당첨 번호를 생성하는 함수 생성

--- CodeStarterCamp_Week2/checkLotto_STEP2.swift
+++ CodeStarterCamp_Week2/checkLotto_STEP2.swift
@@ -1,5 +1,7 @@
 
-//func makeRandomNumber() -> Array<Int> {
-//    let randomNumber = [Int](1...45)
-//}
-
+func makeLottoNumbers() -> Array<Int> {
+    let initNumbers = [Int](1...45)
+    let lottoNumbers = initNumbers.shuffled()[0...5].sorted()
+    
+    return lottoNumbers
+}
