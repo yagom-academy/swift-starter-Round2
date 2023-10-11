7f55ac02ca33ab4532bb489d4ee8492e4ff6152d
joonHyoung <redsnow78@gmail.com>
2023-10-11 14:21:20 +0900

    makeLottoNumbers 로또당첨번호, checkWinningLottoNumbers 로또 당청번호 체크 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,44 @@
 
 import Foundation
 
-print("Hello, World!")
+var myLottoNumbers: [Int] = []
+myLottoNumbers = [1, 2, 3, 4, 5, 6]
 
+checkWinningLottoNumbers(myNumbers: myLottoNumbers, winnigNumbers: makeLottoNumbers())
+
+func makeLottoNumbers() -> Set<Int> {
+    var lottoNumbers: Set<Int> = Set<Int>()
+    
+    while lottoNumbers.count < 6 {
+        let oneLottoNumber: Int = Int.random(in: 1...45)
+        lottoNumbers.insert(oneLottoNumber)
+    }
+    
+    return lottoNumbers
+}
+
+func checkWinningLottoNumbers(myNumbers: Array<Int>, winnigNumbers: Set<Int> ) {
+    // Set 의 집합연산 활용을 위해 myNumbers를 Set type 변경
+    let changedMyNumbers = Set(myNumbers)
+    // 당첨번호와 내로또번호의 교집합 - 당첨번호 확인
+    let intersectionNumbers: Set<Int> = winnigNumbers.intersection(changedMyNumbers)
+    
+    if intersectionNumbers.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        // 겹치는 번호에서 "[", "]" 제거 - 고차 함수
+        /*
+        let printIntersectionNumbers = intersectionNumbers.map { String($0) }.joined(separator: ", ")
+        print("축하합니다! 겹치는 번호는 \(printIntersectionNumbers) 입니다!")
+        */
+        print("축하합니다! 겹치는 번호는", terminator: " ")
+        for (i, number) in intersectionNumbers.enumerated() {
+            if i < intersectionNumbers.count - 1 {
+                print("\(number)", terminator: ", ")
+            } else {
+                print("\(number)", terminator: "")
+            }
+        }
+        print(" 입니다!")
+    }
+}
