afc8366e42b7b1169c55b7704776750a48beda02
joonHyoung <redsnow78@gmail.com>
2023-10-13 15:35:42 +0900

    회차별 로또번호 저장, 확인 함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -13,6 +13,37 @@ myLottoNumbers = [1, 2, 3, 4, 5, 6]
 
 checkWinningLottoNumbers(myNumbers: myLottoNumbers, winnigNumbers: makeLottoNumbers())
 
+var listWinnigLottoNumbers: [String: Set<Int>] = [:]
+
+for _ in 1...5 {
+    saveWinningLottoNumbers(winnigNumbers: makeLottoNumbers())
+}
+
+printWinningNumbersForRound(round: 2)
+
+
+func printWinningNumbersForRound(round: Int) {
+    let roundString = makeRoundCountString(roundCount: round)
+    
+    if let winnigNumbers = listWinnigLottoNumbers[roundString] {
+        print("\(roundString)의 로또 당첨 번호는 " + makePrintIntersectionNumbers(intersectionNumbers: winnigNumbers) + " 입니다.")
+    }
+}
+
+func saveWinningLottoNumbers(winnigNumbers: Set<Int>) {
+    let roundCountString = makeRoundCountString(roundCount: listWinnigLottoNumbers.count + 1)
+    
+    listWinnigLottoNumbers[roundCountString] = winnigNumbers
+}
+
+func makeRoundCountString(roundCount: Int) -> String {
+    return String(roundCount) + "회차"
+}
+
+func makePrintIntersectionNumbers(intersectionNumbers: Set<Int>) -> String {
+    return intersectionNumbers.map { String($0) }.joined(separator: ", ")
+}
+
 func makeLottoNumbers() -> Set<Int> {
     var lottoNumbers: Set<Int> = Set<Int>()
     
@@ -37,8 +68,7 @@ func makeReturnMessage(intersectionNumbers: Set<Int>) -> String {
     if intersectionNumbers.isEmpty {
         returnMessage = "아쉽지만 겹치는 번호가 없습니다."
     } else {
-        let printIntersectionNumbers = intersectionNumbers.map { String($0) }.joined(separator: ", ")
-        returnMessage = "축하합니다! 겹치는 번호는 " + printIntersectionNumbers + " 입니다!"
+        returnMessage = "축하합니다! 겹치는 번호는 " + makePrintIntersectionNumbers(intersectionNumbers: intersectionNumbers) + " 입니다!"
     }
     
     return returnMessage
