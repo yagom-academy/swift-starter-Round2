b9b56465f4117811b931c311aef702053a7c37ae
UDI <honran.dev.94@gmail.com>
2023-10-18 16:05:46 +0900

    Feat : 당첨 로또 번호 회차별 저장
    - 로또 추첨을 5번 진행
    - 각 회차별 로또 번호 저장
    - 회차별 로또번호 검색 진행

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -16,14 +16,27 @@ func generateLottoNumbers() -> Set<Int> {
     
     return randomIntegers
 }
+var lottoDictionary: Dictionary<Int?, Set<Int>?> = [Int?: Set<Int>?]()
+lottoDictionary = [:]
 
-let myLottoNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
-let lottoIntersection: Set<Int> = myLottoNumbers.intersection(generateLottoNumbers())
-
-
-if lottoIntersection.count != 0 {
-    let setAsString = lottoIntersection.map { String($0) }.joined(separator:", ")
-    print("축하합니다! 겹치는 번호는 \(setAsString) 입니다!")
-} else {
-    print("아쉽지만 겹치는 번호가 없습니다.")
+func saveLottoNumbers() {
+    for count in 1...5{
+        lottoDictionary[count] = generateLottoNumbers()
+    }
 }
+
+func findLottoDrawing() {
+    print("찾는 회차 입력 : ")
+    if let inputValue = readLine(), let intValue = Int(inputValue) {
+        if let lottoNumbers = lottoDictionary[intValue] {
+            print("\(intValue)회차의 로또 당첨 번호는 \(lottoNumbers ?? Set<Int>()) 입니다.")
+        } else {
+            print("\(intValue)회차의 로또번호 추첨을 진행하지 않았습니다.")
+        }
+    } else {
+        print("올바른 숫자를 입력하세요.")
+    }
+}
+
+saveLottoNumbers()
+findLottoDrawing()
