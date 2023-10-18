d3e9708988824a0104ce194a14d0c0ef32f8245e
lucidly.na@gmail.com <lucidly.na@gmail.com>
2023-10-18 13:55:01 +0900

    로또 번호 생성 함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,11 @@
 
 import Foundation
 
-print("Hello, World!")
+var lottoGameResult: Set<Int> = Set<Int>()
+
+while lottoGameResult.count < 6 {
+    lottoGameResult.insert(Int.random(in: 1...45))
+}
+
+print("당첨번호 : \(lottoGameResult)")
 
