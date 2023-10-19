d51b9d534cd83a3acf0dd5223155f2a6572eb4cc
seokyung402 <105649543+seokyung402@users.noreply.github.com>
2023-10-19 19:04:40 +0900

    STEP 2 완료

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,4 +8,24 @@
 
 import Foundation
 
-print("Hello, World!")
+var lottoNum: Set<Int> = []
+func lottoWin() {
+    while lottoNum.count < 6 {
+        lottoNum.insert(Int.random(in: 1...45))
+    }
+}
+
+lottoWin()
+
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+
+func drawLot() {
+    let num: Set<Int> = lottoNum.intersection(myLottoNumbers)
+    let numString = num.sorted().map {String($0)}.joined(separator: ", ")
+
+    if num.count > 0 {
+        print("축하합니다! 겹치는 번호는 \(numString) 입니다!")
+    } else { print("아쉽지만 겹치는 번호가 없습니다.") }
+}
+
+drawLot()
