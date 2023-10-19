a72d827a6eaaa7e396fd0ce062358fea289c8510
lucie <146562581+lucidly-lucie@users.noreply.github.com>
2023-10-19 16:00:46 +0900

    Week2 [STEP 2] lucie (#627)
    * 로또 당첨번호 생성기 순서도 추가
    
    * 로또 번호 생성 함수 추가
    
    * 로또 번호 대조 함수 추가
    
    * 로또번호 생성 함수 추가
    
    * 로또 실행 함수 추가
    
    * 로또번호 대조 함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,7 +6,31 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
 
-print("Hello, World!")
+func drawLottoNumbers() -> Set<Int>{
+    var lottoNumbers: Set<Int> = []
+    while lottoNumbers.count < 6 {
+        lottoNumbers.insert(Int.random(in: 1...45))
+    }
+        return lottoNumbers
+}
+    
+func matchedLotto(myLottoNumbers: Set<Int>) {
+    let matchedNumbers: Set<Int> = drawLottoNumbers().intersection(myLottoNumbers)
+    let openNumbers = matchedNumbers.map { String($0) }.joined(separator: ", ")
+    if !matchedNumbers.isEmpty
+    {
+        print("축하합니다! 겹치는 번호는 \(openNumbers) 입니다!")
+    }
+    else
+    {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+    
+func playLotto() {
+    let myLottoNumbers : Set<Int> = [1, 2, 3, 4, 5, 6]
+    matchedLotto(myLottoNumbers: myLottoNumbers)
+    }
 
+playLotto()
