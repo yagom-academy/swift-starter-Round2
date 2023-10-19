c7350b8d652244732c6c557745f1b80e6c13810b
seokyung402 <105649543+seokyung402@users.noreply.github.com>
2023-10-19 22:38:25 +0900

    STEP 2 수정완료

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,24 +8,25 @@
 
 import Foundation
 
-var lottoNum: Set<Int> = []
-func lottoWin() {
-    while lottoNum.count < 6 {
-        lottoNum.insert(Int.random(in: 1...45))
+var lottoNumber: Set<Int> = []
+func winLotto() -> Set<Int> {
+    while lottoNumber.count < 6 {
+        lottoNumber.insert(Int.random(in: 1...45))
     }
+    return lottoNumber
 }
-
-lottoWin()
 
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
-func drawLot() {
-    let num: Set<Int> = lottoNum.intersection(myLottoNumbers)
-    let numString = num.sorted().map {String($0)}.joined(separator: ", ")
+func drawLotto() {
+    let sameNumber: Set<Int> = winLotto().intersection(myLottoNumbers)
+    let numberString = sameNumber.sorted().map { String($0) }.joined(separator: ", ")
 
-    if num.count > 0 {
-        print("축하합니다! 겹치는 번호는 \(numString) 입니다!")
-    } else { print("아쉽지만 겹치는 번호가 없습니다.") }
+    if sameNumber.count > 0 {
+        print("축하합니다! 겹치는 번호는 \(numberString) 입니다!")
+    } else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
 }
 
-drawLot()
+drawLotto()
