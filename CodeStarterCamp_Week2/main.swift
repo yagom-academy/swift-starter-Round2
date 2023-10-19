a6917f3e5fb104a8c907dc33c4e6e2c70b76ee50
lucidly.na@gmail.com <lucidly.na@gmail.com>
2023-10-19 14:46:01 +0900

    로또번호 생성 함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,21 +6,12 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
 
-var lottoGameResult: Set<Int> = Set<Int>()
-
-while lottoGameResult.count < 6 {
-    lottoGameResult.insert(Int.random(in: 1...45))
-}
-
-let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-
-let matchedNumbers: Set<Int> = lottoGameResult.intersection(myLottoNumbers)
-let openNumbers = matchedNumbers.map { String($0) }.joined(separator: ", ")
-
-if matchedNumbers.count >= 1 {
-    print("축하합니다! 겹치는 번호는 \(openNumbers) 입니다!") }
-else {print("아쉽지만 겹치는 번호가 없습니다.")
+func drawLottoNumbers() -> Set<Int>{
+    var lottoNumbers: Set<Int> = []
+    while lottoNumbers.count < 6 {
+        lottoNumbers.insert(Int.random(in: 1...45))
+    }
+        return lottoNumbers
 }
 
