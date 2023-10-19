10b928840b21c65df6816c5b57d7a0a9b9d4a0ab
lucidly.na@gmail.com <lucidly.na@gmail.com>
2023-10-19 14:48:48 +0900

    로또번호 대조 함수 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -14,10 +14,23 @@ func drawLottoNumbers() -> Set<Int>{
     }
         return lottoNumbers
 }
-
+    
+func matchedLotto(myLottoNumbers: Set<Int>) {
+    let matchedNumbers: Set<Int> = drawLottoNumbers().intersection(myLottoNumbers)
+    let openNumbers = matchedNumbers.map { String($0) }.joined(separator: ", ")
+    if !matchedNumbers.isEmpty
+    {
+        print("축하합니다! 겹치는 번호는 \(openNumbers) 입니다!")
+    }
+    else
+    {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
     
 func playLotto() {
     let myLottoNumbers : Set<Int> = [1, 2, 3, 4, 5, 6]
     matchedLotto(myLottoNumbers: myLottoNumbers)
     }
 
+playLotto()
