e0cb7e12c2da6aa72b39079e89e89f4ffa0d5368
lucidly.na@gmail.com <lucidly.na@gmail.com>
2023-10-22 01:12:56 +0900

    이전 과제 내역 삭제

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,27 +6,6 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-/*
- func matchedLotto(myLottoNumbers: Set<Int>) {
- let matchedNumbers: Set<Int> = drawLottoNumbers().intersection(myLottoNumbers)
- let openNumbers = matchedNumbers.map { String($0) }.joined(separator: ", ")
- if !matchedNumbers.isEmpty
- {
- print("축하합니다! 겹치는 번호는 \(openNumbers) 입니다!")
- }
- else
- {
- print("아쉽지만 겹치는 번호가 없습니다.")
- }
- }
- 
- func playLotto() {
- let myLottoNumbers : Set<Int> = [1, 2, 3, 4, 5, 6]
- matchedLotto(myLottoNumbers: myLottoNumbers)
- }
- 
- playLotto()
- */
 
 func drawLottoNumbers() -> Set<Int>{
     var lottoNumbers: Set<Int> = []
@@ -61,5 +40,6 @@ func searchHistory(회차: String) {
 }
 
 
+print(lottoDrawHistory())
 searchHistory(회차: "3회차")
 
