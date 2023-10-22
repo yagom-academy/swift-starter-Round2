ce141318f7619c4774595af4df574011093db9c9
lucie <146562581+lucidly-lucie@users.noreply.github.com>
2023-10-22 18:29:18 +0900

    Week2 [STEP 3] lucie (#633)
    * 로또 당첨번호 생성기 순서도 추가
    
    * 로또 번호 생성 함수 추가
    
    * 로또 번호 대조 함수 추가
    
    * 로또번호 생성 함수 추가
    
    * 로또 실행 함수 추가
    
    * 로또번호 대조 함수 추가
    
    * 로또 번호 히스토리 함수 추가
    
    * 회차에 따른 로또 번호 확인 함수 추가
    
    * 이전 과제 내역 삭제
    
    * lottoDrawHistory->recordLottoNumbers 함수 명칭 변경 및 반복문 수정
    
    * lottoDrawHistory->recordLottoNumbers 함수 명칭 변경 및 반복문 수정
    
    * drawLottoNembers-> generateLottoNumbers 함수 명칭 변경
    
    ---------
    
    Co-authored-by: DuDu <whqtkf12@naver.com>

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,31 +6,34 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-
-func drawLottoNumbers() -> Set<Int>{
+func generateLottoNumbers() -> Set<Int> {
     var lottoNumbers: Set<Int> = []
     while lottoNumbers.count < 6 {
         lottoNumbers.insert(Int.random(in: 1...45))
     }
-        return lottoNumbers
+  
+    return lottoNumbers
 }
-    
-func matchedLotto(myLottoNumbers: Set<Int>) {
-    let matchedNumbers: Set<Int> = drawLottoNumbers().intersection(myLottoNumbers)
-    let openNumbers = matchedNumbers.map { String($0) }.joined(separator: ", ")
-    if !matchedNumbers.isEmpty
+
+func recordLottoNumber() -> Dictionary<String, Set<Int>> {
+    var numberHistory: Dictionary<String, Set<Int>> = [:]
+    for drawCount in 1...5 {
+        numberHistory["\(drawCount)회차"] = generateLottoNumbers()
+    }
+    return numberHistory
+}
+
+func searchHistory(회차: String) {
+    let lottoResult = recordLottoNumber()
+    if let resultNumbers = lottoResult[회차]
     {
-        print("축하합니다! 겹치는 번호는 \(openNumbers) 입니다!")
+        let stringResultNumbers = resultNumbers.map { String($0) }.joined(separator: ", ")
+        print("\(회차)의 로또 당첨 번호는 \(stringResultNumbers) 입니다.")
     }
     else
     {
-        print("아쉽지만 겹치는 번호가 없습니다.")
+        print("\(회차)의 결과가 없습니다.")
     }
 }
-    
-func playLotto() {
-    let myLottoNumbers : Set<Int> = [1, 2, 3, 4, 5, 6]
-    matchedLotto(myLottoNumbers: myLottoNumbers)
-    }
 
-playLotto()
+searchHistory(회차: "3회차")
