0c5570dbc74468fcda051692fb8e1034f314d02c
podo <snowcountry@icloud.com>
2023-10-22 17:41:29 +0900

    feat: Add function for checking lotto number

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,7 +6,27 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
+func makeLottoNumbers() -> Set<Int> {
+    var lottoNumbers: Set<Int> = []
+    
+    while lottoNumbers.count < 6 {
+       let lottoNumber = Int.random(in: 1...45)
+        lottoNumbers.insert(lottoNumber)
+    }
+    
+    return lottoNumbers
+}
 
-print("Hello, World!")
+func checkLottoNumbers() {
+    let myLottoNumbers: [Int] = [13, 22, 29, 37, 40, 44]
+    let winningLottoNumbers = makeLottoNumbers()
+    let resultMyLottoNumbers = Set(myLottoNumbers).intersection(winningLottoNumbers).map { String($0) }.joined(separator:", ")
+    
+    if resultMyLottoNumbers.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는 \(resultMyLottoNumbers) 입니다!")
+        }
+    }
 
+checkLottoNumbers()
