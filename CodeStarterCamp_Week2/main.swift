b484a1c0a2fb20df875017bf0e9f7da64ea1cc7f
Orderlee <nuncateolvidar3@gmail.com>
2023-10-24 01:23:13 +0900

    fixed week2/main.swift

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,33 +1,28 @@
-// Step 2
 func generateLottoNumbers() -> [Int] {
     var lottoNumbers: Set<Int> = []
     while lottoNumbers.count < 6 {
-        let randomNumber = Int.random(in: 1...6)
+        let randomNumber = Int.random(in: 1...45)
         lottoNumbers.insert(randomNumber)
     }
     let sortedLottoNumbers = lottoNumbers.sorted()
     return sortedLottoNumbers
 }
 
-
-
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-
 
 func findMatchingNumbers() {
     let lottoResult = generateLottoNumbers()
     print("로또 당첨 번호: \(lottoResult)")
+    
     let matchingNumbers = Set(myLottoNumbers).intersection(lottoResult)
-    if myLottoNumbers != lottoResult {
+    
+    if matchingNumbers.isEmpty {
         print("아쉽지만 겹치는 번호가 없습니다.")
-    } else if myLottoNumbers == lottoResult {
+    }
+    else {
         let matchingNumbersString = matchingNumbers.sorted().map { String($0) }.joined(separator: ", ")
         print("축하합니다! 겹치는 번호는 \(matchingNumbersString) 입니다!")
     }
 }
 
-
-
 findMatchingNumbers()
-
-
