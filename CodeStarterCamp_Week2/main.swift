c6463f903461b0db0f99451692207dcc506464f6
gaeng2y <gaeng2y@gmail.com>
2023-11-22 23:20:08 +0900

    Round2 - Step3 로또 당첨 번호를 회차별로 저장하고, 확인하자!

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,7 +9,10 @@
 import Foundation
 
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+var round = 1
+var lotto: [String: Set<Int>] = [:]
 
+@discardableResult
 func generateLottoNumber() -> Set<Int> {
     var result = Set<Int>()
     
@@ -17,6 +20,9 @@ func generateLottoNumber() -> Set<Int> {
         let randomNumber = Int.random(in: 1...45)
         result.insert(randomNumber)
     }
+    
+    lotto["\(round)회차"] = result
+    round += 1
     
     return result
 }
@@ -28,9 +34,29 @@ func checkLottoNumbers(lottoNumbers: Set<Int>) {
         print("아쉽지만 겹치는 번호가 없습니다.")
         return
     }
-    let joinedNumbers = matchedNumbers.map { String($0) }.joined(separator: ", ")
+    let joinedNumbers = matchedNumbers.toString
     print("축하합니다! 겹치는 번호는 \(joinedNumbers) 입니다!")
 }
 
-let lottoNumbers = generateLottoNumber()
-checkLottoNumbers(lottoNumbers: lottoNumbers)
+func printLottoNumbers() {
+    guard let input = readLine(),
+          let round = Int(input),
+          let numbers = lotto["\(round)회차"] else {
+        print("잘못 입력하셨거나 없는 회차를 입력했습니다.")
+        return
+    }
+    
+    let joinedNumbers = numbers.toString
+    print("\(round)회차의 로또 당첨 번호는 \(joinedNumbers) 입니다.")
+}
+
+for _ in 0..<5 {
+    generateLottoNumber()
+}
+printLottoNumbers()
+
+extension Set where Element == Int {
+    var toString: String {
+        self.map { String($0) }.joined(separator: ", ")
+    }
+}
