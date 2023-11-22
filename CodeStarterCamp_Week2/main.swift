1e2bba09abf9ab9ee9f92eefab99543e1223d6dd
gaeng2y <gaeng2y@gmail.com>
2023-11-22 20:45:35 +0900

    Step 2 내 번호와 맞추어보기

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,33 @@
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
+func lottoNumberGenerator() -> [Int] {
+    var numbers = Array(1...45)
+    var result = [Int]()
+    
+    for _ in 0..<6 {
+        guard let randomNumbers = numbers.randomElement() else { continue }
+        result.append(randomNumbers)
+        
+        guard let index = numbers.firstIndex(of: randomNumbers) else { continue }
+        numbers.remove(at: index)
+    }
+    
+    return result
+}
+
+func checkLottoNumbers(lottoNumbers: [Int]) {
+    let matchedNumbers = lottoNumbers.filter { myLottoNumbers.contains($0) }
+    
+    guard !matchedNumbers.isEmpty else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+        return
+    }
+    let joinedNumbers = matchedNumbers.map { String($0) }.joined(separator: ", ")
+    print("축하합니다! 겹치는 번호는 \(joinedNumbers) 입니다!")
+}
+
+let lottoNumbers = lottoNumberGenerator()
+checkLottoNumbers(lottoNumbers: lottoNumbers)
