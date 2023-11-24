510ed1664992ec7be5b86d5172745dca6738901f
이대호 <idaeho@idaehoui-MacBookAir.local>
2023-11-24 22:43:24 +0900

    Round2[Step3]-Daeho

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -9,47 +9,56 @@
 import Foundation
 
 
-// 나의 로또 번호
-let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-
-
-// 로또 생성 함수
+// 로또 번호 생성 함수
 func createLottoNumber() -> Set<Int> {
     var createLottoNumber: Set<Int> = Set<Int>()
+    
     
     while createLottoNumber.count < 6 {
         createLottoNumber.insert(Int.random(in: 1...45))
     }
-    
+
     return createLottoNumber
+
+}
+
+// 생성된 로또 번호 및 회차를 저장하는 함수
+func storeLotto() -> Dictionary<String, Set<Int>> {
+    // 회차 와 로또 당첨 번호를 저장하는 딕셔너리 타입에 변수 생성
+    var lottoInfomation: Dictionary<String, Set<Int>> = Dictionary<String, Set<Int>>()
     
+    for number in 1...5 {
+        lottoInfomation["\(number)회차"] = createLottoNumber()
+    }
+    return lottoInfomation
     
 }
 
-// 나의 로또 번호랑 생성된 로또번호 비교 함수
-func compareLottoNumber(){
-    var resultLottoNumber: Array<Int> = Array<Int>()
+
+// 로또 번호 및 회차 출력
+func checkLottoNumber(round: Int) {
+    
     var lottoNumber: String = ""
-    let createLotto = createLottoNumber()
-    
-    for number in myLottoNumbers{
-        if createLotto.contains(number){
-            resultLottoNumber.append(number)
+    let lottoInfomation = storeLotto()
+ 
+    // 해당 하는 회차에 값이 있으면 회차 및 로또 번호 출력
+    // 옵셔널 바인딩으로 안전하게 로또 번호 출력
+    if let numbers = lottoInfomation["\(round)회차"] {
+        
+        for number in numbers {
+            lottoNumber.append("\(number), ")
         }
+        
+        lottoNumber.removeLast()
+        lottoNumber.removeLast()
+        print("\(round)회차의 로또 당첨 번호는 \(lottoNumber) 입니다.")
+    }else{
+        print("회차 정보가 없습니다.")
     }
-    
-    guard !resultLottoNumber.isEmpty else{
-        print("아쉽지만 겹치는 번호가 없습니다.")
-        return
-    }
-    
-    for number in resultLottoNumber{
-        lottoNumber.append("\(number),")
-    }
-
-    lottoNumber.removeLast()
-    print("축하합니다.! 겹치는 번호는 \(lottoNumber) 입니다!")
-    
 }
 
-compareLottoNumber()
+checkLottoNumber(round : 3)
+
+
+    
+    
