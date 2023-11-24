a5f82144c1d7ccbba791ddd5f67fba8b98f541d0
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2023-11-25 03:51:11 +0900

    Step 2_코딩

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,45 @@
 
 import Foundation
 
-print("Hello, World!")
+/// 로또 당첨 번호 생성
+/// - Returns: 랜덤으로 생성된 6개의 숫자가 포함된 Set 컬렉션타입
+func drawNumbers() -> Set<Int> {
+    var num: Int
+    var lotto: Set<Int> = Set<Int>()
+    
+    while lotto.count < 6 {
+        num = Int.random(in: 1...45)
+        if lotto.contains(num) {
+            continue
+        }
+        else {
+            lotto.insert(num)
+        }
+    }
+    
+    return lotto
+}
 
+/// 찍은 번호와 로또 당첨 번호의 겹치는 숫자를 확인하는 함수
+func guessLotto() {
+    let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+    var lottoNumbers: Set<Int> = Set<Int>()
+    var correctNumbers: [Int] = []
+    
+    lottoNumbers = drawNumbers()
+    for number in myLottoNumbers {
+        if (lottoNumbers.contains(number)){
+            correctNumbers.append(number)
+        }
+    }
+    
+    if correctNumbers.count > 0 {
+        let string = correctNumbers.map{ String($0) }.joined(separator: ", ")
+        print("축하합니다! 겹치는 번호는 \(string) 입니다!")
+    }
+    else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+
+guessLotto()
