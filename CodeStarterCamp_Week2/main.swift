58e280e733f3dfcee1fef712deb99bdc91d30c08
이대호 <idaeho@idaehoui-MacBookAir.local>
2023-11-27 14:35:12 +0900

    Round2[Step3]-Daeho 코드 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -10,26 +10,34 @@ import Foundation
 
 
 // 로또 번호 생성 함수
-func createLottoNumber() -> Set<Int> {
+func createLottoNumber() -> Array<Int> {
     var createLottoNumber: Set<Int> = Set<Int>()
-    
+    var makeLottoArray: Array<Int> = Array<Int>()
     
     while createLottoNumber.count < 6 {
         createLottoNumber.insert(Int.random(in: 1...45))
     }
+    
+    // 배열 타입으로 변환 해주기 위하여 구현
+    for number in createLottoNumber{
+        makeLottoArray.append(number)
+    }
 
-    return createLottoNumber
+  
+    return makeLottoArray
 
 }
 
 // 생성된 로또 번호 및 회차를 저장하는 함수
-func storeLotto() -> Dictionary<String, Set<Int>> {
+func storeLotto() -> Dictionary<String, Array<Int>>{
     // 회차 와 로또 당첨 번호를 저장하는 딕셔너리 타입에 변수 생성
-    var lottoInfomation: Dictionary<String, Set<Int>> = Dictionary<String, Set<Int>>()
+    var lottoInfomation: Dictionary<String, Array<Int>> = Dictionary<String, Array<Int>>()
     
+
     for number in 1...5 {
         lottoInfomation["\(number)회차"] = createLottoNumber()
     }
+
     return lottoInfomation
     
 }
@@ -38,27 +46,22 @@ func storeLotto() -> Dictionary<String, Set<Int>> {
 // 로또 번호 및 회차 출력
 func checkLottoNumber(round: Int) {
     
-    var lottoNumber: String = ""
+
     let lottoInfomation = storeLotto()
  
     // 해당 하는 회차에 값이 있으면 회차 및 로또 번호 출력
     // 옵셔널 바인딩으로 안전하게 로또 번호 출력
     if let numbers = lottoInfomation["\(round)회차"] {
-        
-        for number in numbers {
-            lottoNumber.append("\(number), ")
-        }
-        
-        lottoNumber.removeLast()
-        lottoNumber.removeLast()
+        var lottoNumber = numbers.map{ String( $0 ) }.joined(separator: ", ")
         print("\(round)회차의 로또 당첨 번호는 \(lottoNumber) 입니다.")
     }else{
         print("회차 정보가 없습니다.")
     }
 }
-
-checkLottoNumber(round : 3)
-
-
-    
+//createLottoNumber()
+checkLottoNumber(round : 1)
+//checkLottoNumber(round : 2)
+//checkLottoNumber(round : 3)
+//
+//    
     
