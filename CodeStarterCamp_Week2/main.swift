86e419a6a11381e1f694d1d497b369f7002468d0
Nat <cestbonciel88@knou.ac.kr>
2023-12-02 21:21:12 +0900

    Fix: changed the state of instances, create print the history Lotto Nums

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,12 +8,11 @@
 import Foundation
 
 //MARK: - [STEP3] 로또 당첨 번호를 회차별로 저장하고, 확인하자.
-var storeLotteryNumbers: [String: [Int]] = [:]
-var currentRound: Int = 0
+let storedLottoNums: [String: [Int]] = saveLotteryNumbers()
+
 
 //MARK: STEP2 에서 만들었던 로또 당첨번호 생성 함수
 func generateLottoNumbers() -> [Int] {
-    
     var winningLotteryNums = Set<Int>()
     
     while winningLotteryNums.count < 6 {
@@ -27,22 +26,23 @@ func generateLottoNumbers() -> [Int] {
 
 
 //MARK: Dictionary 타입 storeLotteryNumbers에 저장하는 saveLotteryNumbers
-func saveLotteryNumbers() {
-    currentRound += 1
-    let lotteryNumbers = generateLottoNumbers()
-    
-    storeLotteryNumbers["\(currentRound)회차"] = lotteryNumbers
+func saveLotteryNumbers() -> [String: [Int]] {
+    var perRoundLottery: [String: [Int]] = [:]
+    for perRound in 1...5 {
+        let lotteryNumber = generateLottoNumbers()
+        perRoundLottery["\(perRound)회차"] = lotteryNumber
+    }
+    return perRoundLottery
 }
 
-//MARK: 예시) 1~7회차까지 로또 당첨번호 생성기에서 회차, 로또번호 저장
-for _ in 1...7 {
-    saveLotteryNumbers()
-                   
-    if let round = storeLotteryNumbers.keys.sorted().last.flatMap({ storeLotteryNumbers[$0] }) {
-        let lastRoundNumber = storeLotteryNumbers.count
-        
-        let formattedNumbers = round.map({ String($0) }).joined(separator: ", ")
-        print("\(lastRoundNumber)회차의 로또 당첨 번호는 \(formattedNumbers) 입니다.")
+//MARK: 1~5회차까지 로또번호 출력하는 함수 생성
+func printLottoNumbers() {
+    let sortedRoundLottery = storedLottoNums.sorted { $0.key.localizedStandardCompare($1.key) == .orderedAscending }
+    for (round, lottery) in sortedRoundLottery {
+        let commaSeparatedNumbers = lottery.map { String($0) }.joined(separator: ", ")
+        print("\(round)의 로또 당첨 번호는 \(commaSeparatedNumbers) 입니다.")
     }
 }
+
+printLottoNumbers()
 
