877e792cd3292884f344c41b7fc74060d3c0211f
forseaest <u3z@naver.com>
2023-12-05 11:49:55 +0900

    [step2] 로또 번호 생성 및 비교하는 함수 작성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,36 @@
 
 import Foundation
 
-print("Hello, World!")
+func makeLotto() -> [Int] {
+    var randomLotto: [Int] = []
 
+    while randomLotto.count != 6 {
+        var randomNumber = Int.random(in: 1...45)
+        if randomLotto.contains(randomNumber) {
+            continue
+        } else {
+            randomLotto.append(randomNumber)
+        }
+    }
+
+    return randomLotto
+}
+
+func checkLotto(myLotto: [Int], prizeLotto: [Int]) {
+    var duplicateNumbers: [Int] = []
+
+    for myLottoNumber in myLotto {
+        if prizeLotto.contains(myLottoNumber) {
+            duplicateNumbers.append(myLottoNumber)
+        }
+    }
+
+    if duplicateNumbers.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는 \(duplicateNumbers.map { String($0) }.joined(separator: ", ")) 입니다!")
+    }
+}
+
+var myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+checkLotto(myLotto: myLottoNumbers, prizeLotto: makeLotto())
