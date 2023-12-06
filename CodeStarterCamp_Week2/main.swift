b3cadfef5e693399573737554ecd9d99df9569f8
vegetable <gkstkdgus604@naver.com>
2023-12-06 19:17:41 +0900

    Round2[step2]-yachae

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,48 +1,51 @@
-//
 //  main.swift
 //  CodeStarterCamp_Week2
 //
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
+
 // 나의 로또 번호
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-let numbers = Array(1...45)
 
-func createLottoNumbers() ->Set<Int>{
-
-    var createLottoNumbers: Set<Int> = [] // 순서가 없고 멤버가 유일한 것을 보장 (중복을 막음)
+func generateLottoNumbers() -> Set<Int> {
+    var generatedLottoNumbers: Set<Int> = [] // 순서가 없고 멤버가 유일한 것을 보장 (중복을 막음)
     
-    while createLottoNumbers.count < 6 {
-        createLottoNumbers.insert(numbers.randomElement()!)
-        //numbers배열에서 랜덤요소를 createLottoNumbers에 인서트함
+    while generatedLottoNumbers.count < 6 {
+        generatedLottoNumbers.insert(Int.random(in: 1..<45))
     }
-    return createLottoNumbers //당첨번호를 반환함
+    return generatedLottoNumbers
 }
+
 // 로또 번호 비교 함수
-func compareLottoNumber(){
-    var resultLottoNumber: Array<Int> = [] //겹치는 로또번호를 저장할 배열
-    var lottoNumber: String = "" // 겹치는 로또 번호를 문자열로 저장할 변수를 빈 문자열로 초기화
-    let createLotto = createLottoNumbers() //당첨번호 저장
+func compareLottoNumbers() -> [Int] {
+    var resultLottoNumbers: [Int] = [] // 겹치는 로또 번호를 저장할 배열
+    let generatedLottoNumbers = generateLottoNumbers() // 당첨 번호 저장
     
-    for num in myLottoNumbers{
-        if createLotto.contains(num){ //num가 포함되어있는지를 확인
-            resultLottoNumber.append(num)//배열에 num을 추가
+    for num in myLottoNumbers {
+        if generatedLottoNumbers.contains(num) { // num가 포함되어 있는지 확인
+            resultLottoNumbers.append(num) // 배열에 num을 추가
         }
     }
-    
-    guard !resultLottoNumber.isEmpty else{ //배열이 비어있지않은지를 확인 비어있는경우 메시지출력
-        print("아쉽지만 겹치는 번호가 없습니다.")
-        return
-    }
-    
-    for number in resultLottoNumber{
-        lottoNumber.append("\(number),") //문자열로 변환하여 추가
-    }
-    
-    lottoNumber.removeLast() // 마지막문자 ,를 제거
-    print("축하합니다.! 겹치는 번호는 \(lottoNumber) 입니다!")
-    
+    return resultLottoNumbers
 }
-
-compareLottoNumber()
+    func printLottoNumbers() {
+        let restultLottoNumbers = compareLottoNumbers()
+        var lottoNumbers: String = "" //겹치는 로또 번호를 문자열로 저장할 변수를 빈 문자열로 초기화
+        
+        for number in restultLottoNumbers {
+            lottoNumbers.append("\(number), ")
+        }
+//        if lottoNumbers.isEmpty {
+//        print("아쉽지만 겹치는 번호가 없습니다.")
+//        } else {
+//            lottoNumbers.removeLast(2)
+//            print("축하합니다! 겹치는 번호는 \(lottoNumbers) 입니다!")
+//        }
+        guard lottoNumbers.isEmpty else {
+            lottoNumbers.removeLast(2)
+            return print("축하합니다! 겹치는 번호는 \(lottoNumbers) 입니다!")
+        }
+        return print("아쉽지만 겹치는 번호가 없습니다.")
+}
+printLottoNumbers()
