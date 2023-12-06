59c2be5a3bb57340fd17c5516f699307e7cc5919
manitw28 <manitw28@naver.com>
2023-12-06 21:52:40 +0900

    step2

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,45 @@
 
 import Foundation
 
-print("Hello, World!")
+var getLotto: Int = 0;
 
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+
+let lottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+
+for myLottoNumber in myLottoNumbers {
+    
+    for lottoNumber in lottoNumbers {
+        
+        if(lottoNumber == myLottoNumber) {
+            getLotto += 1;
+        }
+    }
+}
+
+if(getLotto == 6) {
+    print("축하합니다! 겹치는 번호는 1, 2, 3, 4, 5, 6 입니다!")
+} else {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+}
+
+
+var newGetLotto: Int = 0;
+
+let newLottoNumbers: [Int] = [11, 12, 13, 14, 15, 16]
+
+for myLottoNumber in myLottoNumbers {
+    
+    for newLottoNumber in newLottoNumbers {
+        
+        if(newLottoNumber == myLottoNumber) {
+            newGetLotto += 1;
+        }
+    }
+}
+
+if(newGetLotto == 6) {
+    print("축하합니다! 겹치는 번호는 1, 2, 3, 4, 5, 6 입니다!")
+} else {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+}
