b0bc3db21dad5c590b13f8bf571307d7152cda69
jihyeonjjang <jihyeon9975@gmail.com>
2023-12-13 20:41:36 +0900

    fix round2 step2

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-func makeNumber() -> Set<Int> {
+func makeLottoNumber() -> Set<Int> {
     var lottoNumber: Set<Int> = Set<Int>()
     while lottoNumber.count < 6 {
         lottoNumber.insert(Int.random(in: 1...45))
@@ -16,28 +16,16 @@ func makeNumber() -> Set<Int> {
     return lottoNumber
 }
 
+
 func compareNumber(lottoNumber: Set<Int>, mylottoNumbers: Array<Int>) {
-    var containNumber: [Int] = []
-    for i in 0...mylottoNumbers.count-1 {
-        if lottoNumber.contains(mylottoNumbers[i]) {
-            containNumber.append(mylottoNumbers[i])
-        }
-    }
-    if containNumber.count != 0 {
-        print("축하합니다! 겹치는 번호는 ", terminator: "")
-        
-        for j in 0...containNumber.count-1 {
-            if j == containNumber.count-1 {
-                print("\(containNumber[j])", terminator: " ")
-            } else {
-                print("\(containNumber[j])", terminator: ", ")
-            }
-        }
-        print("입니다!")
+    var containNumber: Set<Int> = Set<Int>()
+    containNumber = lottoNumber.intersection(mylottoNumbers)
+    if !(containNumber.isEmpty) {
+        print("축하합니다! 겹치는 번호는 \(containNumber.map{String($0)}.joined(separator:", ")) 입니다!")
     } else {
         print("아쉽지만 겹치는 번호가 없습니다.")
     }
 }
 
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
-compareNumber(lottoNumber: makeNumber(), mylottoNumbers: myLottoNumbers)
+compareNumber(lottoNumber: makeLottoNumber(), mylottoNumbers: myLottoNumbers)
