47b9b5fc39b466eb32e63b51cf95bc9cef54d514
manitw28 <manitw28@naver.com>
2023-12-15 22:15:10 +0900

    Round2 Step2 Resubmit

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,20 +8,17 @@
 
 import Foundation
 
+//Step2
 var myLottoNumbers: Set<Int> = [];
 var winLottoNumbers: Set<Int> = [];
 let numbers = Array(1...45);
 
-// 내 로또 번호 입력
 while myLottoNumbers.count < 6 {
-    print("내 로또 번호: ", terminator: "")
-    let input = Int(readLine()!)!
-    myLottoNumbers.insert(input)
+    myLottoNumbers.insert(numbers.randomElement()!)
 }
-var ascMyNum = myLottoNumbers.sorted(by: <)
-print(ascMyNum)
+var ascendMyNum = myLottoNumbers.sorted(by: <)
+print(ascendMyNum)
 
-// 당첨 번호 랜덤 추첨
 while winLottoNumbers.count < 6 {
     winLottoNumbers.insert(numbers.randomElement()!)
 }
@@ -29,34 +26,21 @@ var ascWinNum = winLottoNumbers.sorted(by: <)
 print(ascWinNum)
 
 var getLotto: Int = 0;
+var sameNumber: Set<Int> = [];
 for myLottoNumber in myLottoNumbers {
-
     for winLottoNumber in winLottoNumbers {
-
         if(winLottoNumber == myLottoNumber) {
+            sameNumber.insert(winLottoNumber)
             getLotto += 1;
         }
     }
 }
 
-
 switch getLotto {
 case 0:
-    print("꽝입니다!");
-case 1:
-    print("아쉽네요! 번호 1개만 맞았습니다.");
-case 2:
-    print("아쉽네요! 번호 2개만 맞았습니다.");
-case 3:
-    print("축하합니다! 번호 3개가 맞았습니다!");
-case 4:
-    print("축하합니다! 번호 4개가 맞았습니다!");
-case 5:
-    print("축하합니다! 번호 5개가 맞았습니다!");
-case 6:
-    print("축하합니다! 번호 6개가 맞았습니다!");
+    print("아쉽지만 겹치는 번호가 없습니다.!");
+case 1...6:
+    print("축하합니다! 겹치는 번호는 \(sameNumber)입니다!");
 default:
     print("번호를 비교하세요.")
 }
-
-
