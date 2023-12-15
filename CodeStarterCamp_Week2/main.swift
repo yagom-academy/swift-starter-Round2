45edf2bb3fd51dc749d26ae74313a4efa3010410
manitw28 <manitw28@naver.com>
2023-12-15 22:43:01 +0900

    Round3 Step3 New

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,39 +8,38 @@
 
 import Foundation
 
-//Step2
-var myLottoNumbers: Set<Int> = [];
-var winLottoNumbers: Set<Int> = [];
-let numbers = Array(1...45);
-
-while myLottoNumbers.count < 6 {
-    myLottoNumbers.insert(numbers.randomElement()!)
+//Step3
+func getNumbers() -> Set<Int> {
+    var number: Int
+    var lottoNumbers: Set<Int> = Set<Int>()
+    
+    while lottoNumbers.count < 6 {
+        number = Int.random(in: 1...45)
+        lottoNumbers.insert(number)
+    }
+    return lottoNumbers
 }
-var ascendMyNum = myLottoNumbers.sorted(by: <)
-print(ascendMyNum)
 
-while winLottoNumbers.count < 6 {
-    winLottoNumbers.insert(numbers.randomElement()!)
+func saveLottoRecord() -> Dictionary<String, Array<Int>> {
+    var lottoRecord: Dictionary<String, Array<Int>> = Dictionary<String, Array<Int>>()
+
+    for lottoTimes in 1...5 {
+        lottoRecord["\(lottoTimes)회차"] = Array(getNumbers())
+    }
+
+    return lottoRecord
 }
-var ascWinNum = winLottoNumbers.sorted(by: <)
-print(ascWinNum)
 
-var getLotto: Int = 0;
-var sameNumber: Set<Int> = [];
-for myLottoNumber in myLottoNumbers {
-    for winLottoNumber in winLottoNumbers {
-        if(winLottoNumber == myLottoNumber) {
-            sameNumber.insert(winLottoNumber)
-            getLotto += 1;
-        }
+let lottoNumberRecord: Dictionary<String?, Array<Int>> = saveLottoRecord()
+
+func printLottoNumbers(for lottoTime: String) {
+
+    if let lottoNumbers = lottoNumberRecord["\(lottoTime)"] {
+        let string = lottoNumbers.map{ String($0) }.joined(separator: ", ")
+        print("\(lottoTime)의 로또 당첨 번호는 \(string) 입니다.")
     }
 }
 
-switch getLotto {
-case 0:
-    print("아쉽지만 겹치는 번호가 없습니다.!");
-case 1...6:
-    print("축하합니다! 겹치는 번호는 \(sameNumber)입니다!");
-default:
-    print("번호를 비교하세요.")
+for time in 1...5 {
+    printLottoNumbers(for: "\(time)회차")
 }
