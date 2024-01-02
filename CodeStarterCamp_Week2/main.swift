8fe5c23a88649b7d5314961eb10ccfffca7a8746
Revolutionary-kunhee <vivakunhee@gmail.com>
2024-01-02 12:51:29 +0900

    step1 순서도 작성

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,34 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week2
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+// Create Array
+
+var integersFm: Array<Int> = Array<Int>()
+
+var integers: [Int] = [123,12,45,23]
+
+integers.append(1)
+integers.append(100)
+
+print(integers)
+
+var bool: Bool = integers.contains(50)
+
+print(bool)
+integers[0] = 50
+print(integers[0])
+print(integers)
+
+integers.remove(at: 0)
+print(integers[0])
+print(integers)
+
+integers.removeLast()
+print(integers)
+
+//integers.removeAll()
+//print(integers)
+
+print(integers.count)
+
+// Dictionary
 
