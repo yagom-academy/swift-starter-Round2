a6fb89da62d15a24c925999ad9f0c83bf20aed62
kwonseokki <kwons0704@gmail.com>
2024-01-05 23:13:58 +0900

    Round2[Step2]

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
+func makeLottoNumber() -> [Int] {
+    let lottoNumbers: [Int] = Array(1...45)
+    var lottoResults: [Int] = []
+    repeat {
+        let lottoNumber = lottoNumbers.randomElement()!
+        if !lottoResults.contains(lottoNumber) {
+            lottoResults.append(lottoNumber)
+        }
+    } while lottoResults.count < 6
+    return lottoResults
+}
+
+func compareLottoNumber(myLottoNumbers: [Int], lottoNumbers: [Int]) {
+    let commonNumbers = Set(myLottoNumbers).intersection(Set(lottoNumbers))
+    if commonNumbers.count != 0 {
+        print("축하합니다! 겹치는 번호는 \(commonNumbers.map {String($0)}.joined(separator: ", ")) 입니다!")
+    } else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+
+let lottoNumbers = makeLottoNumber()
+
+compareLottoNumber(myLottoNumbers: myLottoNumbers, lottoNumbers: lottoNumbers)
