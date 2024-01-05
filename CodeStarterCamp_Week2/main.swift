e3f22e7ca2565889e93c8e23c8fbf76c6266777a
Revolutionary-kunhee <vivakunhee@gmail.com>
2024-01-05 15:43:57 +0900

    feedback 반영

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -3,28 +3,24 @@ import Foundation
 // 로또 번호 생성 함수
 
 func makeLottoNums() -> Set<Int> {
-    var lotto: Set<Int> = []
+    var winningNumbers: Set<Int> = []
+    let candidates: [Int] = Array(1...45).shuffled()
     
-    while lotto.count < 6 {
-        let randomInt: Int = Int.random(in: 1...45)
-        lotto.insert(randomInt)
-    }
+    winningNumbers = Set(candidates.prefix(6))
     
-    return lotto
+    return winningNumbers
 }
 
 // 로또 번호 당첨 확인 함수
 
-func checkMyLotto(myNumbers: [Int], realNumbers: Set<Int>) -> Void {
+func checkMyLotto(myNumbers: Set<Int>, realNumbers: Set<Int>) -> Void {
     
-    let setMyNumbers: Set<Int> = Set(myNumbers)
-    
-    let intersectionLotto: Set<Int> = setMyNumbers.intersection(realNumbers)
+    let intersectionLotto: Set<Int> = myNumbers.intersection(realNumbers)
     
     if intersectionLotto.count == 0 {
         print("아쉽지만 겹치는 번호가 없습니다")
     } else {
-        let matchLotto: [String] = intersectionLotto.map{String($0)}
+        let matchLotto: [String] = intersectionLotto.map{ String($0) }
         let matchLottoString: String = matchLotto.joined(separator: ",")
         
         print("축하합니다! 겹치는 번호는 \(matchLottoString) 입니다! ")
@@ -33,6 +29,6 @@ func checkMyLotto(myNumbers: [Int], realNumbers: Set<Int>) -> Void {
 }
 
 let lotto: Set<Int> = makeLottoNums()
-let myLottoNumbers: [Int] = [39, 12, 15, 23, 45, 9]
+let myLottoNumbers: Set<Int> = [39, 12, 15, 23, 45, 9]
 
 checkMyLotto(myNumbers: myLottoNumbers, realNumbers: lotto)
