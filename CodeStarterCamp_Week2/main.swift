933783a9c35ab0e34e25d0e44411e96c083ac645
Revolutionary-kunhee <vivakunhee@gmail.com>
2024-01-08 23:22:50 +0900

    Error Handling 추가

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,8 +1,12 @@
 import Foundation
 
-// 로또 번호 생성 함수
+// Error enum
+enum mismatchError: Error {
+    case nonexistence
+}
 
-func makeLottoNums() -> Set<Int> {
+// 로또 번호 생성 함수
+func makeLottoNumbers() -> Set<Int> {
     var winningNumbers: Set<Int> = []
     let candidates: [Int] = Array(1...45).shuffled()
     
@@ -11,26 +15,37 @@ func makeLottoNums() -> Set<Int> {
     return winningNumbers
 }
 
-// 로또 번호 당첨 확인 함수
-
-func checkMyLotto(myNumbers: Set<Int>, realNumbers: Set<Int>) -> Void {
+// 로또 번호 당첨 검증 함수
+func verifyWinningNumbers(userNumbers: Set<Int>, officialNumbers: Set<Int>) throws {
     
-    let intersectionLotto: Set<Int> = myNumbers.intersection(realNumbers)
+    let intersectionLotto: Set<Int> = userNumbers.intersection(officialNumbers)
+    let matchLotto: [String] = intersectionLotto.map(String.init)
+    let matchLottoString: String = matchLotto.joined(separator: ",")
     
     guard intersectionLotto.count > 0 else {
-        print("아쉽지만 겹치는 번호가 없습니다")
-        return
+        throw mismatchError.nonexistence
     }
     
-    let matchLotto: [String] = intersectionLotto.map{ String($0) }
-    let matchLottoString: String = matchLotto.joined(separator: ",")
-        
     print("축하합니다! 겹치는 번호는 \(matchLottoString) 입니다! ")
-    
-    
 }
 
-let lotto: Set<Int> = makeLottoNums()
+// 로또 당첨 확인함수
+func checkMyLotto(myNumbers: Set<Int>, realNumbers: Set<Int>){
+    
+    do {
+        try verifyWinningNumbers(userNumbers: myNumbers, officialNumbers: realNumbers)
+        
+    } catch mismatchError.nonexistence {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } catch {
+        print("알 수 없는 오류")
+    }
+}
+
+// print("축하합니다! 겹치는 번호는 \(matchLottoString) 입니다! ")
+
+let lotto: Set<Int> = makeLottoNumbers()
 let myLottoNumbers: Set<Int> = [39, 12, 15, 23, 45, 9]
 
 checkMyLotto(myNumbers: myLottoNumbers, realNumbers: lotto)
+
