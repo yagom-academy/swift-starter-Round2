bcf064113b3db3b82d997a9d85988667e83c2611
hayden365 <ysmh100@gmail.com>
2024-01-10 00:02:30 +0900

    step3

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,9 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
 
-let generatedNumbers = generateLottoByLength(length: 6)
+// Step2
+
 let myNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
+let generatedNumbers = generateLottoByLength(length: 6)
 compareMyNumberToLotto(myNumbers: myNumbers)
+
+// Step3
+
+var lottoDictByRound: [Int: Set<Int>] = [:]
+
+func printLottoByRound(selectRound: Int, lottoNumber: Set<Int>) {
+    print("""
+    \(selectRound)회차의 로또 당첨 번호는 \(lottoNumber) 입니다.
+""")
+}
+
+let rounds = 5
+let selectRound = 2
+
+for round in 1...rounds {
+    let generatedNumbers =  generateLottoByLength(length: 6)
+    lottoDictByRound[round] = generatedNumbers
+}
+
+if let lottoNumbers = lottoDictByRound[selectRound] {
+    printLottoByRound(selectRound: selectRound, lottoNumber: lottoNumbers)
+} else {
+    print("No lotto numbers found for round \(selectRound)")
+}
