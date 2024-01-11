40cf8a234d5043df760e0345c11fa4af8c4a74a0
cty7574 <cty7574@gmail.com>
2024-01-11 22:19:48 +0900

    코드수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -19,9 +19,8 @@ func makeLotteryNumbers() -> [Int] {
     return result.sorted()
 }
 
-func makeLotteries() -> [String: [Int]] {
+func makeLotteries(lotterySize: Int) -> [String: [Int]] {
     var lotteries: [String: [Int]] = [:]
-    let lotterySize: Int = 5
     
     for i in 1...lotterySize {
         let round = "\(i)회차"
@@ -31,16 +30,18 @@ func makeLotteries() -> [String: [Int]] {
     return lotteries
 }
 
-func printLotteries(lotteries: [String: [Int]]) {
-    for i in 1...lotteries.count {
-        let round: String = "\(i)회차"
-        guard let lottery = lotteries[round] else { continue }
-        
-        let lotteryString: String = String(describing: lottery).dropFirst().dropLast().description
-        print(round + "의 로또 당첨 번호는 " + lotteryString + " 입니다.")
+func printSingleLottery(lotteries: [String: [Int]], round: Int) {
+    let roundText: String = "\(round)회차"
+    guard let lottery = lotteries[roundText] else {
+        print("회차 정보를 확인해주세요!")
+        return
     }
+    
+    let lotteryText: String = String(describing: lottery).dropFirst().dropLast().description
+    print(roundText + "의 로또 당첨 번호는 " + lotteryText + " 입니다.")
 }
 
-printLotteries(lotteries: makeLotteries())
+printSingleLottery(lotteries: makeLotteries(lotterySize: 5), round: 4)
+
 
 
