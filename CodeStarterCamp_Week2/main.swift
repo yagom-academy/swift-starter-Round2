ae65dc29423e0d383b9cd63870652489f52e2311
Revolutionary-kunhee <vivakunhee@gmail.com>
2024-01-11 17:25:35 +0900

    로또 회차별 번호 조회 함수

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -18,8 +18,11 @@ func makeLottoNumbers() -> Set<Int> {
     var winningNumbers: Set<Int> = []
     let candidates: [Int] = Array(1...45).shuffled()
     
-    winningNumbers = Set(candidates.prefix(6))
     
+    winningNumbers = Set(candidates.prefix(6))
+    currentRound += 1
+    historyOfLotto["\(currentRound)회차"] = Array(candidates.prefix(6))
+
     return winningNumbers
 }
 
@@ -50,8 +53,37 @@ func checkMyLotto(myNumbers: Set<Int>, realNumbers: Set<Int>){
     }
 }
 
-let lotto: Set<Int> = makeLottoNumbers()
-let myLottoNumbers: Set<Int> = [39, 12, 15, 23, 45, 9]
+// 로또 회차별 번호 조회 함수
+func showLottoHistory(_ round: Int = 0){
+    guard round > 0 else {
+        print(historyOfLotto)
+        return
+    }
+    
+    let numbers: [Int] = historyOfLotto["\(currentRound)회차"] ?? []
+    let numbersToString: String = numbers.sorted().map(String.init).joined(separator: ",")
+    
+    print("\(numbers)회차의 로또 당첨 번호는 \(numbersToString)입니다.")
+    
+}
 
-checkMyLotto(myNumbers: myLottoNumbers, realNumbers: lotto)
+// 로또 역사 변수
+var currentRound: Int = 0
+var historyOfLotto: [String:[Int]] = [:]
 
+// 오피셜 로또 변수
+var lottoRound: Int = 0
+var recordOfficialLotto: [String:[Int]] = [:]
+
+// 나의 로또 변수
+//let lotto: Set<Int> = makeLottoNumbers()
+//let myLottoNumbers: Set<Int> = [39, 12, 15, 23, 45, 9]
+//checkMyLotto(myNumbers: myLottoNumbers, realNumbers: lotto)
+
+makeLottoNumbers()
+makeLottoNumbers()
+makeLottoNumbers()
+makeLottoNumbers()
+makeLottoNumbers()
+
+showLottoHistory(1)
