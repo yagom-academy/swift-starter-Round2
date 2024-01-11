7e3b5ce141b45d6d0e49b2fdd28fe665ae506739
kwonseokki <seokki0706@gmail.com>
2024-01-11 09:42:53 +0900

    Round2[Step3] 피드백 반영

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,10 +8,10 @@
 
 import Foundation
 
-// 회차와 번호를 저장하는 Dictionary
-var dict = [String: [Int]]()
 
-// 회차와 로또번호를 저장하는 함수
+var lottoRoundInfo = [String: [Int]]()
+
+
 func makeLottoRound() {
     var lottoNumber = Set<Int>()
     
@@ -19,13 +19,13 @@ func makeLottoRound() {
         lottoNumber.insert(Int.random(in: 1...45))
     }
     
-    dict["\(dict.count + 1)회차"] = lottoNumber.map{Int($0)}
+    lottoRoundInfo["\(lottoRoundInfo.count + 1)회차"] = lottoNumber.map{Int($0)}
 }
 
-// 원하는 회차를 찾는 함수
-func findLottoRound(roundNumber round: Int) {
-    if let roundInfo = dict["\(round)회차"] {
-        print("\(round)회차의 로또 당첨 번호는 \(roundInfo.map{String($0)}.joined(separator: ", ")) 입니다.")
+
+func findLottoRound(roundNumber: Int) {
+    if let roundInfo = lottoRoundInfo["\(roundNumber)회차"] {
+        print("\(roundNumber)회차의 로또 당첨 번호는 \(roundInfo.map{String($0)}.joined(separator: ", ")) 입니다.")
     } else {
         print("진행되지 않은 회차입니다.")
     }
