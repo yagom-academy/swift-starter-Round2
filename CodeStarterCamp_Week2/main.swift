c2d280503c5c54bdb078285534a8357c1c8d82a3
xero <chanyeongsim@simCY-MacBook-Pro.local>
2024-01-15 17:17:13 +0900

    Round2[Step2] - Xero

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,44 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week2
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
+func lottoNumber() -> Array<Int> {
+    var lottoNumbers = [Int]()
+        while lottoNumbers.count < 6 {
+            var randomNumber = Int.random(in: 1...45)
+            if lottoNumbers.contains(randomNumber){
+                continue
+            } else {
+                lottoNumbers.append(randomNumber)
+            }
+            lottoNumbers.sort()
+        }
+    return lottoNumbers
+    }
+var lottoNumbers = lottoNumber()
+var myNumbers: [Int] = [1,2,3,4,5,6]
 
-import Foundation
+func checkLotto(lottoNumbers: [Int], myNumbers: [Int]) -> [Int] {
+    lottoNumber()
+    
+    var duplicates: [Int] = []
 
-print("Hello, World!")
+    for value in lottoNumbers {
+        if myNumbers.contains(value) {
+            duplicates.append(value)
+        }
+    }
+
+    return duplicates
+}
+
+let matchNumbers = checkLotto(lottoNumbers: lottoNumbers, myNumbers: myNumbers)
+
+print("로또 번호 :", lottoNumbers)
+print("나의 번호 :", myNumbers)
+
+if matchNumbers.isEmpty {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+} else {
+    print("축하합니다! 겹치는 번호는 \(matchNumbers)"+"입니다")
+}
+
+
+lottoNumber()
 
