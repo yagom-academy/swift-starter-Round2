e48a96459102a741c3f2ba9789f1c609a98a1a82
xero <chanyeongsim@simCY-MacBook-Pro.local>
2024-01-15 22:23:12 +0900

    코드 구성 변경

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,21 +1,23 @@
+var lottoRound = [String: Array<Int>]()
+
+
 func lottoNumber() -> Array<Int> {
     var lottoNumbers = [Int]()
-        while lottoNumbers.count < 6 {
-            var randomNumber = Int.random(in: 1...45)
-            if lottoNumbers.contains(randomNumber){
-                continue
-            } else {
-                lottoNumbers.append(randomNumber)
-            }
+    while lottoNumbers.count < 6 {
+        var randomNumber = Int.random(in: 1...45)
+        if lottoNumbers.contains(randomNumber) {
+            continue
+        } else {
+            lottoNumbers.append(randomNumber)
+        }
             lottoNumbers.sort()
         }
     return lottoNumbers
-    }
-var lottoNumbers = lottoNumber()
-var myNumbers: [Int] = [1,2,3,4,5,6]
+}
+
+
 
 func checkLotto(lottoNumbers: [Int], myNumbers: [Int]) -> [Int] {
-    lottoNumber()
     
     var duplicates: [Int] = []
 
@@ -28,17 +30,19 @@ func checkLotto(lottoNumbers: [Int], myNumbers: [Int]) -> [Int] {
     return duplicates
 }
 
-let matchNumbers = checkLotto(lottoNumbers: lottoNumbers, myNumbers: myNumbers)
-
-print("로또 번호 :", lottoNumbers)
-print("나의 번호 :", myNumbers)
-
-if matchNumbers.isEmpty {
-    print("아쉽지만 겹치는 번호가 없습니다.")
-} else {
-    print("축하합니다! 겹치는 번호는 \(matchNumbers)"+"입니다")
+func printResult() {
+    var lottoNumbers = lottoNumber()
+    var myNumbers: [Int] = [1,2,3,4,5,6]
+    let matchNumbers = checkLotto(lottoNumbers: lottoNumbers, myNumbers: myNumbers)
+    
+    print("로또 번호 :", lottoNumbers)
+    print("나의 번호 :", myNumbers)
+    
+    if matchNumbers.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는 \(matchNumbers)"+"입니다")
+    }
 }
 
-
-lottoNumber() 
-
+printResult()
