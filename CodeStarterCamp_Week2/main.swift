70cb25aea3fd71ee45351108183a7b6325e96339
jimin387 <tig05015@naver.com>
2024-01-21 09:14:03 +0900

    Round2[Step3] - 고래안고래

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,28 +6,28 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
+var Round: Int = 0
+var roundNumbers: Dictionary<String, Set<Int>> = [String: Set<Int>]()
 
-func makeLottoNumbers() -> Set<Int> {
-    var result: Set<Int> = Set<Int>()
-    
-    while result.count != 6 {
-        result.insert(Int.random(in: 1...45))
+func makeLottoNumbers() {
+    var lottoNumbers: Set<Int> = Set<Int>()
+    Round += 1
+    while lottoNumbers.count != 6 {
+        lottoNumbers.insert(Int.random(in: 1...45))
     }
-    return result
+    roundNumbers["\(Round)회차"] = lottoNumbers
 }
 
-func checkLottoNumbers(myNumbers: Set<Int>, winningNumbers: Set<Int>) {
-    let result: Set<Int> = myNumbers.intersection(winningNumbers)
-    
-    if result.isEmpty {
-        print("아쉽지만 겹치는 번호가 없습니다.")
-    } else {
-        let sameNumbers: String = result.map(String.init).joined(separator: ", ")
-        print("축하합니다! 겹치는 번호는 \(sameNumbers) 입니다!")
+func checkLottoNumbers(round: Int) {
+    if let lottoNum = roundNumbers["\(Round)회차"] {
+        print("\(round)회차의 로또 당첨 번호는 \(lottoNum.map(String.init).joined(separator: ", ")) 입니다.")
     }
 }
 
-let myLottoNumbers: Set<Int> = [1, 2, 3, 4, 5, 6]
-let winningNumbers = makeLottoNumbers()
-checkLottoNumbers(myNumbers: myLottoNumbers, winningNumbers: winningNumbers)
+for _ in 1...5 {
+    makeLottoNumbers()
+}
+ 
+checkLottoNumbers(round: 2)
+
 
