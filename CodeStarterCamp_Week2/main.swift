8fb98720802c563be886e3ce589ac60cd8b06f4d
xero <chanyeongsim@simCY-MacBook-Pro.local>
2024-01-22 17:17:27 +0900

    수정사항 반영

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -2,7 +2,6 @@ let rounds = 5
 var roundNumber = 0
 var recordDictionary: [Int:Set<Int>] = [:]
 
-//로또번호 생성 함수
 func createLotto() -> Set<Int> {
     var lottoNumbers: Set<Int> = Set<Int>()
     while lottoNumbers.count < 6 {
@@ -13,33 +12,30 @@ func createLotto() -> Set<Int> {
     return lottoNumbers
 }
 
-//Dictionary 생성, 출력 함수
-func recordNumbers(roundNumber: Int) {
+func checkResult(roundNumber: Int) {
     if let value = recordDictionary[roundNumber] {
         let sortedValue = value.sorted().map { String($0) }.joined(separator: ", ")
         print("\(roundNumber)회차의 로또 당첨 번호는 \(sortedValue) 입니다.")
     } else {
         print("회차 번호가 잘못 지정되었습니다. 1 ~ \(rounds) 사이의 값으로 지정해주세요.")
     }
-    }
-
-//Dictionary 저장 함수
-func recordRoundNumbers() {
-    for round in 1...rounds {
-        recordDictionary[round] = createLotto()
-        }
 }
 
-//결과 출력 함수
+func recordRoundNumbers() {
+    for round in 1...rounds {
+    recordDictionary[round] = createLotto()
+    }
+}
+
 func printLottoRound() {
     recordRoundNumbers()
-    recordNumbers(roundNumber: 1)
-    recordNumbers(roundNumber: 2)
-    recordNumbers(roundNumber: 3)
-    recordNumbers(roundNumber: 4)
-    recordNumbers(roundNumber: 5)
-    recordNumbers(roundNumber: 5)  //중복확인용
-    recordNumbers(roundNumber: -1)  //에러메시지 출력 확인용
+    checkResult(roundNumber: 1)
+    checkResult(roundNumber: 2)
+    checkResult(roundNumber: 3)
+    checkResult(roundNumber: 4)
+    checkResult(roundNumber: 5)
+    checkResult(roundNumber: 5)  //중복확인용
+    checkResult(roundNumber: -1)  //에러메시지 출력 확인용
 
 }
 
@@ -47,10 +43,10 @@ printLottoRound()
 
 /*
  예제
- recordNumbers(roundNumber: 2)
- recordNumbers(roundNumber: 3)
- recordNumbers(roundNumber: 4)
- recordNumbers(roundNumber: 5)
- recordNumbers(roundNumber: 5)  //중복확인용
- recordNumbers(roundNumber: 6)  //에러메시지 출력 확인용
+ checkResult(roundNumber: 2)
+ checkResult(roundNumber: 3)
+ checkResult(roundNumber: 4)
+ checkResult(roundNumber: 5)
+ checkResult(roundNumber: 5)  //중복확인용
+ checkResult(roundNumber: 6)  //에러메시지 출력 확인용
  */
