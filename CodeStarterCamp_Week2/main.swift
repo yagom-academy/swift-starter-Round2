2685c42bf4024e9fc2875356ccf074b9d1cb4824
ChaeeunShin <shince0805@gmail.com>
2024-02-15 14:25:39 +0900

    [step2] 번호 맞춰보기

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,7 +6,23 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
 
-print("Hello, World!")
 
+let myLottoNumbers: [Int] = [3, 17, 23, 28, 31, 45]
+let winningNumbers: Set<Int> = drawRandomNumber()
+
+checkWinning(compareWith: winningNumbers, from: myLottoNumbers)
+
+func drawRandomNumber() -> Set<Int> {
+    return Set((1...45).shuffled().prefix(6))
+}
+
+func checkWinning(compareWith winningNumbers: Set<Int>, from myNumbers: [Int]) {
+    let matchingNumbers = winningNumbers.intersection(myNumbers)
+    
+    if matchingNumbers.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는 \(matchingNumbers.map({ String($0) }).joined(separator: ", ")) 입니다!")
+    }
+}
