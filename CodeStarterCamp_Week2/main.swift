a94d91b59badef474e97d17566eaf6ca9aa85a80
ezeut <ezeut@naver.com>
2024-02-17 13:48:36 +0900

    Add code to match the number

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,7 +6,12 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
+let myLottoNumbers: [Int] = [1, 16, 19, 23, 33, 45]
+let lottoWinningNumbers: Set<Int> = makeLottoNumbers()
+let matchedNumbers: Set<Int> = lottoWinningNumbers.intersection(myLottoNumbers)
 
-print("Hello, World!")
-
+if matchedNumbers.isEmpty {
+    print("아쉽지만 겹치는 번호가 없습니다.")
+} else {
+    print("축하합니다! 겹치는 번호는 \(matchedNumbers.map({ String($0) }).joined(separator: ", ")) 입니다!")
+}
