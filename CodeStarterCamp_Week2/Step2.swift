58774c38fce9f6a627a60054963447b75d528e49
ChaeeunShin <shince0805@gmail.com>
2024-02-19 16:33:24 +0900

    [step2] 파일 분리

---
+++ CodeStarterCamp_Week2/Step2.swift
@@ -0,0 +1,25 @@
+//
+//  Step2.swift
+//  CodeStarterCamp_Week2
+//
+//  Created by Chaeeun Shin on 2/19/24.
+//
+
+let myLottoNumbers: [Int] = [3, 17, 23, 28, 31, 45]
+let winningNumbers: Set<Int> = drawRandomNumbers()
+
+//checkWinning(compareWith: winningNumbers, from: myLottoNumbers)
+
+func drawRandomNumbers() -> Set<Int> {
+    return Set((1...45).shuffled().prefix(6))
+}
+
+func checkWinning(compareWith winningNumbers: Set<Int>, from myNumbers: [Int]) {
+    let matchingNumbers = winningNumbers.intersection(myNumbers)
+    
+    if matchingNumbers.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        print("축하합니다! 겹치는 번호는 \(matchingNumbers.map({ String($0) }).joined(separator: ", ")) 입니다!")
+    }
+}
