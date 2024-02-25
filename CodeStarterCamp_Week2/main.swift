f4b93b25dcd08069de2a8c227c194929ea7c8eef
Jay <shinju1412@gmail.com>
2024-02-25 23:53:37 +0900

    Step3 함수 개선 및 수정

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,18 +8,25 @@
 
 import Foundation
 
-var previousLottoResults: Dictionary<String, Set<Int>> = [String: Set<Int>]()
+var previousLottoResults: Dictionary<String, Set<Int>> = Dictionary<String, Set<Int>>()
+var presentRound: Int = 0
 
-savePreviousResults()
+savePreviousResults(roundsToGenerate: 5)
+checkPreviousResults(round: "2회차")
+savePreviousResults(roundsToGenerate: 5)
+checkPreviousResults(round: "10회차")
 
-if let result = previousLottoResults["2회차"] {
-    print("2회차의 로또 당첨 번호는 \(result.sorted().map{ String($0) }.joined(separator: ", ")) 입니다.")
-} else {
-    print("previousLottoResults == nil")
+func checkPreviousResults(round: String) {
+    if let result = previousLottoResults[round] {
+        print("\(round)의 로또 당첨 번호는 \(result.sorted().map{ String($0) }.joined(separator: ", ")) 입니다.")
+    } else {
+        print("해당 회차 로또는 아직 추첨되지 않았습니다.")
+    }
 }
 
-func savePreviousResults() {
-    for count in 1...5{
-        previousLottoResults["\(count)회차"] = generateLottoNumbers()
+func savePreviousResults(roundsToGenerate: Int) {
+    for _ in 1...roundsToGenerate{
+        presentRound += 1
+        previousLottoResults["\(presentRound)회차"] = generateLottoNumbers()
     }
 }
