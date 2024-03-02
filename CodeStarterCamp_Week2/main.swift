cda8dc10ff0827c6343c73853b7b8c5d45d4d2d3
o2k <o2k@o2kui-MacBookAir.local>
2024-03-02 16:18:55 +0900

    [Round2 [Step2] - o2k

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,1 +1,29 @@
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+
+func generateRandomNumbers() -> [Int] {
+    var numbers = Set<Int>()
+    
+    while numbers.count < 6 {
+        let randomNumber = Int.random(in: 1...45)
+        numbers.insert(randomNumber)
+    }
+    
+    return Array(numbers)
+}
+
+let winningNumbers = generateRandomNumbers()
+
+func checkLottoNumbers(_ winningNumbers: [Int], _ myNumbers: [Int]) {
+    let intersection = Set(winningNumbers).intersection(Set(myNumbers))
+    
+    if intersection.isEmpty {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        let matchingNumbers = intersection.sorted()
+        print("축하합니다! 겹치는 번호는 \(matchingNumbers) 입니다!")
+    }
+}
+
+print("당첨 번호는 \(winningNumbers) 입니다.")
+checkLottoNumbers(winningNumbers, myLottoNumbers)
 
