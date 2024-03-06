6a786012c4c0f959e25ed33b01e6cf9bcf57bbcc
therethen <horosiro@nate.com>
2024-03-07 01:17:15 +0900

    Step3 Complete

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -54,3 +54,58 @@ if matchNumbers.count > 0 {
 
 
 print("내가 생성한 번호 : \(emptyArrayCollection)")
+
+
+//step3. 로또 번호 만들어서 딕셔너리에 저장하기
+
+//회차별 당첨번호 저장할 딕셔너리 변수 생성
+var myLottoNumbersCollection: Dictionary<String,Any> = [:]
+
+//회차에 사용할 변수
+var time: Int = 0
+//회차 증가->key값으로 사용할 문자열 생성 함수
+func increaseTime(by tryTime:Int)->String{
+    //회차 전역변수 증가
+    time = tryTime + 1
+    //0회차 문구 생성
+    let completeTime: String = String(time)+"회차"
+    //0회차 문자열 값 리턴
+    return completeTime
+}
+//Step2에서 만들었던 로또 번호 생성 메소드 활용
+func createMyLottoNum(){
+    //추출 값이 겹치지 않도록 set 컬렉션 셍성
+    var emptySetCollection: Set<Int> = Set<Int>()
+    //set 컬렉션의 멤버가 6개가 될때까지 반복
+    while emptySetCollection.count < 6 {
+        //1~46 범위의 랜덤 넘버 생성
+        let randomNumber = Int.random(in: 1...46)
+        //set 컬렉션에 추가
+        emptySetCollection.insert(randomNumber)
+    }
+    //정렬을 위해 Array 컬렉션 생성
+    var emptyArrayCollection: Array<Int> = Array<Int>()
+    //set 컬렉션의 sorted 메소드 사용 후 array 컬렉션에 대입
+    emptyArrayCollection = emptySetCollection.sorted()
+    //생성될때마다 회차증가 함수 실행 ->키값으로 저장
+    let lottoTime: String = increaseTime(by: time)
+    //딕셔너리 컬렉션에 "회차"를 키값으로 로또 번호 어레이 입력
+    myLottoNumbersCollection[lottoTime] = emptyArrayCollection
+    print(myLottoNumbersCollection)
+}
+
+//5회차까지 로또 넘버 반복 생성
+for _ in 0...4{
+    createMyLottoNum()
+}
+//옵셔널 값 꺼내기
+func getValueFromOptional(whatTime:String){
+    var value: Array<Int> = Array()
+    if let lottoNumbers = myLottoNumbersCollection[whatTime]{
+        value = lottoNumbers as! Array<Int>
+    }
+    print("\(whatTime)의 로또 당첨 번호는 \(value) 입니다.")
+
+}
+//실행
+getValueFromOptional(whatTime: "5회차")
